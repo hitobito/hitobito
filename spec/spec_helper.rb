da801cb07e4fc1fb4bdaeb5c5bdcf65894eabe24
Pascal Zumkehr <zumkehr@puzzle.ch>
2012-09-04 10:39:49 +0200

    put coverage reports into spec/coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  coverage_dir 'spec/coverage'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'

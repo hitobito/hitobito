d8f975fcae70ea9abc3a93f6b4b36a5b7903578b
amaierhofer <a.maierhofer@gmail.com>
2012-09-10 16:18:55 +0200

    adding email and password to person Fabricator

--- spec/fabricators/people_fabricator.rb
+++ spec/fabricators/people_fabricator.rb
@@ -2,6 +2,8 @@ Fabricator(:person) do
   first_name { Faker::Name.first_name }
   last_name { Faker::Name.last_name }
   nickname { Faker::Name.first_name }
+  email { |attrs| "#{attrs[:first_name]}.#{attrs[:last_name]}@jubla.example.com" }
+  password { "password" }
 end
 
 Fabricator(:company, from: :person) do

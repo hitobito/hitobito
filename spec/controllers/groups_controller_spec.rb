fa7c958cfcef274ffc8a0549146ed7a3ed04c224
amaierhofer <a.maierhofer@gmail.com>
2012-09-12 10:07:51 +0200

    using devise from controller spec

---
+++ spec/controllers/groups_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+describe GroupsController do
+
+  let(:group) { Group.first }
+  let(:person) { Person.first }
+
+  it "redirects to login" do
+    get :show, id: group.id 
+    should redirect_to "/users/sign_in"
+  end
+
+  it "renders template when signed in" do
+    sign_in(person)
+    get :show, id: group.id
+    should render_template('groups/show')
+  end
+
+end

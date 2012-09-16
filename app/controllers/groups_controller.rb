e55480f9014d89b3ec8eefc6ede76e05c4f8ba65
Andreas Maierhofer <maierhofer@puzzle.ch>
2012-09-16 22:58:08 +0200

    create group and child groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,12 +19,16 @@ class GroupsController < CrudController
     super
   end
 
+  def create(&block)
+    assign_attributes
+    created = with_callbacks(:create, :save) { entry.save }
+    respond_with(entry, :success => created, &block)
+  end
   
   def new
     # set parent group for authorization
-    entry
-    entry.parent_id = params.delete(:parent_id) if params.has_key?(:parent_id)
-    entry.type = params.delete(:type) if params.has_key?(:type)
+    #entry
+    #entry.parent_id = params.delete(:parent_id) if params.has_key?(:parent_id)
     @current_ability = Ability::WithGroup.new(current_user, entry.parent) if entry.parent
     authorize! :new, Group
     super
@@ -36,6 +40,11 @@ class GroupsController < CrudController
   end
 
   private 
+  def build_entry 
+    group = model_params[:type].constantize.new if model_params.has_key?(:type)
+    group.parent_id = model_params[:parent_id] if model_params.has_key?(:parent_id)
+    group
+  end
   def assign_attributes 
     if model_params && entry.new_record? 
       entry.type = model_params.delete(:type)

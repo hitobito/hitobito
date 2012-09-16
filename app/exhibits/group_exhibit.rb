b45748bd8424c20094d091d106c1a41883aba993
Andreas Maierhofer <maierhofer@puzzle.ch>
2012-09-16 22:10:11 +0200

    filter attributes from #attr_used?

--- app/exhibits/group_exhibit.rb
+++ app/exhibits/group_exhibit.rb
@@ -9,55 +9,30 @@ class GroupExhibit < DisplayCase::Exhibit
     object.class.name == 'Group' || object.class.base_class.name == 'Group'
   end
 
-  def self.custom_fields
-    @types ||= {
-      federalboard: [:bank_account],
-      organizationboard: [],
-      state: [],
-      professionalgroup: [],
-      workgroup: [],
-      simplegroup: []
-    }
-  end
-
-  def possible_types
-    __getobj__.class.possible_children.collect(&:model_name)
-  end
-
   def type_name
     "#{type}, #{type.class}"
   end
 
-
   def possible_children_options
-    context.options_from_collection_for_select(possible_types, :to_s, :human)
+    types = __getobj__.class.possible_children.collect(&:model_name)
+    context.options_from_collection_for_select(types, :to_s, :human)
   end
 
-  def type_selection(f)
-    f.labeled_collection_select :type, possible_types, :to_s, :human
+  def attributes
+    cls = __getobj__.class
+    cls.attribute_names.select { |name| cls.attr_used?(name) }
   end
 
+
+  private
   def attrs_for_remote
     url = context.fields_groups_path(group: { parent_id: parent.id })
     url = URI.unescape(url)
     { data: { remote: true, replace: true, url: url }  } 
   end
 
-  def custom_fields(f)
-    markup = ""
-    custom_fields = GroupExhibit.custom_fields[type_as_sym] || []
-    custom_fields.each do |field|
-      markup << custom_field(f, field)
-    end
-    markup.html_safe
-  end
-
   def type_as_sym
     type && type.split('::').last.downcase.to_sym
-  end
-
-  def custom_field(f, field)
-    f.labeled_input_field(field)
   end
 
 end

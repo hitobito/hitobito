2112786fc9b8b26151487d261c921d2fcfe18280
Andreas Maierhofer <maierhofer@puzzle.ch>
2012-09-17 12:36:32 +0200

    use bootstrap dropdown for new group selection

--- app/exhibits/group_exhibit.rb
+++ app/exhibits/group_exhibit.rb
@@ -3,17 +3,21 @@ class GroupExhibit < DisplayCase::Exhibit
   extend Forwardable
   def_delegators :context, :content_tag
 
-
   def self.applicable_to?(object)
     return false if object.class.name == 'ActiveRecord::Relation'
     object.class.name == 'Group' || object.class.base_class.name == 'Group'
   end
 
-
   def type_name
     "#{type}, #{type.class}, #{klass}"
   end
 
+  def possible_children
+    self.class.possible_children.collect(&:model_name).map do |name|
+      link = context.new_group_path(group: { parent_id: self.id, type: name})
+      OpenStruct.new(target: link, name: name.human)
+    end
+  end
   def possible_children_options
     types = self.class.possible_children.collect(&:model_name)
     context.options_from_collection_for_select(types, :to_s, :human)

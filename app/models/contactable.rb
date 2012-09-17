5793b25fcf148a427065184d155fd89efab8aaf7
Andreas Maierhofer <maierhofer@puzzle.ch>
2012-09-17 15:11:03 +0200

    save nested attributes

--- app/models/contactable.rb
+++ app/models/contactable.rb
@@ -13,9 +13,16 @@ module Contactable
     scope :preload_accounts, includes(:phone_numbers, :social_accounts)
     scope :preload_public_accounts, scoped.extending(Person::PreloadPublicAccounts)
 
-    accepts_nested_attributes_for :phone_numbers, :social_accounts
-    
+    accepts_nested_attributes_for :phone_numbers, :social_accounts, allow_destroy: true
+
+    before_validation :set_self_in_nested
   end
 
-  
+
+  private
+  def set_self_in_nested
+    # don't try to set self in frozen nested attributes (-> marked for destroy)
+    (phone_numbers + social_accounts).each {|e| e.contactable = self unless e.frozen? }
+  end
+
 end

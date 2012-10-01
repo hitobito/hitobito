16434c78f98dd8faa3f5d23ef6763eb3160295dc
Andreas Maierhofer <maierhofer@puzzle.ch>
2012-10-01 11:38:13 +0200

    ensure superior attributes are rendered correctly

--- vendor/wagons/jubla/spec/controllers/groups_controller_spec.rb
+++ vendor/wagons/jubla/spec/controllers/groups_controller_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 describe GroupsController do
   let(:flock) { groups(:bern) }
   let(:leader) { Fabricate(Group::Flock::Leader.name.to_sym, group: flock).person }
+  let(:superior) { Fabricate(Group::StateAgency::Leader.name.to_sym, group: groups(:be_agency)).person }
 
 
   describe "#edit" do
     render_views
+
     it "renders ok" do
       sign_in(leader)
+      get :edit, id: flock.id
+      response.body.should_not =~ /Jubla Versicherung/m
+    end
+
+
+    it "renders ok" do
+      sign_in(superior)
       get :edit, id: flock.id
       response.body.should =~ /Jubla Versicherung/m
     end

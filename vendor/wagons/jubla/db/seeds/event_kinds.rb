44d93326fdc2a3590aea7061e1cafb10214b8ef3
Pascal Zumkehr <zumkehr@puzzle.ch>
2012-10-01 12:01:10 +0200

    add event seeds

---
+++ vendor/wagons/jubla/db/seeds/event_kinds.rb
@@ -0,0 +1,10 @@
+Event::Kind.seed(:short_name,
+ {label: 'Scharleiterkurs',
+  short_name: 'SLK'},
+  
+ {label: 'Gruppenleiterkurs',
+  short_name: 'GLK'},
+  
+ {label: 'Coachkurs',
+  short_name: 'CK'}
+)
---
+++ vendor/wagons/jubla/db/seeds/event_questions.rb
@@ -0,0 +1,10 @@
+Event::Question.seed(:event_id, :question,
+  {question: 'ÖV Abo',
+   choices: 'GA, ½-Tax, Keines'},
+  
+  {question: 'Vegetarier',
+   choices: 'ja, nein'},
+   
+  {question: 'Schub',
+   choices: 'ja, nein'},
+)

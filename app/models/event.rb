588cb4491f1e169a515ea332e9b7b08960b5815d
pascal simon <simon@puzzle.ch>
2012-10-02 14:51:59 +0200

    add dates, questions to events form refs #2708

--- app/models/event.rb
+++ app/models/event.rb
@@ -67,7 +67,7 @@ class Event < ActiveRecord::Base
   ### CALLBACKS
   
   
-  accepts_nested_attributes_for :dates, allow_destroy: true
+  accepts_nested_attributes_for :dates, :questions , allow_destroy: true
 
   # TODO: copy all event_questions without event_id into a newly created event (probably in controller, not here)
 
--- app/models/event/date.rb
+++ app/models/event/date.rb
@@ -12,7 +12,23 @@
 class Event::Date < ActiveRecord::Base
   
   attr_accessible :label, :start_at, :finish_at
+  attr_accessor :start_at_date, :start_at_time, :finish_at_date, :finish_at_time
   
   belongs_to :event
+
+  before_validation :merge_start_at, :merge_finish_at
+
+  private
+  def merge_start_at
+    @start_at = merge_date_time(start_at_date, start_at_time)
+  end
+
+  def merge_finish_at
+    @finish_at = merge_date_time(finish_at_date, finish_at_time)
+  end
+
+  def merge_date_time(d,t)
+    DateTime.new(d.year, d.month, d.day, t.hour, t.min, t.sec)
+  end
   
 end

8fad35d69f2035a0f0c75f5f494396a30ec9b329
Pascal Zumkehr <zumkehr@puzzle.ch>
2012-10-02 15:31:08 +0200

    patch for demodulized route keys

--- config/initializers/active_model_name.rb
+++ config/initializers/active_model_name.rb
@@ -7,13 +7,22 @@ module ActiveModel
       initialize_without_sti(*args)
       
       if @klass != @klass.base_class
-        @param_key = @klass.base_class.model_name.param_key
-        @route_key = @klass.base_class.model_name.route_key
+        base_name = @klass.base_class.model_name
+        @param_key = base_name.param_key
+        @route_key = base_name.route_key
+        @singular_route_key = base_name.singular_route_key
+      elsif @klass.demodulized_route_keys
+        @route_key = ActiveSupport::Inflector.pluralize(self.demodulize.underscore).freeze
         @singular_route_key = ActiveSupport::Inflector.singularize(@route_key).freeze
-
       end
     end
     
     alias_method_chain :initialize, :sti
   end
+end
+
+class ActiveRecord::Base
+  # set this to true if route_keys should be demodulize
+  # e.g. Event::Application -> 'applications'
+  class_attribute :demodulized_route_keys
 end

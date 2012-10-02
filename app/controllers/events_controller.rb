63ff05de5ec7765fed1e876c8d1a4cd80acf1fcc
pascal simon <simon@puzzle.ch>
2012-10-02 08:43:38 +0200

    additional form fields for event form refs #2708

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,6 @@
 class EventsController < CrudController
 
   self.nesting = Group
-  decorates :group, :event
 
   # load group before authorization
   prepend_before_filter :parent
--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class EventDecorator < BaseDecorator
+class EventDecorator < ApplicationDecorator
   decorates :event
 
 end

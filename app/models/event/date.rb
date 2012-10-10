a0d1079a716e94012f300276e723000b63e7ed51
pascal simon <simon@puzzle.ch>
2012-10-10 09:22:18 +0200

    add sep fields for hours/mins for event dates

--- app/models/event/date.rb
+++ app/models/event/date.rb
@@ -11,29 +11,57 @@
 
 class Event::Date < ActiveRecord::Base
   
-  attr_accessible :label, :start_at, :finish_at
-  attr_accessor :start_at_date, :start_at_time, :finish_at_date, :finish_at_time
+  attr_accessible :label, :start_at_date, :start_at_h, :start_at_min, :start_at, :finish_at
+  attr_writer :start_at_date, :start_at_h, :start_at_min
+  attr_accessor :finish_at_date, :finish_at_h, :finish_at_min
   
   belongs_to :event
 
-  # TODO: fix this, commented out because of event unit specs
-  #before_validation :merge_start_at, :merge_finish_at
+  before_validation :merge_start_at, :merge_finish_at
+
+  def start_at_date
+    @start_at_date ||= start_at.try(:to_date)
+  end
+
+  def start_at_h
+    @start_at_h ||= start_at.try(:hour)
+  end
+
+  def start_at_min
+    @start_at_min ||= start_at.try(:min)
+  end
+
+  def finish_at_date
+    @finish_at_date ||= finish_at.try(:to_date)
+  end
+
+  def finish_at_h
+    @finish_at_h ||= finish_at.try(:hour)
+  end
+
+  def finish_at_min
+    @finish_at_min ||= finish_at.try(:min)
+  end
 
   private
   def merge_start_at
-    @start_at = merge_date_time(start_at_date, start_at_time)
+    if start_at_date.present?
+      date = start_at_date.to_date
+      self.start_at = merge_date_time(date, start_at_h, start_at_min)
+    end
   end
 
   def merge_finish_at
-    @finish_at = merge_date_time(finish_at_date, finish_at_time)
+    if finish_at_date.present?
+      date = finish_at_date.to_date
+      self.finish_at = merge_date_time(date, finish_at_h, finish_at_min)
+    end
   end
 
-  def merge_date_time(d,t)
-    if t.present?
-      DateTime.new(d.year, d.month, d.day, t.hour, t.min, t.sec)
-    else
-      DateTime.new(d.year, d.month, d.day)
-    end
+  def merge_date_time(date,h,min)
+    h = h.blank? ? 00 : h
+    min = min.blank? ? 00 : min
+    DateTime.new(date.year, date.month, date.day, h.to_i, min.to_i)
   end
 
   def to_s
---
+++ spec/models/event/date_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe Event::Date do
+
+  it 'should only store date when no time is given' do
+    date1 = '12.12.2012'
+    date2 = '13.12.2012'
+    event_date = Event::Date.new(label: 'foobar')
+    event_date.event = Fabricate(:event, group: groups(:top_group))
+    event_date.start_at_date = date1
+    event_date.finish_at_date = date2
+    event_date.valid?.should be true
+    event_date.start_at.should == date1.to_datetime
+    event_date.finish_at.should == date2.to_datetime
+  end
+
+  it 'should have date and time when hours and min are given' do
+    date1 = '12.12.2012'
+    hours = 18
+    min = 10
+    event_date = Event::Date.new(label: 'foobar')
+    event_date.event = Fabricate(:event, group: groups(:top_group))
+    event_date.start_at_date = date1
+    event_date.start_at_h = hours
+    event_date.start_at_min = min
+    event_date.valid?.should be true
+    event_date.start_at.should == DateTime.new(2012,12,12,18,10)
+  end
+
+  it 'should get hours,mins and date seperately' do
+    date = DateTime.now
+    event_date = Event::Date.new(label: 'foobar')
+    event_date.event = Fabricate(:event, group: groups(:top_group))
+    # start_at
+    event_date.start_at = date
+    event_date.start_at_date.should == date.to_date
+    event_date.start_at_h.should == date.hour
+    event_date.start_at_min.should == date.min
+    # finish_at
+    event_date.finish_at = date
+    event_date.finish_at_date.should == date.to_date
+    event_date.finish_at_h.should == date.hour
+    event_date.finish_at_min.should == date.min
+  end
+
+end

6bc10183330b2aede1141d8622159a065231c326
Andreas Maierhofer <maierhofer@puzzle.ch>
2012-10-11 10:05:11 +0200

    remove print css from participations

--- config/application.rb
+++ config/application.rb
@@ -72,7 +72,7 @@ module Jubla
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.assets.precompile += %w(ie.css ie7.css event/participation_print.css)
+    config.assets.precompile += %w(ie.css ie7.css)
 
     
     config.generators do |g|

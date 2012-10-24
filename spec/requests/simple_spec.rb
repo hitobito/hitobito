8264cd6917f042ee35f9deba752ebd5a004d61d5
Andreas Maierhofer <maierhofer@puzzle.ch>
2012-10-24 09:34:05 +0200

    verify clicking on links in simple_spec

--- spec/requests/simple_spec.rb
+++ spec/requests/simple_spec.rb
@@ -15,6 +15,10 @@ describe "" do
     find('.dropdown-menu').should_not be_visible
     click_link 'Person hinzufügen'
     find('.dropdown-menu').should be_visible
+    within(:css, '.dropdown-menu') do
+      click_link 'Rolle'
+    end
+    should have_content 'Rolle hinzufügen'
   end
 
 
@@ -24,6 +28,9 @@ describe "" do
     find('.typeahead.dropdown-menu').should_not have_content 'Top Leader'
     fill_in "Person", with: "Top"
     find('.typeahead.dropdown-menu').should have_content 'Top Leader'
+    find('.typeahead.dropdown-menu').click
+    click_button 'Speichern'
+    should have_content 'Rolle Rolle für Top Leader in TopGroup wurde erfolgreich erstellt.'
   end
 
 end

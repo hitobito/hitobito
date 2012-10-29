adf5f75df59b3d503f78f21fa1b0db2ca1f42f58
Andreas Maierhofer <maierhofer@puzzle.ch>
2012-10-29 14:46:45 +0100

    fixes #3048

--- app/models/duration.rb
+++ app/models/duration.rb
@@ -1,47 +1,63 @@
 class Duration < Struct.new(:start_at, :finish_at)
   include StandardHelper
   include ActionView::Helpers::TranslationHelper
+  attr_reader :start, :finish
+
 
   def to_s
-    return format(start_at.to_date) if start_at && finish_at.nil? # single date only
-    return format(finish_at.to_date) if start_at.nil? && finish_at # single date only
-
-    # both set, different dates
-    if start_at.to_date != finish_at.to_date
-      return "#{format(start_at.to_date)} - #{format(finish_at.to_date)}".strip
-    end
-
-    # both set, same dates, no time
-    if start_at.to_date == finish_at.to_date && (start_at == start_at.midnight && 
-                                                 finish_at == finish_at.midnight)
-      return "#{format(start_at.to_date)}"
-    end
-
-    # both set, same dates, start_at has time
-    if start_at.to_date == finish_at.to_date && (start_at != start_at.midnight && 
-                                                 finish_at == finish_at.midnight)
-      return "#{format(start_at.to_date)} #{format(start_at)}"
-    end
-
-
-    # both set, same dates, finish_at has time
-    if start_at.to_date == finish_at.to_date && (start_at == start_at.midnight && 
-                                                 finish_at != finish_at.midnight)
-      return "#{format(start_at.to_date)} #{format(start_at)}"
-    end
-
-    # both set, same dates, both have times, they are the same
-    if start_at == finish_at
-      return "#{format(start_at.to_date)} #{format(start_at)}"
-    end
-
-    # both set, same dates, both have times, they are different
-    if start_at.to_date == finish_at.to_date && (start_at != start_at.midnight && 
-                                                 finish_at != finish_at.midnight)
-      return "#{format(start_at.to_date)} #{format(start_at)} - #{format(finish_at)}"
-    end
-
+    populate
+    start = simple_format(:start)
+    finish = simple_format(:finish)
+    content = ""
+    content << start unless start.blank?
+    content << " - " if start.present? and finish.present?
+    content << finish unless finish.blank?
+    return content
   end
+
+  private
+  def populate
+    @start = kind(start_at) if start_at
+    @finish = kind(finish_at) if finish_at
+  end
+
+  def kind(date)
+    date.seconds_since_midnight == 0.0 ? :date : :time
+  end
+
+  def simple_format(symbol)
+    case send(symbol)
+    when :date then format_date(symbol)
+    when :time then format_date_time(symbol)
+    end
+  end
+
+  def format_date(symbol)
+    if symbol == :finish
+      return nil if start_at && start_at.to_date == finish_at.to_date
+    end
+    value = send("#{symbol}_at".to_sym)
+    format(value.to_date)
+  end
+
+  def format_date_time(symbol)
+    if start == :time && finish == :time
+      if (start_at == finish_at)
+        return format_date(symbol) if symbol == :finish
+      end
+
+      if (start_at.to_date == finish_at.to_date)
+        return format(send("#{symbol}_at".to_sym)) if symbol == :finish
+      end
+
+      if start_at.to_date != finish_at.to_date
+        return format_date(symbol)
+      end
+    end
+    value = send("#{symbol}_at".to_sym)
+    format(value.to_date) + " " + format(value)
+  end
+
 
   def format(date)
     f(date)

b49375f0db842dffb13f85e0420982054ab999b7
Pascal Zumkehr <zumkehr@puzzle.ch>
2012-11-07 18:54:12 +0100

    refactor event restricted roles

--- app/models/restricted_role.rb
+++ app/models/restricted_role.rb
@@ -16,20 +16,31 @@ module RestrictedRole
     restricted_roles.each do |attr, type|
       role = restricted_role(attr, type)
       if role.try(:person_id) != send("#{attr}_id")
-        role.destroy if role
+        if role 
+          # be on the save side with destroy_all
+          restricted_role_scope(type).destroy_all
+          @restricted_role[attr] = nil # clear cache
+        end
         if id = restricted_role_id(attr, type).presence
-          role = type.new
-          role.person_id = id
-          role.group = self
-          role.save!
+          build_restricted_role(type.new, id).save!
         end
       end
     end
   end
   
+  def build_restricted_role(role, id)
+    role.person_id = id
+    role.group = self
+    role
+  end
+  
+  def restricted_role_scope(type)
+    roles.where(type: type.sti_name)
+  end
+  
   def restricted_role(attr, type)
     @restricted_role ||= {}
-    @restricted_role[attr] ||= roles.where(type: type.sti_name).first
+    @restricted_role[attr] ||= restricted_role_scope(type).first
   end
   
   def restricted_role_id(attr, type)
@@ -46,11 +57,16 @@ module RestrictedRole
     def restricted_role(attr, type)
       after_save :create_restricted_roles
       restricted_roles[attr] = type
-      role_types << type
+      self.role_types += [type]
       
       # getter for the person
-      define_method attr do
-        restricted_role(attr, type).try(:person)
+      define_method attr do        
+        if new_record?
+          id = restricted_role_id(attr, type)
+          Person.find(id) if id
+        else
+          restricted_role(attr, type).try(:person)
+        end
       end
       
       # getter for the person id

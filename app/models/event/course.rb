87b6ef7dc5402e7a919d8c51490ebb30791f87f6
Andreas Maierhofer <maierhofer@puzzle.ch>
2012-12-13 12:15:17 +0100

    also consider start_at as qualification_date, fixes #3500

--- app/models/event/course.rb
+++ app/models/event/course.rb
@@ -26,10 +26,10 @@
 #
 
 class Event::Course < Event
-  
+
   # This statement is required because this class would not be loaded otherwise.
   require_dependency 'event/course/role/participant'
-  
+
   self.role_types = [Event::Role::Leader,
                      Event::Role::AssistantLeader,
                      Event::Role::Cook,
@@ -38,30 +38,32 @@ class Event::Course < Event
                      Event::Course::Role::Participant]
   self.participant_type = Event::Course::Role::Participant
   self.supports_applications = true
-  
+
   attr_accessible :number, :kind_id, :state, :priorization, :requires_approval
 
   belongs_to :kind
-  
+
   validates :kind_id, presence: true
-  
+
   def label_detail
     "#{kind.short_name} #{number} #{group_names}"
   end
-    
+
   # Does this event provide qualifications
   def qualifying?
     kind_id? && kind.qualifying?
   end
-  
+
   # The date on which qualification obtained in this course start
   def qualification_date
     @qualification_date ||= begin
-      date = dates.reorder('event_dates.finish_at DESC').first
-      if date && date.finish_at
-        date.finish_at.to_date
+      candidates = dates.reorder('event_dates.finish_at DESC')
+      date = candidates.find {|d| d.finish_at.present? }
+      date ||= candidates.first
+      if date
+        date.finish_at  || date.start_at
       end
-    end
+    end.to_date
   end
-  
+
 end
--- spec/models/event/course_spec.rb
+++ spec/models/event/course_spec.rb
@@ -28,24 +28,32 @@
 require 'spec_helper'
 
 describe Event::Course do
-  
-      
+
+
   subject do
     Fabricate(:course, groups: [groups(:top_group)] )
   end
-  
+
+  its(:qualification_date) { should == Date.new(2012,5,11) }
   context "#qualification_date" do
     before do
       add_date("2011-01-20")
       add_date("2011-02-15")
       add_date("2011-01-02")
     end
-    
+
     its(:qualification_date) { should == Date.new(2011, 02, 20) }
+
+    def add_date(start_at, event = subject)
+      start_at = Time.zone.parse(start_at)
+      event.dates.create(start_at: start_at, finish_at: start_at + 5.days)
+    end
   end
-  
-  def add_date(start_at, event = subject)
-    start_at = Time.zone.parse(start_at)
-    event.dates.create(start_at: start_at, finish_at: start_at + 5.days)
+
+  context "multiple start_at" do
+    before { subject.dates.create(start_at: Date.new(2012,5,14)) }
+    its(:qualification_date) { should == Date.new(2012,5,11) }
   end
+
+
 end

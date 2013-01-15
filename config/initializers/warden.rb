0c6f0cf1b90268741d506c896553ec55dc8fe520
Andreas Maierhofer <maierhofer@puzzle.ch>
2013-01-15 16:04:41 +0100

    disable recording of timestamps in warden, fixes #3613

---
+++ config/initializers/warden.rb
@@ -0,0 +1,15 @@
+# https://redmine.puzzle.ch/issues/3613
+#
+# Devise modules (rememberable, trackable) use save to update fields.
+# This automatically also updates updated_at field.
+#
+# We use warden hooks to disable automatic updating of timestamps when
+# user logs into and logs out of application.
+
+Warden::Manager.prepend_after_set_user :except => :fetch do |record, warden, options|
+  record.define_singleton_method(:record_timestamps, Proc.new { false } )
+end
+
+Warden::Manager.prepend_before_logout do |record, warden, options|
+  record.define_singleton_method(:record_timestamps, Proc.new { false } )
+end

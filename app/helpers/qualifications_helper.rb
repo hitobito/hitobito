0d369398965c2bf9638c3436d029cd2df7e7cc8e
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-01-28 18:30:25 +0100

    add quali finish_at prefix, fixes #3664

--- app/helpers/qualifications_helper.rb
+++ app/helpers/qualifications_helper.rb
@@ -2,10 +2,21 @@
 module QualificationsHelper
   
   def format_qualification_kind_validity(kind)
-    if kind.validity.present?
-      "#{f(kind.validity)} Jahre"
+    format_unbounded_value(kind.validity) {|d| "#{d} Jahre" }
+  end
+  
+  def format_qualification_finish_at(quali)
+    format_unbounded_value(quali.finish_at) {|d| "bis #{d}" }
+  end
+  
+  private
+  
+  def format_unbounded_value(value)
+    if value.present?
+      yield f(value)
     else
       "unbeschränkt"
     end
   end
+  
 end

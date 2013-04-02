e194137c8016cf65ce9f762358cbe43347a1840e
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-04-02 10:50:03 +0200

    fix daylight saving time bug

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -50,50 +50,50 @@ describe Role do
       it "is invalid without type" do
         should have(1).errors_on(:type)
       end
-  
+
       it "is invalid with non-existing type" do
         subject.type = "Foo"
         should have(1).errors_on(:type)
       end
-      
+
       it "is invalid with type from other group" do
         subject.type = "Group::TopGroup::Leader"
         should have(1).errors_on(:type)
       end
-  
+
       it "is valid with allowed type" do
         subject.type = "Group::BottomLayer::Leader"
         should be_valid
       end
     end
-    
+
     context "primary group" do
       before { subject.type = "Group::BottomLayer::Leader" }
-      
+
       it "is set for first role" do
         subject.save.should be_true
         person.primary_group_id.should == group.id
       end
-      
+
       it "is not set on subsequent roles" do
         Fabricate(Group::TopGroup::Member.name.to_s, person: person, group: groups(:top_group))
         subject.save
         person.primary_group_id.should_not == group.id
       end
-      
+
       it "is reset if role is destroyed" do
         subject.save
         subject.destroy.should be_true
         person.primary_group_id.should be_nil
       end
-      
+
       it "is not reset if role is destroyed and other roles in the same group exist" do
         Fabricate(Group::BottomLayer::Member.name.to_s, person: person, group: group)
         subject.save
         subject.destroy.should be_true
         person.primary_group_id.should == group.id
       end
-      
+
       it "is not reset if role is destroyed and primary group is another group" do
         subject.save
         person.update_column :primary_group_id, 42
@@ -101,46 +101,46 @@ describe Role do
         person.primary_group_id.should == 42
       end
     end
-    
+
     context "contact data callback" do
-      
+
       it "sets contact data flag on person" do
         subject.type = "Group::BottomLayer::Leader"
         subject.save!
         person.should be_contact_data_visible
       end
-      
+
       it "sets contact data flag on person with flag" do
         person.update_attribute :contact_data_visible, true
         subject.type = "Group::BottomLayer::Leader"
         subject.save!
         person.should be_contact_data_visible
       end
-      
+
       it "removes contact data flag on person " do
         person.update_attribute :contact_data_visible, true
         subject.type = "Group::BottomLayer::Leader"
         subject.save!
-        
+
         role = Role.find(subject.id)  # reload from db to get the correct class
         role.destroy
-        
+
         person.reload.should_not be_contact_data_visible
       end
-           
+
       it "does not remove contact data flag on person when other roles exist" do
         Fabricate(Group::TopGroup::Member.name.to_s, group: groups(:top_group), person: person)
         subject.type = "Group::BottomLayer::Leader"
         subject.save!
-        
+
         role = Role.find(subject.id)  # reload from db to get the correct class
         role.destroy
-        
+
         person.reload.should be_contact_data_visible
       end
     end
   end
-  
+
   describe ".normalize_label" do
     it "reuses existing label" do
       a1 = Fabricate(Group::BottomLayer::Leader.name.to_s, label: 'Foo', group: groups(:bottom_layer_one))
@@ -148,16 +148,16 @@ describe Role do
       a2.label.should == 'Foo'
     end
   end
-  
+
   describe "#available_labels" do
     subject { Group::BottomLayer::Leader.available_labels }
-    
+
     it "includes labels from database" do
       a = Fabricate(Group::BottomLayer::Leader.name.to_s, label: 'Foo', group: groups(:bottom_layer_one))
       a = Fabricate(Group::BottomLayer::Leader.name.to_s, label: 'FOo', group: groups(:bottom_layer_one))
       should == ['Foo']
     end
-        
+
     it "includes labels from this type only" do
       a = Fabricate(Group::BottomLayer::Leader.name.to_s, label: 'Foo', group: groups(:bottom_layer_one))
       Fabricate(Group::BottomLayer::Member.name.to_s, label: 'Bar', group: groups(:bottom_layer_one))
@@ -171,10 +171,10 @@ describe Role do
       a.destroy
       Role.with_deleted.where(id: a.id).should_not be_exists
     end
-    
+
     it "flags old roles" do
       a = Fabricate(Group::BottomLayer::Leader.name.to_s, label: 'Foo', group: groups(:bottom_layer_one))
-      a.created_at = Time.zone.now - Settings.role.minimum_days_to_archive.days - 1
+      a.created_at = Time.zone.now - Settings.role.minimum_days_to_archive.days - 1.day
       a.destroy
       Role.only_deleted.find(a.id).should be_present
     end

c1f3d3ab636a695bf380312b0d3ec67531438da2
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-04-02 17:39:23 +0200

    remove report before running

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,33 @@
 class ApplicationController < ActionController::Base
-  
+
   include DecoratesBeforeRendering
   include Userstamp
-  
+
   alias_method :decorate, :__decorator_for__
-  
+
   protect_from_forgery
   helper_method :current_user, :person_home_path
-  
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => "Sie sind nicht berechtigt, diese Seite anzuzeigen"
-  end
-  
+  end if Rails.env.production?
+
   before_filter :authenticate_person!
   check_authorization :unless => :devise_controller?
 
-  
+
   private
 
   def current_user
     current_person
   end
-  
+
   def current_person
     @current_person ||= super.tap do |user|
       Person::PreloadGroups.for(user)
     end
   end
-  
+
   def person_home_path(person)
     group_person_path(person.default_group_id, person)
   end
@@ -39,5 +39,5 @@ class ApplicationController < ActionController::Base
   def reset_stamper
     Person.reset_stamper
   end
-  
+
 end
--- vendor/wagons/jubla/lib/tasks/tarantula.rake
+++ vendor/wagons/jubla/lib/tasks/tarantula.rake
@@ -1,5 +1,6 @@
 
 desc "Crawl app with tarantula"
 task :tarantula do
+  sh 'rm -rf ../../../tmp/tarantula'
   sh 'bash -c "RAILS_ENV=test ../../../script/with_mysql rake db:test:prepare app:ts:restart app:tarantula:test"'
 end

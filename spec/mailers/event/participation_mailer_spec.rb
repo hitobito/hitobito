8a55fb795870ebe9309010273ba424834d104e63
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-05-16 12:21:02 +0200

    fix mailer specs

--- spec/mailers/event/participation_mailer_spec.rb
+++ spec/mailers/event/participation_mailer_spec.rb
@@ -2,24 +2,24 @@
 require "spec_helper"
 
 describe Event::ParticipationMailer do
-  
+
   before do
     SeedFu.quiet = true
     SeedFu.seed [Rails.root.join('db', 'seeds')]
   end
-  
+
   let(:person) { people(:top_leader) }
   let(:event) { Fabricate(:event) }
   let(:participation) { Fabricate(:event_participation, event: event, person: person) }
   let(:mail) { Event::ParticipationMailer.confirmation(participation) }
-  
+
   subject { mail.body }
-  
+
   it "renders the headers" do
     mail.subject.should eq "Bestätigung der Anmeldung"
     mail.to.should eq(["top_leader@example.com"])
-    mail.from.should eq(["noreply@jubla.ch"])
-  end 
+    mail.from.should eq(["noreply@localhost"])
+  end
 
   describe "event data" do
     it "renders set attributes only" do
@@ -46,23 +46,23 @@ describe Event::ParticipationMailer do
       should =~ /Daten:<br\/>Vorweekend: 18.10.2012 - 21.10.2012<br\/>Kurs: 21.10.2012/
     end
   end
-  
+
   describe "#confirmation" do
     it { should =~ /Hallo Top/}
   end
-  
+
   describe "#approval" do
     let(:approvers) do
       [Fabricate.build(:person, email: "approver0@example.com", first_name: 'firsty'),
        Fabricate.build(:person, email: "approver1@example.com", first_name: 'lasty')]
     end
     let(:mail) { Event::ParticipationMailer.approval(participation, approvers) }
-    
+
     it "should send to approvers" do
       mail.to.should == ["approver0@example.com", "approver1@example.com"]
       mail.subject.should == 'Freigabe einer Kursanmeldung'
     end
-    
+
     it { should =~ /Hallo firsty, lasty/}
     it { should =~ /Top Leader hat sich/}
   end

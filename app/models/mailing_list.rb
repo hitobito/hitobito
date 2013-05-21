75f2c8951f8713602a05361939d71e622507dd5c
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-05-21 14:58:54 +0200

    do not include deleted roles in mailing lists

--- app/models/mailing_list.rb
+++ app/models/mailing_list.rb
@@ -14,45 +14,45 @@
 #
 
 class MailingList < ActiveRecord::Base
-  
-  attr_accessible :name, :description, :publisher, :mail_name, 
+
+  attr_accessible :name, :description, :publisher, :mail_name,
                   :additional_sender, :subscribable, :subscribers_may_post
-                  
-  
+
+
   belongs_to :group
-  
+
   has_many :subscriptions, dependent: :destroy
-  
-  
+
+
   validates :mail_name, uniqueness: { case_sensitive: false },
                         format: /\A[a-z][a-z0-9\-\_\.]*\Z/,
                         allow_blank: true
   validate :assert_mail_name_is_not_protected
 
-  
-  
+
+
   def to_s
     name
   end
-  
+
   def mail_address
     "#{mail_name}@#{mail_domain}" if mail_name?
   end
-  
+
   def mail_domain
     Settings.email.list_domain
   end
-  
+
   def subscribed?(person)
     people.where(id: person.id).exists?
   end
-  
+
   def exclude_person(person)
-    subscriptions.where(subscriber_id: person.id, 
+    subscriptions.where(subscriber_id: person.id,
                         subscriber_type: Person.sti_name,
                         excluded: false).
                   destroy_all
-                  
+
     if subscribed?(person)
       sub = subscriptions.new
       sub.subscriber = person
@@ -60,7 +60,7 @@ class MailingList < ActiveRecord::Base
       sub.save!
     end
   end
-  
+
   def people
     condition = OrCondition.new
     # person subscribers
@@ -69,30 +69,31 @@ class MailingList < ActiveRecord::Base
                  "subscriptions.subscriber_id = people.id",
                  Person.sti_name,
                  false)
-                 
+
     # event subscribers
     condition.or("subscriptions.subscriber_type = ? AND " <<
                  "subscriptions.subscriber_id = event_participations.event_id AND " <<
                  "event_participations.active = ?",
                  Event.sti_name,
                  true)
-                 
+
     # group subscribers
     condition.or("subscriptions.subscriber_type = ? AND " <<
                  "subscriptions.subscriber_id = sub_groups.id AND " <<
                  "groups.lft >= sub_groups.lft AND groups.rgt <= sub_groups.rgt AND " <<
-                 "roles.type = related_role_types.role_type",
+                 "roles.type = related_role_types.role_type AND " <<
+                 "roles.deleted_at IS NULL",
                  Group.sti_name)
-                 
+
     Person.only_public_data.
            joins("LEFT JOIN roles ON people.id = roles.person_id").
            joins("LEFT JOIN groups ON roles.group_id = groups.id").
            joins("LEFT JOIN event_participations ON event_participations.person_id = people.id").
            joins(", subscriptions ").
-           joins("LEFT JOIN groups sub_groups " << 
-                 "ON subscriptions.subscriber_type = 'Group'" << 
+           joins("LEFT JOIN groups sub_groups " <<
+                 "ON subscriptions.subscriber_type = 'Group'" <<
                  "AND subscriptions.subscriber_id = sub_groups.id ").
-           joins("LEFT JOIN related_role_types " << 
+           joins("LEFT JOIN related_role_types " <<
                  "ON related_role_types.relation_type = 'Subscription' " <<
                  "AND related_role_types.relation_id = subscriptions.id").
            where(subscriptions: { mailing_list_id: id }).
@@ -102,9 +103,9 @@ class MailingList < ActiveRecord::Base
            where(condition.to_a).
            uniq
   end
-  
+
   private
-  
+
   def assert_mail_name_is_not_protected
     if mail_name? && main = Settings.email.retriever.config.user_name.presence
       if mail_name.downcase == main.split('@', 2).first.downcase

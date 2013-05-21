27692b1c23f0535adad2e5ea3319a21e90a8e2a4
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-05-21 16:58:36 +0200

    order courses by event kind label, display message for empty list

--- app/controllers/event/lists_controller.rb
+++ app/controllers/event/lists_controller.rb
@@ -1,6 +1,6 @@
 class Event::ListsController < ApplicationController
   include YearBasedPaging
-  
+
   attr_reader :group_id
   helper_method :group_id
 
@@ -9,13 +9,13 @@ class Event::ListsController < ApplicationController
 
   def events
     authorize!(:index, Event)
-      
+
     events = Event.upcoming.
                    in_hierarchy(current_user).
                    includes(:dates, :groups).
                    where('events.type != ? OR events.type IS NULL', Event::Course.sti_name).
                    order('event_dates.start_at ASC')
-                  
+
     @events_by_month = EventDecorator.decorate(events).group_by do |entry|
       if entry.dates.present?
         l(entry.dates.first.start_at, format: :month_year)
@@ -28,29 +28,29 @@ class Event::ListsController < ApplicationController
   def courses
     authorize!(:index, Event::Course)
     set_group_vars
-    scope = Event::Course.order('event_kinds.id').in_year(year).list
+    scope = Event::Course.order('event_kinds.label').in_year(year).list
     courses = EventDecorator.decorate(limit_scope_for_user(scope))
     @courses_by_kind = courses.group_by { |entry| entry.kind.label }
-    @courses_by_kind.each do |kind, entries| 
+    @courses_by_kind.each do |kind, entries|
       entries.sort_by! {|e| e.dates.first.try(:start_at) || Time.zone.now }.
               collect! {|e| EventDecorator.new(e) }
     end
   end
 
   private
-  
+
   def set_group_vars
     if can?(:manage_courses, current_user)
       # assign default group on initial request
-      unless params[:year].present? 
+      unless params[:year].present?
         params[:group_id] = Group.course_offerers.
                                   where(id: current_user.groups_hierarchy_ids).
                                   where("groups.id <> ?", Group.root.id).
                                   select(:id).
                                   first.
                                   try(:id)
-      end 
-      @group_id = params[:group_id].to_i 
+      end
+      @group_id = params[:group_id].to_i
     end
 
   end
--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,5 +1,5 @@
 module LayoutHelper
-  
+
   # render a single button
   def action_button(label, url, icon = nil, options = {})
     if @in_button_group || options[:in_button_group]
@@ -8,7 +8,7 @@ module LayoutHelper
       button_group { button(label, url, icon, options) }
     end
   end
-  
+
   def button_group(&block)
     if @in_button_group
       capture(&block)
@@ -16,14 +16,14 @@ module LayoutHelper
       in_button_group { content_tag(:div, class: 'btn-group', &block) }
     end
   end
-  
+
   def in_button_group(&block)
     @in_button_group = true
     yield
   ensure
     @in_button_group = false
   end
- 
+
   def pill_dropdown_button(dropdown, ul_classes = 'pull-right')
     dropdown.button_class = nil
     content_tag(:ul, class: "nav nav-pills #{ul_classes}") do
@@ -32,46 +32,50 @@ module LayoutHelper
       end
     end
   end
-  
+
   def icon(name)
     content_tag(:i, '', class: "icon icon-#{name}")
   end
-  
+
   def section(title, &block)
     render(layout: 'shared/section', locals: {title: title}, &block)
   end
-  
+
   def section_table(title, collection, add_path = nil, &block)
     collection.inspect # force relation evaluation
     if add_path || collection.present?
       if add_path
         title = safe_join([title,
                            content_tag(:span,
-                                       action_button('Erstellen', add_path, 'plus', class: 'btn-small'), 
+                                       action_button('Erstellen', add_path, 'plus', class: 'btn-small'),
                                        class: 'pull-right')])
       end
-      render(layout: 'shared/section_table', 
-             locals: {title: title, collection: collection, add_path: add_path}, 
+      render(layout: 'shared/section_table',
+             locals: {title: title, collection: collection, add_path: add_path},
              &block)
-    end 
+    end
   end
-  
+
   def grouped_table(grouped_lists, column_count, &block)
-    render(layout: 'shared/grouped_table',
-           locals: {grouped_lists: grouped_lists, column_count: column_count},
-           &block)
+    if grouped_lists.present?
+      render(layout: 'shared/grouped_table',
+             locals: {grouped_lists: grouped_lists, column_count: column_count},
+             &block)
+    else
+      content_tag(:div, ti(:no_list_entries), :class => 'table')
+    end
   end
-  
+
   def muted(text = nil, &block)
     content_tag(:span, text, class: 'muted', &block)
   end
-  
+
   def value_with_muted(value, mute)
     safe_join([f(value), muted(mute)], ' ')
   end
-    
+
   private
-  
+
   def button(label, url, icon_name = nil, options = {})
     add_css_class options, 'btn'
     link_to(url, options) do

288ea24daf18e29291f96ec7a25a2369c307db95
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-06-04 12:48:46 +0200

    load abilities on first access only

--- app/abilities/ability.rb
+++ app/abilities/ability.rb
@@ -5,7 +5,11 @@ class Ability
   attr_reader :user_context
 
   class << self
-    attr_reader :abilities, :subject_classes
+    attr_reader :subject_classes
+
+    def abilities
+      @abilities ||= load_abilities
+    end
 
     def register(*ability_classes)
       @subject_classes ||= {}
@@ -13,18 +17,25 @@ class Ability
         ability_class.subject_classes.each do |subject_class|
           @subject_classes[subject_class] = ability_class
         end
-
-        AbilityDsl::Config.new(ability_class).define
       end
     end
 
     def add_config(permission, subject_class, actions, condition)
-      @abilities ||= {}
       @abilities[subject_class] ||= {}
       @abilities[subject_class][permission] ||= {}
       actions.each do |action|
         @abilities[subject_class][permission][action] = condition
       end
+    end
+
+    private
+
+    def load_abilities
+      @abilities = {}
+      subject_classes.values.uniq.each do |ability_class|
+        AbilityDsl::Config.new(ability_class).define
+      end
+      @abilities
     end
   end
 

a356870090364ab76e1c991dc9ad0c565011994f
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-07-15 13:32:36 +0200

    prefix RAILS_ to env var names

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,12 +1,12 @@
 Airbrake.configure do |config|
-  config.api_key     = ENV['AIRBRAKE_API_KEY']
-  config.host        = ENV['AIRBRAKE_HOST']
-  config.port        = ENV['AIRBRAKE_PORT'] || 443
+  config.api_key     = ENV['RAILS_AIRBRAKE_API_KEY']
+  config.host        = ENV['RAILS_AIRBRAKE_HOST']
+  config.port        = ENV['RAILS_AIRBRAKE_PORT'] || 443
   config.secure      = config.port == 443
   config.ignore         << ActionController::MethodNotAllowed
   config.ignore         << ActionController::RoutingError
   config.ignore         << ActionController::UnknownHttpMethod
   config.params_filters << 'RAILS_DB_PASSWORD'
   config.params_filters << 'RAILS_MAIL_RETRIEVER_PASSWORD'
-  config.params_filters << 'AIRBRAKE_API_KEY'
+  config.params_filters << 'RAILS_AIRBRAKE_API_KEY'
 end

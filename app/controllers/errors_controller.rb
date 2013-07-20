b5bbc0182ff2d04ec8c051fb9d47056bda0bb937
Pascal Zumkehr <spam@codez.ch>
2013-07-20 12:29:31 +0200

    skip stamper filters for errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,7 @@
 class ErrorsController < ApplicationController
+  skip_before_filter :set_stamper
   skip_before_filter :authenticate_person!
+  skip_after_filter :reset_stamper
+
   skip_authorization_check
 end

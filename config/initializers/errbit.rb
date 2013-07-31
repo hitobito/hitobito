adc3991fc4441aae0d6e83020d42748f137c5884
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-07-31 13:29:10 +0200

    convert airbrake port to integer

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -8,7 +8,7 @@
 Airbrake.configure do |config|
   config.api_key     = ENV['RAILS_AIRBRAKE_API_KEY']
   config.host        = ENV['RAILS_AIRBRAKE_HOST']
-  config.port        = ENV['RAILS_AIRBRAKE_PORT'] || 443
+  config.port        = (ENV['RAILS_AIRBRAKE_PORT'] || 443).to_i
   config.secure      = config.port == 443
   config.ignore         << ActionController::MethodNotAllowed
   config.ignore         << ActionController::RoutingError

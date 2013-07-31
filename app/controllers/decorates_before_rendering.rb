55650219d5451cc36c248cf54391218651dc310d
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-07-31 12:55:24 +0200

    integrate customized decorates before rendering

---
+++ app/controllers/decorates_before_rendering.rb
@@ -0,0 +1,75 @@
+# encoding: utf-8
+
+# Copyright (c) 2012 Rob Hanlon, MIT License
+
+require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/class/attribute'
+
+# Decorates the specified fields. For instance, if you have
+#
+#   class StuffController < ApplicationController
+#     include DecoratesBeforeRendering
+#
+#     decorates :thing_1, :thing_2
+#   end
+#
+# @thing_1 and @thing_2 will be decorated right before a rendering occurs.
+#
+module DecoratesBeforeRendering
+  module ClassMethods
+    def decorates(*unsigiled_ivar_names)
+      self.__ivars_to_decorate__ = unsigiled_ivar_names.map { |i| "@#{i}" }
+    end
+  end
+
+  def render(*args)
+    __decorate_ivars__
+    super(*args)
+  end
+
+private
+
+  def __decorate_ivars__
+    ivars_to_decorate = self.class.__ivars_to_decorate__
+
+    return if ivars_to_decorate.nil?
+
+    ivars_to_decorate.each do |ivar_name|
+      ivar = instance_variable_get(ivar_name)
+      instance_variable_set(ivar_name, __decorator_for__(ivar)) unless ivar.nil?
+    end
+  end
+
+  def __decorator_for__(ivar)
+    __decorator_name_for__(ivar).constantize.decorate(ivar)
+  end
+
+  def __decorator_name_for__(ivar)
+    org_ivar = ivar
+    decorator_name = "#{__model_name_for__(ivar)}Decorator"
+    while (decorator_name.constantize rescue nil) == nil
+      superclass = ivar.respond_to?(:model_name) ? ivar.superclass : ivar.class.superclass
+      raise ArgumentError, "#{org_ivar} does not have an associated decorator" if superclass == Module
+      superclass_decorator_name = (superclass == Object ? "Object" : superclass.model_name)
+      superclass_decorator_name += "Decorator"
+      ivar = superclass
+      decorator_name = superclass_decorator_name
+    end
+    decorator_name
+  end
+
+  def __model_name_for__(ivar)
+    if ivar.respond_to?(:model_name)
+      ivar
+    elsif ivar.class.respond_to?(:model_name)
+      ivar.class
+    else
+      raise ArgumentError, "#{ivar} does not have an associated model"
+    end.model_name
+  end
+
+  def self.included(base)
+    base.class_attribute :__ivars_to_decorate__, :instance_accessor => false
+    base.extend ClassMethods
+  end
+end

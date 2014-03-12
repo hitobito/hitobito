cf4022d61a6aa5f9bbb462c22eeb903214e872ea
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-03-12 09:12:20 +0100

    remove unused method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -83,12 +83,4 @@ class EventsController < CrudController
     send(path, group, options)
   end
 
-  # TODO: dead code?
-  def type_name(event_type)
-    if event_type == Event
-      'simple'
-    else
-      event_type.name.demodulize.underscore
-    end
-  end
 end

817e589e9433c0164b3040ce619122f8f0b847bb
Andreas Maierhofer <maierhofer@puzzle.ch>
2014-07-23 16:24:12 +0200

    verify existence before migrating qualification

--- db/migrate/20140707084827_add_qualification_kinds_for_leaders.rb
+++ db/migrate/20140707084827_add_qualification_kinds_for_leaders.rb
@@ -6,6 +6,40 @@
 #  https://github.com/hitobito/hitobito.
 
 class AddQualificationKindsForLeaders < ActiveRecord::Migration
+
+  class Updater < Struct.new(:postfix, :category, :migration)
+    QUALIFICATION_KIND_IDS = QualificationKind.pluck(:id)
+    EVENT_KIND_IDS = Event::Kind.pluck(:id)
+
+    QUERY = "INNER JOIN event_kinds_%{postfix} ON event_kinds_%{postfix}.qualification_kind_id = qualification_kinds.id"
+
+    def update
+      records = query
+      current_records = current(query)
+
+      migration.say("rejecting #{records.size - current_records.size} #{category}")
+
+      current_records.each do |qualification_kind_id, event_kind_id|
+        Event::KindQualificationKind.create!(event_kind_id: event_kind_id,
+                                            qualification_kind_id: qualification_kind_id,
+                                            category: category.to_s,
+                                            role: 'participant')
+      end
+    end
+
+    private
+
+    def query
+      QualificationKind.joins(QUERY % { postfix: postfix }).pluck(:id, :event_kind_id) or []
+    end
+
+    def current(list)
+      list.select do |qualification_kind_id, event_kind_id|
+        QUALIFICATION_KIND_IDS.include?(qualification_kind_id) && EVENT_KIND_IDS.include?(event_kind_id)
+      end
+    end
+  end
+
   def change
     create_table :event_kind_qualification_kinds do |t|
       t.belongs_to :event_kind, null: false
@@ -14,34 +48,9 @@ class AddQualificationKindsForLeaders < ActiveRecord::Migration
       t.string :role, null: false
     end
 
-    join_query = "INNER JOIN event_kinds_%{postfix} ON event_kinds_%{postfix}.qualification_kind_id = qualification_kinds.id"
-
-    qualifications = QualificationKind.joins(join_query % {postfix: 'qualification_kinds'}).
-                                       pluck(:id, :event_kind_id) or []
-    qualifications.each do |qualification_kind_id, event_kind_id|
-      Event::KindQualificationKind.create!(event_kind_id: event_kind_id,
-                                           qualification_kind_id: qualification_kind_id,
-                                           category: 'qualification',
-                                           role: 'participant')
-    end
-
-    preconditions = QualificationKind.joins(join_query % {postfix: 'preconditions'}).
-                                      pluck(:id, :event_kind_id) or []
-    preconditions.each do |qualification_kind_id, event_kind_id|
-      Event::KindQualificationKind.create!(event_kind_id: event_kind_id,
-                                           qualification_kind_id: qualification_kind_id,
-                                           category: 'precondition',
-                                           role: 'participant')
-    end
-
-    prolongations = QualificationKind.joins(join_query % {postfix: 'prolongations'}).
-                                      pluck(:id, :event_kind_id) or []
-    prolongations.each do |qualification_kind_id, event_kind_id|
-      Event::KindQualificationKind.create!(event_kind_id: event_kind_id,
-                                           qualification_kind_id: qualification_kind_id,
-                                           category: 'prolongation',
-                                           role: 'participant')
-    end
+    Updater.new(:qualification_kinds, :qualification, self).update
+    Updater.new(:preconditions, :precondition, self).update
+    Updater.new(:prolongations, :prolongation, self).update
 
     drop_table :event_kinds_qualification_kinds
     drop_table :event_kinds_prolongations
@@ -51,3 +60,4 @@ class AddQualificationKindsForLeaders < ActiveRecord::Migration
     add_index(:event_kind_qualification_kinds, :role)
   end
 end
+

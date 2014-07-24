e024b121312e18aa9ed8746d1d06180daf646f1b
Andreas Maierhofer <maierhofer@puzzle.ch>
2014-07-24 15:53:58 +0200

    only update ov question once

--- db/migrate/20140724090529_update_event_questions_wording.rb
+++ db/migrate/20140724090529_update_event_questions_wording.rb
@@ -7,8 +7,7 @@
 
 class UpdateEventQuestionsWording < ActiveRecord::Migration
   def change
-    Event::Question.find_by(event_id: nil,
-                            question: 'Ich habe folgendes ÖV Abo').
-                            update_attribute(:question, 'Ich habe während dem Kurs folgendes ÖV Abo')
+    question = Event::Question.find_by(event_id: nil, question: 'Ich habe folgendes ÖV Abo')
+    question && question.update_attribute(:question, 'Ich habe während dem Kurs folgendes ÖV Abo')
   end
 end

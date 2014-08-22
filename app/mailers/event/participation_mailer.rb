6f310725c2416f5091ea0ecdf631198eb2cffff4
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-08-22 22:42:17 +0200

    explicitly set view context for draper in mailer using decorators to avoid helper methods not found

--- app/mailers/event/participation_mailer.rb
+++ app/mailers/event/participation_mailer.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2014, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -13,28 +13,32 @@ class Event::ParticipationMailer < ActionMailer::Base
   # Include all helpers that are required directly or indirectly (in decorators)
   helper :standard, :layout, :auto_link
 
-  def confirmation(participation)
-    person = participation.person
+  attr_reader :participation
 
-    compose(participation,
-            CONTENT_CONFIRMATION,
+  def confirmation(participation)
+    @participation = participation
+
+    compose(CONTENT_CONFIRMATION,
             [person],
             'recipient-name' => person.greeting_name)
   end
 
   def approval(participation, recipients)
-    compose(participation,
-            CONTENT_APPROVAL,
+    @participation = participation
+
+    compose(CONTENT_APPROVAL,
             recipients,
-            'participant-name' => participation.person.to_s,
+            'participant-name' => person.to_s,
             'recipient-names'  => recipients.collect(&:greeting_name).join(', '))
   end
 
   private
 
-  def compose(participation, content_key, recipients, values = {})
-    @participation = participation
-    @event = participation.event
+  def compose(content_key, recipients, values = {})
+    # Assert the current mailer's view context is stored as Draper::ViewContext.
+    # This is done in the #view_context method overriden by Draper.
+    # Otherwise, decorators will not have access to all helper methods.
+    view_context
 
     content = CustomContent.get(content_key)
     values['event-details']   = event_details
@@ -46,19 +50,19 @@ class Event::ParticipationMailer < ActionMailer::Base
   end
 
   def participation_url
-    group_event_participation_url(@event.groups.first, @event, @participation)
+    group_event_participation_url(event.groups.first, event, participation)
   end
 
   # rubocop:disable MethodLength
   def event_details
     infos = []
-    infos << @event.name
-    infos << labeled(:dates)    { @event.dates.map(&:to_s).join('<br/>') }
+    infos << event.name
+    infos << labeled(:dates)    { event.dates.map(&:to_s).join('<br/>') }
     infos << labeled(:motto)
     infos << labeled(:cost)
-    infos << labeled(:description) { @event.description.gsub("\n", '<br/>') }
-    infos << labeled(:location) { @event.location.gsub("\n", '<br/>') }
-    infos << labeled(:contact)  { "#{@event.contact}<br/>#{@event.contact.email}" }
+    infos << labeled(:description) { event.description.gsub("\n", '<br/>') }
+    infos << labeled(:location) { event.location.gsub("\n", '<br/>') }
+    infos << labeled(:contact)  { "#{event.contact}<br/>#{event.contact.email}" }
     infos << answers_details
     infos << additional_information_details
     infos << participation_details
@@ -67,18 +71,18 @@ class Event::ParticipationMailer < ActionMailer::Base
   # rubocop:enable MethodLength
 
   def labeled(key)
-    value = @event.send(key).presence
+    value = event.send(key).presence
     if value
-      label = @event.class.human_attribute_name(key)
+      label = event.class.human_attribute_name(key)
       formatted = block_given? ? yield : value
       "#{label}:<br/>#{formatted}"
     end
   end
 
   def answers_details
-    if @participation.answers.present?
+    if participation.answers.present?
       text = ["#{Event::Participation.human_attribute_name(:answers)}:"]
-      @participation.answers.each do |a|
+      participation.answers.each do |a|
         text << "#{a.question.question}: #{a.answer}"
       end
       text.join('<br/>')
@@ -86,16 +90,24 @@ class Event::ParticipationMailer < ActionMailer::Base
   end
 
   def additional_information_details
-    if @participation.additional_information?
+    if participation.additional_information?
       t('activerecord.attributes.event/participation.additional_information') +
       ':<br/>' +
-      @participation.additional_information.gsub("\n", '<br/>')
+      participation.additional_information.gsub("\n", '<br/>')
     end
   end
 
   def participation_details
     ["#{Event::Role::Participant.model_name.human}:",
-     @participation.person.decorate.complete_contact].join('<br/>')
+     person.decorate.complete_contact].join('<br/>')
+  end
+
+  def person
+    participation.person
+  end
+
+  def event
+    participation.event
   end
 
 end

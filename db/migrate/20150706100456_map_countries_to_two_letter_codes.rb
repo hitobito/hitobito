7ab00f476be92f79a752438c323119dc91352cd5
Andreas Maierhofer <maierhofer@puzzle.ch>
2015-07-06 14:37:57 +0200

    add missing headers to migration

--- db/migrate/20150706100456_map_countries_to_two_letter_codes.rb
+++ db/migrate/20150706100456_map_countries_to_two_letter_codes.rb
@@ -1,5 +1,10 @@
 # encoding: UTF-8
 
+#  Copyright (c) 2012-2015, Pfadibewegung Schweiz. This file is part of
+#  hitobito_youth and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_youth.
+
 class MapCountriesToTwoLetterCodes < ActiveRecord::Migration
   def up
     say_with_time('updating groups') do

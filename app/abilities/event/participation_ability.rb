8bba27c1a9e0dbc83747c4cb74634779c8345c28
Andreas Maierhofer <maierhofer@puzzle.ch>
2015-07-15 06:48:45 +0200

    reuse constraints from participation ability, refs #11571

--- app/abilities/event/participation_ability.rb
+++ app/abilities/event/participation_ability.rb
@@ -43,40 +43,13 @@ class Event::ParticipationAbility < AbilityDsl::Base
     her_own || for_participations_read_events
   end
 
-  def her_own_or_for_participations_full_events
-    her_own || for_participations_full_events
-  end
-
   def her_own_if_application_possible
     her_own && event.application_possible?
-  end
-
-  def in_same_layer_or_different_prio
-    in_same_layer || different_prio
-  end
-
-  def in_same_layer_or_below_or_different_prio
-    in_same_layer_or_below || different_prio
   end
 
   private
 
   def participation
     subject
-  end
-
-  def different_prio
-    return false if participation.active? || !participation.application_id?
-
-    # This is a bit more than really needed, to restrict further we would
-    # need the actual course the participation should be displayed for,
-    # which we do not have.
-    appl = participation.application
-    (appl.waiting_list? || appl.priority_2_id? || appl.priority_3_id?) &&
-    permission_in_layers?(course_offerers)
-  end
-
-  def course_offerers
-    @course_offerers ||= Group.course_offerers.pluck(:id)
   end
 end

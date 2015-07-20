abde5f633cc279261668006fade44a67dbebdf6d
Pascal Zumkehr <zumkehr@puzzle.ch>
2015-07-20 09:53:40 +0200

    use complete zip codes, fixes #11679

--- db/seeds/support/location_seeder.rb
+++ db/seeds/support/location_seeder.rb
@@ -7,16 +7,19 @@
 
 require 'csv'
 
-# Imports an ISO-8859-1 CSV with the columns zip_code, town and canton.
+# Imports a CSV with the columns zip_code, town and canton.
 #
 # To update the locations.csv file:
 # * Get the file manually from https://www.post.ch/de/pages/downloadcenter-match
-# * Strip all unneeded rows and columns
+# * Strip all unneeded rows and columns.
+#   BEWARE: There are two zip_code columns, only one contains 3006!
 # * Add a header for zip_code, town and canton
-# * Save with separator ; encoded as ISO-8859-1
+# * Save with separator ; encoded as UTF-8 (Libre Office: Save as > Edit Filter Settings)
 class LocationSeeder
 
   FILE = Rails.root.join('db', 'seeds', 'support', 'locations.csv')
+  SEPARATOR = ';'
+  ENCODING = 'UTF-8'
 
   def seed
     Location.delete_all
@@ -28,18 +31,19 @@ class LocationSeeder
   def bulk_insert
     data.each_slice(500).each_with_index do |values, slice|
       puts " - Location: inserting slice #{slice}"
-      Location.connection.execute("insert into locations(canton, zip_code, name) values #{values.join(',')}")
+      insert = "INSERT INTO locations (canton, zip_code, name) VALUES #{values.join(',')}"
+      Location.connection.execute(insert)
     end
   end
 
   def data
     csv.each_with_object([]) do |row, data|
-      data << "('#{row['canton']}', #{row['zip_code']}, \"#{row['town']}\")"
+      data << "(\"#{row['canton']}\", #{row['zip_code']}, \"#{row['town']}\")"
     end.uniq
   end
 
   def csv
-    CSV.read(FILE, headers: true, col_sep: ';', encoding: 'ISO-8859-1')
+    CSV.read(FILE, headers: true, col_sep: SEPARATOR, encoding: ENCODING)
   end
 
 end

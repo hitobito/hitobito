1196ab6555ea663b07a5635754bcfcf1930501ba
Pascal Zumkehr <zumkehr@puzzle.ch>
2015-08-11 18:54:52 +0200

    try with wagon exec

--- bin/ci/wagon_commit.sh
+++ bin/ci/wagon_commit.sh
@@ -2,5 +2,5 @@
 
 . hitobito/bin/ci/wagon_setup.sh
 
-bundle exec rake db:create db:test:prepare --trace &&
+bundle exec rake db:create wagon:exec CMD="bundle exec rake app:db:test:prepare" --trace &&
 bundle exec rake ci:wagon --trace
--- bin/ci/wagon_nightly_master.sh
+++ bin/ci/wagon_nightly_master.sh
@@ -2,6 +2,6 @@
 
 . hitobito/bin/ci/wagon_setup.sh
 
-bundle exec rake db:create db:test:prepare --trace &&
+bundle exec rake db:create wagon:exec CMD="bundle exec rake app:db:test:prepare" --trace &&
 bundle exec rake ci:wagon:nightly --trace &&
 bundle exec rake wagon:exec CMD='rake app:tarantula:test app:tx:auth app:tx:push' --trace

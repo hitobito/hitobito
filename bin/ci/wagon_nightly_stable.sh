5383e1113c80110b5ddf4dd36e5f0da65d105894
Pascal Zumkehr <zumkehr@puzzle.ch>
2015-08-13 08:51:25 +0200

    remove db:test:prepare again from ci scripts

--- bin/ci/wagon_nightly_stable.sh
+++ bin/ci/wagon_nightly_stable.sh
@@ -1,3 +1,9 @@
+#  Copyright (c) 2015, Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+
 # Runs a wagon nightly build for the stable branch
 
 . hitobito/bin/ci/wagon_setup.sh
--- bin/ci/wagon_setup.sh
+++ bin/ci/wagon_setup.sh
@@ -1,3 +1,9 @@
+#  Copyright (c) 2015, Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+
 # set up everything required to run rake jobs for hitobito wagons on jenkins
 
 cd hitobito

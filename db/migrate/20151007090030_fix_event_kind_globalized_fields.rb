15e701abe700b72c72a164161728a5ab0afce33f
Mathis Hofer <hofer@puzzle.ch>
2015-10-07 14:10:31 +0200

    remove wagon field from event kinds migration, refs #11554, #11553

--- db/migrate/20151007090030_fix_event_kind_globalized_fields.rb
+++ db/migrate/20151007090030_fix_event_kind_globalized_fields.rb
@@ -2,11 +2,9 @@ class FixEventKindGlobalizedFields < ActiveRecord::Migration
   def up
     Event::Kind.add_translation_fields!({ general_information: :text }, migrate_data: true)
     Event::Kind.add_translation_fields!({ application_conditions: :text }, migrate_data: true)
-    Event::Kind.add_translation_fields!({ documents_text: :text }, migrate_data: true)
 
     remove_column :event_kinds, :general_information
     remove_column :event_kinds, :application_conditions
-    remove_column :event_kinds, :documents_text
   end
 
   def down
@@ -14,10 +12,8 @@ class FixEventKindGlobalizedFields < ActiveRecord::Migration
 
     add_column(:event_kinds, :general_information, :text)
     add_column(:event_kinds, :application_conditions, :text)
-    add_column(:event_kinds, :documents_text, :text)
 
     remove_column :event_kind_translations, :general_information
     remove_column :event_kind_translations, :application_conditions
-    remove_column :event_kind_translations, :documents_text
   end
 end

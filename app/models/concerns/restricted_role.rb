a93fb7e3a7470e159a8caeeef0503bda3c847922
Mathis Hofer <hofer@puzzle.ch>
2015-10-20 16:59:51 +0200

    handle string ids when determining restricted role changes, refs #13257

--- app/models/concerns/restricted_role.rb
+++ app/models/concerns/restricted_role.rb
@@ -23,7 +23,7 @@ module RestrictedRole
     @restricted_role_changes = {}
     restricted_roles.each do |attr, type|
       role = restricted_role(attr, type)
-      if role.try(:person_id) != send("#{attr}_id")
+      if role.try(:person_id) != send("#{attr}_id").to_i
         destroy_previous_role(attr, type) if role
         id = restricted_role_id(attr, type).presence
         build_restricted_role(type.new, id).save! if id

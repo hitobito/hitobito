8d9d61b1ea7ad44b2aca263abbe765ddd27bdc33
Pascal Simon <simon@puzzle.ch>
2015-11-05 17:30:06 +0100

    set new primary group migration, refs #13751

--- db/migrate/20151103115637_update_peoples_primary_group.rb
+++ db/migrate/20151103115637_update_peoples_primary_group.rb
@@ -1,10 +1,10 @@
 class UpdatePeoplesPrimaryGroup < ActiveRecord::Migration
   def up
-    people = Person.joins(:roles).where(primary_group_id: nil).having('count(distinct roles.group_id) = 1')
-    people.update_all('primary_group_id = roles.group_id')
-    #people.find_each do |p|
-      #group_id = p.roles.first.group_id
-      #p.update_column(:primary_group_id, group.id)
-    #end
+    # people with no primary group and only one active role
+    people = Person.joins(:roles).where(primary_group_id: nil).group('people.id').having('count(distinct roles.group_id) = 1')
+    people.find_each do |p|
+      group_id = p.roles.first.group_id
+      p.update_column(:primary_group_id, group_id)
+    end
   end
 end

e16347207bac9a1438aff6ccb814a51f75a643a9
Pascal Simon <simon@puzzle.ch>
2015-11-09 10:05:37 +0100

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007121335) do
+ActiveRecord::Schema.define(version: 20151103115637) do
 
   create_table "additional_emails", force: :cascade do |t|
     t.integer "contactable_id",                  null: false

c2d36dfde02c1e3809f21b865e1a98391f6d31bf
Pascal Simon <simon@puzzle.ch>
2016-02-16 13:04:10 +0100

    upload rpm fix syntax

--- config/rpm/upload_rpm.sh
+++ config/rpm/upload_rpm.sh
@@ -7,7 +7,7 @@ fi
 
 rpmdir="/var/lib/mock/epel-6-x86_64/result"
 
-if [ ! -f $rpmdir/$RPM_NAME-*.rpm ]; do
+if [ ! -f $rpmdir/$RPM_NAME-*.rpm ]; then
   echo "no rpm file for given project for upload found."
   echo "rpm name: ${RPM_NAME}"
   echo "rpm location: ${rpmdir}"

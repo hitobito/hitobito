9fd15e36710af360b6c3520c186eab80821d813a
Mathis Hofer <hofer@puzzle.ch>
2016-11-16 12:21:02 +0100

    remove tags from address and full person exports

--- app/domain/export/csv/people/people_address.rb
+++ app/domain/export/csv/people/people_address.rb
@@ -28,7 +28,7 @@ module Export::Csv::People
 
     def person_attributes
       [:first_name, :last_name, :nickname, :company_name, :company, :email,
-       :address, :zip_code, :town, :country, :gender, :birthday, :roles, :tags]
+       :address, :zip_code, :town, :country, :gender, :birthday, :roles]
     end
 
     def association_attributes

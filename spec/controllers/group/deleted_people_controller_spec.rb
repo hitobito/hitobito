31f964d9c9a41dca752f3ce2d124c0d44fb809a3
Dinuu <martin@zeilenwerk.ch>
2016-11-28 11:43:48 +0100

    add tests

---
+++ spec/controllers/group/deleted_people_controller_spec.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+require 'spec_helper'
+
+describe Group::DeletedPeopleController do
+
+  let(:group) { groups(:top_group) }
+  let(:person) { people(:top_leader)  }
+
+  context 'authenticated' do
+
+    before { sign_in(person) }
+
+    it 'render index view' do
+      get :index, group_id: group.id
+      is_expected.to render_template('group/deleted_people/index')
+    end
+
+    it 'find entry' do
+      expect(Group::DeletedPeopleController.send(:find_entry)).to eq(person)
+    end
+
+  end
+end

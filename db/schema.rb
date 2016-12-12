4e155b67314711205ebbac9272e5bb899a06f751
Dinuu <martin@zeilenwerk.ch>
2016-12-12 16:06:37 +0100

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system. 
 
 ActiveRecord::Schema.define(version: 20170103142035) do
 

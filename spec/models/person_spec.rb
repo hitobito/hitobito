7627ef175d6d331d6f1b23fdb252abb5f9370c34
Pascal Zumkehr <zumkehr@puzzle.ch>
2017-01-04 13:54:28 +0100

    add missing comment

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,4 +1,4 @@
- encoding: utf-8
+# encoding: utf-8
 
 #  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3

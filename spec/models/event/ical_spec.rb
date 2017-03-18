678d51f8edd20896adbd226b655f8dcc409b28ff
Diego Steiner <diego.steiner@renuo.ch>
2017-03-18 17:36:00 +0100

    Introduce ical format in event model

---
+++ spec/models/event/ical_spec.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+require 'spec_helper'
+
+describe Event do
+  let(:event) { events(:top_course) }
+  let(:event_date) { event.dates.first }
+
+  describe '#ical' do
+    subject(:ical) { event.ical }
+
+    it 'contains the event dates' do
+      is_expected.to all(be_a(Icalendar::Event))
+      expect(ical.first).to have_attributes(
+        dtstart: event_date.start_at ,
+        dtend: event_date.finish_at,
+        summary: event_date.label
+      )
+      #expect(ical.to_ical).to include('VCALENDAR', 'DTSTART', 'DTEND', 'SUMMARY')
+    end
+  end
+end

9db26620f4c5a83daf2173415109655722f33bcc
Pascal Zumkehr <zumkehr@puzzle.ch>
2017-05-31 17:30:08 +0200

    update course qualifications only on form submit, refs #286

--- app/controllers/event/qualifications_controller.rb
+++ app/controllers/event/qualifications_controller.rb
@@ -9,32 +9,26 @@ class Event::QualificationsController < ApplicationController
 
   before_action :authorize
 
-  decorates :event, :leaders, :participants, :participation, :group
+  decorates :event, :leaders, :participants, :group
 
-  helper_method :event, :participation
+  helper_method :event
 
   def index
     entries
   end
 
   def update
-    qualifier.issue
-
-    @nothing_changed = qualifier.nothing_changed?
-
-    respond_to do |format|
-      format.html { redirect_to group_event_qualifications_path(group, event) }
-      format.js   { render 'qualification' }
+    Qualification.transaction do
+      entries
+      (@leaders + @participants).uniq.each do |participation|
+        qualifier = Event::Qualifier.for(participation)
+        qualified = Array(params[:participation_ids]).include?(participation.id.to_s)
+        qualified ? qualifier.issue : qualifier.revoke
+      end
     end
-  end
 
-  def destroy
-    qualifier.revoke
-
-    respond_to do |format|
-      format.html { redirect_to group_event_qualifications_path(group, event) }
-      format.js   { render 'qualification' }
-    end
+    redirect_to group_event_qualifications_path(group, event),
+                notice: t('event.qualifications.update.flash.success')
   end
 
   private
@@ -45,20 +39,12 @@ class Event::QualificationsController < ApplicationController
     @participants ||= participations(*types.select(&:participant?))
   end
 
-  def qualifier
-    @qualifier ||= Event::Qualifier.for(participation)
-  end
-
   def event
     @event ||= group.events.find(params[:event_id])
   end
 
   def group
     @group ||= Group.find(params[:group_id])
-  end
-
-  def participation
-    @participation ||= event.participations.find(params[:id])
   end
 
   def participations(*role_types)
@@ -69,4 +55,5 @@ class Event::QualificationsController < ApplicationController
     not_found unless event.course_kind? && event.qualifying?
     authorize!(:qualify, event)
   end
+
 end

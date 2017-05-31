e47551f68fdff704287c4906f1b5ac2623fd8f78
Pascal Zumkehr <zumkehr@puzzle.ch>
2017-05-31 10:42:12 +0200

    company name field ain't no standard form field, refs #176

--- app/controllers/person/company_name_controller.rb
+++ app/controllers/person/company_name_controller.rb
@@ -13,22 +13,21 @@ class Person::CompanyNameController < ApplicationController
 
   # GET ajax, for auto complete fields, only the company_name
   def index
-    people = []
-    if params.key?(:q) && params[:q].size >= 3
-      people = list_entries.limit(10)
-    end
-    company_names = people.map do |person|
-      company = person.company_name.strip
-      { id: company, label: company }
-    end
-
-    render json: company_names
+    render json: entries.map { |name| { id: name, label: name } }
   end
 
   private
 
+  def entries
+    if params.key?(:q) && params[:q].size >= 3
+      list_entries.limit(10).pluck(:company_name).map(&:strip)
+    else
+      []
+    end
+  end
+
   def list_entries
-    Person.select(:company_name).distinct.order(:company_name)
+    Person.distinct.order(:company_name)
   end
 
   def authorize_action

bb00d849049034ba2a1a78eaedb17daf07a231b8
Matthias Viehweger <viehweger@puzzle.ch>
2017-06-07 19:02:16 +0200

    Allow to edit people-filters, see #223
    - Show edit and delete in Submenu
    - Allow edit/update if one can delete
    - Hide Search-Button for existing Filters
    - Adapt specs to accept more template-messages

--- app/controllers/people_filters_controller.rb
+++ app/controllers/people_filters_controller.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -11,7 +11,7 @@ class PeopleFiltersController < CrudController
 
   decorates :group
 
-  hide_action :index, :show, :edit, :update
+  hide_action :index, :show
 
   skip_authorize_resource only: [:create]
 

d1c2999792305a272e8524162d1a2af61917af2a
Andreas Maierhofer <maierhofer@puzzle.ch>
2017-11-22 14:08:00 +0100

    Add top-level invoice navigation

---
+++ spec/features/group/invoices_controller_spec.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz, Pfadibewegung Schweiz.
+#  This file is part of hitobito and licensed under the Affero General Public
+#  License version 3 or later. See the COPYING file at the top-level
+#  directory or at https://github.com/hitobito/hitobito.
+
+require 'spec_helper'
+
+describe InvoicesController do
+
+  it 'shows invoices link when person is authorised' do
+    sign_in(people(:bottom_member))
+    visit root_path
+    expect(page).to have_link 'Rechnungen'
+  end
+
+  it 'hides invoices link when person is authorised' do
+    sign_in(people(:top_leader))
+    visit root_path
+    expect(page).not_to have_link 'Rechnungen'
+  end
+
+end
+

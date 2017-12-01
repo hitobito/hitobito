dc5365ec0581e2486e2cb556005ae95846eb1413
Janiss Binder <binder@puzzle.ch>
2017-12-01 15:35:43 +0100

    fixed invalid migration

--- db/migrate/20171201094234_allow_null_recipient_in_invoice.rb
+++ db/migrate/20171201094234_allow_null_recipient_in_invoice.rb
@@ -1,10 +1,12 @@
--#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
--#  hitobito and licensed under the Affero General Public License version 3
--#  or later. See the COPYING file at the top-level directory or at
--#  https://github.com/hitobito/hitobito.
+# encoding: utf-8
 
-class AllowNullRecipientInIncoice < ActiveRecord::Migration
-  def up
+#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+      
+class AllowNullRecipientInInvoice < ActiveRecord::Migration
+  def change
     change_column_null :invoices, :recipient_id, true
   end
 end

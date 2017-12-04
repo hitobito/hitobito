71f0b75ed3f51ee057d7d2691dfd967ec9ae1494
Andreas Maierhofer <maierhofer@puzzle.ch>
2017-12-04 15:15:10 +0100

    Render invoices of person on separate tab

---
+++ app/controllers/person/invoices_controller.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class Person::InvoicesController < InvoicesController
+
+  private
+
+  def list_entries
+    scope = Invoice.
+      includes(recipient: [:groups, :roles]).
+      joins(:recipient).where(recipient: person).list
+
+    scope = scope.page(params[:page]).per(50)
+    Invoice::Filter.new(params).apply(scope)
+  end
+
+  def person
+    @person ||= group.people.find(params[:id])
+  end
+
+  def group
+    @group ||= Group.find(params[:group_id])
+  end
+
+  def authorize_class
+    authorize!(:index_invoices, person)
+  end
+
+end
---
+++ spec/controllers/person/invoices_controller_spec.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2015 Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+require 'spec_helper'
+
+describe Person::InvoicesController do
+  let(:group)         { groups(:bottom_layer_one) }
+  let(:top_leader)    { people(:top_leader) }
+  let(:bottom_member) { people(:bottom_member) }
+
+
+  it 'may not index person invoices if we have no finance permission in layer' do
+    sign_in(bottom_member)
+    expect do
+      get :index, group_id: groups(:top_group).id, id: top_leader.id
+    end.to raise_error(CanCan::AccessDenied)
+  end
+
+  it 'may  index my own invoices' do
+    sign_in(top_leader)
+    get :index, group_id: groups(:top_group).id, id: top_leader.id
+    expect(assigns(:invoices)).to have(2).items
+  end
+
+end
+

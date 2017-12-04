593ebe69b271cc4c9ecb8f974950ce98616e0e35
Andreas Maierhofer <maierhofer@puzzle.ch>
2017-12-04 16:09:24 +0100

    Use group index_invoices ability action

--- app/controllers/invoice_configs_controller.rb
+++ app/controllers/invoice_configs_controller.rb
@@ -1,20 +1,23 @@
+# encoding: utf-8
+
+#  Copyright (c) 2017, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 class InvoiceConfigsController < CrudController
 
   self.nesting = Group
   self.permitted_attrs = [:payment_information, :address, :iban, :account_number]
 
-
   private
 
-  def entry
+  def find_entry
     parent.invoice_config
-  end
-
-  def authorize_class
-    authorize!(:create, parent.invoices.build)
   end
 
   def path_args(_)
     [parent, :invoice_config]
   end
+
 end
--- spec/controllers/invoices_controller_spec.rb
+++ spec/controllers/invoices_controller_spec.rb
@@ -1,3 +1,10 @@
+# encoding: utf-8
+
+#  Copyright (c) 2017, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 require 'spec_helper'
 
 describe InvoicesController do
@@ -19,13 +26,13 @@ describe InvoicesController do
       expect(response).to be_success
     end
 
-    it "may not index when person has finance permission on layer group" do
+    it "may not index when person has no finance permission on layer group" do
       expect do
         get :index, group_id: groups(:top_layer).id
       end.to raise_error(CanCan::AccessDenied)
     end
 
-    it "may not edit when person has finance permission on layer group" do
+    it "may not edit when person has no finance permission on layer group" do
       invoice = Invoice.create!(group: groups(:top_layer), title: 'test', recipient: person)
       expect do
         get :edit, group_id: groups(:top_layer).id, id: invoice.id

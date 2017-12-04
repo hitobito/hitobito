d8626c8117c148cdb096ce047c6d0fea1500e7cd
Andreas Maierhofer <maierhofer@puzzle.ch>
2017-12-04 09:35:37 +0100

    Add Label links to invoice export dropdown

--- app/helpers/dropdown/invoices_export.rb
+++ app/helpers/dropdown/invoices_export.rb
@@ -20,6 +20,7 @@ module Dropdown
 
     def init_items
       pdf_links
+      label_links
     end
 
     def pdf_links
@@ -31,6 +32,49 @@ module Dropdown
     def add_item(key, options = {})
       path = params.merge(options).merge(format: :pdf)
       super(translate(key), path.merge(options), data: { invoice_export: true })
+    end
+
+    def label_links
+      if LabelFormat.exists?
+        label_item = add_item(:labels, main_label_link)
+        add_last_used_format_item(label_item)
+        add_label_format_items(label_item)
+      end
+    end
+
+    def add_last_used_format_item(parent)
+      if user.last_label_format_id?
+        last_format = user.last_label_format
+        parent.sub_items << Item.new(last_format.to_s,
+                                     export_label_format_path(last_format.id),
+                                     data: { invoice_export: true },
+                                     target: :new)
+        parent.sub_items << Divider.new
+      end
+    end
+
+    def add_label_format_items(parent)
+      LabelFormat.list.for_person(user).each do |label_format|
+        parent.sub_items << Item.new(label_format, export_label_format_path(label_format.id),
+                                     data: { invoice_export: true },
+                                     target: :new, class: 'export-label-format')
+      end
+    end
+
+    def export_label_format_path(id)
+      params.merge(format: :pdf, label_format_id: id)
+    end
+
+    def main_label_link
+      if user.last_label_format_id
+        export_label_format_path(user.last_label_format_id)
+      else
+        '#'
+      end
+    end
+
+    def user
+      template.current_user
     end
 
   end

954ca91dbe846848d7de098aba1a9530b6787097
Matthias Viehweger <viehweger@puzzle.ch>
2017-12-07 16:29:16 +0100

    Inform user about necessity to configure payments, see #381

--- app/controllers/group/merge_controller.rb
+++ app/controllers/group/merge_controller.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -28,6 +28,7 @@ class Group::MergeController < ApplicationController
 
   def respond_success
     flash[:notice] = translate(:success, new_group_name: merger.new_group_name)
+    flash[:alert]  = translate(:invoice_config_not_merged)
     redirect_to group_path(merger.new_group)
   end
 

d08f3dd9ce3a7ce6aac708e1f41fbb333bbcc368
Matthias Viehweger <viehweger@puzzle.ch>
2017-12-07 15:51:06 +0100

    Check the passed phone-numbers to ensure that one is present, see #420

--- app/models/event/participation_contact_data.rb
+++ app/models/event/participation_contact_data.rb
@@ -122,11 +122,25 @@ class Event::ParticipationContactData
 
   def assert_required_contact_attrs_valid
     required_attributes.each do |a|
-      next if a == 'phone_numbers' && person.phone_numbers.any?
+      next assert_phone_number_present if a == 'phone_numbers'
 
       if model_params[a.to_s].blank?
         errors.add(a, t('errors.messages.blank'))
       end
+    end
+  end
+
+  def assert_phone_number_present
+    phone_changes = { add: [], sub: [] }
+    changed_attrs = model_params.fetch('phone_numbers_attributes', {})
+
+    phone_changes = changed_attrs.each_with_object(phone_changes) do |(_key, entry), memo|
+      key = entry['_destroy'] == 'false' ? :add : :sub
+      memo[key] << entry['number']
+    end
+
+    if (phone_changes[:add] - phone_changes[:sub]).empty?
+      errors.add('phone_numbers', t('errors.messages.blank'))
     end
   end
 

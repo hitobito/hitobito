f23a55515958edb98e15b3d7d40e7946ad8aa6a6
Andreas Maierhofer <maierhofer@puzzle.ch>
2018-01-12 15:17:06 +0100

    Add preferred labels to mail_relay/address_list

--- app/domain/mail_relay/address_list.rb
+++ app/domain/mail_relay/address_list.rb
@@ -1,27 +1,59 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2018, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 module MailRelay
   class AddressList
 
-    attr_reader :people, :preferred_labels
+    attr_reader :people, :labels
 
-    def initialize(people, preferred_labels = [])
+    def initialize(people, labels = [])
       @people = Array(people)
-      @preferred_labels = []
+      @labels = labels
     end
 
     def entries
-      sanitized(emails + additional_emails)
+      people.flat_map do |person|
+        preferred_emails(person).presence || default_emails(person)
+      end.reject(&:blank?).uniq
     end
 
-    def sanitized(list)
-      list.select(&:present?).uniq
+    private
+
+    def preferred_emails(person)
+      additional_emails(person).select do |email|
+        sanitized_labels.include?(email.label.strip.downcase)
+      end.collect(&:email)
     end
 
-    def emails
-      people.collect(&:email)
+    def default_emails(person)
+      [person.email] + additional_emails(person).select(&:mailings?).collect(&:email)
     end
 
-    def additional_emails
-      AdditionalEmail.mailing_emails_for(people)
+    def sanitized_labels
+      @sanitized_labels ||= labels.collect do |label|
+        label.strip.downcase
+      end.compact
+    end
+
+    def additional_emails(person)
+      @additional_emails ||= additional_emails_scope.
+        each_with_object(hash_with_array) do |email, memo|
+          memo[email.contactable_id] << email
+      end
+      @additional_emails[person.id]
+    end
+
+    def hash_with_array
+      Hash.new { |h, k| h[k] = [] }
+    end
+
+    def additional_emails_scope
+      AdditionalEmail.where(contactable_type: Person.sti_name,
+                            contactable_id: people.collect(&:id))
     end
 
   end

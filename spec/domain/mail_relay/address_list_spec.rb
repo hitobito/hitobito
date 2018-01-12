9e07347b205041028264aa73e47c9102284788e9
Andreas Maierhofer <maierhofer@puzzle.ch>
2018-01-12 15:17:06 +0100

    Add preferred labels to mail_relay/address_list

--- spec/domain/mail_relay/address_list_spec.rb
+++ spec/domain/mail_relay/address_list_spec.rb
@@ -1,10 +1,20 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2018, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 require 'spec_helper'
 
 describe MailRelay::AddressList do
 
+  let(:top_leader)    { people(:top_leader) }
+  let(:bottom_member) { people(:bottom_member) }
+
   it 'contains main and additional mailing emails' do
-    e1 = Fabricate(:additional_email, contactable: people(:top_leader), mailings: true)
-    Fabricate(:additional_email, contactable: people(:bottom_member), mailings: false)
+    e1 = Fabricate(:additional_email, contactable: top_leader, mailings: true)
+    Fabricate(:additional_email, contactable: bottom_member, mailings: false)
     expect(entries).to match_array([
       'bottom_member@example.com',
       'hitobito@puzzle.ch',
@@ -21,7 +31,32 @@ describe MailRelay::AddressList do
     ])
   end
 
-  def entries(labels = [] )
-    described_class.new(Person.all, labels).entries
+  it 'it uses only additional_email if label matches' do
+    e1 = Fabricate(:additional_email, contactable: top_leader, label: 'foo')
+    expect(entries([top_leader], %w(foo))).to match_array([
+      e1.email,
+    ])
+  end
+
+  it 'it uses all matching additional_emails' do
+    e1 = Fabricate(:additional_email, contactable: top_leader, label: 'foo')
+    e2 = Fabricate(:additional_email, contactable: top_leader, label: 'bar')
+    expect(entries([top_leader], %w(foo bar))).to match_array([
+      e1.email,
+      e2.email,
+    ])
+  end
+
+  it 'falls back to default behviour of no label matches' do
+    e1 = Fabricate(:additional_email, contactable: top_leader, mailings: true)
+    Fabricate(:additional_email, contactable: top_leader, label: 'buz', mailings: false)
+    expect(entries([top_leader], %w(foo bar))).to match_array([
+      'top_leader@example.com',
+      e1.email,
+    ])
+  end
+
+  def entries(people = Person.all, labels = [])
+    described_class.new(people, labels).entries
   end
 end

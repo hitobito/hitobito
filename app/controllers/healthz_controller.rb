5e0765ae99a02494ca32432f33a72b321313fe44
Pascal Simon <simon@puzzle.ch>
2018-02-07 09:26:30 +0100

    add missing utf8 and copyright comments

--- app/controllers/healthz_controller.rb
+++ app/controllers/healthz_controller.rb
@@ -1,3 +1,10 @@
+# encoding: utf-8
+
+#  Copyright (c) 2017, Hitobito AG. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 # This controller circumvents authorization so OpenShift/Kubernetes can
 # inspect the application health without credentials.
 # If we'd return 401 the application would be treated as unhealthy.

32ff1651c68739bbfc9f8ad833aeab5c702ed795
Andreas Maierhofer <maierhofer@puzzle.ch>
2018-02-23 13:34:21 +0100

    Set impersonating person as papertrail whodunnit
    refs hitobito/hitobito#356

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,4 +55,9 @@ class ApplicationController < ActionController::Base
   def html_request?
     request.formats.any? { |f| f.html? || f == Mime::ALL }
   end
+
+  def user_for_paper_trail
+    origin_user_id = session[:origin_user]
+    origin_user_id ? origin_user_id : super
+  end
 end

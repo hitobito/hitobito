f3d1666dfebe6ee28d271bb6232659faa6b60d7d
Andreas Maierhofer <maierhofer@puzzle.ch>
2018-05-14 15:07:05 +0200

    Add missing invoice_mailer spec
    refs hitobito/hitobito#540

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -10,23 +10,25 @@ class InvoiceMailer < ApplicationMailer
 
   CONTENT_INVOICE_NOTIFICATION = 'content_invoice_notification'.freeze
 
-  def notification(recipient_name, recipient_mail, sender, invoice, invoice_file)
-    @recipient_name = recipient_name
-    @recipient_mail = recipient_mail
-    @sender         = sender
-    @invoice        = InvoiceDecorator.decorate(invoice)
+  def notification(invoice, sender)
+    @sender  = sender
+    @invoice = InvoiceDecorator.decorate(invoice)
 
-    attachments[invoice.filename] = invoice_file
+    attachments[invoice.filename] = generate_pdf
 
-    custom_content_mail(@recipient_mail, CONTENT_INVOICE_NOTIFICATION,
+    custom_content_mail(@invoice.recipient_email, CONTENT_INVOICE_NOTIFICATION,
                         values_for_placeholders(CONTENT_INVOICE_NOTIFICATION),
                         with_personal_sender(@sender))
   end
 
   private
 
+  def generate_pdf
+    Export::Pdf::Invoice.render(@invoice, pdf_options)
+  end
+
   def placeholder_recipient_name
-    @recipient_name
+    @invoice.recipient_name || @invoice.recipient_email
   end
 
   def placeholder_invoice_number
@@ -54,12 +56,18 @@ class InvoiceMailer < ApplicationMailer
     end
   end
 
+  def placeholder_group_address
+    [group.name,
+     group.address,
+     [group.zip_code, group.town].compact.join(' ').presence].compact.join(', ')
+  end
+
   def placeholder_group_name
-    @sender.primary_group.name
+    group.name
   end
 
   def placeholder_payment_information
-    @invoice.invoice_config.payment_information
+    @invoice.invoice_config.payment_information.to_s
   end
 
   def content_tag(name, content = nil)
@@ -67,4 +75,11 @@ class InvoiceMailer < ApplicationMailer
     "<#{name}>#{content}</#{name}>"
   end
 
+  def group
+    @invoice.group
+  end
+
+  def pdf_options
+    { articles: true, esr: true }
+  end
 end
--- spec/models/custom_content_spec.rb
+++ spec/models/custom_content_spec.rb
@@ -23,7 +23,7 @@ describe CustomContent do
 
   context '.list' do
     it 'contains one entry per main item' do
-      expect(CustomContent.list.size).to eq(4)
+      expect(CustomContent.list.size).to eq(5)
     end
   end
 

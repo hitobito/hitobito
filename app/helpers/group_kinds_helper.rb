70388c00caaa69bf29a425265d62043db2140a35
Roland Studer <roland.studer@gmail.com>
2018-05-26 15:39:48 +0200

    better formatting, explains permissions contextually, fixes #554

--- app/helpers/group_kinds_helper.rb
+++ app/helpers/group_kinds_helper.rb
@@ -5,49 +5,25 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
-module RoleKindsHelper
+module GroupKindsHelper
+  # def human_role_kind_permissions(role_kind)
+  #   text = ""
+  #   content_tag(:ul) do
+  #     out = role_kind.permissions.map do |permission|
+  #       content_tag(:li) do 
+  #         permission_with_tooltip(permission)
+  #       end
+  #     end
+  #     out.join().html_safe
+  #   end
+  # end
 
-  # to diplay permissions like "Schreiben auf Kommission / Lesen auf Dachverband, Verein" 
-  # the role needs to receive to context of layer, optionally takes the name of the gorup as context.
-  def human_role_kind_permissions(role_kind, layer, group = t("activerecord.models.group", count: 1))
-    out = role_kind.permissions.map do |permission|
-      content_tag(:li) do 
-        permission_with_tooltip(permission, layer, group)
-      end
-    end
-    out.join().html_safe
-  end
+  # private
 
-  private
-
-  def layer_with_sublayers(layer)
-    layers = Role::TypeList.new(Group.root_types.first).role_types.keys
-    index = layers.find_index(layer)
-    out = index ? layers[index..-1] : []
-    out.join(", ")
-  end
-
-  def permission_with_tooltip(permission, layer, group)
-    content_tag(:span, {rel: 'tooltip', title: human_permission_description(permission, layer, group)}) do 
-      human_permission(permission, layer, group)
-    end
-  end
-
-  def human_permission(permission, layer, group)
-    t("activerecord.attributes.role.class.permission.#{permission}.short", 
-      default: permission, 
-      layer: layer, 
-      layer_with_sublayers: layer_with_sublayers(layer),
-      group: group)
-  end
-
-  def human_permission_description(permission, layer, group)
-    t("activerecord.attributes.role.class.permission.#{permission}.description", 
-      default: '', 
-      layer: layer, 
-      layer_with_sublayers: layer_with_sublayers(layer),
-      group: group)
-  end
+  # def sub_layers(type)
+  #   all_child_layer_types(type).join(", ")
+  # end
+  
 end
 
     

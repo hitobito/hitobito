50eac0c58f326050b603b7656930460052c57756
Janiss Binder <binder@puzzle.ch>
2018-06-12 16:21:51 +0200

    refactored searchable participants, refs #531
    makes it work with youth wagon and background jobs

--- app/domain/event/participation_filter.rb
+++ app/domain/event/participation_filter.rb
@@ -8,23 +8,27 @@
 class Event::ParticipationFilter
 
   PREDEFINED_FILTERS = %w(all teamers participants)
+  SEARCH_COLUMNS = %w(people.first_name people.last_name people.nickname).freeze
+
+  class_attribute :load_entries_includes
+  self.load_entries_includes = [:roles, :event,
+                                answers: [:question],
+                                person: [:additional_emails, :phone_numbers,
+                                         :primary_group]
+                               ]
 
   attr_reader :event, :user, :params, :counts
 
-  def initialize(event, user, params = {}, search_results_count)
+  def initialize(event, user, params = {})
     @event = event
     @user = user
     @params = params
-    @search_results_count = search_results_count
   end
 
-  def apply
-    scope = apply_filter_scope(records)
-  end
-
-  def list_entries(records)
+  def list_entries
+    records = params[:q].present? ? load_entries.where(search_condition) : load_entries
     @counts = populate_counts(records)
-    apply_default_sort(apply_filter_scope(records))     
+    apply_default_sort(apply_filter_scope(records))
   end
 
   def predefined_filters
@@ -33,22 +37,25 @@ class Event::ParticipationFilter
 
   private
 
+  def search_condition
+    SearchStrategies::SqlConditionBuilder.new(params[:q], SEARCH_COLUMNS).search_conditions
+  end
+
   def apply_default_sort(records)
     records = records.order_by_role(event) if Settings.people.default_sort == 'role'
     records.merge(Person.order_by_name)
   end
 
   def populate_counts(records)
-    counts = predefined_filters.each_with_object({}) do |name, memo|
+    predefined_filters.each_with_object({}) do |name, memo|
       memo[name] = apply_filter_scope(records, name).count
     end
-    counts[:query] = @search_results_count if params[:q]
-    counts      
   end
 
   def load_entries
     event.active_participations_without_affiliate_types.
       includes(load_entries_includes).
+      references(:people).
       uniq
   end
 

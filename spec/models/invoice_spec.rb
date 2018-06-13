d3417fe1ef7bd801a77488a1b775a2fc0d06514d
Andreas Maierhofer <maierhofer@puzzle.ch>
2018-06-13 17:07:51 +0200

    Do not allow invoices without items in state sent either

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -46,11 +46,14 @@ describe Invoice do
     expect(invoice.errors.full_messages).to include(/Rechnungseinstellung ist nicht gültig/)
   end
 
-  it 'validates that an invoice in state issued has at least has one invoice_item' do
-      invoice = create_invoice
-      invoice.update(state: :issued)
-      expect(invoice).not_to be_valid
-      expect(invoice.errors.full_messages).to include(/Rechnungsposten muss ausgefüllt werden/)
+  it 'validates that an invoice in state issued or sent has at least has one invoice_item' do
+    invoice = create_invoice
+    invoice.update(state: :issued)
+    expect(invoice).not_to be_valid
+    expect(invoice.errors.full_messages).to include(/Rechnungsposten muss ausgefüllt werden/)
+    invoice.reload.update(state: :sent)
+    expect(invoice).not_to be_valid
+    expect(invoice.errors.full_messages).to include(/Rechnungsposten muss ausgefüllt werden/)
   end
 
   it 'computes sequence_number based of group_id and invoice_config.sequence_number' do

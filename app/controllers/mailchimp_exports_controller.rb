2dca0a94fd709af046a44b084a3b1eecfecea182
mat <mat@zeilenwerk.ch>
2018-06-26 09:19:07 +0200

    Connect Mailchimp button to the backend.

--- app/controllers/mailchimp_exports_controller.rb
+++ app/controllers/mailchimp_exports_controller.rb
@@ -5,14 +5,12 @@ class MailchimpExportsController < ApplicationController
   skip_authorization_check
 
   def new
-    #TODO affect UI behavior via js.erb partial
     MailchimpExportJob.new(permitted_params[:group_id]).enqueue!
   end
 
   private
 
   def permitted_params
-    #TODO
     params
   end
 end

36b6f068bf5dc6f661bb61198c0bdf470f87629e
mat <mat@zeilenwerk.ch>
2018-07-02 09:18:08 +0200

    Refactor mailchimp synchronization's domain object.

--- app/jobs/mailchimp_synchronization_job.rb
+++ app/jobs/mailchimp_synchronization_job.rb
@@ -1,15 +1,21 @@
 class MailchimpSynchronizationJob < BaseJob
 
+  self.parameters = [:mailing_list_id]
+
   def initialize(mailing_list_id)
     super()
     @mailing_list_id = mailing_list_id
   end
 
   def perform
-    Mailchimp::Synchronizator.sync(mailing_list)
+    Synchronize::Mailchimp.synchronize(@mailing_list_id)
   end
 
   private
+
+  # def send_mail(recipient, file, format)
+  #   MailchimpSynchronizationsMailer.completed(recipient, list).deliver_now
+  # end
 
   def mailing_list
     @mailing_list ||= MailingList.find(@mailing_list_id)

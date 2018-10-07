aac553749ae5ca1a18e6ca36a69d58a516d58a3f
EC2 Default User <ec2-user@ip-172-31-32-130.eu-west-1.compute.internal>
2018-10-07 17:51:06 +0000

    correcting a typo

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -93,7 +93,7 @@ module PeopleHelper
   end
 
   def link_to_address(person)
-    unless person.address.nil? || person.zip_code.nil? || person.town.nil?/hitobito/app/helpers/people_helper.rb
+    unless person.address.nil? || person.zip_code.nil? || person.town.nil?
       link_to(icon('map-marker', class: 'fa-2x'), openstreetmap_url(person), target: '_blank')
     end
   end
@@ -108,4 +108,5 @@ module PeopleHelper
     end
     osm_url
   end
+
 end

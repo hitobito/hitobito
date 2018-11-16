1191683a85d3f3599a68fb0b8f2ddd88fc42123f
Janiss Binder <binder@puzzle.ch>
2018-11-16 15:01:44 +0100

    Conform to linter and style guide

--- app/domain/synchronize/mailchimp/destroyer.rb
+++ app/domain/synchronize/mailchimp/destroyer.rb
@@ -1,9 +1,16 @@
+#  Copyright (c) 2012-2018, Grünliberale Partei Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 require 'digest/md5'
 
 module Synchronize
   module Mailchimp
-
     class Destroyer
+
+      attr_reader :mailchimp_list_id, :people_to_be_deleted, :gibbon
+
       def initialize(mailchimp_list_id, mailchimp_api_key, people_to_be_deleted)
         @mailchimp_list_id = mailchimp_list_id
         @people_to_be_deleted = people_to_be_deleted
@@ -17,21 +24,19 @@ module Synchronize
       private
 
       def delete_people_on_the_list
-        @gibbon.batches.create(body: {
-          operations: deleting_operations
-        })
+        gibbon.batches.create(body: { operations: deleting_operations })
       end
 
       def deleting_operations
-        @people_to_be_deleted.map do |person|
+        people_to_be_deleted.map do |person|
           {
-            method: "DELETE",
-            path: "lists/#{@mailchimp_list_id}/members/#{subscriber_hash person.email}"
+            method: 'DELETE',
+            path: "lists/#{mailchimp_list_id}/members/#{subscriber_hash person.email}"
           }
         end
       end
 
-      def subscriber_hash email
+      def subscriber_hash(email)
         Digest::MD5.hexdigest email.downcase
       end
     end
--- app/domain/synchronize/mailchimp/synchronizator.rb
+++ app/domain/synchronize/mailchimp/synchronizator.rb
@@ -1,14 +1,22 @@
+#  Copyright (c) 2012-2018, Grünliberale Partei Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 require 'digest/md5'
 
 module Synchronize
   module Mailchimp
-
     class Synchronizator
+
+      attr_reader :mailing_list, :gibbon, :people_on_the_list, :people_on_the_mailchimp_list
+
       def initialize(mailing_list)
         @mailing_list = mailing_list
-        @gibbon = Gibbon::Request.new(api_key: @mailing_list.mailchimp_api_key)
-        @people_on_the_list = @mailing_list.people
-        @people_on_the_mailchimp_list = @gibbon.lists(@mailing_list.mailchimp_list_id).members.retrieve.body["members"]
+        @gibbon = Gibbon::Request.new(api_key: mailing_list.mailchimp_api_key)
+        @people_on_the_list = mailing_list.people
+        @people_on_the_mailchimp_list =
+          gibbon.lists(mailing_list.mailchimp_list_id).members.retrieve.body['members']
       end
 
       def call
@@ -19,56 +27,57 @@ module Synchronize
       private
 
       def subscribe_people_on_the_list
-        @gibbon.batches.create(body: {
-          operations: subscribing_operations
-        })
+        gibbon.batches.create(body: { operations: subscribing_operations })
       end
 
       def delete_people_not_on_the_list
-        @gibbon.batches.create(body: {
-          operations: deleting_operations
-        })
+        gibbon.batches.create(body: { operations: deleting_operations })
       end
 
       def subscribing_operations
         people_to_be_subscribed.map do |person|
           {
-            method: "POST",
-            path: "lists/#{@mailing_list.mailchimp_list_id}/members",
-            body: {
-              email_address: person.email,
-              status: "subscribed",
-              merge_fields: {
-                FNAME: person.first_name,
-                LNAME: person.last_name
-              }
-            }.to_json
+            method: 'POST',
+            path: "lists/#{mailing_list.mailchimp_list_id}/members",
+            body: subscriber_body(person)
           }
         end
+      end
+
+      def subscriber_body(person)
+        {
+          email_address: person.email,
+          status: 'subscribed',
+          merge_fields: {
+            FNAME: person.first_name,
+            LNAME: person.last_name
+          }
+        }.to_json
       end
 
       def deleting_operations
         people_to_be_deleted.map do |person|
           {
-            method: "DELETE",
-            path: "lists/#{@mailing_list.mailchimp_list_id}/members/#{subscriber_hash person["email_address"]}"
+            method: 'DELETE',
+            path: "lists/#{mailing_list.mailchimp_list_id}/members/"\
+              "#{subscriber_hash person['email_address']}"
           }
         end
       end
 
       def people_to_be_subscribed
-        @people_on_the_list.reject do |person|
-          @people_on_the_mailchimp_list.map{|p| p["email_address"]}.include? person.email
+        people_on_the_list.reject do |person|
+          people_on_the_mailchimp_list.map { |p| p['email_address'] }.include? person.email
         end
       end
 
       def people_to_be_deleted
-        @people_on_the_mailchimp_list.reject do |subscriber|
-          @people_on_the_list.map(&:email).include? subscriber["email_address"]
+        people_on_the_mailchimp_list.reject do |subscriber|
+          people_on_the_list.map(&:email).include? subscriber['email_address']
         end
       end
 
-      def subscriber_hash email
+      def subscriber_hash(email)
         Digest::MD5.hexdigest email.downcase
       end
 

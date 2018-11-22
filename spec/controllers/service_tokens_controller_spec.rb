e88af70f4d7108d86bb9195c3fc57c8b9577d343
Janiss Binder <binder@puzzle.ch>
2018-11-22 12:00:06 +0100

    Test service tokens controller authorization

--- spec/controllers/service_tokens_controller_spec.rb
+++ spec/controllers/service_tokens_controller_spec.rb
@@ -6,4 +6,22 @@
 require 'spec_helper'
 
 describe ServiceTokensController do
+    let(:role)   { Fabricate(Group::BottomLayer::Leader.name.to_sym, group: groups(:bottom_layer_one)) }
+    let(:person) { role.person }
+
+    before { sign_in(person) }
+
+    context 'authorization' do
+      it 'may index when person has permission' do
+        get :index, group_id: role.group
+        expect(response).to be_success
+      end
+
+      it "may not index when person has no permission on top group" do
+        expect do
+          get :index, group_id: groups(:top_group).id
+        end.to raise_error(CanCan::AccessDenied)
+      end
+    end
+
 end

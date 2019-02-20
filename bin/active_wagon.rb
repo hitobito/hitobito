b0d7497ecd5e94b51d970bc120f0ecba922a2e3e
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-02-20 17:26:23 +0100

    Add active_wagon.rb to facilitate working with wagons

---
+++ bin/active_wagon.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+require 'pathname'
+
+## Allows switching wagons quickly (depends on https://direnv.net/)
+
+class Setup
+
+  def run
+    write_and_copy('.envrc', environment)
+    write_and_copy('.tool-versions', 'ruby 2.2.7')
+    write('Wagonfile', gemfile)
+    FileUtils.rm_rf(root.join('tmp'))
+  end
+
+  def write(name, content)
+    File.write(root.join(name), strip_heredoc(content))
+  end
+
+  def write_and_copy(name, content)
+    write(name, content)
+    FileUtils.cp(root.join(name), root.join("../hitobito_#{wagon}"))
+  end
+
+  def wagon(name = ARGV.first)
+    if !available.include?(name)
+      puts "Specify one of the following: #{available.join('|')}"
+      exit
+    end
+    name
+  end
+
+  def gemfile
+    <<-EOF
+    group :development do
+      ENV.fetch('WAGONS').split.each do |wagon|
+        puts File.expand_path("../../hitobito_\#{wagon}/hitobito_\#{wagon}.gemspec", __FILE__)
+        Dir[File.expand_path("../../hitobito_\#{wagon}/hitobito_\#{wagon}.gemspec", __FILE__)].each do |spec|
+          gem File.basename(spec, '.gemspec'), :path => File.expand_path('..', spec)
+        end
+      end
+    end
+    EOF
+  end
+
+  def environment
+    <<-EOF
+    echo PATH_add bin
+    export RAILS_DB_ADAPTER=mysql2
+    export RAILS_DB_NAME=hit_#{wagon}_development
+    export RAILS_TEST_DB_NAME=hit_#{wagon}_test
+    export RAILS_PRODUCTION_DB_NAME=hit_#{wagon}_production
+    export RUBYOPT=-W0
+    export WAGONS="#{wagons.join(' ')}"
+    EOF
+  end
+
+  def root
+    @root ||= Pathname.new(File.expand_path('../../', __FILE__))
+  end
+
+  def wagons
+    [wagon] + dependencies.fetch(wagon, [])
+  end
+
+  def dependencies
+    %w(pbs cevi).product([%w(youth)]).to_h.merge('jubla' => %w(youth jubla_ci))
+  end
+
+  def available(excluded = %w(youth jubla_ci site))
+    @available ||= root.parent.entries
+      .collect { |x| x.to_s[/hitobito_(.*)/, 1]  }
+      .compact.reject(&:empty?) - excluded
+  end
+
+  def strip_heredoc(string)
+    val = string.scan(/^[ \t]*(?=\S)/).min
+    indent = val ? val.size : 0
+    string.gsub(/^[ \t]{#{indent}}/, '')
+  end
+end
+
+Setup.new.run

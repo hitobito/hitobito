bcb98d32aba9a555baf85be32ec337decc408957
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-03-06 16:24:09 +0100

    Add license headers

--- app/helpers/table_displays/column.rb
+++ app/helpers/table_displays/column.rb
@@ -1,7 +1,5 @@
-# encoding: utf-8
-
-#  Copyright (c) 2012-2019, Jungwacht Blauring Schweiz. This file is part of
-#  hitobito and licensed under the Affero General Public License version 3
+#  Copyright (c) 2012-2018, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
--- spec/domain/export/tabular/people/table_displays_spec.rb
+++ spec/domain/export/tabular/people/table_displays_spec.rb
@@ -1,3 +1,8 @@
+#  Copyright (c) 2012-2018, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 require 'spec_helper'
 
 describe Export::Tabular::People::TableDisplays do

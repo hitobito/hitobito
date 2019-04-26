2335b71c7c31f865861085b5e2cf841321c5b50d
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-04-26 13:13:31 +0200

    Add integer constraints, send via hidden field, disable value select

--- app/assets/javascripts/modules/people/people_filter_attribute.js.coffee
+++ app/assets/javascripts/modules/people/people_filter_attribute.js.coffee
@@ -22,13 +22,14 @@ app.PeopleFilterAttribute = {
     form.removeClass('people_filter_attribute_form_template')
     form.removeClass('hidden')
     form.find('.attribute_key_dropdown').val(e.target.value)
+    form.find('.attribute_key_hidden_field').val(e.target.value)
     form.appendTo '#people_filter_attribute_forms'
     e.target.value = ''
 
   setAttributeNameTimestamp: (form) ->
     time = new Date().getTime()
 
-    app.PeopleFilterAttribute.renameAttributeName(form.find('.attribute_key_dropdown'), time)
+    app.PeopleFilterAttribute.renameAttributeName(form.find('.attribute_key_hidden_field'), time)
     app.PeopleFilterAttribute.renameAttributeName(form.find('.attribute_constraint_dropdown'), time)
     app.PeopleFilterAttribute.renameAttributeName(form.find('.attribute_value_input'), time)
 
@@ -37,8 +38,11 @@ app.PeopleFilterAttribute = {
     selector.attr('name', selector.attr('name').replace(regex, "[#{time}]"))
 
   enableForm: (form) ->
-    debugger
-    form.find('.attribute_key_dropdown').removeAttr('disabled')
+    field = form.find('.attribute_key_hidden_field').attr('value')
+    type  = form.closest('[data-types]').data('types')[field]
+
+    form.find('option[value=greater], option[value=smaller]').remove() unless type == 'integer'
+    form.find('.attribute_key_hidden_field').removeAttr('disabled')
     form.find('.attribute_constraint_dropdown').removeAttr('disabled')
     form.find('.attribute_value_input').removeAttr('disabled')
 
--- app/helpers/people_filter_helper.rb
+++ app/helpers/people_filter_helper.rb
@@ -5,7 +5,7 @@
 
 module PeopleFilterHelper
 
-  def people_filter_attribute_forms(filter)
+  def people_filter_attribute_forms(filter, types)
     return unless filter
 
     filter.args.each_with_index.map do |(_k, attr), i|
@@ -13,27 +13,44 @@ module PeopleFilterHelper
     end.join.html_safe
   end
 
+  def people_filter_attribute_types
+    attributes = Person.filter_attrs_list.collect(&:second).collect(&:to_s)
+    Person.columns_hash.slice(*attributes).transform_values { |v| v.type }
+  end
+
   def people_filter_attribute_form_template
     people_filter_attribute_form(nil, 0, disabled: :disabled)
   end
 
   # rubocop:disable AbcSize, MethodLength
-  def people_filter_attribute_form(attr = nil, count = 0, html_options = {})
+  def people_filter_attribute_form(attr, count, html_options = {})
+    key, constraint, value = attr.to_h.symbolize_keys.slice(:key, :constraint, :value).values
+    type = people_filter_attribute_types[key]
     time = (Time.zone.now.to_f * 1000).to_i + count
 
+    filters = [[t('.match'), :match], [t('.equal'), :equal]]
+    if key.blank? || type == :integer
+      filters += [[t('.smaller'), :smaller], [t('.greater'), :greater]]
+    end
+
     content_tag(:div, class: 'people_filter_attribute_form controls controls-row') do
-      content = select(:filters, "attributes[#{time}][key]",
-                       Person.filter_attrs_list,
-                       { selected: (attr[:key] if attr) },
-                       html_options.merge(class: 'span attribute_key_dropdown'))
+      content = hidden_field_tag("filters[attributes][#{time}][key]",
+                                 key,
+                                 disabled: attr.blank?,
+                                 class: 'attribute_key_hidden_field')
+
+      content << select(:filters, "attributes[#{time}][key]",
+                        Person.filter_attrs_list,
+                        { selected: key },
+                        html_options.merge(disabled: true, class: 'span attribute_key_dropdown'))
 
       content << select(:filters, "attributes[#{time}][constraint]",
-                        [[t('.match'), :match], [t('.equal'), :equal]],
-                        { selected: (attr[:constraint] if attr) },
+                        filters,
+                        { selected: constraint },
                         html_options.merge(class: 'span2 attribute_constraint_dropdown'))
 
       content << text_field_tag("filters[attributes][#{time}][value]",
-                                (attr[:value] if attr),
+                                value,
                                 html_options.merge(class: 'span2 attribute_value_input'))
 
       content << link_to(icon(:trash), '#',
@@ -41,4 +58,5 @@ module PeopleFilterHelper
                          style: 'padding-left: 7px; line-height: 2em')
     end
   end
+
 end

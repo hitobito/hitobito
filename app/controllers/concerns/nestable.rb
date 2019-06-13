2d0472145cae93ddcd9f613ff0b055613b972985
Matthias Viehweger <viehweger@puzzle.ch>
2019-06-13 14:15:21 +0200

    Add optional_nesting and prepend Nestable
    This should help with rails-upgrades in the future, due to the
    removal of alias_method_chain.

--- app/controllers/concerns/nestable.rb
+++ app/controllers/concerns/nestable.rb
@@ -1,6 +1,4 @@
-# encoding: utf-8
-
-#  Copyright (c) 2012-2015, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2019, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -15,28 +13,50 @@ module Nestable
 
   # Adds the :nesting class attribute and parent helper methods
   # to the including controller.
-  def self.included(controller)
-    controller.class_attribute :nesting
+  def self.prepended(klass)
+    klass.class_attribute :nesting
+    klass.class_attribute :optional_nesting
 
-    controller.helper_method :parent, :parents
-
-    controller.alias_method_chain :model_scope, :nesting
-    controller.alias_method_chain :path_args, :nesting
+    klass.helper_method :parent, :parents
   end
 
   private
 
   # Returns the direct parent ActiveRecord of the current request, if any.
   def parent
-    parents.reverse.find { |p| p.is_a?(ActiveRecord::Base) }
+    parents.select { |p| p.is_a?(ActiveRecord::Base) }.last
   end
 
   # Returns the parent entries of the current request, if any.
   # These are ActiveRecords or namespace symbols, corresponding
   # to the defined nesting attribute.
   def parents
-    nests = Array(nesting)
-    @parents ||= nests.collect do |p|
+    @parents ||= load_parents
+  end
+
+  def load_parents
+    if optional_nesting.present?
+      load_optional_parent || load_fixed_parents
+    else
+      load_fixed_parents
+    end
+  end
+
+  def load_optional_parent
+    parent = nil
+    Array(optional_nesting).each do |clazz|
+      key = parent_key_name(clazz)
+      id = params["#{key}_id"]
+      if id && request.path =~ /\/#{key.pluralize}\/#{id}\//
+        parent = [parent_entry(clazz)]
+        break
+      end
+    end
+    parent
+  end
+
+  def load_fixed_parents
+    Array(nesting).map do |p|
       if p.is_a?(Class) && p < ActiveRecord::Base
         parent_entry(p)
       else
@@ -48,26 +68,30 @@ module Nestable
   # Loads the parent entry for the given ActiveRecord class.
   # By default, performs a find with the class_name_id param.
   def parent_entry(clazz)
-    id = params["#{clazz.name.underscore}_id"]
-    model_ivar_set(clazz.find(id))
+    model_ivar_set(clazz.find(params["#{parent_key_name(clazz)}_id"]))
+  end
+
+  def parent_key_name(clazz)
+    clazz.name.demodulize.underscore
   end
 
   # An array of objects used in url_for and related functions.
-  def path_args_with_nesting(last)
+  def path_args(last)
     parents + [last]
   end
 
   # Uses the parent entry (if any) to constrain the model scope.
-  def model_scope_with_nesting
+  def model_scope
     if parent.present?
       parent_scope
     else
-      model_scope_without_nesting
+      super
     end
   end
 
   # The model scope for the current parent resource.
   def parent_scope
-    parent.send(model_class.name.underscore.pluralize.split('/').last)
+    parent.send(model_class.name.demodulize.underscore.pluralize)
   end
+
 end
--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,6 +1,4 @@
-# encoding: utf-8
-
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2019, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -112,6 +110,6 @@ class ListController < ApplicationController
   include Searchable
   include Sortable
   include Rememberable
-  include Nestable
+  prepend Nestable
 
 end

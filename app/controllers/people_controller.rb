5aab1b8c4640534df89dbf5d25d355f4b3508421
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-07-16 17:07:49 +0200

    Conform to linter and styleguide

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -256,7 +256,9 @@ class PeopleController < CrudController
   end
 
   def person_filter
-    @person_filter ||= Person::Filter::List.new(@group, current_user || service_token_user, list_filter_args)
+    @person_filter ||= Person::Filter::List.new(@group,
+                                                current_user || service_token_user,
+                                                list_filter_args)
   end
 
   def service_token_user

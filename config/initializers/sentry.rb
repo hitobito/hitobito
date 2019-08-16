ef9fa007abbb794dde6e4cf962d23cc875c82814
Matthias Viehweger <viehweger@puzzle.ch>
2019-08-16 14:21:05 +0200

    Configure Raven with environment and tag project if possible
    Otherwise, use inferred default-env and no project-tag

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,4 +4,21 @@ Raven.configure do |config|
   if ENV['OPENSHIFT_BUILD_COMMIT']
     config.release = ENV['OPENSHIFT_BUILD_COMMIT']
   end
+
+  if ENV['SENTRY_CURRENT_ENV'].blank?
+    project, stage = [
+      ENV['OPENSHIFT_BUILD_NAMESPACE'], # hit-jubla-int
+      ENV['RAILS_DB_NAME'],             # hit_jubla_development
+      "hitobito-#{Rails.env}"           # hitobito-development
+    ].compact.first.split(/[-_]/)[-2..-1]
+
+    config.tags[:project]      = project
+    config.current_environment = case stage
+                                 when /^int/i  then 'integration'
+                                 when /^prod/i then 'production'
+                                 when /^sta/i  then 'staging'
+                                 when /^dev/i  then 'development'
+                                 else stage
+                                 end
+  end
 end

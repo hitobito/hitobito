53483ac9bd6b53245d698383395713f58993803c
Matthias Viehweger <viehweger@puzzle.ch>
2019-08-16 14:19:50 +0200

    Let Sentry work out the release if do not know

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,7 @@
 Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.release = ENV['OPENSHIFT_BUILD_COMMIT']
-end
 
+  if ENV['OPENSHIFT_BUILD_COMMIT']
+    config.release = ENV['OPENSHIFT_BUILD_COMMIT']
+  end
+end

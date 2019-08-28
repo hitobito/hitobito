ab03ca070f917f3c4daaeec68e8ac3bcb69215a2
Carlo Beltrame <carlo@beltra.me>
2019-08-28 10:31:01 +0200

    Display first tab that has results, or the person tab by default

--- app/controllers/full_text_controller.rb
+++ app/controllers/full_text_controller.rb
@@ -9,7 +9,7 @@ class FullTextController < ApplicationController
 
   skip_authorization_check
 
-  helper_method :entries
+  helper_method :entries, :tab_class
 
   respond_to :html
 
@@ -57,5 +57,16 @@ class FullTextController < ApplicationController
 
   def with_query
     params[:q].to_s.size >= 2 ? yield : []
+  end
+
+  def active_tab
+    return :people if @people.length > 0
+    return :groups if @groups.length > 0
+    return :events if @events.length > 0
+    :people
+  end
+
+  def tab_class(tab)
+    'active' if active_tab == tab
   end
 end

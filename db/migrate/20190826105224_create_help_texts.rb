3b9d2cba59955ce1aa3833676b54863252358f48
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-09-18 15:21:32 +0200

    Simplify migration code, add indices

--- db/migrate/20190826105224_create_help_texts.rb
+++ db/migrate/20190826105224_create_help_texts.rb
@@ -1,26 +1,15 @@
 class CreateHelpTexts < ActiveRecord::Migration
-  def up
+  def change
     create_table :help_texts do |t|
       t.string :controller_name, null: false
       t.string :entry_class, null: true
       t.string :key, null: false
-
-      t.timestamps
     end
-    HelpText.create_translation_table!(
-      {
-        body: :text
-      }
-    )
+    add_index(:help_texts, [:controller_name, :key], unique: true)
 
-    HelpText.globalize_migrator.drop_translations_index
-    remove_index(HelpText.globalize_migrator.translations_table_name,
-                 name: HelpText.globalize_migrator.translation_locale_index_name)
-    HelpText.globalize_migrator.create_translations_index
-  end
-
-  def down
-    drop_table :help_texts
-    HelpText.drop_translation_table!
+    reversible do |dir|
+      dir.up   { HelpText.create_translation_table!(body: :text) }
+      dir.down { HelpText.drop_translation_table! }
+    end
   end
 end

1cd41c8766ceef7acbcdf175466619a833be90af
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-11-07 14:08:03 +0100

    Add missing license headers

--- spec/domain/help_texts/renderer_spec.rb
+++ spec/domain/help_texts/renderer_spec.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2019, Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pbs.
+
 require 'spec_helper'
 
 describe HelpTexts::Renderer do

587972585ddf8deb401ae01f46c61eea547f8a70
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-12-12 11:39:29 +0100

    Add new abilities to UI, cleanup locales

--- spec/controllers/service_tokens_controller_spec.rb
+++ spec/controllers/service_tokens_controller_spec.rb
@@ -6,22 +6,45 @@
 require 'spec_helper'
 
 describe ServiceTokensController do
+
+  before { sign_in(person) }
+
+  context 'authorization' do
     let(:role)   { Fabricate(Group::BottomLayer::Leader.name.to_sym, group: groups(:bottom_layer_one)) }
     let(:person) { role.person }
 
-    before { sign_in(person) }
-
-    context 'authorization' do
-      it 'may index when person has permission' do
-        get :index, group_id: role.group
-        expect(response).to be_success
-      end
-
-      it "may not index when person has no permission on top group" do
-        expect do
-          get :index, group_id: groups(:top_group).id
-        end.to raise_error(CanCan::AccessDenied)
-      end
+    it 'may index when person has permission' do
+      get :index, group_id: role.group
+      expect(response).to be_success
     end
 
+    it "may not index when person has no permission on top group" do
+      expect do
+        get :index, group_id: groups(:top_group).id
+      end.to raise_error(CanCan::AccessDenied)
+    end
+  end
+
+  context 'authorized' do
+    let(:person) { people(:top_leader) }
+
+    it 'may update flags' do
+      token = service_tokens(:rejected_top_group_token)
+
+      patch :update, group_id: token.layer.id, id: token.id, service_token: {
+        people: true,
+        people_below: true,
+        groups: true,
+        events: true,
+        invoices: true,
+        event_participations: true
+      }
+      expect(token.reload).to be_people
+      expect(token.reload).to be_people_below
+      expect(token.reload).to be_groups
+      expect(token.reload).to be_events
+      expect(token.reload).to be_invoices
+      expect(token.reload).to be_event_participations
+    end
+  end
 end

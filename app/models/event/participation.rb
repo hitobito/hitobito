f60c0d2927aa7bd5492238f646c1bdeb69e1e5f3
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-12-18 14:35:50 +0100

    Replace deprecated original_exception calls

--- app/models/event/participation.rb
+++ app/models/event/participation.rb
@@ -121,7 +121,7 @@ class Event::Participation < ActiveRecord::Base
   def save(*args)
     super
   rescue ActiveRecord::StatementInvalid => e
-    raise e unless e.original_exception.message =~ /Incorrect string value/
+    raise e unless e.cause.message =~ /Incorrect string value/
     errors.add(:base, :emoji_suspected)
     false
   end

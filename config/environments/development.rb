04e4c1b526530edb3e3ea234e0ec5639d40d43d3
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-12-19 10:36:24 +0100

    Result of running rails:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,66 +1,49 @@
-# encoding: utf-8
-
-#  Copyright (c) 2012-2019, Jungwacht Blauring Schweiz. This file is part of
-#  hitobito and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
-
-Hitobito::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # Show full error reports.
+  config.consider_all_requests_local = true
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {
-    host: 'localhost:3000',
-    protocol: (%w(true yes 1).include?(ENV['RAILS_HOST_SSL']) ? 'https' : 'http'),
-    locale: nil }
-
-  # Mail sender
-  config.action_mailer.delivery_method = (ENV['RAILS_MAIL_DELIVERY_METHOD'].presence || :smtp).to_sym
-
-  if ENV['RAILS_MAIL_DELIVERY_CONFIG'].present?
-    case config.action_mailer.delivery_method.to_s
-    when 'smtp'
-      config.action_mailer.smtp_settings =
-        YAML.load("{ #{ENV['RAILS_MAIL_DELIVERY_CONFIG']} }").symbolize_keys
-    when 'sendmail'
-      config.action_mailer.sendmail_settings =
-        YAML.load("{ #{ENV['RAILS_MAIL_DELIVERY_CONFIG']} }").symbolize_keys
-    end
+  # Enable/disable caching. By default caching is disabled.
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
   else
-    # mailcatcher
-    config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
   end
 
-  # Print deprecation notices to the Rails logger
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
 
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
   config.assets.debug = true
 
-  config.middleware.use "Rack::RequestProfiler", printer: RubyProf::CallStackPrinter
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
 
-  # do not assume interleaved requests in development
-  config.log_tags = []
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 
-  # Allow using better_errors in Docker
-  BetterErrors::Middleware.allow_ip! ENV['DOCKER_HOST_IP'] if ENV['DOCKER_HOST_IP'].present?
-
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end

f092b474f07772ed2eae357f0ac5e0bdf59b790e
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-12-19 12:42:36 +0100

    Load test locale files in config/test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,4 +43,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+  locales_path = Dir[Rails.root.join('spec', 'support', 'locales', '**', '*.{rb,yml}')]
+  config.i18n.load_path += locales_path
 end

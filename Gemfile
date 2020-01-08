8fd83a1ecf3f707269305d061d174579934a4b09
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-01-08 14:13:58 +0100

    Upgrade to rails 5.1.7

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@
 
 source 'https://rubygems.org'
 
-gem 'rails', '= 5.0.7.2'
+gem 'rails', '= 5.1.7'
 
 gem 'activerecord-session_store'
 gem 'acts-as-taggable-on'
--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,6 @@ module Hitobito
     config.active_support.escape_html_entities_in_json = true
 
     config.active_record.time_zone_aware_types = [:datetime, :time]
-    config.active_record.raise_in_transactional_callbacks = true
 
     config.active_job.queue_adapter = :delayed_job
 

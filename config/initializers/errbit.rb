e2978a1324dc5630c3aeb794d24d5b8304dee99a
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-01-08 09:09:51 +0100

    Upgrade errbit gem and initializer

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -5,21 +5,26 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
-Airbrake.configure do |config|
-  # if no host is given, use airbrake only in test mode
-  config.test_mode   = ENV['RAILS_AIRBRAKE_HOST'].blank?
+if ENV['RAILS_AIRBRAKE_HOST'] && ENV['RAILS_AIRBRAKE_API_KEY']
+  Airbrake.configure do |config|
+    config.environment = Rails.env
+    config.ignore_environments = [:development, :test]
+    # if no host is given, ignore all environments
+    config.ignore_environments << :production if ENV['RAILS_AIRBRAKE_HOST'].blank?
 
-  config.api_key     = ENV['RAILS_AIRBRAKE_API_KEY']
-  config.host        = ENV['RAILS_AIRBRAKE_HOST']
-  config.port        = (ENV['RAILS_AIRBRAKE_PORT'] || 443).to_i
-  config.secure      = config.port == 443
-  config.ignore         << ActionController::MethodNotAllowed
-  config.ignore         << ActionController::RoutingError
-  config.ignore         << ActionController::UnknownHttpMethod
-  config.params_filters << 'RAILS_DB_PASSWORD'
-  config.params_filters << 'RAILS_MAIL_RETRIEVER_PASSWORD'
-  config.params_filters << 'RAILS_AIRBRAKE_API_KEY'
-  config.params_filters << 'RAILS_SECRET_TOKEN'
-  config.params_filters << 'RAILS_MAIL_RETRIEVER_CONFIG'
-  config.params_filters << 'RAILS_MAIL_DELIVERY_CONFIG'
+    config.project_id     = 1
+    config.project_key    = ENV['RAILS_AIRBRAKE_API_KEY']
+    config.host           = ENV['RAILS_AIRBRAKE_HOST']
+    config.port           = (ENV['RAILS_AIRBRAKE_PORT'] || 443).to_i
+    config.secure         = config.port == 443
+    config.ignore         << ActionController::MethodNotAllowed
+    config.ignore         << ActionController::RoutingError
+    config.ignore         << ActionController::UnknownHttpMethod
+    config.params_filters << 'RAILS_DB_PASSWORD'
+    config.params_filters << 'RAILS_MAIL_RETRIEVER_PASSWORD'
+    config.params_filters << 'RAILS_AIRBRAKE_API_KEY'
+    config.params_filters << 'RAILS_SECRET_TOKEN'
+    config.params_filters << 'RAILS_MAIL_RETRIEVER_CONFIG'
+    config.params_filters << 'RAILS_MAIL_DELIVERY_CONFIG'
+  end
 end

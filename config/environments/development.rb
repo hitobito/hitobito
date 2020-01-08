fadb8985b0664f23edfcf4208272bcdbc887656c
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-01-08 14:40:49 +0100

    Changes from running rails app:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,11 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+    }
   else
     config.action_controller.perform_caching = false
 
@@ -45,5 +50,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end

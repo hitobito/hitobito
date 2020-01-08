0059f5db40fe4dabaf0f7fc248923db00339347e
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-01-08 14:40:49 +0100

    Changes from running rails app:update

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,17 +15,17 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
+    'Cache-Control' => "public, max-age=#{1.hour.seconds.to_i}"
   }
 
-  # Show full error reports and disable caching
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Set to false for faster wagon tests once db is setup
   config.active_record.maintain_test_schema = true
 
-  # Raise exceptions instead of rendering exception templates
+  # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.

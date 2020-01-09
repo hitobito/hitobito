5f759bfe7561f808086a0e1156654548795e7a8c
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-01-09 11:35:58 +0100

    Specify case explicitly for uniqueness validation

--- app/models/help_text.rb
+++ app/models/help_text.rb
@@ -18,7 +18,7 @@
 class HelpText < ActiveRecord::Base
   COLUMN_BLACKLIST = %w(id created_at updated_at deleted_at).freeze
 
-  validates :name, uniqueness: { scope: [:controller, :model, :kind] }
+  validates :name, uniqueness: { scope: [:controller, :model, :kind], case_sensitive: false }
   validates :body, presence: true
   before_validation :assign_combined_fields, if: :new_record?
 

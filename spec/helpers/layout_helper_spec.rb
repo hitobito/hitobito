6e5b9cd7b58d3fb2a413b89a3dbd7235dafc23fe
Nicolas Da Mutten <nicolas@damutten.ch>
2020-02-29 22:32:21 +0100

    Implement layout helper tests.

--- spec/helpers/layout_helper_spec.rb
+++ spec/helpers/layout_helper_spec.rb
@@ -22,22 +22,52 @@ describe LayoutHelper do
   describe '#header_logo_css' do
 
     it 'should find the logo directly on the visible group' do
-      pending 'Not implemented'
-      fail
+      group = Group::BottomLayer.new(name: "Bottom Two")
+      group.logo = File.open('spec/fixtures/person/test_picture.jpg')
+      group.save
+
+      assign(:group, group)
+      expect(helper.header_logo_css).to eql("<style>header.logo a.logo-image { background-image: url(#{asset_path(group.logo)}); }</style>")
     end
 
     it 'should find the logo on a parent group' do
-      pending 'Not implemented'
-      fail
+      parent_group = Group::BottomGroup.new(name: "Group 11", parent: groups(:bottom_layer_one))
+      parent_group.logo = File.open('spec/fixtures/person/test_picture.jpg')
+      parent_group.save
+
+      group = Group::BottomGroup.new(name: "Group 111")
+      group.parent = parent_group
+      group.save
+
+      assign(:group, group)
+      expect(helper.header_logo_css).to eql("<style>header.logo a.logo-image { background-image: url(#{asset_path(group.parent.logo)}); }</style>")
     end
 
-    it 'should return when not viewing a group' do
-      expect(header_logo_css).to be nil
+    it 'should return the correct logo, when multiple are available.' do
+      parent_parent_group = Group::BottomGroup.new(name: 'Bottom One', parent: groups(:top_layer))
+      parent_parent_group.logo = File.open('spec/fixtures/person/test_picture2.jpg')
+      parent_parent_group.save
+
+      parent_group = Group::BottomGroup.new(name: 'Group 11', parent: groups(:bottom_layer_one))
+      parent_group.logo = File.open('spec/fixtures/person/test_picture.jpg')
+      parent_group.save
+
+      group = Group::BottomGroup.new(name: 'Group 111')
+      group.parent = parent_group
+      group.save
+
+      assign(:group, group)
+      expect(helper.header_logo_css).to eql("<style>header.logo a.logo-image { background-image: url(#{asset_path(group.parent.logo)}); }</style>")
     end
 
-    it 'should return when no logo is found' do
-      pending 'Not implemented'
-      fail
+    it 'should return nil when not viewing a group' do
+      expect(helper.header_logo_css).to be nil
+    end
+
+    it 'should return nil when no logo is found' do
+      group = groups(:bottom_group_one_one_one)
+      assign(:group, group)
+      expect(helper.header_logo_css).to be nil
     end
 
   end

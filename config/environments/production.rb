a3a7e41363d28a334af30eb9db6ccff93d790e15
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-03-03 14:39:38 +0100

    Add LIVENESS_IP_ADDRESS to whitelisted hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -136,4 +136,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.hosts << ENV["RAILS_HOST_NAME"]
+  config.hosts << ENV["LIVENESS_IP_ADDRESS"]
 end

b43abaab4da1f1b6f0457923e418b4d788d7ea60
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-04-01 21:37:43 +0200

    Support additional api in mailchimp client

--- app/domain/synchronize/mailchimp/client.rb
+++ app/domain/synchronize/mailchimp/client.rb
@@ -9,60 +9,171 @@ require 'digest/md5'
 module Synchronize
   module Mailchimp
     class Client
-      attr_reader :list_id, :count, :api
+      attr_reader :list_id, :count, :api, :merge_fields, :member_fields
 
-      def initialize(mailing_list, count = 50)
+      def initialize(mailing_list, member_fields: [], merge_fields: [], count: 50, debug: false)
         @list_id = mailing_list.mailchimp_list_id
         @count   = count
+        @merge_fields = merge_fields
+        @member_fields = member_fields
 
-        @api = Gibbon::Request.new(api_key: mailing_list.mailchimp_api_key)
+        @api = Gibbon::Request.new(api_key: mailing_list.mailchimp_api_key, debug: debug)
       end
 
-      def members
-        fetch_members
+      def fetch_merge_fields
+        paged do |list, params|
+          body = api.lists(list_id).merge_fields.retrieve(params: params).body.to_h
+          body['merge_fields'].each do |entry|
+            list << entry.slice('tag', 'name', 'type').deep_symbolize_keys
+          end
+          body['total_items']
+        end
+      end
+
+      def fetch_members
+        fields = %w(email_address status tags merge_fields)
+        fields += member_fields.collect(&:first).collect(&:to_s)
+
+        paged do |list, params|
+          body = api.lists(list_id).members.retrieve(params: params).body.to_h
+          body['members'].each do |entry|
+            list << entry.slice(*fields).deep_symbolize_keys
+          end
+          body['total_items']
+        end
+      end
+
+      def fetch_segments
+        paged do |list, params|
+          body = api.lists(list_id).segments.retrieve(params: params).body.to_h
+          body['segments'].each do |entry|
+            list << entry.slice('id', 'name', 'member_count').symbolize_keys
+          end
+          body['total_items']
+        end
+      end
+
+      def create_segments(names)
+        execute_batch(names) do |name|
+          create_segment_operation(name)
+        end
+      end
+
+      def create_merge_fields(list)
+        execute_batch(list) do |name, type, options|
+          create_merge_field_operation(name, type, options)
+        end
+      end
+
+      def update_segments(list)
+        execute_batch(list) do |segment_id, emails|
+          update_segment_operation(segment_id, emails)
+        end
       end
 
       def delete(emails)
         execute_batch(emails) do |email|
-          delete_operation(email)
+          unsubscribe_member_operation(email)
+        end
+      end
+
+      def delete_segments(segment_ids)
+        execute_batch(segment_ids) do |segment_id|
+          destroy_segment_operation(segment_id)
+        end
+      end
+
+      def update_members(people)
+        execute_batch(people) do |person|
+          update_member_operation(person)
         end
       end
 
       def subscribe(people)
         execute_batch(people) do |person|
-          subscribe_operation(person)
+          subscribe_member_operation(person)
         end
       end
 
-      def delete_operation(email)
-        subscriber_id = Digest::MD5.hexdigest(email.downcase)
+      def fetch_batch(batch_id)
+        api.batches(batch_id).retrieve.body.fetch('response_body_url')
+      end
+
+      def create_merge_field_operation(name, type, options = {})
         {
-          method: 'DELETE',
-          path: "lists/#{list_id}/members/#{subscriber_id}"
+          method: 'POST',
+          path: "lists/#{list_id}/merge-fields",
+          body: { tag: name.upcase, name: name, type: type, options: options }.to_json
         }
       end
 
-      def subscribe_operation(person)
+      def create_segment_operation(name)
+        {
+          method: 'POST',
+          path: "lists/#{list_id}/segments",
+          body: { name: name, static_segment: [] }.to_json
+        }
+      end
+
+      def destroy_segment_operation(segment_id)
+        {
+          method: 'DELETE',
+          path: "lists/#{list_id}/segments/#{segment_id}",
+        }
+      end
+
+      def update_segment_operation(segment_id, emails)
+        {
+          method: 'POST',
+          path: "lists/#{list_id}/segments/#{segment_id}",
+          body: { members_to_add: emails }.to_json
+        }
+      end
+
+      def unsubscribe_member_operation(email)
+        {
+          method: 'DELETE',
+          path: "lists/#{list_id}/members/#{subscriber_id(email)}"
+        }
+      end
+
+      def subscribe_member_operation(person)
         {
           method: 'POST',
           path: "lists/#{list_id}/members",
+          body: subscriber_body(person).merge(status: :subscribed).to_json
+        }
+      end
+
+      def update_member_operation(person)
+        {
+          method: 'PUT',
+          path: "lists/#{list_id}/members/#{subscriber_id(person.email)}",
           body: subscriber_body(person).to_json
         }
       end
 
+      def subscriber_body(person)
+        {
+          email_address: person.email,
+          merge_fields: {
+            FNAME: person.first_name,
+            LNAME: person.last_name,
+          }.merge(merge_field_values(person))
+        }.merge(member_field_values(person))
+      end
+
       private
 
-      def fetch_members(list = [], offset = 0)
-        params = { count: count, offset: offset }
+      def subscriber_id(email)
+        Digest::MD5.hexdigest(email.downcase)
+      end
 
-        body = api.lists(list_id).members.retrieve(params: params).body.to_h
-        body['members'].each do |entry|
-          list << entry.slice('email_address', 'status').symbolize_keys
-        end
-
+      def paged(list = [], offset = 0, &block)
+        total_items = block.call(list, count: count, offset: offset)
         next_offset = offset + count
-        if body['total_items'] > next_offset
-          fetch_members(list, next_offset)
+        if total_items > next_offset
+          paged(list, next_offset, &block)
         else
           list
         end
@@ -70,21 +181,50 @@ module Synchronize
 
       def execute_batch(list)
         operations = list.collect do |item|
-          yield item
+          yield(item).tap do |operation|
+            logger.info "mailchimp: #{list_id}, op: #{operation[:method]}, item: #{item}"
+            logger.info operation
+          end
         end
 
-        api.batches.create(body: { operations: operations })
+        if operations.present?
+          batch_id = api.batches.create(body: { operations: operations }).body.fetch('id')
+          wait_for_finish(batch_id)
+        end
       end
 
-      def subscriber_body(person)
-        {
-          email_address: person.email,
-          status: 'subscribed',
-          merge_fields: {
-            FNAME: person.first_name,
-            LNAME: person.last_name
-          }
-        }
+      def wait_for_finish(batch_id, count = 0)
+        sleep count * count
+        body = api.batches(batch_id).retrieve.body
+        status = body.fetch('status')
+
+        logger.info "batch #{batch_id}, status: #{status}"
+        fail "Batch #{batch_id} did not finish in due time, last status: #{status}" if count > 10
+
+        if status != 'finished'
+          wait_for_finish(batch_id, count + 1)
+        else
+          attrs = %w(total_operations finished_operations errored_operations response_body_url)
+          body.slice(*attrs).tap do |updates|
+            logger.info updates
+          end
+        end
+      end
+
+      def merge_field_values(person)
+        merge_fields.collect do |field, type, options, evaluator|
+          [field.upcase, evaluator.call(person)]
+        end.to_h.deep_symbolize_keys
+      end
+
+      def member_field_values(person)
+        member_fields.collect do |field, evaluator|
+          [field,  evaluator.call(person)]
+        end.to_h.deep_symbolize_keys
+      end
+
+      def logger
+        Rails.logger
       end
     end
   end
--- spec/domain/synchronize/mailchimp/client_spec.rb
+++ spec/domain/synchronize/mailchimp/client_spec.rb
@@ -10,12 +10,9 @@ describe Synchronize::Mailchimp::Client do
   let(:top_leader)   { people(:top_leader) }
   let(:client)       { described_class.new(mailing_list) }
 
-  def stub_members(*members, total_items: nil, offset: 0)
-    entries = members.collect do |email, status|
-      { email_address: email, status: status || 'subscribed' }
-    end
 
-    stub_request(:get, "https://us12.api.mailchimp.com/3.0/lists/2/members?count=#{client.count}&offset=#{offset}").
+  def stub_collection(path, offset, count = client.count, body: )
+    stub_request(:get, "https://us12.api.mailchimp.com/3.0/#{path}?count=#{count}&offset=#{offset}").
       with(
         headers: {
           'Accept'=>'*/*',
@@ -26,11 +23,79 @@ describe Synchronize::Mailchimp::Client do
 
         }
     ).
-    to_return(status: 200, body: { members: entries, total_items: total_items || entries.count }.to_json, headers: {})
+    to_return(status: 200, body: body.to_json, headers: {})
+  end
+
+  def stub_merge_fields(*fields, total_items: nil, offset: 0)
+    entries = fields.collect do |tag, name, type|
+      { tag: tag, name: name, type: type }
+    end
+    stub_collection("lists/2/merge-fields", offset, body: { merge_fields: entries, total_items: total_items || entries.count})
+  end
+
+  def stub_members(*members, total_items: nil, offset: 0)
+    entries = members.collect do |email, status = 'subscribed', tags = [], merge_fields = {}, extra_fields = {}|
+      { email_address: email, status: status, tags: tags, merge_fields: merge_fields }.merge(extra_fields)
+    end
+    stub_collection("lists/2/members", offset, body: { members: entries, total_items: total_items || entries.count})
+  end
+
+  def stub_segments(*segments, total_items: nil, offset: 0)
+    entries = segments.collect do |name, id|
+      { name: name, id: id.to_i }
+    end
+    stub_collection("lists/2/segments", offset, body: { segments: entries, total_items: total_items || entries.count})
+  end
+
+  context '#merge_fields' do
+    subject { client.fetch_merge_fields }
+
+    it 'returns empty merge_fields list' do
+      stub_merge_fields
+      expect(subject).to be_empty
+    end
+
+    it 'returns merge_fields with id' do
+      stub_merge_fields(['FNAME', 'First Name', 'text'],
+                        ['GENDER', 'Gender', 'text'])
+      expect(subject).to have(2).items
+
+      first = subject.first
+      expect(first[:tag]).to eq 'FNAME'
+      expect(first[:name]).to eq 'First Name'
+      expect(first[:type]).to eq 'text'
+
+      second = subject.second
+      expect(second[:tag]).to eq 'GENDER'
+      expect(second[:name]).to eq 'Gender'
+      expect(second[:type]).to eq 'text'
+    end
+  end
+
+  context '#segments' do
+    subject { client.fetch_segments }
+
+    it 'returns empty segment list' do
+      stub_segments
+      expect(subject).to be_empty
+    end
+
+    it 'returns segments with id' do
+      stub_segments(%w(a 1), %w(b 2))
+      expect(subject).to have(2).items
+
+      first = subject.first
+      expect(first[:name]).to eq 'a'
+      expect(first[:id]).to eq 1
+
+      second = subject.second
+      expect(second[:name]).to eq 'b'
+      expect(second[:id]).to eq 2
+    end
   end
 
   context '#members' do
-    subject { client.members }
+    subject { client.fetch_members }
 
     it 'returns empty member list' do
       stub_members
@@ -50,8 +115,25 @@ describe Synchronize::Mailchimp::Client do
       expect(second[:status]).to eq 'unsubscribed'
     end
 
+    it 'returns members with tags' do
+      stub_members(['a@example.com', nil, [{ id: 1, name: 'test:ab' }, { id: 2, name: 'test' }]])
+      expect(subject.first[:tags]).to eq [{ id: 1, name: 'test:ab' }, { id: 2, name: 'test' }]
+    end
+
+    it 'returns members with merge fields' do
+      stub_members(['a@example.com', nil, nil, { FNAME: 'A', LNAME: 'B', GENDER: 'm' }])
+      expect(subject.first[:merge_fields]).to eq({ FNAME: 'A', LNAME: 'B', GENDER: 'm' })
+    end
+
+    it 'returns members with custom member fields' do
+      expect(client).to receive(:member_fields).and_return([['company']])
+      stub_members(['a@example.com', nil, nil, {}, { company: :acme }])
+      expect(subject.first[:company]).to eq 'acme'
+    end
+
+
     context 'paging' do
-      let(:client)       { described_class.new(mailing_list, 2) }
+      let(:client)       { described_class.new(mailing_list, count: 2) }
 
       it 'fetches until total has been reached' do
         stub_members(%w(a@example.com), %w(b@example.com), total_items: 5)
@@ -65,14 +147,59 @@ describe Synchronize::Mailchimp::Client do
     end
   end
 
-  context '#subscribe_operation' do
-    subject { client.subscribe_operation(top_leader) }
+  context '#create_merge_field_operation' do
+    subject { client.create_merge_field_operation('Gender', 'dropdown', { choices: %w(m w) }) }
+
+    it 'POSTs to segments list resource' do
+      expect(subject[:method]).to eq 'POST'
+      expect(subject[:path]).to eq 'lists/2/merge-fields'
+    end
+
+    it 'body includes name and static_segment fields' do
+      body = JSON.parse(subject[:body])
+      expect(body['tag']).to eq 'GENDER'
+      expect(body['name']).to eq 'Gender'
+      expect(body['type']).to eq 'dropdown'
+      expect(body['options']['choices']).to eq %w(m w)
+    end
+  end
+
+  context '#create_segment_operation' do
+    subject { client.create_segment_operation('a') }
+
+    it 'POSTs to segments list resource' do
+      expect(subject[:method]).to eq 'POST'
+      expect(subject[:path]).to eq 'lists/2/segments'
+    end
+
+    it 'body includes name and static_segment fields' do
+      body = JSON.parse(subject[:body])
+      expect(body['name']).to eq 'a'
+      expect(body['static_segment']).to eq []
+    end
+  end
+
+  context '#update_segment_operation' do
+    subject { client.update_segment_operation(1, %w(leader@example.com member@example.com)) }
+
+    it 'POSTs to segments list resource' do
+      expect(subject[:method]).to eq 'POST'
+      expect(subject[:path]).to eq "lists/2/segments/1"
+    end
+
+    it 'body includes name and static_segment fields' do
+      body = JSON.parse(subject[:body])
+      expect(body['members_to_add']).to eq %w(leader@example.com member@example.com)
+    end
+  end
+
+  context '#subscribe_member_operation' do
+    subject { client.subscribe_member_operation(top_leader) }
 
     it 'POSTs to members list resource' do
       expect(subject[:method]).to eq 'POST'
       expect(subject[:path]).to eq 'lists/2/members'
     end
-
 
     it 'body includes status, email_address, FNAME and LNAME fields' do
       body = JSON.parse(subject[:body])
@@ -81,10 +208,55 @@ describe Synchronize::Mailchimp::Client do
       expect(body['merge_fields']['FNAME']).to eq 'Top'
       expect(body['merge_fields']['LNAME']).to eq 'Leader'
     end
+
+    it 'body includes member fields' do
+      member_field = ['id', ->(p) { p.id }]
+      expect(client).to receive(:member_fields).and_return([member_field])
+      body = JSON.parse(subject[:body])
+      expect(body['id']).to eq top_leader.id
+    end
+
+    it 'body includes merge fields' do
+      merge_field = ['Gender', 'dropdown', { choices: %w(w m) }, ->(p) { p.gender }]
+      expect(client).to receive(:merge_fields).and_return([merge_field])
+      body = JSON.parse(subject[:body])
+      expect(body['merge_fields']['GENDER']).to eq top_leader.gender
+    end
   end
 
-  context '#delete_operation' do
-    subject { client.delete_operation(@email) }
+  context '#update_member_operation' do
+    subject { client.update_member_operation(top_leader) }
+
+    it 'POSTs to members list resource' do
+      expect(subject[:method]).to eq 'PUT'
+      expect(subject[:path]).to eq 'lists/2/members/f55f27b511af2735650c330490da54f5'
+    end
+
+    it 'body includes status, email_address, FNAME and LNAME fields' do
+      body = JSON.parse(subject[:body])
+      expect(body['email_address']).to eq 'top_leader@example.com'
+      expect(body['merge_fields']['FNAME']).to eq 'Top'
+      expect(body['merge_fields']['LNAME']).to eq 'Leader'
+    end
+
+    it 'body includes member fields' do
+      member_field = ['id', ->(p) { p.id }]
+      expect(client).to receive(:member_fields).and_return([member_field])
+      body = JSON.parse(subject[:body])
+      expect(body['id']).to eq top_leader.id
+    end
+
+    it 'body includes merge fields' do
+      merge_field = ['Gender', 'dropdown', { choices: %w(w m) }, ->(p) { p.gender }]
+      expect(client).to receive(:merge_fields).and_return([merge_field])
+      body = JSON.parse(subject[:body])
+      expect(body['merge_fields']['GENDER']).to eq top_leader.gender
+    end
+  end
+
+
+  context '#unsubscribe_member_operation' do
+    subject { client.unsubscribe_member_operation(@email) }
 
     it 'DELETEs email specific resource' do
       @email = 'top_leader@example.com'

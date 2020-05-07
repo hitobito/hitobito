848f26e986a11af3825b96c6a88ce077b4a99e59
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-05-07 18:28:55 +0200

    Conform to linter and styleguide

--- app/models/payment_reminder_config.rb
+++ app/models/payment_reminder_config.rb
@@ -16,36 +16,35 @@ class PaymentReminderConfig < ActiveRecord::Base
   validates_by_schema
 
   LEVELS = (1..3)
+  DEFAULTS = [
+    [
+      30,
+      'Zahlungserinnerung',
+      'Im hektischen Alltag kann es vorkommen, eine fällige Zahlung zu übersehen. ' \
+      "\nDanke, dass Sie die Überweisung in den nächsten "\
+      "Tagen vornehmen. Sollten Sie die Zahlung bereits veranlasst haben, betrachten Sie bitte " \
+      'dieses Schreiben als gegenstandslos.'
+    ],
+    [
+      14,
+      'Zweite Mahnung',
+      'Trotz unserer Zahlungserinnerung haben Sie unsere Rechnung noch ' \
+      "nicht beglichen.\nWir bitten Sie den fehlbaren Betrag zu begleichen."
+    ],
+    [
+      5,
+      'Dritte Mahnung',
+      'Wir fordern Sie nun ein letztes Mal auf, den offenen Betrag innert Wochenfrist, ' \
+      'zu begleichen.'
+    ]
+  ].zip(LEVELS.to_a).to_h.invert
 
   validates :level, length: { in: LEVELS }
 
   scope :list, -> { order(:level) }
 
   def with_defaults(level)
-    defaults = [
-      [
-        30,
-        'Zahlungserinnerung',
-        'Im hektischen Alltag kann es vorkommen, eine fällige Zahlung zu übersehen. ' \
-        "\nDanke, dass Sie die Überweisung in den nächsten "\
-        "Tagen vornehmen. Sollten Sie die Zahlung bereits veranlasst haben, betrachten Sie bitte " \
-        'dieses Schreiben als gegenstandslos.'
-      ],
-      [
-        14,
-        'Zweite Mahnung',
-        'Trotz unserer Zahlungserinnerung haben Sie unsere Rechnung noch ' \
-        "nicht beglichen.\nWir bitten Sie den fehlbaren Betrag zu begleichen."
-      ],
-      [
-        5,
-       'Dritte Mahnung',
-       'Wir fordern Sie nun ein letztes Mal auf, den offenen Betrag innert Wochenfrist, ' \
-       'zu begleichen.'
-      ]
-    ].zip(LEVELS.to_a).to_h.invert
-
-    due_days, title, text = defaults.fetch(level)
+    due_days, title, text = DEFAULTS.fetch(level)
     self.attributes = { due_days: due_days, title: title, text: text, level: level }
   end
 end

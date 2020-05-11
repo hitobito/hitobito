52f96df150bb951f7519afcfeb648825ed94008e
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-05-11 17:43:26 +0200

    Load a person subscriptions via domain class

---
+++ app/domain/person/subscriptions.rb
@@ -0,0 +1,50 @@
+class Person::Subscriptions
+
+  def initialize(person)
+    @person = person
+  end
+
+  def mailing_lists
+    scope = MailingList
+
+    scope
+      .where(id: direct.select('mailing_list_id'))
+      .or(scope.where(id: from_events.select('mailing_list_id')))
+      .or(scope.where(id: from_groups.select('mailing_list_id')))
+      .where.not(id: exclusions.select('mailing_list_id'))
+      .distinct
+  end
+
+  def direct
+    @person.subscriptions.where(excluded: false)
+  end
+
+  def exclusions
+    @person.subscriptions.where(excluded: true)
+  end
+
+  def from_events
+    event_ids = @person.event_participations.active.select('event_id')
+    Subscription.events.where(subscriber_id: event_ids)
+  end
+
+  def from_groups
+    sql = <<~SQL
+      related_role_types.role_type = ? AND
+      groups.lft <= ? AND groups.rgt >= ? AND
+      (tags.name IS NULL OR tags.name IN (?))
+    SQL
+
+    condition = OrCondition.new
+    @person.roles.each do |role|
+      condition.or(sql, role.type, role.group.lft, role.group.rgt, @person.tag_list)
+    end
+
+    Subscription
+      .groups
+      .joins('INNER JOIN groups ON groups.id = subscriptions.subscriber_id')
+      .joins(:related_role_types)
+      .left_joins(:tags)
+      .where(condition.to_a)
+  end
+end

5329bf2bd77b5a969274456f002f3a34f3c4a3d1
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-05-12 09:38:56 +0200

    Add abos tab to person#show

--- spec/domain/person/subscriptions_spec.rb
+++ spec/domain/person/subscriptions_spec.rb
@@ -1,5 +1,9 @@
-require 'spec_helper'
+#  Copyright (c) 2020, Grünliberale Partei Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
 
+require 'spec_helper'
 
 describe Person::Subscriptions do
 

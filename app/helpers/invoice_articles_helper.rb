9561d83876fed05dffe5ec51c44a609a9cda5858
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-05-13 15:32:46 +0200

    Use currency when formating invoice_article unit cost

---
+++ app/helpers/invoice_articles_helper.rb
@@ -0,0 +1,12 @@
+#  Copyright (c) 2012-2020, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+module InvoiceArticlesHelper
+
+  def format_invoice_article_unit_cost(invoice_article)
+    number_to_currency(invoice_article.unit_cost)
+  end
+
+end

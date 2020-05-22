492c38718daab951a986158be09d9fe9b2dafe0f
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-05-22 10:42:37 +0200

    Update schema from pending migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_091840) do
+ActiveRecord::Schema.define(version: 2020_05_11_140032) do
 
   create_table "additional_emails", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "contactable_type", null: false

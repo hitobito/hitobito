c39940b13eca74d6fc1ea71bd2eb84638ffc26fb
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-07-02 22:58:07 +0200

    Add not_expired scope to oauth/access_grants

--- app/models/oauth/access_grant.rb
+++ app/models/oauth/access_grant.rb
@@ -19,15 +19,11 @@ module Oauth
 
     scope :list, -> { order(created_at: :desc) }
     scope :for,  ->(uri) { where(redirect_uri: uri) }
-    scope :active, -> { where(revoked_at: nil) }
+    scope :not_expired, -> { where('DATE_ADD(created_at, INTERVAL expires_in SECOND) > UTC_TIME') }
+    scope :active, -> { not_expired.where(revoked_at: nil) }
 
     def to_s
       token
     end
-
-    def expired?
-      (created_at + expires_in) < Time.zone.now
-    end
-
   end
 end

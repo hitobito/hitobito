8250b6b65a9fd5bc39bbf8d53356652df14b9c87
Carlo Beltrame <carlo@beltra.me>
2020-08-12 14:26:22 +0200

    Set root password in development
    For a more consistent development experience across wagons

---
+++ db/seeds/development/root.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+#  Copyright (c) 2020, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+Person.first.update!(password: 'hito42bito', first_name: 'Puzzle', last_name: 'ITC', birthday: '1999-09-09')

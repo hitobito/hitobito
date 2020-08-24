c14d1dd6f50e0ad5cf80522c67403f537f595ac5
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-08-24 12:01:53 +0200

    Add missing license headers to mailchimp files

--- app/domain/synchronize/mailchimp/result_type.rb
+++ app/domain/synchronize/mailchimp/result_type.rb
@@ -1,3 +1,8 @@
+#  Copyright (c) 2018, Grünliberale Partei Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 module Synchronize
   module Mailchimp
     class ResultType < ActiveRecord::Type::Value
--- spec/domain/synchronize/mailchimp/synchronizator_spec.rb
+++ spec/domain/synchronize/mailchimp/synchronizator_spec.rb
@@ -1,3 +1,8 @@
+#  Copyright (c) 2020, Pfadibewegung Schweiz. This file is part of
+#  hitobito_pbs and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pbs.
+
 require 'spec_helper'
 require 'digest/md5'
 

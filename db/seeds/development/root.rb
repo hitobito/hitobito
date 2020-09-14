999d9e6c75e3a64c687082dd09d595dd8dc6daef
Carlo Beltrame <carlo@beltra.me>
2020-09-14 18:21:00 +0200

    Update root person in development without validations, to work around wagon validations

--- db/seeds/development/root.rb
+++ db/seeds/development/root.rb
@@ -5,4 +5,11 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
-Person.first.update!(password: 'hito42bito', first_name: 'Puzzle', last_name: 'ITC', birthday: '1999-09-09')
+p = Person.first
+p.assign_attributes(
+    password: 'hito42bito',
+    first_name: 'Puzzle',
+    last_name: 'ITC',
+    birthday: '1999-09-09'
+)
+p.save!(validate: false)

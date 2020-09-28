4e858caeaba1f3c20ff522c1a1c09552058120cc
psunix <psunix@users.noreply.github.com>
2020-09-28 08:24:27 +0200

    Make it possible to filter people by absent tags, refs hitobito/hitobito_cvp#47 (#1028)

---
+++ app/domain/person/filter/tag_absence.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+class Person::Filter::TagAbsence < Person::Filter::Tag
+
+  def apply(scope)
+    scope.where.not(people: { id: tagged_people_ids }).distinct
+  end
+
+  private
+
+  def tagged_people_ids
+    ActsAsTaggableOn::Tagging
+      .joins(:tag)
+      .where(taggable_type: Person.to_s)
+      .where(tags_condition)
+      .select('DISTINCT taggable_id')
+  end
+
+end

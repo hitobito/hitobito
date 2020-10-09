517d4d42cd0519e4b0a6de89510c0637d68172ff
Matthias Viehweger <viehweger@puzzle.ch>
2020-10-09 17:39:32 +0200

    Conform to linter and styleguide

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,4 +1,10 @@
-# encoding: utf-8
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 # == Schema Information
 #
 # Table name: groups
@@ -24,12 +30,7 @@
 #  updater_id                  :integer
 #  deleter_id                  :integer
 #  require_person_add_requests :boolean          default(FALSE), not null
-#
 
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
-#  hitobito and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
 require 'spec_helper'
 
 describe Group do

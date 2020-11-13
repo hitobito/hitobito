1de5e123a7cca341bfc847e3c4b2fef75961ae89
Matthias Viehweger <viehweger@puzzle.ch>
2020-11-13 16:27:21 +0100

    Spec new attribute behaviour

--- spec/models/event/question_spec.rb
+++ spec/models/event/question_spec.rb
@@ -1,5 +1,10 @@
-# encoding: utf-8
 # frozen_string_literal: true
+
+#  Copyright (c) 2012-2020, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 
 # == Schema Information
 #
@@ -13,22 +18,97 @@
 #  required         :boolean
 #
 
-
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
-#  hitobito and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
-
 require 'spec_helper'
 
 describe Event::Question do
 
-  let(:event) { events(:top_course) }
+  context 'with an event assigned' do
+    let(:event) { events(:top_course) }
 
-  it 'adds answer to participation after create' do
-    expect do
-      event.questions.create!(question: 'Test?', required: true)
-    end.to change { Event::Answer.count }.by(1)
+    it 'adds answer to participation after create' do
+      expect do
+        event.questions.create!(question: 'Test?', required: true)
+      end.to change { Event::Answer.count }.by(1)
+    end
   end
-  
+
+  context 'has validations' do
+    subject { described_class.new(question: 'Is this a Spec') }
+
+    it 'is valid without choices' do
+      expect(subject.choice_items).to be_empty
+
+      is_expected.to be_valid
+    end
+
+    it 'is valid with several choices' do
+      subject.choices = 'ja,nein,vielleicht'
+      expect(subject.choice_items).to have(3).items
+
+      is_expected.to be_valid
+    end
+
+    it 'is valid with one choice if single-choice' do
+      subject.choices = 'ja'
+      expect(subject.choice_items).to have(1).item
+      expect(subject.checkbox).to be true
+
+      is_expected.to be_valid
+    end
+    it 'is invalid with one choice normally' do
+      subject.choices = 'ja'
+      expect(subject.choice_items).to have(1).item
+      expect(subject.checkbox).to be false
+
+      is_expected.to_not be_valid
+    end
+  end
+
+  context 'with single-choice' do
+    subject { described_class.new(question: 'Test?', choices: 'ja', checkbox: true) }
+
+    it 'has basic assumptions' do
+      expect(subject.choice_items).to have(1).item
+      expect(subject.checkbox).to be true
+    end
+
+    it 'is set to multiple-choice so that checkboxes are rendered' do
+      expect do
+        subject.save!
+      end.to change(:multiple_choice).from(false).to(true)
+    end
+
+    it 'is valid with one choice' do
+      expect(subject.choice_items).to have(1).item
+
+      is_expected.to be_valid
+    end
+
+    it 'is invalid without choice' do
+      subject.choices = ''
+      expect(subject.choice_items).to be_empty
+
+      is_expected.to_not be_valid
+    end
+
+    it 'is invalid with several choices' do
+      subject.choices = 'ja,nein'
+      expect(subject.choice_items).to have(2).item
+
+      is_expected.to_not be_valid
+    end
+
+    it 'may be required' do
+      subject.required = true
+
+      is_expected.to be_valid
+    end
+
+    it 'may be optional' do
+      subject.required = false
+
+      is_expected.to be_valid
+    end
+  end
+
 end

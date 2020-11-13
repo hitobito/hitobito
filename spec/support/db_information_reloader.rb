bdb397b2c6858401a706aa66bd24b404a03c76a3
Matthias Viehweger <viehweger@puzzle.ch>
2020-11-13 12:28:26 +0100

    Reload column-information for spec-runs

---
+++ spec/support/db_information_reloader.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2020, Stiftung für junge Auslandssschweizer. This file is part of
+#  hitobito_sjas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sjas.
+
+[
+  Person,
+  Group
+].each(&:reset_column_information)

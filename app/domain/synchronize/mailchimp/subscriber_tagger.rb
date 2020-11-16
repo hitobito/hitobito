8191b9499c72b4b723740bb4425d14cf6a2513c9
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-11-16 11:16:59 +0100

    Use InvalidEmailTagger from Mailchimp SubscriberTagger

---
+++ app/domain/synchronize/mailchimp/subscriber_tagger.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+#
+#  Copyright (c) 2012-2020, CVP Schweiz. This file is part of
+#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_cvp.
+
+module Synchronize
+  module Mailchimp
+    class SubscriberTagger
+      attr_reader :emails, :list
+
+      def initialize(emails, list)
+        @emails = emails
+        @list = list
+      end
+
+      def tag!
+        emails.each do |email|
+          subscriber = subscribers[email]
+          next unless subscriber
+
+          kind = subscriber.primary? ? :primary : :additional
+          Contactable::InvalidEmailTagger.new(subscriber.person, email, kind).tag!
+        end
+      end
+
+      def subscribers
+        @subscribers ||= Synchronize::Mailchimp::Subscriber
+          .mailing_list_subscribers(list).index_by(&:email)
+      end
+    end
+  end
+end
---
+++ spec/domain/synchronize/mailchimp/subscriber_tagger_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+#
+#  Copyright (c) 2012-2020, CVP Schweiz. This file is part of
+#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_cvp.
+
+require 'spec_helper'
+
+describe Synchronize::Mailchimp::SubscriberTagger do
+  let(:list) { mailing_lists(:leaders) }
+  let(:person) { people(:top_leader) }
+
+  before do
+    Subscription.create!(mailing_list: list, subscriber: person)
+  end
+
+  it 'tags by primary email' do
+    Synchronize::Mailchimp::SubscriberTagger.new([person.email], list).tag!
+    expect(person.tags).to have(1).item
+    expect(person.tags.first.name).to eq 'category_validation:email_primary_invalid'
+  end
+
+  it 'tags by secondary email' do
+    AdditionalEmail.create!(email: 'foo@bar.com', contactable: person, label: 'Privat', mailings: true)
+    list.update(mailchimp_include_additional_emails: true)
+    Synchronize::Mailchimp::SubscriberTagger.new(['foo@bar.com'], list).tag!
+    expect(person.tags).to have(1).item
+    expect(person.tags.first.name).to eq 'category_validation:email_additional_invalid'
+  end
+end

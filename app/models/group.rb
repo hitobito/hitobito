dacffa732cec66a6171f5a9436f3ec363bb2879a
psunix <psunix@s-it.ch>
2020-11-26 09:19:57 +0100

    Make rubocop happy

--- app/models/group.rb
+++ app/models/group.rb
@@ -202,7 +202,8 @@ class Group < ActiveRecord::Base
     elsif layer?
       duplicates = PersonDuplicate.where(id: layer_person_duplicate_ids)
     end
-    duplicates.includes(person_1: [{ roles: :group }, :groups], person_2: [{ roles: :group }, :groups])
+    duplicates.includes(person_1: [{ roles: :group }, :groups],
+                        person_2: [{ roles: :group }, :groups])
   end
 
   private

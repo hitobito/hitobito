8025261e170647533263cc91124328e577af9040
Carlo Beltrame <carlo@beltra.me>
2020-12-08 16:17:53 +0100

    Prevent any HTML tags that are not supported by the Trix editor

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -10,3 +10,28 @@ ActiveSupport.on_load(:action_view) do
   include ActionText::ContentHelper
   include ActionText::TagHelper
 end
+
+Rails.application.config.after_initialize do
+
+  # Only allow tags that are supported in Trix.
+  # List based on the following files in Trix:
+  # https://github.com/basecamp/trix/blob/master/src/trix/config/block_attributes.coffee
+  # https://github.com/basecamp/trix/blob/master/src/trix/config/text_attributes.coffee
+  ActionText::ContentHelper.allowed_tags.clear.merge [
+      "div",
+      "blockquote",
+      "h1",
+      "pre",
+      "ul",
+      "ol",
+      "li",
+      "strong",
+      "em",
+      "a",
+      "del",
+      ActionText::Attachment::TAG_NAME,
+      "figure",
+      "figcaption"
+  ]
+
+end

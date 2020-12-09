3135455e356d47a8f8a6f065a7fd71ffeb2b199c
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-12-10 00:44:11 +0100

    Parse street and number from address string
    A better option is probably https://github.com/openvenues/libpostal

---
+++ app/domain/address/parser.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2020, CVP Schweiz. This file is part of
+#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_cvp.
+
+class Address::Parser
+
+  REGEX = /^(.*?)[,?\s*]?(\d+\s?\w?)?$/.freeze
+
+  def initialize(string)
+    @string = string.gsub(',', '')
+  end
+
+  def street
+    match[1]
+  end
+
+  def number
+    match[2]&.gsub(' ', '')
+  end
+
+  private
+
+  def match
+    @match ||= REGEX.match(@string)
+  end
+
+end

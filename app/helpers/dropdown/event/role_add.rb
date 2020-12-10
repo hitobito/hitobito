18ffb0c355c086cefce1ad243b0cbc9e31aab2c4
Matthias Viehweger <viehweger@puzzle.ch>
2020-12-10 11:20:11 +0100

    Allow to prefill a person for a new event-role
    This streamlines the process a little when participants want to also
    become part of the leader-team.

--- app/helpers/dropdown/event/role_add.rb
+++ app/helpers/dropdown/event/role_add.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2020, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -10,14 +10,15 @@ module Dropdown
   module Event
     class RoleAdd < Dropdown::Base
 
-      attr_reader :group, :event
+      attr_reader :group, :event, :person
 
-      def initialize(template, group, event)
+      def initialize(template, group, event, person = nil)
         label = translate("add_to_#{event.klass.name.underscore}",
                           default: full_translation_key(:add))
         super(template, label, :plus)
         @group = group
         @event = event
+        @person = person
         init_items
       end
 
@@ -25,9 +26,10 @@ module Dropdown
 
       def init_items
         event.role_types.reject(&:restricted?).each do |type|
-          link = template.new_group_event_role_path(group,
-                                                    event,
-                                                    event_role: { type: type.sti_name })
+          event_role_attrs = { type: type.sti_name }
+          event_role_attrs[:person_id] = person.id if person
+
+          link = template.new_group_event_role_path(group, event, event_role: event_role_attrs)
           add_item(type.label, link)
         end
       end

f4a2172a255b298399c6ea2b04e34b3de8d36ed5
Carlo Beltrame <carlo@beltra.me>
2020-12-14 18:02:19 +0100

    Remove leftover from debugging

--- app/domain/no_attachments_validator.rb
+++ app/domain/no_attachments_validator.rb
@@ -2,7 +2,6 @@ class NoAttachmentsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.body.attachments.any?
       record.errors[attribute] << I18n.t('errors.messages.attachments_not_allowed')
-      Rails::Html::Sanitizer
     end
   end
 end

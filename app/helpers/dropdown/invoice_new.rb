b9e4c9449afa34f668a872c220a80c95969b454b
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-12-15 05:47:36 +0100

    Inline single button into InvoiceNew Dropdown

--- app/helpers/dropdown/invoice_new.rb
+++ app/helpers/dropdown/invoice_new.rb
@@ -1,4 +1,3 @@
-# encoding: utf-8
 # frozen_string_literal: true
 
 #  Copyright (c) 2017, Jungwacht Blauring Schweiz. This file is part of
@@ -8,26 +7,56 @@
 
 module Dropdown
   class InvoiceNew < Base
+    attr_reader :people, :mailing_list
 
-    # attr_reader :params
-
-    def initialize(template, label, finance_groups, people, icon)
-      super(template, label, icon)
-      @finance_groups = finance_groups
+    def initialize(template, people: [], mailing_list: nil)
+      super(template, label, :plus)
       @people = people
+      @mailing_list = mailing_list
       init_items
+    end
+
+    def button_or_dropdown
+      if finance_groups.one?
+        single_button
+      else
+        to_s
+      end
     end
 
     private
 
-    def init_items
-      finance_group_links
+    def label
+      I18n.t('crud.new.title', model: Invoice.model_name.human)
     end
 
-    def finance_group_links
-      @finance_groups.each do |group|
-        add_item(group.name, template.new_invoices_for_people_path(group, @people))
+    def single_button
+      data = { checkable: true } if template.action_name == 'index'
+      template.action_button(label, path(finance_groups.first), :plus, data: data)
+    end
+
+    def path(finance_group) # rubocop:disable Metrics/MethodLength
+      if mailing_list
+        template.new_group_invoice_list_path(
+          finance_group,
+          invoice_list: { receiver_id: mailing_list.id, receiver_type: mailing_list.class }
+        )
+      else
+        template.new_group_invoice_list_path(
+          finance_group,
+          invoice_list: { recipient_ids: people.collect(&:id).join(',') }
+        )
       end
+    end
+
+    def init_items
+      finance_groups.each do |finance_group|
+        add_item(finance_group.name, path(finance_group))
+      end
+    end
+
+    def finance_groups
+      template.current_user.finance_groups
     end
   end
 end
--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -47,15 +47,8 @@ module InvoicesHelper
     end
   end
 
-  def new_invoices_for_people_path(group, people)
-    if people.is_a?(MailingList)
-      new_group_invoice_list_path(group, {
-        invoice_list: { receiver_id: people.id, receiver_type: people.class }
-      })
-    else
-      ids = people.collect(&:id).join(',')
-      new_group_invoice_list_path(group, invoice_list: { recipient_ids: ids })
-    end
+  def invoice_button(people: [], mailing_list: nil)
+    Dropdown::InvoiceNew.new(self, people: people, mailing_list: mailing_list).button_or_dropdown
   end
 
   def invoices_export_dropdown

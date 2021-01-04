bf2db0c92fd4c6a80ca40b84fd4a255128a31d19
Matthias Viehweger <viehweger@puzzle.ch>
2021-01-04 13:20:00 +0100

    Conform to linter and styleguide

--- app/controllers/event/application_market_controller.rb
+++ app/controllers/event/application_market_controller.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 #  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
@@ -35,7 +35,7 @@ class Event::ApplicationMarketController < ApplicationController
   end
 
   def remove_from_waiting_list
-    application.update_column(:waiting_list, false)
+    application.update_column(:waiting_list, false) # rubocop:disable Rails/SkipsModelValidations
     render 'waiting_list'
   end
 

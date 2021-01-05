32d247f500b2ec2212da2932c58315ba5773040b
Andreas Maierhofer <maierhofer@puzzle.ch>
2021-01-05 10:41:40 +0100

    Correct header comment

--- app/domain/invoice/batch_create.rb
+++ app/domain/invoice/batch_create.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 
 #  Copyright (c) 2012-2020, CVP Schweiz. This file is part of
-#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_cvp.
+#  https://github.com/hitobito/hitobito.
 
 class Invoice::BatchCreate
   attr_reader :invoice_list, :invoice

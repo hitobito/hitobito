f0d0028c1171f0f390a8ba53e4e1f09c0aa46471
psunix <psunix@s-it.ch>
2021-01-06 17:30:41 +0100

    Extend group setting spec

--- spec/models/group_setting_spec.rb
+++ spec/models/group_setting_spec.rb
@@ -23,6 +23,7 @@ describe GroupSetting do
   it 'encrypts username, password' do
     setting.username = 'david.hasselhoff'
     setting.password = 'knightrider'
+    setting.provider = 'aspsms'
     value = setting.value
 
     expect(value).not_to include('username')
@@ -30,6 +31,9 @@ describe GroupSetting do
 
     expect(value).not_to include('password')
     expect(value).to include('encrypted_password')
+
+    expect(value).not_to include('encrypted_provider')
+    expect(value).to include('provider')
 
     encrypted_password = value['encrypted_password']
     expect(encrypted_password[:encrypted_value]).to be_present

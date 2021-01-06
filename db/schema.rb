81a5857c36582c7d5e31461013459cedbaa87997
psunix <psunix@s-it.ch>
2021-01-06 16:36:27 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_123403) do
+ActiveRecord::Schema.define(version: 2021_01_05_120000) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false

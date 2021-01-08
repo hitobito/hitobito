8aaf852345d26e323c34b0fcd0bee58fd7b847cc
Nils Rauch <nils.rauch@gmail.com>
2021-01-08 14:38:37 +0100

    Conform to rubocop

--- app/domain/person/subscriptions.rb
+++ app/domain/person/subscriptions.rb
@@ -55,6 +55,6 @@ class Person::Subscriptions
   private
 
   def exclusions_by_subscription_tags
-    SubscriptionTag.where(subscription_tags: { tag_id: @person.tag_ids, excluded: true }).pluck(:subscription_id)
+    SubscriptionTag.where(tag_id: @person.tag_ids, excluded: true).pluck(:subscription_id)
   end
 end

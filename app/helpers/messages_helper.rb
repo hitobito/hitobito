feb8667b395914a6c158e0b404c6fa29bfc415c9
Andreas Maierhofer <a.maierhofer@gmail.com>
2021-01-14 15:58:02 +0100

    Cleanup headers

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2021, CVP Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 module MessagesHelper
 
   def add_message_button(type, path = path_args(model_class))
--- spec/controllers/messages/previews_controller_spec.rb
+++ spec/controllers/messages/previews_controller_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 #  Copyright (c) 2012-2021, CVP Schweiz. This file is part of
-#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 

e7b9553b86ae02b7b79496dfdd61a5a6d3925f26
Matthias Viehweger <viehweger@puzzle.ch>
2021-01-19 15:56:38 +0100

    Make migration reversible and output more understandable

--- db/migrate/20210107151626_translate_event_questions.rb
+++ db/migrate/20210107151626_translate_event_questions.rb
@@ -1,20 +1,19 @@
 class TranslateEventQuestions < ActiveRecord::Migration[6.0]
   def up
-    Event::Question.create_translation_table!(
-      {
-        question: :string,
-        choices: :string
-      },
-      { migrate_data: true }
-    )
-
-    remove_column :event_questions, :question
-    remove_column :event_questions, :choices
+    say_with_time('creating translation table for event-questions') do
+      Event::Question.create_translation_table!(
+        {
+          question: :string,
+          choices: :string
+        },
+        { migrate_data: true, remove_source_columns: true }
+      )
+    end
   end
 
   def down
-    add_column :event_questions, :question, :string
-    add_column :event_questions, :choices, :string
-    Event::Question.drop_translation_table! migrate_data: true
+    say_with_time('dropping translation-table for event-questions') do
+      Event::Question.drop_translation_table! migrate_data: true
+    end
   end
 end

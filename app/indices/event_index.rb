ee7f79b2490805928b49231d570819afd0bdf600
Matthias Viehweger <viehweger@puzzle.ch>
2021-01-21 09:49:15 +0100

    Allow searching for translated event-name

--- app/indices/event_index.rb
+++ app/indices/event_index.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2017, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2021, Pfadibewegung Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -8,7 +8,8 @@
 module EventIndex; end
 
 ThinkingSphinx::Index.define_partial :event do
-  indexes name, number, sortable: true
+  indexes number, sortable: true
+  indexes translations.name, as: :name, sortable: true
 
   indexes groups.name, as: :group_name
 end

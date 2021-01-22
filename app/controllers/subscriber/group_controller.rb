b33a6f09f2f54ed4c87d90455f804233f25ee306
Nils Rauch <nils.rauch@gmail.com>
2021-01-22 07:44:46 +0100

    Remove compact

--- app/controllers/subscriber/group_controller.rb
+++ app/controllers/subscriber/group_controller.rb
@@ -90,7 +90,7 @@ module Subscriber
         SubscriptionTag.new(subscription: entry,
                             tag_id: tag[:id],
                             excluded: tag[:excluded])
-      end.compact
+      end
     end
 
     def collect_included_tags

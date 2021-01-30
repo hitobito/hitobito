b256049896b91ad041661e0daacb0cfb291323c9
Matthias Viehweger <viehweger@puzzle.ch>
2021-01-30 22:20:05 +0100

    Include a default fallback to silence deprecation

--- config/application.rb
+++ config/application.rb
@@ -42,12 +42,13 @@ module Hitobito
     # All languages should fall back on each other to avoid empty attributes
     # if an entry is created in a different language.
     # This is read by globalize as well
-    config.i18n.fallbacks = {
+    config.i18n.fallbacks = [:de,
+    {
       de: [:de, :fr, :it, :en],
       fr: [:fr, :it, :en, :de],
       it: [:it, :fr, :en, :de],
       en: [:en, :de, :fr, :it]
-    }
+    }]
     I18n.config.enforce_available_locales = true
 
     # Route errors over the Rails application.

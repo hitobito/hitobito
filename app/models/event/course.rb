6b9832679f8177c4c4ad8dbf15b06d393dc01cb1
Matthias Viehweger <viehweger@puzzle.ch>
2021-02-23 12:06:55 +0100

    Remove configuration that does not take effect anyway
    Courses are generally globally visible as of this feature.

--- app/models/event/course.rb
+++ app/models/event/course.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2020, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2021, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -58,6 +58,7 @@ class Event::Course < Event
 
   self.used_attributes += [:number, :kind_id, :state, :priorization, :group_ids,
                            :requires_approval, :display_booking_info, :waiting_list]
+  self.used_attributes -= [:globally_visible]
 
   self.role_types = [Event::Role::Leader,
                      Event::Role::AssistantLeader,

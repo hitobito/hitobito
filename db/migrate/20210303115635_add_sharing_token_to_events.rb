2ef584ba2b7853d658692cc224fbabc4e8b32f65
Carlo Beltrame <carlo@beltra.me>
2021-03-10 17:08:50 +0100

    Add a shared access token to all existing events

--- db/migrate/20210303115635_add_sharing_token_to_events.rb
+++ db/migrate/20210303115635_add_sharing_token_to_events.rb
@@ -2,5 +2,13 @@ class AddSharingTokenToEvents < ActiveRecord::Migration[6.0]
   def change
     add_column :events, :shared_access_token, :string, null: true
     add_index :events, :shared_access_token
+
+    reversible do |dir|
+      dir.up do
+        Event.find_each do |e|
+          e.update_attribute(:shared_access_token, Devise.friendly_token)
+        end
+      end
+    end
   end
 end

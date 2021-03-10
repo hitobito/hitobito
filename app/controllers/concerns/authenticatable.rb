cbf50e70d4cd996704fb23655bd3a3d610d46022
Carlo Beltrame <carlo@beltra.me>
2021-03-10 16:56:56 +0100

    Revert needlessly changed files

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -54,7 +54,6 @@ module Authenticatable
   def prepare_authorization_via_shared_access_token
     if current_person && params.key?(:shared_access_token)
       current_person.shared_access_token = params[:shared_access_token]
-      true
     end
   end
 

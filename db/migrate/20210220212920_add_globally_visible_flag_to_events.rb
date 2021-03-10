52232f65a18978a3fbff03a332efe1d9c93bfe98
Carlo Beltrame <carlo@beltra.me>
2021-03-10 17:49:58 +0100

    Reset column information for travis

--- db/migrate/20210220212920_add_globally_visible_flag_to_events.rb
+++ db/migrate/20210220212920_add_globally_visible_flag_to_events.rb
@@ -2,5 +2,7 @@ class AddGloballyVisibleFlagToEvents < ActiveRecord::Migration[6.0]
   def change
     # intentionally nullable and without default
     add_column :events, :globally_visible, :boolean
+
+    Event.reset_column_information
   end
 end

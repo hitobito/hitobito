ca16dfc00be56bd54474c3a08dd037d4cfa51103
Carlo Beltrame <carlo@beltra.me>
2021-03-10 16:56:56 +0100

    Revert needlessly changed files

--- spec/decorators/event_decorator_spec.rb
+++ spec/decorators/event_decorator_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2021, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -163,5 +163,7 @@ describe EventDecorator, :draper_with_helpers do
 
       it { is_expected.to match(/public_event/) }
     end
+
   end
+
 end

45a6cd6b1a5b6077d058f0f88a1feaf7e3a87513
Mio Sidler <sidler@puzzle.ch>
2021-04-06 15:21:02 +0200

    MailRetrieverJob initial finish

--- app/domain/mailing_list/bulk_mail_retriever.rb
+++ app/domain/mailing_list/bulk_mail_retriever.rb
@@ -1,39 +1,38 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2018, Jungwacht Blauring Schweiz. This file is part of
-#  hitobito and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
+# Copyright (c) 2012-2021, Hitobito AG. This file is part of
+# hitobito and licensed under the Affero General Public License version 3
+# or later. See the COPYING file at the top-level directory or at
+# https://github.com/hitobito/hitobito.
 
 module MailingList
-    class BulkMailRetriever
-        # retrieves mail, checks if it can be assigned to a mailing list and points it to dispatch via FileStore, makes log entry
-        # currently in mail_relay/base.rb and mail_relay/lists.rb
+  class BulkMailRetriever
+    # retrieves mail, checks if it can be assigned to a mailing list and points it to dispatch via FileStore, makes log entry
+    # currently in mail_relay/base.rb and mail_relay/lists.rb
 
-        # Retrieve, process and delete all mails from the mail server. 
-        def perform       # analog zu relay_current in MailRelay::Base 
-            loop do
-              mails, last_exception = relay_batch
-              raise(last_exception) if last_exception.present?
-              break if mails.size < retrieve_count
-            end
-        end
+    # Number of emails to retrieve in one batch.
+    class_attribute :retrieve_count
+    #self.retrieve_count = 5
+    @retrieve_count = 5
+ 
 
-        def relay_batch
-            last_exception = nil
-    
-            mails = Mail.find_and_delete(count: retrieve_count) do |message|
-              proccessed_error = process(message)
-              last_exception = proccessed_error if proccessed_error
-            end
-    
-            [mails || [], last_exception]
-          rescue EOFError => e
-            logger.warn(e)
-            [[], nil]
-        end
-
-        
-
+    # Retrieve, process and delete all mails from the mail server. 
+    def perform       # analog zu relay_current in MailRelay::Base 
+      loop do
+      # mails, last_exception = relay_batch
+      mails = retrieve_batch
+      # raise(last_exception) if last_exception.present?
+      break if mails.size < @retrieve_count
+      end
     end
+
+    def retrieve_batch
+      # last_exception = nil
+      #imap_connector = ImapConnector.new()
+      mails = Mail.find_and_delete(count: @retrieve_count)
+      # rescue EOFError => e
+      #   logger.warn(e)
+      #   [[], nil]
+    end
+  end
 end
--- app/jobs/mailing_list/mail_retriever_job.rb
+++ app/jobs/mailing_list/mail_retriever_job.rb
@@ -1,42 +1,27 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
-#  hitobito and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
+# Copyright (c) 2012-2021, Hitobito AG. This file is part of
+# hitobito and licensed under the Affero General Public License version 3
+# or later. See the COPYING file at the top-level directory or at
+# https://github.com/hitobito/hitobito.
 
 
 module MailingList
-    class MailRetrieverJob < RecurringJob
+  class MailRetrieverJob < RecurringJob
 
-        run_every Settings.email.retriever.interval.minutes
+    run_every Settings.email.retriever.interval.minutes
     
-        def perform_internal
-        # only run if a retriever address is defined
-        if Settings.email.retriever.config.address
-            MailingList::BulkMailRetriever.perform  #perform analog zu relay_current in MailRelay::Base aufbauen
-        end
-        end
-        
-        # conver below logic in story error handling
-
-        # def error(job, exception)
-        # if exception.is_a?(MailRelay::Error)
-        #     super(job, exception.original, mail: extract_mail_for_errbit(exception))
-        # else
-        #     super(job, exception)
-        # end
-        # end
-    
-        # def extract_mail_for_errbit(exception)
-        # exception.mail.to_s
-        # rescue StandardError # See https://github.com/mikel/mail/issues/544
-        # begin
-        #     exception.mail.inspect
-        # rescue Exception # Be sure to get notified whatever happens
-        #     nil
-        # end
-        # end
-    
+    def perform_internal
+      # only run if a retriever address is defined
+      if Settings.email.retriever.config.address
+        retriever.perform
+      end
     end
-end  
+        
+    private
+
+    def retriever
+      MailingList::BulkMailRetriever.new
+    end
+  end
+end

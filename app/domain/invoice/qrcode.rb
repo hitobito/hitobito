7514f4ba2c874dd30ef3fd895e84c53d4a9bf978
Andreas Maierhofer <maierhofer@puzzle.ch>
2021-04-15 11:21:12 +0200

    Support blank amount in qrcode
    refs hitobito/hitobito_die_mitte#148

--- app/domain/invoice/qrcode.rb
+++ app/domain/invoice/qrcode.rb
@@ -49,7 +49,8 @@ class Invoice::Qrcode
   end
 
   def payment
-    { amount: format('%<total>.2f', total: @invoice.total), currency: @invoice.currency }
+    amount = format('%<total>.2f', total: @invoice.total) unless @invoice.total.zero?
+    { amount: amount, currency: @invoice.currency }
   end
 
   def debitor

5ec7c8cc695576b233bc2e7d48c3109786089ae5
Carlo Beltrame <carlo@beltra.me>
2021-04-27 15:52:08 +0200

    Fix different locale fallback config in production, refs #1211

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,9 +71,6 @@ Rails.application.configure do
 
   # Mail sender
   config.action_mailer.delivery_method = (ENV['RAILS_MAIL_DELIVERY_METHOD'].presence || :smtp).to_sym
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
 
   if ENV['RAILS_MAIL_DELIVERY_CONFIG'].present?
     case config.action_mailer.delivery_method

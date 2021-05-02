13fdb8cac9a9b1cf2242b8b1ea140a9007d0a85f
Matthias Viehweger <viehweger@puzzle.ch>
2021-05-02 23:16:02 +0200

    Adapt specs to changes in parameter-naming

--- spec/regressions/events/courses_controller_spec.rb
+++ spec/regressions/events/courses_controller_spec.rb
@@ -108,9 +108,9 @@ describe Events::CoursesController, type: :controller do
         get :index
         expect(navigation.all('li').size).to eq 2
         expect(navigation.all('li')[0].text.strip).to eq 'Vorbasiskurse'
-        expect(navigation.all('li a')[0][:href]).to eq list_courses_path(category: category.id, group_id: top_layer.id)
+        expect(navigation.all('li a')[0][:href]).to eq list_courses_path(category: category.id, filter: { group_ids: [top_layer.id]})
         expect(navigation.all('li')[1].text.strip).to eq 'Andere Kurse'
-        expect(navigation.all('li a')[1][:href]).to eq list_courses_path(category: 0, group_id: top_layer.id)
+        expect(navigation.all('li a')[1][:href]).to eq list_courses_path(category: 0, filter: { group_ids: [top_layer.id]})
       end
 
       it 'displays available course kinds when filtering by course category' do
@@ -120,7 +120,7 @@ describe Events::CoursesController, type: :controller do
         get :index, params: { category: category.id }
         expect(navigation.all('li ul li').size).to eq 1
         expect(navigation.all('li ul li')[0].text.strip).to eq 'Gruppenleiterkurs'
-        expect(navigation.all('li ul li a')[0][:href]).to eq list_courses_path(category: category.id, group_id: top_layer.id, anchor: 'Gruppenleiterkurs')
+        expect(navigation.all('li ul li a')[0][:href]).to eq list_courses_path(category: category.id, filter: { group_ids: [top_layer.id] }, anchor: 'Gruppenleiterkurs')
       end
 
       it 'displays other course kinds when filtering for kinds without category' do
@@ -130,7 +130,7 @@ describe Events::CoursesController, type: :controller do
         get :index, params: { category: 0 }
         expect(navigation.all('li ul li').size).to eq 3
         expect(navigation.all('li ul li')[1].text.strip).to eq 'Scharleiterkurs'
-        expect(navigation.all('li ul li a')[1][:href]).to eq list_courses_path(category: 0, group_id: top_layer.id, anchor: 'Scharleiterkurs')
+        expect(navigation.all('li ul li a')[1][:href]).to eq list_courses_path(category: 0, filter: { group_ids: [top_layer.id] }, anchor: 'Scharleiterkurs')
       end
     end
   end

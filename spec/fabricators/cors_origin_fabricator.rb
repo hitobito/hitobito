42f75b1f4fe0100f347ffed6aaa02a81e2dad5c9
psunix <psunix@s-it.ch>
2021-05-06 06:58:33 +0200

    Annotate, refactor cors check

--- spec/fabricators/cors_origin_fabricator.rb
+++ spec/fabricators/cors_origin_fabricator.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: cors_origins
+#
+#  id               :bigint           not null, primary key
+#  auth_method_type :string(255)
+#  auth_method_id   :bigint
+#  origin           :string(255)      not null
+#
 #  Copyright (c) 2018, Pfadibewegung Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at

b37fa0f232e8afb5ab5e6c90bde85a7c449181e1
Carlo Beltrame <carlo@beltra.me>
2021-05-11 16:06:10 +0200

    Adjust header comments

--- app/models/cors_origin.rb
+++ app/models/cors_origin.rb
@@ -9,7 +9,7 @@
 #  auth_method_id   :bigint
 #  origin           :string(255)      not null
 #
-#  Copyright (c) 2021, hitobito AG. This file is part of
+#  Copyright (c) 2021, Pfadibewegung Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.

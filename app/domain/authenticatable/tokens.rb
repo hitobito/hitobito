b13e0c8a2df74b117c4f9f2e43ae41837ed595a2
Carlo Beltrame <carlo@beltra.me>
2021-05-11 16:06:10 +0200

    Adjust header comments

--- app/domain/authenticatable/tokens.rb
+++ app/domain/authenticatable/tokens.rb
@@ -1,4 +1,6 @@
-#  Copyright (c) 2018, Pfadibewegung Schweiz. This file is part of
+# frozen_string_literal: true
+
+#  Copyright (c) 2021, Pfadibewegung Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.

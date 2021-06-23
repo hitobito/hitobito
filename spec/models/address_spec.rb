314c0b28c053c60d38d94ef107c323028d9f74fe
Matthias Viehweger <viehweger@puzzle.ch>
2021-06-23 23:03:07 +0200

    Support house-numbers with letters in address-completion

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,9 +1,9 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2020, CVP Schweiz. This file is part of
-#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  Copyright (c) 2012-2021, CVP Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_cvp.
+#  https://github.com/hitobito/hitobito.
 #
 # == Schema Information
 #
@@ -25,6 +25,6 @@ require 'spec_helper'
 describe Address do
   it 'serializes numbers as array' do
     bs_bern = addresses(:bs_bern)
-    expect(bs_bern.numbers).to eq %w(36 37 38 40 41)
+    expect(bs_bern.numbers).to eq %w(36 37 38 40 41 5a 5b 6A 6B)
   end
 end

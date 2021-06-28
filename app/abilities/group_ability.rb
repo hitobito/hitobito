b07005942cea74fb26340777037d9d73f8bf90f3
Matthias Viehweger <viehweger@puzzle.ch>
2021-06-28 13:59:52 +0200

    Follow rename of ServiceTokenAbility

--- app/abilities/group_ability.rb
+++ app/abilities/group_ability.rb
@@ -1,4 +1,6 @@
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2021, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -40,7 +42,7 @@ class GroupAbility < AbilityDsl::Base
 
     permission(:layer_full).may(:create).with_parent_in_same_layer
     permission(:layer_full).may(:destroy).in_same_layer_except_permission_giving
-    permission(:layer_full).may(:index_service_tokens).service_token_core_constraints
+    permission(:layer_full).may(:index_service_tokens).service_token_in_same_layer
     permission(:layer_full).
       may(:update, :reactivate, :index_person_add_requests, :index_notes,
           :manage_person_tags, :activate_person_add_requests, :deactivate_person_add_requests,
@@ -59,7 +61,7 @@ class GroupAbility < AbilityDsl::Base
       may(:update, :reactivate, :index_person_add_requests, :index_notes,
           :manage_person_tags, :index_deleted_people).in_same_layer_or_below
     permission(:layer_and_below_full).may(:modify_superior).in_below_layers
-    permission(:layer_and_below_full).may(:index_service_tokens).service_token_core_constraints
+    permission(:layer_and_below_full).may(:index_service_tokens).service_token_in_same_layer
     permission(:layer_and_below_full).
       may(:activate_person_add_requests, :deactivate_person_add_requests).
       in_same_layer
@@ -132,7 +134,7 @@ class GroupAbility < AbilityDsl::Base
     user.roles.any? { |r| r.class.member? }
   end
 
-  def service_token_core_constraints
+  def service_token_in_same_layer
     in_same_layer
   end
 

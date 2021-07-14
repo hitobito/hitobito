a7a43d037ec2c030c1fe13143546036284c60b83
Matthias Viehweger <viehweger@puzzle.ch>
2021-07-14 19:53:49 +0200

    Reload schema-information to pick up wagon-changes

--- spec/support/db_information_reloader.rb
+++ spec/support/db_information_reloader.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2020, Stiftung für junge Auslandssschweizer. This file is part of
+#  Copyright (c) 2020-2021, Stiftung für junge Auslandssschweizer. This file is part of
 #  hitobito_sjas and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sjas.
 
 [
   Person,
-  Group
+  Group,
+  Role
 ].each(&:reset_column_information)

83e9e66cc2eaf88308647041d6b8b8e1966652ef
Nils Rauch <nils.rauch@gmail.com>
2021-08-02 08:22:50 +0200

    Add error class and refactor payment provider

--- app/domain/payment_provider.rb
+++ app/domain/payment_provider.rb
@@ -11,29 +11,29 @@ class PaymentProvider
   end
 
   def initial_setup
-    @client = Epics::Client.setup(config.password,
+    @client = Epics::Client.setup(@config.password,
                                   payment_provider_setting.url,
                                   payment_provider_setting.host_id,
-                                  config.user_identifier,
-                                  config.partner_identifier)
+                                  @config.user_identifier,
+                                  @config.partner_identifier)
 
-    config.update(keys: client.send(:dump_keys))
+    @config.update(keys: client.send(:dump_keys))
   end
 
   def INI
     ok = client.INI
 
-    raise 'INI request failed' unless ok
+    raise PaymentProviders::EbicsError.new('INI request failed') unless ok
   end
 
   def ini_letter
-    @ini_letter ||= client.ini_letter(config.payment_provider)
+    @ini_letter ||= client.ini_letter(@config.payment_provider)
   end
 
   def HIA
     ok = client.HIA
 
-    raise 'HIA request failed' unless ok
+    raise PaymentProviders::EbicsError.new('HIA request failed') unless ok
   end
 
   def HPB
@@ -44,36 +44,35 @@ class PaymentProvider
 
   private
 
-  attr_reader :config
-
   def check_bank_public_keys!(bank_x, bank_e)
     authentication_key_ok = correct_public_key?(bank_x,
                                                 payment_provider_setting.authentication_hash)
     encryption_key_ok = correct_public_key?(bank_e,
                                             payment_provider_setting.encryption_hash)
-    return if authentication_key_ok && encryption_key_ok
+    return true if authentication_key_ok && encryption_key_ok
     
     if !authentication_key_ok && encryption_key_ok
-      raise 'Authentication public key does not match'
+      raise PaymentProviders::EbicsError.new('Authentication public key does not match')
     elsif authentication_key_ok && !encryption_key_ok
-      raise 'Encryption public key does not match'
+      raise PaymentProviders::EbicsError.new('Encryption public key does not match')
     else
-      raise 'Authentication and encryption public keys do not match'
+      raise PaymentProviders::EbicsError.new('Authentication and encryption ' \
+                                             'public keys do not match')
     end
   end
 
   def client
-    @client ||= Epics::Client.new(config.keys,
-                                  config.password,
+    @client ||= Epics::Client.new(@config.keys,
+                                  @config.password,
                                   payment_provider_setting.url,
                                   payment_provider_setting.host_id,
-                                  config.user_identifier,
-                                  config.partner_identifier)
+                                  @config.user_identifier,
+                                  @config.partner_identifier)
   end
 
   def payment_provider_setting
     @payment_provider_setting ||= Settings.payment_providers.select do |provider|
-      provider.name == config.payment_provider
+      provider.name == @config.payment_provider
     end.first
   end
 

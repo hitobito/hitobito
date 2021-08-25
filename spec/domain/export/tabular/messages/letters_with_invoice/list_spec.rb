21d1730a8aaa636c95a40ab44d7bb8321560c5af
Nils Rauch <nils.rauch@gmail.com>
2021-08-25 08:51:46 +0200

    Add person id to invoice message export

--- spec/domain/export/tabular/messages/letters_with_invoice/list_spec.rb
+++ spec/domain/export/tabular/messages/letters_with_invoice/list_spec.rb
@@ -15,14 +15,14 @@ describe Export::Tabular::Messages::LettersWithInvoice::List do
   context 'used labels' do
     its(:attributes) do
       should == [:esr_number, :recipient_email, :recipient_address, :reference, :total,
-                 :first_name, :last_name, :company_name, :company, :email, :address,
+                 :id, :first_name, :last_name, :company_name, :company, :email, :address,
                  :zip_code, :town, :country, :gender, :birthday, :salutation, :title,
                  :correspondence_language, :household_key]
     end
 
     its(:labels) do
       should == ['Referenz Nummer', 'Empfänger E-Mail', 'Empfänger Adresse', 'Referenz',
-                 'Total inkl. MwSt.', 'Vorname', 'Nachname', 'Firmenname', 'Firma',
+                 'Total inkl. MwSt.', 'Person-ID', 'Vorname', 'Nachname', 'Firmenname', 'Firma',
                  'Haupt-E-Mail', 'Adresse', 'PLZ', 'Ort', 'Land', 'Geschlecht',
                  'Geburtstag', 'Anrede', 'Titel', 'Korrespondenzsprache', 'Haushalts-ID']
     end

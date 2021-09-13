84ba4a7cbce0e6dbaee661380942c45ce969f994
Matthias Viehweger <viehweger@puzzle.ch>
2021-09-13 15:37:38 +0200

    Change whitespace, indentation or line-breaks

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,33 +1,13 @@
 # frozen_string_literal: true
 
+#  Copyright (c) 2012-2021, CVP Schweiz. This file is part of
+#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 # == Schema Information
 #
 # Table name: messages
-#
-#  id                 :bigint           not null, primary key
-#  failed_count       :integer          default(0)
-#  heading            :boolean          default(FALSE)
-#  invoice_attributes :text(65535)
-#  recipient_count    :integer          default(0)
-#  salutation         :string(255)
-#  sent_at            :datetime
-#  state              :string(255)      default("draft")
-#  subject            :string(256)
-#  success_count      :integer          default(0)
-#  text               :text(65535)
-#  type               :string(255)      not null
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#  invoice_list_id    :bigint
-#  mailing_list_id    :bigint
-#  sender_id          :bigint
-#
-# Indexes
-#
-#  index_messages_on_invoice_list_id  (invoice_list_id)
-#  index_messages_on_mailing_list_id  (mailing_list_id)
-#  index_messages_on_sender_id        (sender_id)
-#
 
 #  id              :bigint           not null, primary key
 #  failed_count    :integer          default(0)
@@ -51,11 +31,6 @@
 #  index_messages_on_mailing_list_id  (mailing_list_id)
 #  index_messages_on_sender_id        (sender_id)
 #
-
-#  Copyright (c) 2012-2021, CVP Schweiz. This file is part of
-#  hitobito_cvp and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
 
 class Message < ActiveRecord::Base
   include I18nEnums

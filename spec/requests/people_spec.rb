ba679318f90d8cb326323bdd896d44139135571c
simfeld <sml.imfeld@gmail.com>
2021-09-26 02:24:17 +0200

    Add specs for new scopes

--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -8,13 +8,13 @@
 require "spec_helper"
 require "json"
 
-RSpec.describe 'GET /groups/:id', type: :request do
+RSpec.describe 'GET /groups/:group_id/people/:person_id', type: :request do
   let(:application) { Fabricate(:application) }
   let(:user)        { people(:bottom_member) }
 
   context 'without access token' do
     it 'redirects to sign_in' do
-      get "/groups/#{user.group_ids[0]}"
+      get "/groups/#{user.group_ids[0]}/people/#{user.id}"
       expect(response).to have_http_status(:found)
       expect(response.redirect_url).to eq('http://www.example.com/users/sign_in')
     end
@@ -24,7 +24,7 @@ RSpec.describe 'GET /groups/:id', type: :request do
     let(:token)  { Fabricate(:access_token, application: application, scopes: { scopes: 'email' }, resource_owner_id: user.id) }
 
     it 'redirects to sign_in' do
-      get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token + 'X'}
+      get "/groups/#{user.group_ids[0]}/people/#{user.id}", headers: { 'Authorization': 'Bearer ' + token.token + 'X'}
       expect(response).to have_http_status(:found)
       expect(response.redirect_url).to eq('http://www.example.com/users/sign_in')
     end
@@ -34,32 +34,32 @@ RSpec.describe 'GET /groups/:id', type: :request do
     let(:token)  { Fabricate(:access_token, application: application, resource_owner_id: user.id) }
 
     it 'fails with 403 (forbidden)' do
-      get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token }
+      get "/groups/#{user.group_ids[0]}/people/#{user.id}", headers: { 'Authorization': 'Bearer ' + token.token }
       expect(response).to have_http_status(:forbidden)
     end
   end
 
   context 'with an unacceptable scope in token' do
-    ['email', 'events', 'people'].each do |scope|
+    ['email', 'events', 'groups'].each do |scope|
       it "fails with 403 (forbidden) for #{scope} scope" do
         token = Fabricate(:access_token, application: application, scopes: { scopes: scope }, resource_owner_id: user.id)
-        get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token }
+        get "/groups/#{user.group_ids[0]}/people/#{user.id}", headers: { 'Authorization': 'Bearer ' + token.token }
         expect(response).to have_http_status(:forbidden)
       end
     end
   end
 
   context 'with an acceptable scope in token' do
-    ['api', 'groups'].each do |scope|
+    ['api', 'people'].each do |scope|
       it "succeeds for #{scope} scope" do
         token = Fabricate(:access_token, application: application, scopes: { scopes: scope }, resource_owner_id: user.id )
-        get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token, 'Accept': 'application/json' }
+        get "/groups/#{user.group_ids[0]}/people/#{user.id}", headers: { 'Authorization': 'Bearer ' + token.token, 'Accept': 'application/json' }
 
         expect(response).to be_successful
         expect(response.content_type).to eq('application/json; charset=utf-8')
-        group = JSON.parse(response.body)
-        expect(group["groups"].size).to eq(1)
-        expect(group["groups"][0]["name"]).to eq("Bottom One")
+        people = JSON.parse(response.body)
+        expect(people["people"].size).to eq(1)
+        expect(people["people"][0]["email"]).to eq("bottom_member@example.com")
       end
     end
   end
@@ -68,7 +68,7 @@ RSpec.describe 'GET /groups/:id', type: :request do
     let(:token)  { Fabricate(:access_token, application: application, scopes: { scopes: 'email' }, resource_owner_id: user.id, expires_in: -1.minute ) }
 
     it 'redirects to login' do
-      get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token }
+      get "/groups/#{user.group_ids[0]}/people/#{user.id}", headers: { 'Authorization': 'Bearer ' + token.token }
       is_expected.to redirect_to('http://www.example.com/users/sign_in')
     end
   end

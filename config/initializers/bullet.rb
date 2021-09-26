51d83dea248f1caa17309c88048f13db7588b178
Matthias Viehweger <viehweger@puzzle.ch>
2021-09-26 23:20:51 +0200

    Update more gems and add new migrations
    mostly to reduce deprecation warning and such

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -12,35 +12,35 @@ if defined? Bullet
   Bullet.rails_logger  = true
 
   # groups loaded for current user
-  Bullet.add_whitelist type: :unused_eager_loading, class_name: 'Person', association: :groups
-  Bullet.add_whitelist type: :unused_eager_loading, class_name: 'Person', association: :roles
+  Bullet.add_safelist type: :unused_eager_loading, class_name: 'Person', association: :groups
+  Bullet.add_safelist type: :unused_eager_loading, class_name: 'Person', association: :roles
 
   # When loading events via the polymorphic Person::AddRequest#body, it is not possible to include
   # :groups
-  Bullet.add_whitelist type: :n_plus_one_query, class_name: 'Event', association: :groups
-  Bullet.add_whitelist type: :n_plus_one_query, class_name: 'Event::Course', association: :groups
+  Bullet.add_safelist type: :n_plus_one_query, class_name: 'Event', association: :groups
+  Bullet.add_safelist type: :n_plus_one_query, class_name: 'Event::Course', association: :groups
 
   # EventKind may not be eager loaded if some event types have kind and others not.
-  Bullet.add_whitelist type: :n_plus_one_query, class_name: 'Event::Course', association: :kind
+  Bullet.add_safelist type: :n_plus_one_query, class_name: 'Event::Course', association: :kind
 
   # event, :person and roles for participation list
   [:application, :answers, :event, :person, :roles].each do |assoc|
-    Bullet.add_whitelist type: :unused_eager_loading,
-                         class_name: 'Event::Participation',
-                         association: assoc
+    Bullet.add_safelist type: :unused_eager_loading,
+      class_name: 'Event::Participation',
+      association: assoc
   end
 
-  Bullet.add_whitelist type: :unused_eager_loading,
-                       class_name: 'Event::Kind',
-                       association: :translations
+  Bullet.add_safelist type: :unused_eager_loading,
+    class_name: 'Event::Kind',
+    association: :translations
 
   # Event::Participation decorator informs marks participants with incomplete answers on
   # participations lists
-  Bullet.add_whitelist type: :unused_eager_loading,
-                       class_name: 'Event::Answer',
-                       association: :question
+  Bullet.add_safelist type: :unused_eager_loading,
+    class_name: 'Event::Answer',
+    association: :question
 
-  Bullet.add_whitelist type: :unused_eager_loading,
+  Bullet.add_safelist type: :unused_eager_loading,
     class_name: 'HelpText',
     association: :translations
 end

6ffdddda1b7651fc60c52ae338af690db8fa081a
Carlo Beltrame <carlo@beltra.me>
2021-09-27 16:09:41 +0200

    Use fabricators instead of fixtures to prevent disturbing other tests

--- spec/models/event/kind_category_spec.rb
+++ spec/models/event/kind_category_spec.rb
@@ -1,16 +1,6 @@
 # encoding: utf-8
-# == Schema Information
-#
-# Table name: event_kinds
-#
-#  id          :integer          not null, primary key
-#  created_at  :datetime
-#  updated_at  :datetime
-#  deleted_at  :datetime
-#  minimum_age :integer
-#
 
-#  Copyright (c) 2012-2014, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2021, Pfadiabteilung Wildert. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -19,9 +9,9 @@ require 'spec_helper'
 
 describe Event::KindCategory do
 
-  let!(:abk) { event_kind_categories(:abk) }
-  let!(:bsk) { event_kind_categories(:bsk) }
-  let!(:afk) { event_kind_categories(:afk) }
+  let!(:abk) { Fabricate(:event_kind_category, { order: 3, label: 'Aufbaukurse' }) }
+  let!(:bsk) { Fabricate(:event_kind_category, { order: 2, label: 'Basiskurse' }) }
+  let!(:afk) { Fabricate(:event_kind_category, { order: 1, label: 'Auffrischungskurse' }) }
 
   it 'orders categories by order column' do
     expect(Event::KindCategory.pluck(:order).to_a).to eq [1, 2, 3]

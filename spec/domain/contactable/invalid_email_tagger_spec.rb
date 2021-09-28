919dc9dfc31643a6e76617654a4ea56b18ea3842
Matthias Viehweger <viehweger@puzzle.ch>
2021-09-28 23:53:35 +0200

    Add failing spec for bug

--- spec/domain/contactable/invalid_email_tagger_spec.rb
+++ spec/domain/contactable/invalid_email_tagger_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2020, CVP Schweiz. This file is part of
+#  Copyright (c) 2012-2021, CVP Schweiz. This file is part of
 #  hitobito_cvp and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_cvp.
@@ -30,5 +30,20 @@ describe Contactable::InvalidEmailTagger do
       described_class.new(person, person.email, :additional).tag!
     end
     expect(person).to have(2).tags
+  end
+
+  it 'does not fail when a similar tag is already present' do
+    ActsAsTaggableOn::Tagging.find_or_create_by!(
+      taggable: person,
+      context: :tags,
+      tag: PersonTags::Validation.email_primary_invalid(create: true)
+    )
+
+    expect(person).to have(1).tag
+    expect(person.tags.first.name).to eq 'category_validation:email_primary_invalid'
+
+    expect do
+      described_class.new(person, person.email, :primary).tag!
+    end.to_not(change { person.tags.count })
   end
 end

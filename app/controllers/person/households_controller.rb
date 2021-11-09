0aa64382b8f03ae94f44220780a87dfad6b5078b
Severin Räz <raez@puzzle.ch>
2021-11-09 14:48:28 +0100

    Update copyrights

--- app/controllers/person/households_controller.rb
+++ app/controllers/person/households_controller.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2012-2018, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2012-2021, Pfadibewegung Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.

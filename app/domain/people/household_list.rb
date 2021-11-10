400fc217148400c0d34c88e475ac5964459700b0
Carlo Beltrame <carlo@beltra.me>
2021-11-10 14:30:39 +0100

    Fix HouseholdList so that it really takes all households into account
    Previously, we patched find_in_batches so that it would order by
    household member count descendingly. But that never really worked,
    because find_in_batches doesn't use SQL OFFSET internally, but rather
    "WHERE id > largest_id_of_previous_batch" in order to step through the
    batches. So here, we are forced to re-implement find_in_batches for our
    use case with some modifications.

--- app/domain/people/household_list.rb
+++ app/domain/people/household_list.rb
@@ -19,28 +19,23 @@ class People::HouseholdList
   def grouped_households
     people = Person.quoted_table_name
 
-    # remove previously added selects, very important to make this query scale
-    @people_scope.unscope(:select, :includes).
+    @people_scope.
+        # remove previously added selects, very important to make this query scale
+        unscope(:select, :includes).
         # group by household, but keep NULLs separate
         select("IFNULL(#{people}.`household_key`, #{people}.`id`) as `key`").
-        # Must select the primary key column because find_in_batches needs it for sorting.
-        select("MIN(#{Person.quoted_table_name}.`id`) as `id`").
-        select("COUNT(#{people}.`household_key`) as `count`").
-        group(:key)
+        group(:key).
+        # Primary sorting criterion
+        select("COUNT(#{people}.`household_key`) as `member_count`").
+        # Secondary, unique sorting criterion
+        select("MIN(#{people}.`id`) as `id`")
   end
 
   def households_in_batches(exclude_non_households: false)
     return unless block_given?
     base_scope = exclude_non_households ? only_households : grouped_households
 
-    # When limiting the number of rows, make sure to always show some households.
-    # For this, we override the order that find_in_batches uses. By default, it
-    # always orders by the primary key column ascendingly.
-    def base_scope.batch_order
-      [Arel::Nodes::SqlLiteral.new('`count`').desc, super]
-    end
-
-    base_scope.find_in_batches(batch_size: 300) do |batch|
+    in_batches(base_scope, batch_size: 300) do |batch|
       involved_people = fetch_people_with_id_or_household_key(batch.map(&:key))
       grouped_people = batch.map do |household|
         involved_people.select do |person|
@@ -75,6 +70,56 @@ class People::HouseholdList
     base_scope.where(household_key: keys_or_ids).
         or(base_scope.where(id: keys_or_ids)).
         load
+  end
+
+  # Copied and adapted from ActiveRecord::Batches#in_batches
+  # We need to order and "offset" the batches by two separate columns, which
+  # activerecord doesn't support natively. Activerecord only supports ordering by the
+  # primary key column and doesn't use SQL OFFSET internally, for performance reasons:
+  # https://github.com/rails/rails/pull/20933
+  def in_batches(base_scope, batch_size: 1000)
+    relation = base_scope
+
+    batch_limit = batch_size
+    if base_scope.limit_value
+      remaining   = base_scope.limit_value
+      batch_limit = remaining if remaining < batch_limit
+    end
+
+    relation = relation.reorder('`member_count` DESC, id ASC').limit(batch_limit)
+    # Retaining the results in the query cache would undermine the point of batching
+    relation.skip_query_cache!
+    batch_relation = relation
+
+    loop do
+      records = batch_relation.records
+      ids = records.map(&:id)
+      yielded_relation = base_scope.where(id: ids)
+      yielded_relation.send(:load_records, records)
+
+      break if ids.empty?
+
+      member_count_offset = records.last.member_count
+      id_offset = ids.last
+
+      yield yielded_relation
+
+      break if ids.length < batch_limit
+
+      if base_scope.limit_value
+        remaining -= ids.length
+
+        if remaining == 0
+          # Saves a useless iteration when the limit is a multiple of the batch size.
+          break
+        elsif remaining < batch_limit
+          relation = relation.limit(remaining)
+        end
+      end
+
+      batch_relation = relation.having('`member_count` < ? OR (`member_count` = ? AND `id` > ?)',
+                                       member_count_offset, member_count_offset, id_offset)
+    end
   end
 
 end

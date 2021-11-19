8461025f09140982e0b5a284cc62615f8e6164f5
Matthias Viehweger <viehweger@puzzle.ch>
2021-11-19 16:55:15 +0100

    Avoid loading the schema of application_records
    That table does not and will never exist. ApplicationRecord is just a
    central place for central methods.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,1 +1,10 @@
-class ApplicationRecord < ActiveRecord::Base; end
+# frozen_string_literal: true
+
+#  Copyright (c) 2021, Katholische Landjugendbewegung Paderborn. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+end

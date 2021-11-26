f6ea496e6330e45d92e4f1f91b18336756b22815
Matthias Viehweger <viehweger@puzzle.ch>
2021-11-26 11:58:38 +0100

    Reduce confusion in user by showing the translated bank-name

--- app/models/payment_provider_config.rb
+++ app/models/payment_provider_config.rb
@@ -24,8 +24,10 @@
 
 class PaymentProviderConfig < ActiveRecord::Base
   include Encryptable
+  include I18nEnums
 
   enum status: [:draft, :pending, :registered]
+  i18n_enum :payment_provider, Settings.payment_providers.map(&:name)
 
   belongs_to :invoice_config
 

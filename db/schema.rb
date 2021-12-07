a563d5b25b3c66ce5ee00c60de4b939c21b01339
Matthias Viehweger <viehweger@puzzle.ch>
2021-12-07 21:55:03 +0100

    Update schema.rb with latest migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_221149) do
+ActiveRecord::Schema.define(version: 2021_11_25_155817) do
 
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false

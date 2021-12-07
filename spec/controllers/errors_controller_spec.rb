5abc20d513cc7ce8b3f8fc46c084cbab3aed12db
Matthias Viehweger <viehweger@puzzle.ch>
2021-12-07 21:51:26 +0100

    Adapt spec to changed action-names

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,4 +1,6 @@
-#  Copyright (c) 2012-2019, Jungwacht Blauring Schweiz. This file is part of
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2021, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -11,7 +13,7 @@ describe ErrorsController do
 
   %w(404 500 503).each do |code|
     it "renders #{code} with correct view and status code" do
-      get :show, params: { code: code }
+      get :"show#{code}"
       expect(response).to render_template(code)
       expect(response.status).to eq code.to_i
     end
@@ -20,7 +22,7 @@ describe ErrorsController do
   describe 'Content-Type' do
     %w(html json).each do |format|
       it "renders #{format} for #{format} format" do
-        get :show, params: { code: '404' }, format: format.to_sym
+        get :show404, format: format.to_sym
         expect(response).to render_template('404')
         expect(response.status).to eq 404
         expect(response.content_type).to match(Regexp.new(format))
@@ -29,7 +31,7 @@ describe ErrorsController do
 
     %w(png jpeg).each do |format|
       it "renders html for #{format}" do
-        get :show, params: { code: '404' }, format: format.to_sym
+        get :show404, format: format.to_sym
         expect(response).to render_template('404')
         expect(response.status).to eq 404
         expect(response.content_type).to match(/html/)

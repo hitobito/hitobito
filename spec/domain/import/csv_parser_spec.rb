eae90ca8c9979009b99e7eccaeb4adaa8ff30267
Matthias Viehweger <viehweger@puzzle.ch>
2021-12-27 22:47:29 +0100

    Adapt to changed wording of stdlib-error

--- spec/domain/import/csv_parser_spec.rb
+++ spec/domain/import/csv_parser_spec.rb
@@ -79,7 +79,7 @@ describe Import::CsvParser do
 
   context 'error when parsing' do
     let(:data) { File.read(path(:utf8, :ods)) }
-    its(:error) { should eq 'Unquoted fields do not allow \\r or \\n (line 2).' }
+    its(:error) { should eq 'Unquoted fields do not allow new line <"\\n"> in line 2.' }
   end
 
   context 'mapping rows' do

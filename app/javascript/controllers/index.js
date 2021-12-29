52b3ff999def1acce58c2ee057719476a77b47e5
Carlo Beltrame <carlo@beltra.me>
2021-12-29 13:33:41 +0100

    Run bundle exec rails stimulus_reflex:install again

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,12 @@
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import StimulusReflex from 'stimulus_reflex'
+import consumer from '../channels/consumer'
+import controller from '../controllers/application_controller'
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+StimulusReflex.initialize(application, { consumer, controller, isolate: true })
+StimulusReflex.debug = process.env.RAILS_ENV === 'development'

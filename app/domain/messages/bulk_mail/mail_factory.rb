9accd34d27f36172716decdc432a897f63d139aa
Mountain Star <github@mtnstar.net>
2022-01-25 11:16:40 +0100

    Update licence headers, update doc

--- app/domain/messages/bulk_mail/mail_factory.rb
+++ app/domain/messages/bulk_mail/mail_factory.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+#  Copyright (c) 2022, Hitobito AG. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 module Messages
     module BulkMail
       class MailFactory

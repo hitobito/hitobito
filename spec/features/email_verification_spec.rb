2620967d104ba3a431738667218d0c1dcd21091c
Mountain Star <github@mtnstar.net>
2022-01-25 13:47:38 +0100

    Clean-up feature spec, satisfy rubocop

--- spec/features/email_verification_spec.rb
+++ spec/features/email_verification_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 #  Copyright (c) 2022, Pfadibewegung Schweiz. This file is part of
-#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
@@ -29,18 +29,18 @@ describe 'Email verification', js: true do
       click_link 'Neue Person erfassen'
       fill_in 'role[new_person][first_name]', with: 'Test'
       fill_in 'role[new_person][last_name]', with: 'User'
-      fill_in 'role[new_person][email]', with: 'someone@puzzle.ch'
+      fill_in 'role[new_person][email]', with: 'someone@example.com'
       first(:button, 'Speichern').click
       is_expected.to have_text('wurde erfolgreich erstellt')
-      is_expected.to have_field('Haupt-E-Mail', with: 'someone@puzzle.ch')
+      is_expected.to have_field('Haupt-E-Mail', with: 'someone@example.com')
     end
 
     it 'should not require confirmation when updating email' do
       visit edit_group_person_path(group_id: group.id, id: person.id)
-      fill_in 'E-Mail', with: 'test@puzzle.ch'
+      fill_in 'E-Mail', with: 'test@example.com'
       expect do
         first(:button, 'Speichern').click
-      end.to change { person.reload.email }.to('test@puzzle.ch')
+      end.to change { person.reload.email }.to('test@example.com')
       is_expected.not_to have_text('E-Mail-Adresse muss noch bestätigt werden')
     end
   end
@@ -57,7 +57,7 @@ describe 'Email verification', js: true do
 
     it 'should send confirmation email when updating email' do
       visit edit_group_person_path(group_id: group.id, id: person.id)
-      fill_in 'E-Mail', with: 'test@puzzle.ch'
+      fill_in 'E-Mail', with: 'test@example.com'
       expect do
         first(:button, 'Speichern').click
       end.not_to change { person.reload.email }
@@ -93,23 +93,23 @@ describe 'Email verification', js: true do
 
     it 'should auto-confirm email' do
       link = click_send_login_button(person, me)
-      expect {
+      expect do
         visit link
         fill_in 'Neues Passwort', with: password
         fill_in 'Neues Passwort bestätigen', with: password
         click_button 'Passwort ändern'
-      }.to change { person.reload.confirmed_at }.from(nil)
+      end.to change { person.reload.confirmed_at }.from(nil)
     end
 
     it 'should not auto-confirm email which has changed in the meantime' do
       link = click_send_login_button(person, me)
-      person.update(email: 'changed-email@puzzle.ch')
-      expect {
+      person.update!(email: 'changed-email@example.com')
+      expect do
         visit link
         fill_in 'Neues Passwort', with: password
         fill_in 'Neues Passwort bestätigen', with: password
         click_button 'Passwort ändern'
-      }.not_to change { person.reload.confirmed_at }.from(nil)
+      end.not_to change { person.reload.confirmed_at }.from(nil)
     end
   end
 
@@ -121,13 +121,13 @@ describe 'Email verification', js: true do
     end
 
     it 'should auto-confirm email' do
-      expect {
+      expect do
         token = person.generate_reset_password_token!
         visit edit_person_password_path(reset_password_token: token)
         fill_in 'Neues Passwort', with: password
         fill_in 'Neues Passwort bestätigen', with: password
         click_button 'Passwort ändern'
-      }.to change { person.reload.confirmed_at }.from(nil)
+      end.to change { person.reload.confirmed_at }.from(nil)
     end
 
     # Prevents the following scenario:
@@ -137,14 +137,14 @@ describe 'Email verification', js: true do
     # 4. The email change is effective immediately because no previous email was ever confirmed
     # 5. Attacker clicks the link from the password reset email and changes password. The new email must not be auto-confirmed.
     it 'should not auto-confirm email which has changed in the meantime' do
-      expect {
+      expect do
         token = person.generate_reset_password_token!
-        person.update(email: 'other-email@puzzle.ch')
+        person.update!(email: 'other-email@example.com')
         visit edit_person_password_path(reset_password_token: token)
         fill_in 'Neues Passwort', with: password
         fill_in 'Neues Passwort bestätigen', with: password
         click_button 'Passwort ändern'
-      }.not_to change { person.reload.confirmed_at }.from(nil)
+      end.not_to change { person.reload.confirmed_at }.from(nil)
     end
   end
 
@@ -152,43 +152,47 @@ describe 'Email verification', js: true do
     let(:person) do
       person = people(:bottom_member)
       person.confirm
-      person.update(email: 'newmail@puzzle.ch')
+      person.update!(email: 'newmail@example.com')
       person.reload
     end
 
     # Prevents the following scenario:
     # 1. Attacker has a verified email address which they control
-    # 2. Attacher changes email to an address which they don't own. Change is postponed, reconfirmation is pending
+    # 2. Attacher changes email to an address which they don't own.
+    #    Change is postponed, reconfirmation is pending
     # 3. Reconfirmation email is sent, but the attacker cannot receive it
     # 4. Attacker requests a password reset email, which is still sent to the old confirmed email
     # 5. Attacker changes password. The new email address must not be auto-confirmed.
     it 'should not auto-confirm email for security reasons' do
-      expect {
+      expect do
         token = person.generate_reset_password_token!
         visit edit_person_password_path(reset_password_token: token)
         fill_in 'Neues Passwort', with: password
         fill_in 'Neues Passwort bestätigen', with: password
         click_button 'Passwort ändern'
-      }.not_to change { person.reload.email }
+      end.not_to change { person.reload.email }
     end
 
     # Prevents the following scenario:
     # 1. Attacker has a verified email address which they control
-    # 2. Attacker changes email to an address which they own. Change is postponed, reconfirmation is pending
+    # 2. Attacker changes email to an address which they own. Change is
+    #    postponed, reconfirmation is pending
     # 3. Attacker receives confirmation email, but doesn't click the link
     # 4. Attacker requests a password reset email, which is still sent to the old confirmed email
-    # 5. Attacker changes email to an address which they don't own. Change is postponed, reconfirmation is pending
+    # 5. Attacker changes email to an address which they don't own. Change is
+    #    postponed, reconfirmation is pending
     # 6. Reconfirmation email is sent, but the attacker cannot receive it
-    # 7. Attacker clicks the link from the password reset email and changes password. The new email must not be auto-confirmed.
+    # 7. Attacker clicks the link from the password reset email and changes password.
+    #    The new email must not be auto-confirmed.
     it 'should not auto-confirm email which has changed again in the meantime' do
-      expect {
+      expect do
         token = person.generate_reset_password_token!
-        person.update(email: 'other-email@puzzle.ch')
+        person.update(email: 'other-email@example.com')
         visit edit_person_password_path(reset_password_token: token)
         fill_in 'Neues Passwort', with: password
         fill_in 'Neues Passwort bestätigen', with: password
         click_button 'Passwort ändern'
-      }.not_to change { person.reload.email }
+      end.not_to change { person.reload.email }
     end
   end
 
@@ -196,24 +200,24 @@ describe 'Email verification', js: true do
     let(:event) { events(:top_event) }
 
     before do
-      event.update(external_applications: true, )
+      event.update!(external_applications: true)
     end
 
     it 'should not confirm email' do
       visit register_group_event_path(group_id: event.groups.first.id, id: event.id)
-      first('#new_person #person_email').fill_in with: 'newguy@puzzle.ch'
+      first('#new_person #person_email').fill_in with: 'newguy@example.com'
       click_button 'Weiter'
       is_expected.to have_text 'Bitte fülle das folgende Formular aus, bevor du dich für den Anlass anmeldest.'
 
       fill_in 'Vorname', with: 'New'
       fill_in 'Nachname', with: 'Guy'
-      fill_in 'Haupt-E-Mail', with: 'newguy@puzzle.ch'
+      fill_in 'Haupt-E-Mail', with: 'newguy@example.com'
       first(:button, 'Speichern').click
       is_expected.to have_text 'Deine persönlichen Daten wurden aufgenommen.'
 
       click_button 'Anmelden'
-      is_expected.to have_text 'newguy@puzzle.ch'
-      expect(Person.find_by(email: 'newguy@puzzle.ch').confirmed?).to be_falsey
+      is_expected.to have_text 'newguy@example.com'
+      expect(Person.find_by(email: 'newguy@example.com').confirmed?).to be_falsey
     end
   end
 
@@ -228,12 +232,12 @@ describe 'Email verification', js: true do
       visit group_self_registration_path(group_id: group.id)
       fill_in 'Vorname', with: 'New'
       fill_in 'Nachname', with: 'Guy'
-      fill_in 'Haupt-E-Mail', with: 'newguy@puzzle.ch'
+      fill_in 'Haupt-E-Mail', with: 'newguy@example.com'
       first(:button, 'Speichern').click
       is_expected.to have_text 'Du hast Dich erfolgreich registriert. Du erhältst in Kürze eine E-Mail mit der Anleitung, wie Du Deinen Account freischalten kannst.'
-      expect(Person.find_by(email: 'newguy@puzzle.ch').confirmed?).to be_falsey
+      expect(Person.find_by(email: 'newguy@example.com').confirmed?).to be_falsey
 
-      mail = find_mail_to('newguy@puzzle.ch')
+      mail = find_mail_to('newguy@example.com')
       expect(mail.subject).to eq('Anleitung für das Setzen Deines Passworts')
     end
   end

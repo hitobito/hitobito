f451ed7fbccce390bc0ce2c5d4818b263c3d57fc
Mountain Star <github@mtnstar.net>
2022-01-25 11:16:40 +0100

    Update licence headers, update doc

--- app/domain/mailing_lists/bulk_mail/imap_mail_validator.rb
+++ app/domain/mailing_lists/bulk_mail/imap_mail_validator.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2012-2021, Hitobito AG. This file is part of
-# hitobito and licensed under the Affero General Public License version 3
-# or later. See the COPYING file at the top-level directory or at
-# https://github.com/hitobito/hitobito.
+#  Copyright (c) 2022, Hitobito AG. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
 
 module MailingLists::BulkMail
   class ImapMailValidator

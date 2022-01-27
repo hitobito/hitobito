a50d02fdc71f1881526bbd4c0bb14ef4f20ee723
Mountain Star <github@mtnstar.net>
2022-01-27 19:09:53 +0100

    Update state after sending bulk mails

--- app/domain/messages/bulk_mail_dispatch.rb
+++ app/domain/messages/bulk_mail_dispatch.rb
@@ -7,7 +7,7 @@
 
 module Messages
   class BulkMailDispatch
-    delegate :update, :success_count, to: '@message'
+    delegate :update, :success_count, :message_recipients, to: '@message'
 
     DELIVERY_RETRIES = 2
     BULK_SIZE = Settings.email.bulk_mail.bulk_size
@@ -20,17 +20,25 @@ module Messages
     end
 
     def run
-      if recipients_generated?(@message)
-        recipients = pending_recipients(@message).limit(BULK_SIZE)
+      check_message_state
+
+      if recipients_generated?
+        recipients = recipients_by(state: :pending).limit(BULK_SIZE)
         deliver_mails(@mail_factory, recipients, DELIVERY_RETRIES)
       else
         BulkMail::CreateRecipients.new.create!(@message)
       end
 
-      reschedule_unless_none_pending(@message)
+      reschedule_unless_none_pending
     end
 
     private
+
+    def check_message_state
+      if @message.pending?
+        @message.update!(state: 'processing')
+      end
+    end
 
     def deliver_mails(mail_factory, recipients, retries)
       delivery = BulkMail::Delivery.new(mail_factory, recipients.map(&:email), retries)
@@ -39,41 +47,61 @@ module Messages
         delivery.deliver
         succeeded, failed = recipient_ids(recipients, [delivery.succeeded, delivery.failed])
 
-        Rails.logger.info "Sent mails, #{succeeded.length} OK, #{failed.length} failed."
+        log "Sent mails, #{succeeded.length} OK, #{failed.length} failed."
 
-        MessageRecipient.where(id: succeeded).update_all(state: :sent)
-        MessageRecipient.where(id: failed).update_all(state: :failed)
+        message_recipients.where(id: succeeded).update_all(state: :sent)
+        message_recipients.where(id: failed).update_all(state: :failed)
       rescue BulkMail::Delivery::RetriesExceeded => e
-        # TODO fail cleanly
-        raise e
+        message_recipients.where.not(state: :sent).update_all(state: :failed, error: 'SMTP server error')
+        @message.update!(state: 'failed')
+        log 'SMTP server error, BulkMailDispatch aborted.'
       end
     end
 
-    def reschedule_unless_none_pending(message)
-      if pending = pending_recipient_count(message) > 0
-        Rails.logger.info "#{pending} recipients pending, rescheduling dispatch job."
+    def reschedule_unless_none_pending
+      update_message_counts
+
+      if pending_recipient_count > 0
+        log "#{pending_recipient_count} recipients pending, rescheduling dispatch job."
         DispatchResult.needs_reenqueue
       else
-        Rails.logger.info "All mails sent, BulkMailDispatch complete."
-        DispatchResult.finished
+        finish_dispatch
       end
     end
 
-    def recipients_generated?(message)
-      message.message_recipients.exists?
+    def finish_dispatch
+      unless @message.failed?
+        log 'All mails sent, BulkMailDispatch complete.'
+        @message.update!(state: 'finished', raw_source: nil)
+      end
+      DispatchResult.finished
     end
 
-    def pending_recipients(message)
-      message.message_recipients.where(state: :pending)
+    def update_message_counts
+      success_count = recipients_by(state: :sent).count
+      failed_count = recipients_by(state: :failed).count
+      @message.update!(success_count: success_count, failed_count: failed_count)
     end
 
-    def pending_recipient_count(message)
-      pending_recipients(message).count
+    def recipients_generated?
+      message_recipients.exists?
     end
 
-    def recipient_ids(recipients, email_groups)
+    def recipients_by(state:)
+      message_recipients.where(state: state)
+    end
+
+    def pending_recipient_count
+      recipients_by(state: :pending).count
+    end
+
+    def recipient_ids(recipients, emails_by_delivery_status)
       by_email = recipients.index_by(&:email)
-      email_groups.map { |emails| by_email.values_at(*emails).map(&:id) }
+      emails_by_delivery_status.map { |emails| by_email.values_at(*emails).map(&:id) }
+    end
+
+    def log(info)
+      Rails.logger.info info
     end
   end
 end

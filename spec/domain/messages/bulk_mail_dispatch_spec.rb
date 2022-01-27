4ff235003c67a53aede84b607d542e8b1d473f1e
Mountain Star <github@mtnstar.net>
2022-01-27 19:09:53 +0100

    Update state after sending bulk mails

--- spec/domain/messages/bulk_mail_dispatch_spec.rb
+++ spec/domain/messages/bulk_mail_dispatch_spec.rb
@@ -68,38 +68,43 @@ describe Messages::BulkMailDispatch do
     end
 
     context 'with recipients' do
-      let(:successful_address) { 'success@example.com' }
-      let(:failed_address) { 'fail@example.com' }
-      let!(:successful_recipient) { MessageRecipient.new(message_id: message.id, person_id: top_leader.id, state: :pending, email: successful_address) }
-      let!(:failed_recipient) { MessageRecipient.new(message_id: message.id, person_id: top_leader.id, state: :pending, email: failed_address) }
-
-      before do
-        message.update(message_recipients: [successful_recipient, failed_recipient])
-      end
+      let(:recipient1_email) { 'recipient1@example.com' }
+      let(:recipient2_email) { 'recipient2@example.com' }
+      let!(:recipient1) { MessageRecipient.create!(message_id: message.id, person_id: top_leader.id, state: :pending, email: recipient1_email) }
+      let!(:recipient2) { MessageRecipient.create!(message_id: message.id, person_id: top_leader.id, state: :pending, email: recipient2_email) }
+      let(:delivery) { double }
 
       def setup_delivery
-        delivery = double
         expect(Messages::BulkMail::Delivery).to receive(:new)
           .with(
             mail_factory,
-           [successful_address, failed_address],
+           [recipient1_email, recipient2_email],
             Messages::BulkMailDispatch::DELIVERY_RETRIES)
           .and_return(delivery)
 
         expect(delivery).to receive(:deliver)
-        expect(delivery).to receive(:succeeded).and_return([successful_address])
-        expect(delivery).to receive(:failed).and_return([failed_address])
+        expect(delivery).to receive(:succeeded).and_return([recipient1_email])
+        expect(delivery).to receive(:failed).and_return([recipient2_email])
       end
 
       it 'persists results' do
         setup_delivery
 
+        expect(message).to receive(:update!).with(state: 'processing').and_call_original
+        expect(message).to receive(:update!).with(state: 'finished', raw_source: nil).and_call_original
+        expect(message).to receive(:update!)
+          .with(failed_count: 1, success_count: 1).and_call_original
+
         result = dispatch.run
+        expect(message.reload).to be_finished
         expect(result.needs_reenqueue?).to be_falsey
 
-        [successful_recipient, failed_recipient].each(&:reload)
-        expect(successful_recipient.state).to eq("sent")
-        expect(failed_recipient.state).to eq("failed")
+        [recipient1, recipient2].each(&:reload)
+        expect(recipient1.state).to eq('sent')
+        expect(recipient2.state).to eq('failed')
+
+        expect(message.success_count).to eq(1)
+        expect(message.failed_count).to eq(1)
       end
 
       it 'does not create any more recipient entries if already present' do
@@ -108,6 +113,57 @@ describe Messages::BulkMailDispatch do
         expect do
           dispatch.run
         end.not_to change { MessageRecipient.count }
+      end
+
+      it 'deletes mail raw source after delivery to save storage' do
+        setup_delivery
+
+        dispatch.run
+
+        expect(message.reload.raw_source).to be_nil
+      end
+
+      context 'on smtp server error' do
+
+        before do
+          expect(Messages::BulkMail::Delivery).to receive(:new)
+            .with(
+              mail_factory,
+              [recipient1_email, recipient2_email],
+              Messages::BulkMailDispatch::DELIVERY_RETRIES)
+            .and_return(delivery)
+
+          expect(delivery).to receive(:deliver).and_raise(Messages::BulkMail::Delivery::RetriesExceeded)
+        end
+
+        it 'marks message and recipient entries as failed' do
+          result = dispatch.run
+          expect(result.needs_reenqueue?).to be_falsey
+
+          expect(message.reload).to be_failed
+          expect(message.success_count).to eq(0)
+          expect(message.failed_count).to eq(2)
+          # keep mail source for analysis
+          expect(message.raw_source).to be_present
+
+          [recipient1, recipient2].each do |r|
+            r.reload
+            expect(r.state).to eq('failed')
+            expect(r.error).to eq('SMTP server error')
+          end
+        end
+
+        it 'marks message and recipient entries as failed but keeps already sent' do
+          MessageRecipient.create!(message_id: message.id, person_id: top_leader.id, state: :sent, email: 'recipient3@example.com')
+
+          result = dispatch.run
+          expect(result.needs_reenqueue?).to be_falsey
+
+          expect(message.reload).to be_failed
+          expect(message.success_count).to eq(1)
+          expect(message.failed_count).to eq(2)
+        end
+
       end
     end
   end

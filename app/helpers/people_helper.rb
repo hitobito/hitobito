59099ea6e2e7fccec1d6676e8375aff0b58626c1
Nils Rauch <rauch@puzzle.ch>
2022-03-03 14:15:06 +0100

    Insert license

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2019, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2022, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.

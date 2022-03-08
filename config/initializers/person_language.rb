4e50671e2073b7ce98556c1ca404a3f9b271dcf8
Mountain Star <github@mtnstar.net>
2022-03-08 16:11:03 +0100

    Try to fix table does not exist error

--- config/initializers/person_language.rb
+++ config/initializers/person_language.rb
@@ -1,3 +1,7 @@
 Rails.application.config.after_initialize do
-  Person::PUBLIC_ATTRS += [:language] if FeatureGate.enabled?(:person_language)
+  # some rake tasks do not load db, so just extend
+  # public attrs if db is loaded
+  if ActiveRecord::Base.connection.table_exists?('people')
+    Person::PUBLIC_ATTRS += [:language] if FeatureGate.enabled?(:person_language)
+  end
 end

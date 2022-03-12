49d6c947a9fdba2b6836279d53b5a0f40f162e43
Carlo Beltrame <carlo@beltra.me>
2022-03-12 23:33:23 +0100

    Fix permissions for non-root user

--- app/abilities/ability.rb
+++ app/abilities/ability.rb
@@ -14,6 +14,7 @@ class Ability
   @@store = AbilityDsl::Store.new
 
   store.register AssignmentAbility,
+                 CalendarAbility,
                  EventAbility,
                  Event::ApplicationAbility,
                  Event::InvitationAbility,

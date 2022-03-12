a1598f885e1e18ad87c86bc7e9f1b45c1ab851d8
simfeld <sml.imfeld@gmail.com>
2022-03-12 23:48:17 +0100

    Add events spec

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2020-2021, Aargauer OL-Verband. This file is part of
+#  Copyright (c) 2021, Pfadibewegung Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -8,13 +8,15 @@
 require "spec_helper"
 require "json"
 
-RSpec.describe 'GET /groups/:id', type: :request do
+describe 'GET /groups/:group_id/events/:event_id', type: :request do
   let(:application) { Fabricate(:application) }
+  let(:event)       { events(:top_course) }
   let(:user)        { people(:bottom_member) }
+  let(:event_url)   { "/groups/#{event.group_ids[0]}/events/#{event.id}" }
 
   context 'without access token' do
     it 'redirects to sign_in' do
-      get "/groups/#{user.group_ids[0]}"
+      get event_url
       expect(response).to have_http_status(:found)
       expect(response.redirect_url).to eq('http://www.example.com/users/sign_in')
     end
@@ -24,7 +26,7 @@ RSpec.describe 'GET /groups/:id', type: :request do
     let(:token) { Fabricate(:access_token, application: application, scopes: { scopes: 'email' }, resource_owner_id: user.id) }
 
     it 'redirects to sign_in' do
-      get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token + 'X'}
+      get event_url, headers: { 'Authorization': 'Bearer ' + token.token + 'X'}
       expect(response).to have_http_status(:found)
       expect(response.redirect_url).to eq('http://www.example.com/users/sign_in')
     end
@@ -34,32 +36,32 @@ RSpec.describe 'GET /groups/:id', type: :request do
     let(:token) { Fabricate(:access_token, application: application, resource_owner_id: user.id) }
 
     it 'fails with 403 (forbidden)' do
-      get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token }
+      get event_url, headers: { 'Authorization': 'Bearer ' + token.token }
       expect(response).to have_http_status(:forbidden)
     end
   end
 
   context 'with an unacceptable scope in token' do
-    ['email', 'events', 'people'].each do |scope|
+    ['email', 'people', 'groups'].each do |scope|
       it "fails for #{scope} scope" do
         token = Fabricate(:access_token, application: application, scopes: { scopes: scope }, resource_owner_id: user.id)
-        get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token }
+        get event_url, headers: { 'Authorization': 'Bearer ' + token.token }
         expect(response).not_to have_http_status(:success)
       end
     end
   end
 
   context 'with an acceptable scope in token' do
-    ['api', 'groups'].each do |scope|
+    ['api', 'events'].each do |scope|
       it "succeeds for #{scope} scope" do
         token = Fabricate(:access_token, application: application, scopes: { scopes: scope }, resource_owner_id: user.id )
-        get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token, 'Accept': 'application/json' }
+        get event_url, headers: { 'Authorization': 'Bearer ' + token.token, 'Accept': 'application/json' }
 
         expect(response).to be_successful
         expect(response.content_type).to eq('application/json; charset=utf-8')
-        group = JSON.parse(response.body)
-        expect(group["groups"].size).to eq(1)
-        expect(group["groups"][0]["name"]).to eq("Bottom One")
+        events = JSON.parse(response.body)
+        expect(events["events"].size).to eq(1)
+        expect(events["events"][0]["name"]).to eq(event.name)
       end
     end
   end
@@ -68,7 +70,7 @@ RSpec.describe 'GET /groups/:id', type: :request do
     let(:token) { Fabricate(:access_token, application: application, scopes: { scopes: 'email' }, resource_owner_id: user.id, expires_in: -1.minute ) }
 
     it 'redirects to login' do
-      get "/groups/#{user.group_ids[0]}", headers: { 'Authorization': 'Bearer ' + token.token }
+      get event_url, headers: { 'Authorization': 'Bearer ' + token.token }
       is_expected.to redirect_to('http://www.example.com/users/sign_in')
     end
   end

307cdb54b4b4636a79fc55393d3bc70463e2ab99
Mountain Star <github@mtnstar.net>
2022-05-09 17:21:08 +0200

    Implement delivery report job

--- app/jobs/mailing_lists/bulk_mail/sender_rejected_message_job.rb
+++ app/jobs/mailing_lists/bulk_mail/sender_rejected_message_job.rb
@@ -5,62 +5,36 @@
 # or later. See the COPYING file at the top-level directory or at
 # https://github.com/hitobito/hitobito.
 
-class MailingLists::BulkMail::SenderRejectedMessageJob < BaseJob
+module MailingLists
+  module BulkMail
+    class SenderRejectedMessageJob < BaseMessageJob
 
-  EMAIL_LOCAL_PART_BRACKETS = /^.*<(.+)@.+\..+>$/.freeze
-  EMAIL_LOCAL_PART = /^(.+)@.+\..+$/.freeze
+      def perform
+        send(reply_message)
+      end
 
-  self.parameters = [:message]
+      private
 
-  def initialize(message)
-    super()
-    @message = message
-  end
+      def send(mail)
+        if defined?(ActionMailer::Base)
+          ActionMailer::Base.wrap_delivery_behavior(mail)
+        end
 
-  def perform
-    send(reply_message)
-  end
+        # set Return-Path as recommended in: https://stackoverflow.com/a/154794
+        mail.header['Return-Path'] = '<>'
+        mail.deliver
+        @message.update!(raw_source: nil)
+      end
 
-  private
+      def reply_message
+        list = list_address
+        from = no_reply_address
+        source_mail.reply do
+          body "Du bist leider nicht berechtigt auf die Liste #{list} zu schreiben."
+          from from
+        end
+      end
 
-  def send(mail)
-    if defined?(ActionMailer::Base)
-      ActionMailer::Base.wrap_delivery_behavior(mail)
     end
-
-    # set Return-Path as recommended in: https://stackoverflow.com/a/154794
-    mail.header['Return-Path'] = '<>'
-    mail.deliver
-    @message.update!(raw_source: nil)
-  end
-
-  def reply_message
-    list = list_address
-    from = no_reply_address
-    source_mail.reply do
-      body "Du bist leider nicht berechtigt auf die Liste #{list} zu schreiben."
-      from from
-    end
-  end
-
-  def no_reply_address
-    app_sender = Settings.email.sender
-    sender =
-      app_sender[EMAIL_LOCAL_PART_BRACKETS, 1] ||
-      app_sender[EMAIL_LOCAL_PART, 1] ||
-      'noreply'
-    "#{sender}@#{app_sender_domain}"
-  end
-
-  def app_sender_domain
-    Settings.email.list_domain || 'localhost'
-  end
-
-  def source_mail
-    Mail.new(@message.raw_source)
-  end
-
-  def list_address
-    @message.mailing_list.mail_address
   end
 end

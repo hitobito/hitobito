79924e89014e83c8ad93c971f64a397fee2ae234
Carlo Beltrame <carlo@beltra.me>
2022-05-13 13:52:07 +0200

    Add ShowFullColumn for displaying more restricted columns

---
+++ app/helpers/table_displays/show_full_column.rb
@@ -0,0 +1,13 @@
+#  Copyright (c) 2022, Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+module TableDisplays
+  class ShowFullColumn < PublicColumn
+
+    def required_permission(attr)
+      :show_full
+    end
+  end
+end

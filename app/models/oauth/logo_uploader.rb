65615c9a5fb7522a3e90ecbc091a6623e01b857f
Matthias Viehweger <viehweger@puzzle.ch>
2022-05-31 00:41:42 +0200

    Use a present default-image

--- app/models/oauth/logo_uploader.rb
+++ app/models/oauth/logo_uploader.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2021, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2021-2022, Pfadibewegung Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -29,7 +29,7 @@ class Oauth::LogoUploader < Uploader::Base
   end
 
   def png_name
-    ['oauth_app', version_name].compact.join('_') + '.png'
+    'oauth_app.png'
   end
 
   private

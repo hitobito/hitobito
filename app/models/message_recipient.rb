1ce045c900142e585c24478e48c805f602a1e9ef
Matthias Viehweger <viehweger@puzzle.ch>
2022-06-03 16:50:26 +0200

    Update schema after migrations have run

--- app/models/message_recipient.rb
+++ app/models/message_recipient.rb
@@ -1,30 +1,6 @@
 # frozen_string_literal: true
 
-# == Schema Information
-#
-# Table name: message_recipients
-#
-#  id           :bigint           not null, primary key
-#  address      :string(255)
-#  email        :string(255)
-#  error        :text(65535)
-#  failed_at    :datetime
-#  phone_number :string(255)
-#  state        :string(255)
-#  created_at   :datetime
-#  invoice_id   :bigint
-#  message_id   :bigint           not null
-#  person_id    :bigint           not null
-#
-# Indexes
-#
-#  index_message_recipients_on_invoice_id  (invoice_id)
-#  index_message_recipients_on_message_id  (message_id)
-#  index_message_recipients_on_person_id   (person_id)
-#
-
-
-#  Copyright (c) 2012-2021, CVP Schweiz. This file is part of
+#  Copyright (c) 2012-2022, CVP Schweiz. This file is part of
 #  hitobito_cvp and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.

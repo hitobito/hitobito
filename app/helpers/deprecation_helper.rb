803b9c078199ecfe70ecfa76246303eb52f51b05
Matthias Viehweger <viehweger@puzzle.ch>
2022-08-09 13:39:08 +0200

    Notify when old code is used

---
+++ app/helpers/deprecation_helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2022-2022, Puzzle ITC GmbH. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+module DeprecationHelper
+  def deprecated_action
+    send_deprecation("#{controller_path}##{action_name} was called unexpectedly.")
+  end
+
+  private
+
+  def send_deprecation(err)
+    Raven.capture_exception(ActiveSupport::DeprecationException.new(err), logger: 'deprecation')
+  end
+end

973db829ea301df08520fcfc2bdb8c96608693a3
Nils Rauch <rauch@puzzle.ch>
2022-09-26 12:37:14 +0200

    Conform to linter and styleguide

--- app/domain/event/filter.rb
+++ app/domain/event/filter.rb
@@ -21,7 +21,8 @@ class Event::Filter
   end
 
   def scope
-    # This must run as an explicite separate query. If you merge this in the following relation, activerecord+kaminari
+    # This must run as an explicite separate query.
+    # If you merge this in the following relation, activerecord+kaminari
     # will mess up the queries (pagination is run on the wrong query).
     event_ids_for_relevant_groups = Event.with_group_id(relevant_group_ids).pluck(:id)
 

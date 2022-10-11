539034a28b511162083e1fb280dbb14f967b35de
Nils Rauch <nils.rauch@gmail.com>
2022-10-11 07:47:32 +0200

    Add license

--- app/helpers/invoice_items_helper.rb
+++ app/helpers/invoice_items_helper.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
 
+#  Copyright (c) 2022, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sjas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sbv.
 
 module InvoiceItemsHelper
 

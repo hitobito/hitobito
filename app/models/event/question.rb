e6de2703088a9b168b3224cfa501057670979296
Daniel Illi-Zuberbühler <daniel-illi@users.noreply.github.com>
2022-10-12 07:01:42 +0200

    Validate event question text (#1882)

--- app/models/event/question.rb
+++ app/models/event/question.rb
@@ -31,19 +31,33 @@ class Event::Question < ActiveRecord::Base
 
   validates_by_schema
 
+  # validate question presence for admin/non-admin questions separately
+  # so we can have different error messages
+  validates :question, presence: { message: :admin_blank }, if: :admin?
+  validates :question, presence: { message: :application_blank }, unless: :admin?
+
   after_create :add_answer_to_participations
 
   scope :global, -> { where(event_id: nil) }
   scope :application, -> { where(admin: false) }
   scope :admin, -> { where(admin: true) }
 
+  def required_attributes
+    [
+      # needed for the required attribute mark in forms
+      # as the relevant validation is conditional
+      'question'
+    ]
+  end
 
   def choice_items
     choices.to_s.split(',').collect(&:strip)
   end
 
   def label
-    question.truncate(30)
+    # use safe navigation so not to break records missing the
+    # question text created before validation was added
+    question&.truncate(30)
   end
 
   def one_answer_available?

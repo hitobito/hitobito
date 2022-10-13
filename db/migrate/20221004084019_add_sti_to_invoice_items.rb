15f07ab2ad036a69385c8feb2eeed497a7db15ff
Nils Rauch <rauch@puzzle.ch>
2022-10-13 07:48:03 +0200

    Use sti_name instead of hardcoded strings

--- db/migrate/20221004084019_add_sti_to_invoice_items.rb
+++ db/migrate/20221004084019_add_sti_to_invoice_items.rb
@@ -7,12 +7,12 @@
 
 class AddStiToInvoiceItems < ActiveRecord::Migration[6.1]
   def up
-    add_column :invoice_items, :type, :string, null: false, default: 'InvoiceItem'
+    add_column :invoice_items, :type, :string, null: false, default: InvoiceItem.sti_name
     add_column :invoice_items, :cost, :decimal, precision: 12, scale: 2, null: true
     add_column :invoice_items, :dynamic_cost_parameters, :text
 
-    InvoiceItem.update_all(type: 'InvoiceItem')
-    InvoiceItem.where(variable_donation: true).update_all(type: 'InvoiceItem::VariableDonation') if defined? InvoiceItem::VariableDonation
+    InvoiceItem.update_all(type: InvoiceItem.sti_name)
+    InvoiceItem.where(variable_donation: true).update_all(type: InvoiceItem::VariableDonation.sti_name) if defined? InvoiceItem::VariableDonation
 
     remove_column :invoice_items, :variable_donation
   end

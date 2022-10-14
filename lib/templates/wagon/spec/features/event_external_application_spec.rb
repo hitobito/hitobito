f9eb2e648a5b07c1b89f546267c38c18ace3b77a
Nils Rauch <nils.rauch@gmail.com>
2022-10-14 11:29:12 +0200

    Add specs and required fixtures to wagon template

---
+++ lib/templates/wagon/spec/features/event_external_application_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe :event_external_application do
+
+  subject { page }
+
+  let(:person) { people(:sekretaer) } # TODO define after copy
+  let(:event) { Fabricate(:event, application_opening_at: 5.days.ago,
+                          external_applications: true, groups: [group]) }
+  let(:group) { person.roles.first.group }
+
+  it 'creates an external event participation' do
+    visit group_public_event_path(group_id: group, id: event)
+
+    find_all('#new_person input#person_email').first.fill_in(with: 'max.muster@hitobito.example.com')
+
+    click_button('Weiter')
+
+    fill_in 'Vorname', with: 'Max'
+    fill_in 'Nachname', with: 'Muster'
+    fill_in 'Haupt-E-Mail', with: 'max.muster@hitobito.example.com'
+
+    expect do
+      find_all('.btn-toolbar.bottom .btn-group button[type="submit"]').first.click # submit
+    end.to change { Person.count }.by(1)
+
+    fill_in('Bemerkungen', with: 'Important infos about my participation')
+
+    expect do
+      click_button('Anmelden')
+    end.to change { Event::Participation.count }.by(1)
+
+    person = Person.find_by(email: 'max.muster@hitobito.example.com')
+    expect(person).to be_present
+
+    is_expected.to have_text("Teilnahme von #{person.full_name} in #{event.name} wurde erfolgreich erstellt. Bitte überprüfe die Kontaktdaten und passe diese gegebenenfalls an.")
+    is_expected.to have_text('Important infos about my participation')
+
+  end
+end

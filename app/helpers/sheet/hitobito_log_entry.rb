c2cb8b83f4fce647b521ba3db24cbbdff8b5b887
Mountain Star <github@mtnstar.net>
2022-10-17 16:57:18 +0200

    Fix main nav

--- app/helpers/sheet/hitobito_log_entry.rb
+++ app/helpers/sheet/hitobito_log_entry.rb
@@ -1,16 +1,13 @@
-# encoding: utf-8
-
-#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
-#  hitobito and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
+# frozen_string_literal: true
 
 module Sheet
-  class HitobitoLogEntry < Base
+  class HitobitoLogEntry < Sheet::Admin
+
     tab 'hitobito_log_entries.tabs.all', 'hitobito_log_entries_path', no_alt: true
 
     Hitobito.logger.categories.each do |category|
       tab category.capitalize, "#{category}_hitobito_log_entries_path"
     end
+
   end
 end

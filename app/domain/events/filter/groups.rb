8912c7771004a43d4d187de85e0bfaae1bf88ba4
Carlo Beltrame <carlo@beltra.me>
2022-10-27 14:35:38 +0200

    Simplify course group filtering

--- app/domain/events/filter/groups.rb
+++ app/domain/events/filter/groups.rb
@@ -15,14 +15,11 @@ module Events::Filter
     end
 
     def to_scope
-      conditions = complete_course_list_allowed? ? Event.all : Event.in_hierarchy(@user)
+      conditions = complete_course_list_allowed? ?
+                     Event.all :
+                     Event.in_hierarchy(@user).or(Event.where(globally_visible: true))
 
-      if group_ids.any?
-        conditions = conditions.with_group_id(group_ids_in_hierarchy)
-        conditions = conditions.or(globally_visible_events_outside_hierarchy) if group_ids_outside_hierarchy.any? # rubocop:disable Metrics/LineLength
-      else
-        conditions = conditions.or(Event.where(globally_visible: true))
-      end
+      conditions = conditions.with_group_id(group_ids) if group_ids.any?
 
       @scope.merge(conditions).distinct
     end
@@ -35,27 +32,6 @@ module Events::Filter
 
     def complete_course_list_allowed?
       @options[:list_all_courses] == true
-    end
-
-    def globally_visible_events_outside_hierarchy
-      Event.where(id: events_in_groups(group_ids_outside_hierarchy),
-                  globally_visible: true).distinct
-    end
-
-    def group_ids_in_hierarchy
-      return group_ids if complete_course_list_allowed?
-
-      @group_ids_in_hierarchy ||= group_ids.select do |g_id|
-        @user.groups_hierarchy_ids.include? g_id.to_i
-      end
-    end
-
-    def group_ids_outside_hierarchy
-      @group_ids_outside_hierarchy ||= group_ids - group_ids_in_hierarchy
-    end
-
-    def events_in_groups(group_ids)
-      Event.with_group_id(group_ids).pluck(:id)
     end
 
     def group_ids

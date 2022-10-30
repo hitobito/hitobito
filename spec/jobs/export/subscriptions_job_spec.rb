5cf6160b0d69d675f4571bc5f942a647f9e828d5
Mountain Star <github@mtnstar.net>
2022-10-30 15:10:46 +0900

    Implement show related role by mailing list subscriptions

--- spec/jobs/export/subscriptions_job_spec.rb
+++ spec/jobs/export/subscriptions_job_spec.rb
@@ -17,15 +17,13 @@ describe Export::SubscriptionsJob do
   let(:export_options) do
     { household: household,
       show_related_roles_only: show_related_roles_only,
-      filename: filename,
-      selection: selection, filename: filename }
+      filename: filename }
   end
 
   let(:mailing_list) { mailing_lists(:info) }
   let(:user) { people(:top_leader) }
   let(:filename) { AsyncDownloadFile.create_name('subscription_export', user.id) }
   let(:household) { true }
-  let(:selection) { false }
   let(:show_related_roles_only) { false }
 
   let(:group) { groups(:top_layer) }
@@ -33,61 +31,63 @@ describe Export::SubscriptionsJob do
   let(:file) { AsyncDownloadFile.from_filename(filename, format) }
   let(:lines) { file.read.lines }
 
-  before do
-    SeedFu.quiet = true
-    SeedFu.seed [Rails.root.join('db', 'seeds')]
+  context 'export' do
+    before do
+      SeedFu.quiet = true
+      SeedFu.seed [Rails.root.join('db', 'seeds')]
 
-    Fabricate(:subscription, mailing_list: mailing_list)
-    Fabricate(:subscription, mailing_list: mailing_list)
-  end
-
-  context 'creates an CSV-Export' do
-    let(:format) { :csv }
-
-    it 'and saves it' do
-      subject.perform
-
-      expect(lines.size).to eq(3)
-      expect(lines[0]).to match(/Name;Adresse;.*/)
+      Fabricate(:subscription, mailing_list: mailing_list)
+      Fabricate(:subscription, mailing_list: mailing_list)
     end
-  end
 
-  context 'creates an Excel-Export' do
-    let(:format) { :xlsx }
+    context 'creates an CSV-Export' do
+      let(:format) { :csv }
 
-    it 'and saves it' do
-      subject.perform
+      it 'and saves it' do
+        subject.perform
 
-      expect(file.generated_file).to be_attached
+        expect(lines.size).to eq(3)
+        expect(lines[0]).to match(/Name;Adresse;.*/)
+      end
+    end
+
+    context 'creates an Excel-Export' do
+      let(:format) { :xlsx }
+
+      it 'and saves it' do
+        subject.perform
+
+        expect(file.generated_file).to be_attached
+      end
     end
   end
 
   context 'show related person roles only' do
     #
-    # bottom layer one:
+    # bottom layer two:
     #  - Bottom Member (Member)
     #  - Bottom Leader (Leader)
     # bottom group one one / Group 11:
     #  - Bottom Member (Leader)
-    # bottom layer two:
+    # bottom layer one:
     #  - Bottom Member (Leader)
 
     let(:mailing_list) { Fabricate(:mailing_list, group: groups(:top_layer)) }
     let(:format) { :csv }
     let(:role_cell_values) do
-      lines.drop(1).collect { |l| l.split(';')[15] }
+      lines.drop(1).collect { |l| l.split(';')[13] }.join(',').split(',').collect(&:strip)
     end
     let(:bottom_member) { people(:bottom_member) }
-    let!(:bottom_leader) { Fabricate(Group::BottomLayer::Leader.name.to_sym, group: groups(:bottom_layer_one)).person }
+    let!(:bottom_leader) { Fabricate(Group::BottomLayer::Leader.name.to_sym, group: groups(:bottom_layer_two)).person }
     let(:show_related_roles_only) { true }
-    let(:selection) { true }
+    let(:household) { false }
 
     before do
       Fabricate(Group::BottomLayer::Leader.name.to_sym, group: groups(:bottom_layer_two), person: bottom_member)
       Fabricate(Group::BottomGroup::Leader.name.to_sym, group: groups(:bottom_group_one_one), person: bottom_member)
 
       Subscription.create!(mailing_list: mailing_list,
-                          subscriber: groups(:bottom_layer_one),
+                          subscriber: groups(:bottom_layer_two),
                           role_types: [Group::BottomLayer::Leader])
     end
 
@@ -95,9 +95,9 @@ describe Export::SubscriptionsJob do
       subject.perform
 
       expect(lines.size).to eq(3)
-      expect(role_cell_values).to include('Leader Bottom One')
+      expect(role_cell_values).not_to include('Leader Bottom One')
       expect(role_cell_values).not_to include('Member Bottom One')
-      expect(role_cell_values).not_to include('Leader Bottom Two')
+      expect(role_cell_values).to include('Leader Bottom Two')
       expect(role_cell_values).not_to include('Leader Bottom One / Group 11')
     end
 
@@ -105,15 +105,15 @@ describe Export::SubscriptionsJob do
       mailing_list.subscriptions.destroy_all
 
       Subscription.create!(mailing_list: mailing_list,
-                          subscriber: bottom_member)
+                           subscriber: bottom_member)
 
       Subscription.create!(mailing_list: mailing_list,
-                          subscriber: bottom_leader)
+                           subscriber: bottom_leader)
 
       subject.perform
 
       expect(lines.size).to eq(3)
-      expect(role_cell_values).to include('Leader Bottom One')
+      expect(role_cell_values).not_to include('Leader Bottom One')
       expect(role_cell_values).to include('Member Bottom One')
       expect(role_cell_values).to include('Leader Bottom Two')
       expect(role_cell_values).to include('Leader Bottom One / Group 11')

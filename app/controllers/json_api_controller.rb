23ebf659d9cffa32356b3446538c4d41bc80d492
Mountain Star <github@mtnstar.net>
2022-11-28 09:33:49 +1300

    Introduce graphiti

---
+++ app/controllers/json_api_controller.rb
@@ -0,0 +1,10 @@
+class JsonApiController < ActionController::API
+  include Graphiti::Rails
+
+  register_exception Graphiti::Errors::RecordNotFound,
+    status: 404
+
+  rescue_from Exception do |e|
+    handle_exception(e)
+  end
+end
---
+++ app/resources/application_resource.rb
@@ -0,0 +1,3 @@
+class ApplicationResource < Graphiti::Resource
+  self.adapter = Graphiti::Adapters::ActiveRecord
+end

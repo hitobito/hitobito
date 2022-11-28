3e69b01d49fef51ed7bebd7fd9ce4e70162103a6
Nils Rauch <rauch@puzzle.ch>
2022-11-28 16:08:21 +0100

    Add specs for people json api, introduce application resource and get first requests going

--- app/resources/person_resource.rb
+++ app/resources/person_resource.rb
@@ -1,4 +1,17 @@
-class PersonResource < Graphiti::Resource
-  self.adapter = Graphiti::Adapters::ActiveRecord
+class PersonResource < ApplicationResource
+  primary_endpoint 'people', [:index, :show, :update]
+
   attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :nickname, :string
+  attribute :company_name, :string
+  attribute :company, :boolean
+  attribute :email, :string
+  attribute :address, :string
+  attribute :zip_code, :string
+  attribute :town, :string
+  attribute :country, :string
+  attribute :gender, :string
+  attribute :birthday, :date
+  attribute :primary_group_id, :integer
 end

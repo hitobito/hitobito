9b27b7fe3f5a7178b11ed53acbf9e3a7e04feced
Matthias Viehweger <viehweger@puzzle.ch>
2022-11-29 19:12:49 +0100

    Change whitespace, indentation or line-breaks

--- app/models/concerns/paranoia/future_deleted_at_scope.rb
+++ app/models/concerns/paranoia/future_deleted_at_scope.rb
@@ -6,22 +6,22 @@
 module Paranoia
   module FutureDeletedAtScope
     extend ActiveSupport::Concern
-    
+
     # Overwriting acts_as_paranoid here, see https://github.com/hitobito/hitobito/issues/1714
     included do
       default_scope { without_deleted }
 
-      scope :without_deleted, -> {      
+      scope :without_deleted, -> {
         unscope(where: :"#{table_name}.#{paranoia_column}")
           .where("#{table_name}": { "#{paranoia_column}": nil })
           .or(unscoped.where(arel_table[:"#{paranoia_column}"].gt(Time.now.utc)))
       }
-      
+
       # https://github.com/ActsAsParanoid/acts_as_paranoid/blob/v0.8.1/lib/acts_as_paranoid/core.rb#L22
       scope :with_deleted, -> { unscope(where: :"#{table_name}.#{paranoia_column}") }
 
       # https://github.com/ActsAsParanoid/acts_as_paranoid/blob/v0.8.1/lib/acts_as_paranoid/core.rb#L26
-      scope :only_deleted, -> { 
+      scope :only_deleted, -> {
         unscope(where: :"#{table_name}.#{paranoia_column}")
           .where.not("#{table_name}": { "#{paranoia_column}": nil })
           .where("#{table_name}": { "#{paranoia_column}": ..Time.now.utc })

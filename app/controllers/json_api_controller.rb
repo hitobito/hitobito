50948bf3da11e3b7cc13d225225e6b3be2e5d44a
Mountain Star <github@mtnstar.net>
2022-11-29 16:23:21 +1300

    Customize json api error rendering ... wip

--- app/controllers/json_api_controller.rb
+++ app/controllers/json_api_controller.rb
@@ -1,10 +1,26 @@
 class JsonApiController < ApplicationController
+  JsonApiUnauthorized
+
   include Graphiti::Rails
 
-  register_exception Graphiti::Errors::RecordNotFound,
-    status: 404
+  def authenticate_person!(*args)
+    super(*args)
+  end
 
   rescue_from Exception do |e|
     handle_exception(e)
   end
+
+  register_exception Graphiti::Errors::RecordNotFound,
+    status: 404
+
+  register_exception CanCan::AccessDenied,
+    status: 403,
+    title: I18n.t('errors.403.title'),
+    message: ->(error) { I18n.t('errors.403.explanation') }
+
+  register_exception CanCan::AccessDenied,
+    status: 401,
+    title: I18n.t('errors.401.title'),
+    message: ->(error) { I18n.t('errors.401.explanation') }
 end

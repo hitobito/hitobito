93eecd550b9f199cfae9beb9bf4506674c4fe35c
Mountain Star <github@mtnstar.net>
2022-11-29 17:19:48 +1300

    Render 401 unauthorized as json response without redirect

--- app/controllers/json_api_controller.rb
+++ app/controllers/json_api_controller.rb
@@ -1,26 +1,29 @@
 class JsonApiController < ApplicationController
-  JsonApiUnauthorized
+  class JsonApiUnauthorized < StandardError; end
 
   include Graphiti::Rails
 
   def authenticate_person!(*args)
-    super(*args)
+    unless sign_in
+      raise JsonApiUnauthorized
+    end
   end
 
   rescue_from Exception do |e|
     handle_exception(e)
   end
 
-  register_exception Graphiti::Errors::RecordNotFound,
-    status: 404
+  register_exception JsonApiUnauthorized,
+    status: 401,
+    title: I18n.t('errors.401.title'),
+    message: ->(error) { I18n.t('errors.401.explanation') }
 
   register_exception CanCan::AccessDenied,
     status: 403,
     title: I18n.t('errors.403.title'),
     message: ->(error) { I18n.t('errors.403.explanation') }
 
-  register_exception CanCan::AccessDenied,
-    status: 401,
-    title: I18n.t('errors.401.title'),
-    message: ->(error) { I18n.t('errors.401.explanation') }
+  ## TODO: customize and test
+  # register_exception Graphiti::Errors::RecordNotFound,
+    # status: 404
 end

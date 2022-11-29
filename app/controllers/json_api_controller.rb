6611c10dcb64df19370d4677b2d4f7aac3076b7c
Nils Rauch <rauch@puzzle.ch>
2022-11-29 08:28:52 +0100

    Introduce graphiti-rails due to deprecations See https://www.graphiti.dev/guides/graphiti-rails-migration

--- app/controllers/json_api_controller.rb
+++ app/controllers/json_api_controller.rb
@@ -1,29 +1,20 @@
 class JsonApiController < ApplicationController
   class JsonApiUnauthorized < StandardError; end
 
-  include Graphiti::Rails
-
   def authenticate_person!(*args)
     unless sign_in_person
       raise JsonApiUnauthorized
     end
   end
 
-  rescue_from Exception do |e|
-    handle_exception(e)
-  end
-
-  register_exception JsonApiUnauthorized,
-    status: 401,
-    title: I18n.t('errors.401.title'),
-    message: ->(error) { I18n.t('errors.401.explanation') }
-
   register_exception CanCan::AccessDenied,
-    status: 403,
+    status: 403, handler: Graphiti::Rails::ExceptionHandler,
     title: I18n.t('errors.403.title'),
     message: ->(error) { I18n.t('errors.403.explanation') }
 
-  ## TODO: customize and test
-  # register_exception Graphiti::Errors::RecordNotFound,
-    # status: 404
+  register_exception JsonApiUnauthorized,
+    status: 401,  handler: Graphiti::Rails::ExceptionHandler,
+    title: I18n.t('errors.401.title'),
+    message: ->(error) { I18n.t('errors.401.explanation') }
+
 end

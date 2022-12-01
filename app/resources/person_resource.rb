e2abffc74337b29758d5b7cc7e0c7385ddaf3eef
Nils Rauch <rauch@puzzle.ch>
2022-12-01 13:03:15 +0100

    Check permission on certain attributes

--- app/resources/person_resource.rb
+++ app/resources/person_resource.rb
@@ -11,17 +11,25 @@ class PersonResource < ApplicationResource
   attribute :zip_code, :string
   attribute :town, :string
   attribute :country, :string
-  attribute :gender, :string
-  attribute :birthday, :date
+  attribute :gender, :string, readable: :show_details?
+  attribute :birthday, :date, readable: :show_details?
   attribute :primary_group_id, :integer, except: [:writeable]
 
-  has_many :phone_numbers, link: false, resource: PhoneNumberResource
-  has_many :social_accounts, link: false, resource: SocialAccountResource
-  has_many :additional_emails, link: false, resource: AdditionalEmailResource
+  has_many :phone_numbers, link: false, resource: PhoneNumberResource, readable: :show_details_or_public?
+  has_many :social_accounts, link: false, resource: SocialAccountResource, readable: :show_details_or_public?
+  has_many :additional_emails, link: false, resource: AdditionalEmailResource, readable: :show_details_or_public?
 
   filter :updated_at, :datetime, single: true do
     eq do |scope, value|
       scope.where(updated_at: value..)
     end
+  end
+
+  def show_details?(model_instance)
+    can?(:show_details, model_instance)
+  end
+
+  def show_details_or_public(model_instance)
+    show_details?(model_instance) || model_instance.public?
   end
 end

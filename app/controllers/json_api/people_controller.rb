6a8239611cb7bf2eb343deb799d63d42b46a3cc2
Nils Rauch <nils.rauch@gmail.com>
2022-12-02 11:48:52 +0100

    Fix rubocop

--- app/controllers/json_api/people_controller.rb
+++ app/controllers/json_api/people_controller.rb
@@ -9,7 +9,9 @@ class JsonApi::PeopleController < JsonApiController
   private
 
   def index_people_scope
-    Person.accessible_by(PersonReadables.new(current_user || service_token_user || current_oauth_token.person))
+    Person.accessible_by(PersonReadables.new(current_user ||
+                                             service_token_user ||
+                                             current_oauth_token.person))
   end
 
 end

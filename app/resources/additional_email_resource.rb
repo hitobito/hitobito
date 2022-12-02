527bfb590246d432f344184f793c878b899ce4be
Nils Rauch <nils.rauch@gmail.com>
2022-12-02 12:24:25 +0100

    DRY contactable resources

--- app/resources/additional_email_resource.rb
+++ app/resources/additional_email_resource.rb
@@ -1,15 +1,5 @@
 class AdditionalEmailResource < ApplicationResource
-  attribute :label, :string
-  attribute :public, :boolean
+  include ContactableResource
 
-  attribute :contactable_id, :integer
-  attribute :contactable_type, :string
   attribute :email, :string
-  
-  # needs to be in child class, since it will otherwise think the parent model is Contactable
-  polymorphic_belongs_to :contactable do
-    group_by(:contactable_type) do
-      on(:Person)
-    end
-  end
 end
--- app/resources/contactable_resource.rb
+++ app/resources/contactable_resource.rb
@@ -1,11 +1,21 @@
-# TODO This is currently not used since graphiti seems to get confused about base classes
+# Can not be a base class since graphiti seems to get confused about base classes
 # for polymorphic relations.
 # Thus, the json api type was always contactable and the relation couldn't be correctly mapped.
 # Tried abstract_class = true, that didn't work either
-class ContactableResource < ApplicationResource
-  attribute :label, :string
-  attribute :public, :boolean
+module ContactableResource
+  extend ActiveSupport::Concern
 
-  attribute :contactable_id, :integer
-  attribute :contactable_type, :string
+  included do
+    attribute :label, :string
+    attribute :public, :boolean
+
+    attribute :contactable_id, :integer
+    attribute :contactable_type, :string
+
+    polymorphic_belongs_to :contactable do
+      group_by(:contactable_type) do
+        on(:Person)
+      end
+    end
+  end
 end

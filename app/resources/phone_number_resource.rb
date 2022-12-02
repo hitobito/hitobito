a02742cccb62c95cf4b576481f3cc707e2076f69
Nils Rauch <nils.rauch@gmail.com>
2022-12-02 12:24:25 +0100

    DRY contactable resources

--- app/resources/phone_number_resource.rb
+++ app/resources/phone_number_resource.rb
@@ -1,15 +1,5 @@
 class PhoneNumberResource < ApplicationResource
-  attribute :label, :string
-  attribute :public, :boolean
+  include ContactableResource
 
-  attribute :contactable_id, :integer
-  attribute :contactable_type, :string
   attribute :number, :string
-
-  # needs to be in child class, since it will otherwise think the parent model is Contactable
-  polymorphic_belongs_to :contactable do
-    group_by(:contactable_type) do
-      on(:Person)
-    end
-  end
 end

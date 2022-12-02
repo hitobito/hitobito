1479f988900509b9cc54d439c8be073066f565e3
Nils Rauch <nils.rauch@gmail.com>
2022-12-02 11:24:50 +0100

    Allow personal oauth token to use people json api

--- app/controllers/json_api/people_controller.rb
+++ app/controllers/json_api/people_controller.rb
@@ -9,7 +9,7 @@ class JsonApi::PeopleController < JsonApiController
   private
 
   def index_people_scope
-    Person.accessible_by(PersonReadables.new(current_user || service_token_user))
+    Person.accessible_by(PersonReadables.new(current_user || service_token_user || current_oauth_token.person))
   end
 
 end

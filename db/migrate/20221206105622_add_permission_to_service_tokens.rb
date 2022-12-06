b512e5b7933c0218203135747a535417c82d9f99
Nils Rauch <rauch@puzzle.ch>
2022-12-06 14:22:51 +0100

    Add permission to service token

---
+++ db/migrate/20221206105622_add_permission_to_service_tokens.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2022, Schweizer Wanderwege. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class AddPermissionToServiceTokens < ActiveRecord::Migration[6.1]
+  def change
+    add_column :service_tokens, :permission, :string, null: false, default: 'layer_read'
+
+    ServiceToken.where(layer_and_below_read: true).update_all(permission: 'layer_and_below_read')
+
+    remove_column :service_tokens, :layer_and_below_read
+  end
+end

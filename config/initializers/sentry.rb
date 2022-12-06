393f9367604403f9311d5c176e67ff7cf124011b
Matthias Viehweger <viehweger@puzzle.ch>
2022-12-06 19:09:03 +0100

    Correct overeager conditional, which did not have to wrap everything
    Bad conditional, bad! Sit! Good, now hide behinde the assignment.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -10,31 +10,29 @@ Raven.configure do |config|
 
   config.release = Rails.application.class.versions(Rails.root.join('VERSION')).first.chomp
 
-  if ENV['SENTRY_CURRENT_ENV'].blank?
-    analyzer = [
-      ENV['OPENSHIFT_BUILD_NAMESPACE'], # hit-jubla-int
-      ENV['RAILS_DB_NAME'],             # hit_jubla_development
-      "hitobito-#{Rails.env}"           # hitobito-development
-    ].compact.first.yield_self do |name|
-      ProjectAnalyzer.new(name)
-    end
-
-    config.tags[:project]      = analyzer.project
-    config.current_environment = analyzer.stage
-
-    config.excluded_exceptions += [
-      'ActiveRecord::ConnectionNotEstablished',
-      'Errno::ECONNREFUSED',
-      'Errno::ECONNRESET',
-      'Errno::EFAULT',
-      'Errno::ENETUNREACH',
-      'Errno::ENOMEM',
-      'Mysql2::Error::ConnectionError',
-      'Net::OpenTimeout',
-      'Net::ReadTimeout',
-      'SignalException',
-      'ThinkingSphinx::ConnectionError',
-      'Timeout::Error',
-    ]
+  analyzer = [
+    ENV['OPENSHIFT_BUILD_NAMESPACE'], # hit-jubla-int
+    ENV['RAILS_DB_NAME'],             # hit_jubla_development
+    "hitobito-#{Rails.env}"           # hitobito-development
+  ].compact.first.yield_self do |name|
+    ProjectAnalyzer.new(name)
   end
+
+  config.tags[:project]      = analyzer.project
+  config.current_environment = analyzer.stage if ENV['SENTRY_CURRENT_ENV'].blank?
+
+  config.excluded_exceptions += [
+    'ActiveRecord::ConnectionNotEstablished',
+    'Errno::ECONNREFUSED',
+    'Errno::ECONNRESET',
+    'Errno::EFAULT',
+    'Errno::ENETUNREACH',
+    'Errno::ENOMEM',
+    'Mysql2::Error::ConnectionError',
+    'Net::OpenTimeout',
+    'Net::ReadTimeout',
+    'SignalException',
+    'ThinkingSphinx::ConnectionError',
+    'Timeout::Error',
+  ]
 end

da8cbf5d78faa4b23fc0fca2cac17e99ee21284b
Mountain Star <github@mtnstar.net>
2022-12-07 17:20:45 +1300

    Render service token in person log

--- app/decorators/paper_trail/version_decorator.rb
+++ app/decorators/paper_trail/version_decorator.rb
@@ -22,10 +22,28 @@ module PaperTrail
 
     def author
       if model.version_author.present?
-        person = Person.where(id: model.version_author).first
-        if person
-          h.link_to_if(can?(:show, person), person.to_s, h.person_path(person.id))
+        author_id = model.version_author
+        author_type = model.whodunnit_type
+        if author_type == ServiceToken.sti_name
+          author_service_token(author_id)
+        else
+          author_person(author_id)
         end
+      end
+    end
+
+    def author_person(author_id)
+      person = Person.where(id: author_id).first
+      if person
+        h.link_to_if(can?(:show, person), person.to_s, h.person_path(person.id))
+      end
+    end
+
+    def author_service_token(author_id)
+      token = ServiceToken.where(id: author_id).first
+      if token
+        layer_id = token.layer_group_id
+        h.link_to_if(can?(:show, token), token.to_s, h.group_service_token_path(layer_id, token.id))
       end
     end
 

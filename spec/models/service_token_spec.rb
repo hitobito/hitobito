a40c0569f86acf3575aa32b42be6c34b210ac208
Mountain Star <github@mtnstar.net>
2022-12-12 14:44:39 +1300

    Fix and extend service token specs

--- spec/models/service_token_spec.rb
+++ spec/models/service_token_spec.rb
@@ -9,17 +9,31 @@ describe ServiceToken do
 
   describe '#dynamic user' do
     it 'gets layer_and_below_read permissions when token has layer_and_below_read' do
-      token = ServiceToken.new(layer: groups(:top_layer), layer_and_below_read: true)
+      token = ServiceToken.new(layer: groups(:top_layer), permission: :layer_and_below_read)
       expect(token.dynamic_user.roles).to have(1).item
       expect(token.dynamic_user.roles.first.group).to eq groups(:top_layer)
       expect(token.dynamic_user.roles.first.permissions).to eq [:layer_and_below_read]
     end
 
-    it 'gets layer_read permissions when token does not have layer_and_below_read' do
-      token = ServiceToken.new(layer: groups(:top_layer), layer_and_below_read: false)
+    it 'gets layer_read permissions when token does have layer_read' do
+      token = ServiceToken.new(layer: groups(:top_layer), permission: :layer_read)
       expect(token.dynamic_user.roles).to have(1).item
       expect(token.dynamic_user.roles.first.group).to eq groups(:top_layer)
       expect(token.dynamic_user.roles.first.permissions).to eq [:layer_read]
+    end
+
+    it 'gets layer_and_below_full permissions when token does have layer_and_below_full' do
+      token = ServiceToken.new(layer: groups(:top_layer), permission: :layer_and_below_full)
+      expect(token.dynamic_user.roles).to have(1).item
+      expect(token.dynamic_user.roles.first.group).to eq groups(:top_layer)
+      expect(token.dynamic_user.roles.first.permissions).to eq [:layer_and_below_full]
+    end
+
+    it 'gets layer_full permissions when token does have layer_full' do
+      token = ServiceToken.new(layer: groups(:top_layer), permission: :layer_full)
+      expect(token.dynamic_user.roles).to have(1).item
+      expect(token.dynamic_user.roles.first.group).to eq groups(:top_layer)
+      expect(token.dynamic_user.roles.first.permissions).to eq [:layer_full]
     end
   end
 

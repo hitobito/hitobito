f411595497b5e5e859d8ae9142edfd39c8a2ce65
Mountain Star <github@mtnstar.net>
2022-12-14 21:10:47 +1300

    Update swagger spec for people#index

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,10 +24,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'www.example.com'
+              default: 'localhost:3000'
             }
           }
         }

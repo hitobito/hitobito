8c1b124d3355e08b7a6ab31f2ad9912be46606f2
Mountain Star <github@mtnstar.net>
2022-12-14 21:10:47 +1300

    Update swagger spec for people#index

--- spec/requests/json_api/people_spec.rb
+++ spec/requests/json_api/people_spec.rb
@@ -5,14 +5,37 @@ RSpec.describe 'json_api/people', type: :request do
   path '/api/people' do
 
     get('list people') do
+      produces 'application/vnd.json+api'
+      parameter({
+        name: 'include',
+        in: :query,
+        required: false,
+        explode: false,
+        schema: {
+          type: :array,
+          enum: [
+            'phone_numbers',
+            'social_accounts',
+            'additional_emails',
+          ],
+          nullable: true,
+          description: 'Contactables'
+        }
+      })
+
       response(200, 'successful') do
         after do |example|
           example.metadata[:response][:content] = {
-            'application/json' => {
+            'application/vnd.json+api' => {
               example: JSON.parse(response.body, symbolize_names: true)
             }
           }
         end
+        run_test!
+      end
+
+      response(200, 'successful') do
+        let(:include) { %w(phone_numbers social_accounts additional_emails) }
         run_test!
       end
     end

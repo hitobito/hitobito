8c793fbc98e3a5306a0791a92facf6f63efec21e
Mountain Star <github@mtnstar.net>
2022-12-20 14:31:07 +1300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_07_232442) do
+ActiveRecord::Schema.define(version: 2022_12_14_112552) do
 
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false

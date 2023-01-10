11283b99e1e9adf0218d45d4f6c645e4db2f3423
Matthias Viehweger <viehweger@puzzle.ch>
2023-01-10 17:03:10 +0100

    Adapt specs after refactor

--- spec/controllers/healthz_controller_spec.rb
+++ spec/controllers/healthz_controller_spec.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2017, Hitobito AG. This file is part of
+#  Copyright (c) 2017-2023, Hitobito AG. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -9,41 +9,74 @@ require 'spec_helper'
 
 describe HealthzController do
 
-  describe 'GET show' do
+  describe 'GET show with token' do
+
+    let(:json) { JSON.parse(response.body) }
+    let(:token) { AppStatus.auth_token }
+
+    context 'when true mail can verify valid e-mail address' do
+      it 'has HTTP status 200' do
+        expect(Truemail).to receive(:valid?).with('hitobito@puzzle.ch').and_return(true)
+
+        get :show, params: { token: token }
+
+        expect(response.status).to eq(200)
+
+        expect(json).to eq('app_status' =>
+                           { 'code' => 'ok',
+                             'details' => { 'truemail_working' => true,
+                                            'validated_email' => 'hitobito@puzzle.ch' } })
+      end
+    end
+
+    context 'when true mail cannot verify valid e-mail address' do
+      it 'has HTTP status 503' do
+        expect(Truemail).to receive(:valid?).with('hitobito@puzzle.ch').and_return(false)
+
+        get :show, params: { token: token }
+
+        expect(response.status).to eq(503)
+
+        expect(json).to eq('app_status' =>
+                           { 'code' => 'service_unavailable',
+                             'details' => { 'truemail_working' => false,
+                                            'validated_email' => 'hitobito@puzzle.ch' } })
+      end
+    end
+
+  end
+
+  describe 'GET show without token' do
 
     let(:json) { JSON.parse(response.body) }
 
-    context 'when things are running smoothly' do
-
+    context 'when true mail can verify valid e-mail address' do
       it 'has HTTP status 200' do
+        expect(Truemail).to receive(:valid?).with('hitobito@puzzle.ch').and_return(true)
 
         get :show
 
         expect(response.status).to eq(200)
 
-        expect(json).to eq('app_status' => { 'code' => 'ok', 'details' => { 'store_ok?' => true }})
-
+        expect(json).to eq('app_status' =>
+                           { 'code' => 'ok',
+                             'details' => { 'truemail_working' => true } })
       end
-
     end
 
-    context 'when the status is unhealthy' do
-
+    context 'when true mail cannot verify valid e-mail address' do
       it 'has HTTP status 503' do
-
-        expect_any_instance_of(AppStatus::Store).to receive(:store_ok?).and_return(false)
+        expect(Truemail).to receive(:valid?).with('hitobito@puzzle.ch').and_return(false)
 
         get :show
 
         expect(response.status).to eq(503)
 
-        expect(json).to eq('app_status' => { 'code' => 'service_unavailable', 'details' => { 'store_ok?' => false }})
-
+        expect(json).to eq('app_status' =>
+                           { 'code' => 'service_unavailable',
+                             'details' => { 'truemail_working' => false } })
       end
-
     end
-
   end
-
 
 end

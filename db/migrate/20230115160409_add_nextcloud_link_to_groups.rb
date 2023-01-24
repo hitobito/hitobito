4ecab6e55aa12f1a22139bdea32e62cb31851c0a
Matthias Viehweger <viehweger@puzzle.ch>
2023-01-24 08:53:15 +0100

    Add license to migration

--- db/migrate/20230115160409_add_nextcloud_link_to_groups.rb
+++ db/migrate/20230115160409_add_nextcloud_link_to_groups.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023-2023, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 class AddNextcloudLinkToGroups < ActiveRecord::Migration[6.1]
   def change
     add_column :groups, :nextcloud_url, :string

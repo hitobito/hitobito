0755acfb956cc5192a8ec13f925c0b4d6b492a2e
Matthias Viehweger <viehweger@puzzle.ch>
2023-01-24 17:17:26 +0100

    Adapt specs to new column

--- spec/domain/export/csv/groups/list_spec.rb
+++ spec/domain/export/csv/groups/list_spec.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2023, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -18,49 +18,69 @@ describe Export::Tabular::Groups::List do
 
   subject { csv }
 
-   its(:headers) do
-     should == ['Id', 'Elterngruppe', 'Name', 'Kurzname', 'Gruppentyp', 'Haupt-E-Mail', 'Adresse', 'PLZ', 'Ort', 'Land', 'Geändert', 'Ebene', 'Beschreibung', 'Telefonnummern', 'Anzahl Mitglieder', 'Social Media']
-   end
+  its(:headers) do
+    should == [
+      'Id',
+      'Elterngruppe',
+      'Name',
+      'Kurzname',
+      'Gruppentyp',
+      'Haupt-E-Mail',
+      'Adresse',
+      'PLZ',
+      'Ort',
+      'Land',
+      'Geändert',
+      'Ebene',
+      'Beschreibung',
+      'Link zu Nextcloud',
+      'Telefonnummern',
+      'Anzahl Mitglieder',
+      'Social Media'
+    ]
+  end
 
-   it 'has 4 items' do
-     expect(subject.size).to eq(4)
-   end
+  it 'has 4 items' do
+    expect(subject.size).to eq(4)
+  end
 
-   context 'first row with contact' do
+  context 'first row with contact' do
 
-     let(:contact) { people(:bottom_member) }
+    let(:contact) { people(:bottom_member) }
 
-     subject { csv[0] }
+    subject { csv[0] }
 
-     its(['Id']) { should == group.id.to_s }
-     its(['Elterngruppe']) { should == group.parent_id.to_s }
-     its(['Name']) { should == group.name }
-     its(['Kurzname']) { should == group.short_name }
-     its(['Gruppentyp']) { should == 'Bottom Layer' }
-     its(['Haupt-E-Mail']) { should == group.email }
-     its(['Adresse']) { should == contact.address }
-     its(['PLZ']) { should == contact.zip_code.to_s }
-     its(['Ort']) { should == contact.town }
-     its(['Land']) { should == contact.country_label }
-     its(['Ebene']) { should == group.id.to_s }
-     its(['Geändert']) { should == "#{I18n.l(group.updated_at.to_date)} #{I18n.l(group.updated_at, format: :time)}" }
-     its(['Anzahl Mitglieder']) { should == '1' }
-     its(['Telefonnummern']) { should == '' }
-     its(['Social Media']) { should == '' }
-   end
+    its(['Id']) { should == group.id.to_s }
+    its(['Elterngruppe']) { should == group.parent_id.to_s }
+    its(['Name']) { should == group.name }
+    its(['Kurzname']) { should == group.short_name }
+    its(['Gruppentyp']) { should == 'Bottom Layer' }
+    its(['Haupt-E-Mail']) { should == group.email }
+    its(['Adresse']) { should == contact.address }
+    its(['PLZ']) { should == contact.zip_code.to_s }
+    its(['Ort']) { should == contact.town }
+    its(['Land']) { should == contact.country_label }
+    its(['Ebene']) { should == group.id.to_s }
+    its(['Geändert']) do
+      should == "#{I18n.l(group.updated_at.to_date)} #{I18n.l(group.updated_at, format: :time)}"
+    end
+    its(['Anzahl Mitglieder']) { should == '1' }
+    its(['Telefonnummern']) { should == '' }
+    its(['Social Media']) { should == '' }
+  end
 
-   context 'second row' do
+  context 'second row' do
 
-     let(:second_group) { groups(:bottom_group_one_one) }
+    let(:second_group) { groups(:bottom_group_one_one) }
 
-     subject { csv[1] }
+    subject { csv[1] }
 
-     its(['Elterngruppe']) { should == group.id.to_s }
-     its(['Ebene']) { should == group.id.to_s }
-     its(['Haupt-E-Mail']) { should == second_group.email }
-     its(['Adresse']) { should == second_group.address }
-     its(['PLZ']) { should == second_group.zip_code.to_s }
-     its(['Ort']) { should == second_group.town }
-     its(['Land']) { should == second_group.country_label }
-   end
+    its(['Elterngruppe']) { should == group.id.to_s }
+    its(['Ebene']) { should == group.id.to_s }
+    its(['Haupt-E-Mail']) { should == second_group.email }
+    its(['Adresse']) { should == second_group.address }
+    its(['PLZ']) { should == second_group.zip_code.to_s }
+    its(['Ort']) { should == second_group.town }
+    its(['Land']) { should == second_group.country_label }
+  end
 end

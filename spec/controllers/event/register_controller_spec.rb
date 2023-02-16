d069a6afb7d9ffaab2ad29fd02ccdc7cec4a2b74
Matthias Viehweger <viehweger@puzzle.ch>
2023-02-16 23:00:22 +0100

    Remove unneeded, outdated, unwanted and not required require

--- spec/controllers/event/register_controller_spec.rb
+++ spec/controllers/event/register_controller_spec.rb
@@ -1,12 +1,11 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2021, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2023, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
 require 'spec_helper'
-require 'deprecation_helper'
 
 describe Event::RegisterController do
 

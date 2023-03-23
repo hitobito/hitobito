4843f7019c3f21f22ca6d5ffe87b59747fd4bbfe
Daniel Illi <daniel@illi.zone>
2023-03-23 23:22:49 +0100

    Spec for BackgroundJobs::Logger

--- app/domain/background_jobs/logger.rb
+++ app/domain/background_jobs/logger.rb
@@ -3,18 +3,20 @@ module BackgroundJobs
     attach_to :background_job
 
     def job_started(message)
-      log(message)
+      log(message, status: 'started')
     end
 
     def job_finished(message)
-      log(message)
+      log(message, status: 'finished')
     end
 
     private
 
     def log(message, **opts)
-      attrs = message.payload.with_indifferent_access.slice(*BackgroundJobLogEntry.column_names)
-                     .merge(updated_at: Time.zone.now, **opts)
+      attrs = message.payload.with_indifferent_access
+                     .slice(*BackgroundJobLogEntry.column_names)
+                     .merge(updated_at: Time.zone.now)
+                     .reverse_merge(**opts)
       BackgroundJobLogEntry.upsert(**attrs)
     rescue
       # Let's not fail the job because of a logging problem, but at least report the error.

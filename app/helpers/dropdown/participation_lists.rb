4bad4ac4cc3a80f815ac55874ebb3aa169c5859a
Nicola Chiapolini <nicola.chiapolini@physik.uzh.ch>
2023-03-26 19:46:25 +0200

    WIP: form works, but triggers wrong route

--- app/helpers/dropdown/participation_lists.rb
+++ app/helpers/dropdown/participation_lists.rb
@@ -21,6 +21,9 @@ module Dropdown
         add_item(event.label,
           build_event_participation_lists_path(event),
                  **participation_lists_options)
+        add_item(event.label+" einladung",
+                 build_event_invite_path(event),
+                 **participation_lists_options)
       end
     end
 
@@ -31,6 +34,11 @@ module Dropdown
     def build_event_participation_lists_path(event)
       type = (event == ::Event) ? nil : event.to_s
       template.group_events_participation_lists_new_path(@group, type: type, label: event.label)
+    end
+
+    def build_event_invite_path(event)
+      type = event == ::Event ? nil : event.to_s
+      template.group_events_participation_lists_invite_path(@group, type: type, label: event.label)
     end
 
     def participation_lists_options

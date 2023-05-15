3a591030bbf51021748773c66b6647fa6778bce5
Nils Rauch <rauch@puzzle.ch>
2023-05-15 11:39:24 +0200

    Get roles that see me via async backend call

--- app/controllers/person/security_tools_controller.rb
+++ app/controllers/person/security_tools_controller.rb
@@ -14,7 +14,7 @@ class Person::SecurityToolsController < ApplicationController
   SUSPEND_PERSON_SITUATION = 'suspend_person_situation_id'
   SUSPEND_PERSON_SOLUTION = 'suspend_person_solution_id'
 
-  before_action :authorize_action, :load_info_texts, :load_groups_and_roles_that_see_me
+  before_action :authorize_action
 
   decorates :group, :person, :security_tools
 
@@ -24,7 +24,16 @@ class Person::SecurityToolsController < ApplicationController
   :dataleak_situation_text, :dataleak_solution_text,
   :suspend_person_situation_text, :suspend_person_solution_text
 
-  respond_to :html
+  def index
+    respond_to do |format|
+      format.html do
+        load_info_texts
+      end
+      format.js do
+        load_groups_and_roles_that_see_me
+      end
+    end
+  end
 
   def password_override
     person.encrypted_password = nil
@@ -92,36 +101,36 @@ class Person::SecurityToolsController < ApplicationController
   end
 
   def load_groups_and_roles_that_see_me
-    @groups_and_roles_that_see_me ||= groups_and_roles_that_see_me
+    @groups_and_roles_that_see_me = groups_and_roles_that_see_me
   end
 
   def groups_and_roles_that_see_me
     groups_and_roles = {}
-    all_groups.each do |group_id, group_name, group_type|
-      group_type.constantize.role_types.each do |role|
-        next unless can_see_me?(role, group_id, group_type)
+    relevant_groups.each do |group_id, group_name, group_type|
+      group_type.constantize.role_types.each do |role_type|
+        next unless can_see_me?(role_type, group_id)
 
         groups_and_roles[group_id] ||= { name: group_name, roles: [] }
-        groups_and_roles[group_id][:roles] << role.label
+        groups_and_roles[group_id][:roles] << role_type.label
       end
     end
     groups_and_roles
   end
 
-  def all_groups
-    @all_groups ||= Group.where(layer_group_id: relevant_layer_ids).order_by_type.pluck(:id, :name, :type)
+  def relevant_groups
+    @relevant_groups ||= Group.where(layer_group_id: relevant_layer_ids)
+                              .order_by_type
+                              .pluck(:id, :name, :type)
   end
 
   def relevant_layer_ids
     @relevant_layer_ids ||= person.groups.flat_map(&:layer_hierarchy).map(&:id).uniq
   end
 
-  def can_see_me?(role, group_id, group_type)
-    return false if group_type != role.name.deconstantize
+  def can_see_me?(role_type, group_id)
+    return false if role_type.permissions.empty?
 
-    test_person = Ability.new(Person.new(roles: [role.new(group_id: group_id)]))
+    test_person = Ability.new(Person.new(roles: [role_type.new(group_id: group_id)]))
     test_person.can?(:show_details, person)
   end
-
-
 end

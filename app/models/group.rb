dc6a07bc369af0997a11413cadf0739fa138468d
Nils Rauch <rauch@puzzle.ch>
2023-05-22 14:24:50 +0200

    Allow adjusting nesting even when archived

--- app/models/group.rb
+++ app/models/group.rb
@@ -439,7 +439,7 @@ class Group < ActiveRecord::Base # rubocop:disable Metrics/ClassLength
   end
 
   def prevent_changes
-    allowed = %w[archived_at updater_id]
+    allowed = %w[archived_at updater_id lft rgt]
     only_archival = changes
       .reject { |_attr, (from, to)| from.blank? && to.blank? }
       .keys.all? { |key| allowed.include? key }

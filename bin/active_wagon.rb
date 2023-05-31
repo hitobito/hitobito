2a19cc524386339fbd4dfe8355b444889670d43b
Matthias Viehweger <viehweger@puzzle.ch>
2023-05-31 23:21:52 +0200

    Add JEMK as youth organization

--- bin/active_wagon.rb
+++ bin/active_wagon.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-#  Copyright (c) 2020-2022, Puzzle ITC. This file is part of
+#  Copyright (c) 2020-2023, Puzzle ITC. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -83,7 +83,7 @@ class Setup
   end
 
   def dependencies
-    %w(pbs cevi pro_natura jubla sjas).product([%w(youth)]).to_h.merge({
+    %w(pbs cevi pro_natura jubla sjas jemk).product([%w(youth)]).to_h.merge({
       'tenants' => %w(generic),
     })
   end

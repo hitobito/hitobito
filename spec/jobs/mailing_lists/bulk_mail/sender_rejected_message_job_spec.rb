fc175aa65eeac4314a12f148618d38916a00b90d
Mountain Star <github@mtnstar.net>
2023-07-03 12:41:06 +0200

    Fix sender rejected message job

--- spec/jobs/mailing_lists/bulk_mail/sender_rejected_message_job_spec.rb
+++ spec/jobs/mailing_lists/bulk_mail/sender_rejected_message_job_spec.rb
@@ -26,7 +26,7 @@ describe MailingLists::BulkMail::SenderRejectedMessageJob do
 
     expect(last_email.to).to eq(["from@example.com"])
     expect(last_email.from).to eq(["noreply@hitobito.example.com"])
-    expect(last_email.subject).to eq("RE: Testflight from 24.4.2021")
+    expect(last_email.subject).to eq("Re: Testflight from 24.4.2021")
     expect(last_email.body.decoded).to eq("Du bist leider nicht berechtigt auf die Liste leaders@hitobito.example.com zu schreiben.")
     expect(bulk_mail.reload.raw_source).to be_nil
   end

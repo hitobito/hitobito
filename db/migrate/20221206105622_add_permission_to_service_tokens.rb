f5152fb3b7f378c961b22475a273a7babd86683b
Mountain Star <github@mtnstar.net>
2023-07-05 16:43:17 +0200

    Make core db migrations work with ruby 3.0

--- db/migrate/20221206105622_add_permission_to_service_tokens.rb
+++ db/migrate/20221206105622_add_permission_to_service_tokens.rb
@@ -13,6 +13,6 @@ class AddPermissionToServiceTokens < ActiveRecord::Migration[6.1]
       ServiceToken.where(layer_and_below_read: true).update_all(permission: :layer_and_below_read)
     end
 
-    remove_column :service_tokens, :layer_and_below_read, :boolean, { null: false, default: false }
+    remove_column :service_tokens, :layer_and_below_read, :boolean, null: false, default: false
   end
 end

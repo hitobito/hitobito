699f68c77c46ed9d250a3f0a0a54269ce8cf2bc1
Carlo Beltrame <carlo@beltra.me>
2023-07-05 15:46:20 +0200

    Use a more complex password for legacy personal access token login

--- spec/controllers/devise/hitobito/sessions_controller_spec.rb
+++ spec/controllers/devise/hitobito/sessions_controller_spec.rb
@@ -10,8 +10,9 @@ require 'spec_helper'
 describe Devise::Hitobito::SessionsController do
   let(:bottom_group) { groups(:bottom_group_one_one) }
   let(:role) { Fabricate('Group::BottomGroup::Member', group: bottom_group) }
+  let(:password) { 'cNb@X7fTdiU4sWCMNos3gJmQV_d9e9' }
   let(:person) do
-    role.person.update_attribute(:password, 'password')
+    role.person.update!(password: password)
     role.person.reload
   end
 
@@ -34,7 +35,7 @@ describe Devise::Hitobito::SessionsController do
       end
 
       it 'logs in person even when they have no login permission' do
-        post :create, params: { person: { email: person.email, password: 'password' } }
+        post :create, params: { person: { email: person.email, password: password } }
         expect(flash[:alert]).not_to be_present
         expect(controller.send(:current_person)).to be_present
         expect(controller.send(:current_person).authentication_token).to be_blank
@@ -52,14 +53,14 @@ describe Devise::Hitobito::SessionsController do
       end
 
       it 'responds with user and new token' do
-        post :create, params: { person: { email: person.email, password: 'password' } }, format: :json
+        post :create, params: { person: { email: person.email, password: password } }, format: :json
         expect(response.body).to match(/^\{.*"authentication_token":".+"/)
         expect(assigns(:person).authentication_token).to be_present
       end
 
       it 'responds with user and existing token' do
         person.generate_authentication_token!
-        post :create, params: { person: { email: person.email, password: 'password' } }, format: :json
+        post :create, params: { person: { email: person.email, password: password } }, format: :json
         expect(response.body).to match(/^\{.*"authentication_token":"#{person.authentication_token}"/)
         expect(assigns(:person).authentication_token).to eq(person.authentication_token)
       end

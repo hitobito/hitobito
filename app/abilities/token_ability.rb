45e1bef0b4458a4966139474596b080983f4d10c
Michael Schär <michael.schaer@pbs.ch>
2023-07-11 13:13:34 +0200

    First try, not working

--- app/abilities/token_ability.rb
+++ app/abilities/token_ability.rb
@@ -96,6 +96,9 @@ class TokenAbility
     can :show, Group do |g|
       token_layer_and_below.include?(g)
     end
+    can :show_details, Group do |g|
+      token_layer_and_below.include?(g)
+    end
   end
 
   def define_invoice_abilities

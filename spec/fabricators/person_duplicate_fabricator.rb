26880bc071d6c4cd64d2cca623d7d5675ed9588a
Matthias Viehweger <viehweger@puzzle.ch>
2023-10-06 14:52:30 +0200

    Resolve DEPRECATIONS

--- spec/fabricators/person_duplicate_fabricator.rb
+++ spec/fabricators/person_duplicate_fabricator.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2020, Die Mitte Schweiz. This file is part of
+#  Copyright (c) 2020-2023, Die Mitte Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
 Fabricator(:person_duplicate) do
   person_1 { Fabricate(:person) }
-  before_save do |d, _t|
+  before_create do |d, _t|
     p2 = person_1.class.new
     People::DuplicateLocator::DUPLICATION_ATTRS.each do |attr|
       p2[attr] = person_1[attr]

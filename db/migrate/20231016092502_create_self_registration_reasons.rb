63c7ab9aab6c9852c7380db84ef2f261db01579a
Mountain Star <github@mtnstar.net>
2023-10-18 16:18:01 +0200

    Add missing licence header

--- db/migrate/20231016092502_create_self_registration_reasons.rb
+++ db/migrate/20231016092502_create_self_registration_reasons.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito
+
 class CreateSelfRegistrationReasons < ActiveRecord::Migration[6.1]
   def change
     create_table :self_registration_reasons do |t|

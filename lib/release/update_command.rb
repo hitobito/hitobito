e98cad89f47ff63206b1d9e93ceb3f581601f91d
Matthias Viehweger <viehweger@puzzle.ch>
2023-10-26 11:36:33 +0200

    Make release more configurable for local runs

--- lib/release/update_command.rb
+++ lib/release/update_command.rb
@@ -18,23 +18,44 @@ module Release
     # - update_production
     #
     # These are defined in the module Release::Commands
-    def initialize(name, releaser_method)
+    def initialize(name, releaser_method) # rubocop:disable Metrics/MethodLength
       super(name, takes_commands: false)
       short_desc("Prepare composition-repo for #{name}-release")
+
+      options.on('-dDIRECTORY', '--dir DIRECTORY', 'Use DIRECTORY as base to find all repos') do |d|
+        @dir = File.expand_path(d)
+      end
+
+      @wagons = "\w+"
+      options.on(
+        '-wWAGONS', '--wagons WAGONS',
+        'Select wagons to work on with a comma-separated list'
+      ) do |wagons|
+        @wagons = wagons.to_s.tr(',', '|')
+      end
+
+      options.on(
+        '-CDIR',
+        '--composition-dir DIR',
+        'Directory of composition-repo to update at the end'
+      ) do |d|
+        @composition_dir = d
+      end
 
       @releaser_method = releaser_method
     end
 
-    def execute(version) # rubocop:disable Metrics/AbcSize
+    def execute(version) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
       releaser = Release::Main.new(all_wagons)
 
-      releaser.hitobito_group_dir = releaser.composition_repo_dir = dir
-      releaser.version            = version
-      releaser.stage              = name.to_sym
-      releaser.message            = "Update #{name} to #{version}"
-      releaser.dry_run            = command_parser.data[:dry_run]
-      releaser.command_list       = command_parser.data[:command_list]
-      releaser.standard_answer    = true
+      releaser.hitobito_group_dir   = dir
+      releaser.composition_repo_dir = composition_dir
+      releaser.version              = version
+      releaser.stage                = name.to_sym
+      releaser.message              = "Update #{name} to #{version}"
+      releaser.dry_run              = command_parser.data[:dry_run]
+      releaser.command_list         = command_parser.data[:command_list]
+      releaser.standard_answer      = true
 
       raise unless releaser.usable?
 
@@ -43,13 +64,17 @@ module Release
 
     private
 
+    def composition_dir
+      @composition_dir || dir
+    end
+
     def dir
       @dir ||= File.expand_path('../../..', __dir__)
     end
 
     def all_wagons
       Pathname.new(dir).children.flat_map do |dep|
-        dep.to_s.scan(/hitobito_(\w+)/).first
+        dep.to_s.scan(/hitobito_(#{@wagons})/).first
       end.compact
     end
   end

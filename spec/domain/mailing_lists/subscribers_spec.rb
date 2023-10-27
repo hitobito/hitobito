d82eb5237f9e1a9fe70673bff4930f10de0c8859
Diego Steiner <diego.steiner@u041.ch>
2023-10-27 17:59:30 +0200

    Add specs for mailing_list filter_chain

---
+++ spec/domain/mailing_lists/subscribers_spec.rb
@@ -0,0 +1,24 @@
+#  Copyright (c) 2020, Grünliberale Partei Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+
+require 'spec_helper'
+
+describe MailingLists::Subscribers do
+
+  let(:subscribers) { described_class.new(mailing_list) }
+  let(:mailing_list) { mailing_lists(:leaders) }
+
+  describe '#people' do 
+    subject(:subscribers_people) { subscribers.people }
+    let!(:top_leader) { people(:top_leader) }
+
+    context 'without filter_chain' do 
+    end
+
+    context 'with filter_chain' do 
+    end
+  end
+end
---
+++ spec/features/subscriber/filter_controller_spec.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+require 'spec_helper'
+
+describe Subscriber::FilterController, js: true do
+
+  let(:list)  { mailing_lists(:leaders) }
+  let(:group) { list.group }
+  let!(:subscriber_id) { groups(:bottom_layer_one).id } # preload
+
+  before do
+    sign_in
+  end
+
+  it 'edit the language filter for mailing list' do 
+    visit edit_group_mailing_list_filter_path(group.to_param, list.to_param)
+    
+    find('a.accordeon-toggle.header[href="#languages]').click
+
+    check('filters_language_allowed_values_de')
+    	
+    all('form .btn-toolbar').first.click_button 'Speichern'
+
+    expect(page).to have_content('Globale Bedingungen wurden erfolgreich aktualisiert')
+    expect(find('#main')).to have_content('Sprache ist Deutsch')
+  end
+end

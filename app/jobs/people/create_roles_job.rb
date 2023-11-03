cded958f352efd8cd65c7e7771ea0452a8e65422
Andreas Maierhofer <maierhofer@puzzle.ch>
2023-11-03 17:01:00 +0100

    Add job

---
+++ app/jobs/people/create_roles_job.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023, Schweizer Alpen-Club. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+#
+class People::CreateRolesJob < RecurringJob
+
+  run_every 15.minutes
+  Error = Class.new(StandardError)
+
+  def perform
+    future_roles.find_each { |role| convert(role) }
+  end
+
+  private
+
+  def future_roles
+    FutureRole.where('convert_on <= ?', Time.zone.today).order(:convert_on)
+  end
+
+  def convert(role)
+    role.convert!
+  rescue => e
+    message = "#{e.message} - FutureRole(#{role.id})"
+    role.update!(label: message)
+    notify(message)
+  end
+
+  def notify(message)
+    Raven.capture_exception(Error.new(message))
+  end
+end
---
+++ spec/jobs/people/create_roles_job_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023, Schweizer Alpen-Club. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+#
+
+require 'spec_helper'
+
+describe People::CreateRolesJob do
+  let(:person) { people(:bottom_member) }
+  let(:top_group) { groups(:top_group) }
+  let(:role_type) { Group::TopGroup::Member.sti_name }
+  let(:tomorrow) { Time.zone.tomorrow }
+
+
+  subject(:job) { described_class.new }
+
+  def create_future_role(attrs = {})
+    defaults = { person: person, group: top_group, convert_to: role_type }
+    Fabricate(:future_role, defaults.merge(attrs))
+  end
+
+  it 'noops if not scheduled' do
+    expect { job.perform }.to not_change { person.roles.count }
+  end
+
+  it 'noops if node is scheduled for tomorrow' do
+    create_future_role(convert_on: Time.zone.tomorrow)
+    expect { job.perform }.to not_change { person.roles.where(type: role_type).count }
+      .and not_change { person.roles.where(type: FutureRole.sti_name).count }
+  end
+
+  it 'destroys and creates role if scheduled for today' do
+    create_future_role(convert_on: Time.zone.today)
+    expect { job.perform }.to change { person.roles.where(type: role_type).count }.by(1)
+      .and change { person.roles.where(type: FutureRole.sti_name).count }.by(-1)
+  end
+
+  it 'destroys and creates multiples' do
+    create_future_role(convert_on: Time.zone.today)
+    create_future_role(convert_on: Time.zone.today, person: people(:top_leader))
+    expect { job.perform }.to change { Role.where(type: role_type).count }.by(2)
+      .and change { Role.where(type: FutureRole.sti_name).count }.by(-2)
+  end
+
+  it 'logs errors and continues' do
+    role = create_future_role(convert_on: Time.zone.tomorrow)
+    create_future_role(convert_on: Time.zone.tomorrow, person: people(:top_leader))
+    allow_any_instance_of(FutureRole).to receive(:convert!).and_wrap_original do |m|
+      raise 'ouch' if m.receiver.person == person
+      m.call
+    end
+    expect(Raven).to receive(:capture_exception).with(described_class::Error.new("ouch - FutureRole(#{role.id})"))
+
+    expect do
+      travel_to(Time.zone.tomorrow) { job.perform  }
+    end.to change { Role.where(type: role_type).count }.by(1)
+      .and change { Role.where(type: FutureRole.sti_name).count }.by(-1)
+  end
+end
+

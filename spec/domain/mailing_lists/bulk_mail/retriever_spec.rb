526c3eee110f98e300e7e9f21b4372de327de258
Niklas Jäggi <jaeggi@puzzle.ch>
2023-11-06 13:10:44 +0100

    fix email subject nil issue

--- spec/domain/mailing_lists/bulk_mail/retriever_spec.rb
+++ spec/domain/mailing_lists/bulk_mail/retriever_spec.rb
@@ -337,6 +337,30 @@ describe MailingLists::BulkMail::Retriever do
       expect(message.subject).to eq("Anlass hinzugef�gt")
       expect(message.state).to eq('pending')
     end
+
+    it 'does process mail with empty subject and enqueues job for mail delivery' do
+      imap_mail = build_imap_mail(42, nil)
+      allow(imap_connector).to receive(:fetch_mail_by_uid).with(42, :inbox).and_return(imap_mail)
+
+      expect(imap_mail).to receive(:original_to).and_return('leaders@localhost:3000')
+      expect(imap_connector).to receive(:delete_by_uid).with(42, :inbox)
+      expect(imap_mail_validator).to receive(:sender_allowed?).and_return(true)
+
+      expect do
+        retriever.perform
+      end.to change { Message::BulkMail.count }.by(1)
+                                               .and change { MailLog.count }.by(1)
+                                                                            .and change { Delayed::Job.where('handler like "%Messages::DispatchJob%"').count }.by(1)
+                                                                                                                                                              .and change { Delayed::Job.where('handler like "%MailingLists::BulkMail::SenderRejectedMessageJob%"').count }.by(0)
+
+      mail_log = MailLog.find_by(mail_hash: 'abcd42')
+      expect(mail_log.status).to eq('retrieved')
+      expect(mail_log.mail_from).to eq('dude@hitobito.example.com')
+
+      message = mail_log.message
+      expect(message.subject).to eq(nil)
+      expect(message.state).to eq('pending')
+    end
   end
 
   private

f4d79476ac7c680cd952ca497548f12299cf9986
Nils Rauch <rauch@puzzle.ch>
2023-11-13 13:33:57 +0100

    Add license

--- app/jobs/people/create_roles_job.rb
+++ app/jobs/people/create_roles_job.rb
@@ -4,7 +4,7 @@
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
-#
+
 class People::CreateRolesJob < RecurringJob
 
   run_every 15.minutes

962962484700a7da582ce7cacf104ad46678cafd
Diego Steiner <diego.steiner@u041.ch>
2023-12-11 14:24:54 +0100

    refactor move reject_blocked_person! to Authenticatable concern

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -14,6 +14,7 @@ module Authenticatable
     end
 
     before_action :authenticate_person!, if: :authenticate?
+    before_action :reject_blocked_person!, if: :reject_blocked_person?
     check_authorization if: :authorize?
   end
 
@@ -126,5 +127,16 @@ module Authenticatable
 
   def doorkeeper_controller?
     is_a?(Doorkeeper::ApplicationController)
+  end
+
+  def reject_blocked_person!
+    if current_person&.blocked?
+        redirect_to blocked_path
+        return false
+    end
+  end
+
+  def reject_blocked_person?
+    true
   end
 end

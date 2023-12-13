4619c1d0a811bb4350be736423f6e49aed4e54fd
Andreas Maierhofer <maierhofer@puzzle.ch>
2023-12-13 16:03:00 +0100

    Disable prawn utf-8 warning

--- config/application.rb
+++ config/application.rb
@@ -131,4 +131,9 @@ module Hitobito
   end
 end
 
+# PDF's built-in fonts have very limited support for internationalized text.
+# If you need full UTF-8 support, consider using an external font instead.
+# To disable this warning, add the following line to your code:
+# Prawn::Fonts::AFM.hide_m17n_warning = true
+Prawn::Fonts::AFM.hide_m17n_warning = true
 require 'prawn/measurement_extensions'

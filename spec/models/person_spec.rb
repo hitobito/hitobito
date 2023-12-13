37182b8c74a74eeb8bb4b6da7127e71c45996b38
Matthias Viehweger <viehweger@puzzle.ch>
2023-12-13 23:21:08 +0100

    Remove potential carrierwave-reference

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2022, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2023, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -508,7 +508,6 @@ describe Person do
   end
 
   xdescribe '#picture' do
-    # include CarrierWave::Test::Matchers
     let(:person) { Fabricate(:person) }
 
     before do

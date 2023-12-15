c2b15e5c8363191d1178a3d269aa538a3081c86f
Diego Steiner <diego.steiner@u041.ch>
2023-12-15 16:13:26 +0100

    chore: update comment headers

--- app/domain/person/block_service.rb
+++ app/domain/person/block_service.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023, Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 class Person::BlockService
   def initialize(person, current_user: nil)
     @person = person

3f1ac20fb26fb454b8b03ac1e1390e9c6b5edff7
Mountain Star <github@mtnstar.net>
2023-12-18 13:27:24 +0100

    Fix/improve decorated role name

--- spec/decorators/role_decorator_spec.rb
+++ spec/decorators/role_decorator_spec.rb
@@ -17,79 +17,98 @@ describe RoleDecorator, :draper_with_helpers do
     travel_to(today.midnight) { example.run }
   end
 
-  describe 'outdated roles' do
-    let(:tomorrow) { Time.zone.tomorrow }
-    let(:title) { node.find_css('i.fa.fa-exclamation-triangle')[0].attr('title') }
+  context 'for_aside' do
+    let(:node) { Capybara::Node::Simple.new(decorator.for_aside) }
+    let(:decorated_name) { decorator.for_aside }
 
-    subject(:node) { Capybara::Node::Simple.new(decorator.for_aside) }
+    it 'includes bis' do
+      role.delete_on = Time.zone.local(2023, 12, 12)
 
-    it 'wraps role#to_s in strong tag wihtout triangle' do
-      expect(node).to have_css('strong', text: role.to_s)
+      formatted_name = '<strong>Leader</strong>&nbsp;(bis 12.12.2023)' 
+
+      expect(decorated_name).to eq(formatted_name)
+    end
+
+    it 'includes outdated info' do
+      role.delete_on = Time.zone.local(2023, 1, 1)
+      warning_title = node.find_css('i.fa.fa-exclamation-triangle')[0].attr('title')
+
+      expect(node).to have_css('i.fa.fa-exclamation-triangle')
+      expect(warning_title).to eq 'Die Rolle konnte nicht wie geplant am 01.01.2023 terminiert werden. Falls das Speichern der Rolle diese nicht terminiert, wende dich bitte an den Support.'
+      expect(decorated_name).to include('<strong>Leader</strong>&nbsp;(bis 01.01.2023)')
+    end
+
+    it 'includes label' do
+      role.label = '42'
+      formatted_name = '<strong>Leader</strong>&nbsp;(42)' 
+
+      expect(decorated_name).to eq(formatted_name)
+    end
+
+    context 'future role' do
+      let(:role) do
+        FutureRole.new(convert_on: Time.zone.local(2023, 12, 12),
+                      convert_to: Group::BottomLayer::Leader)
+      end
+
+      it 'includes ab' do
+        formatted_name = '<strong>Leader</strong>&nbsp;(ab 12.12.2023)' 
+
+        expect(decorated_name).to eq(formatted_name)
+      end
+
+      it 'includes ab and label' do
+        role.label = '42'
+        formatted_name = '<strong>Leader</strong>&nbsp;(42)&nbsp;(ab 12.12.2023)' 
+
+        expect(decorated_name).to eq(formatted_name)
+      end
+    end
+
+  end
+
+  context 'for_history' do
+    let(:node) { Capybara::Node::Simple.new(decorator.for_history) }
+    let(:decorated_name) { decorator.for_history }
+
+    it 'role#to_s without strong tag and without triangle' do
+      expect(node).not_to have_css('strong', text: role.to_s)
       expect(node).not_to have_css('i.fa.fa-exclamation-triangle')
     end
 
-    context 'role marked for deletion' do
-      it 'does not render triangle if delete_on is in the future' do
-        role.delete_on = tomorrow
-        expect(node).not_to have_css('i.fa.fa-exclamation-triangle')
-      end
+    it 'never includes bis and is not bold' do
+      role.delete_on = Time.zone.local(2023, 12, 12)
 
-      it 'does renders triangle if outdated' do
-        role.delete_on = today
-        expect(node).to have_css('i.fa.fa-exclamation-triangle')
-        expect(node).to have_css('strong', text: role.to_s)
-        expect(title).to eq 'Die Rolle konnte nicht wie geplant am 13.11.2023 terminiert werden. Falls das Speichern der Rolle diese nicht terminiert, wende dich bitte an den Support.'
-      end
+      expect(decorated_name).to eq('Leader')
     end
 
-    context 'role marked for conversion' do
-      let(:group) { groups(:top_group) }
+    it 'includes outdated info and is not bold' do
+      role.delete_on = Time.zone.local(2023, 1, 1)
+      warning_title = node.find_css('i.fa.fa-exclamation-triangle')[0].attr('title')
+
+      expect(node).to have_css('i.fa.fa-exclamation-triangle')
+      expect(warning_title).to eq 'Die Rolle konnte nicht wie geplant am 01.01.2023 terminiert werden. Falls das Speichern der Rolle diese nicht terminiert, wende dich bitte an den Support.'
+      expect(decorated_name).not_to include('<strong>Leader</strong>&nbsp;(bis 01.01.2023)')
+      expect(decorated_name).to include('Leader')
+    end
+
+    it 'includes label and is not bold' do
+      role.label = '42'
+      formatted_name = 'Leader&nbsp;(42)' 
+
+      expect(decorated_name).to eq(formatted_name)
+    end
+
+    context 'future role' do
       let(:role) do
-        Fabricate.build(:future_role, convert_to: group.role_types.first, group: group)
+        FutureRole.new(convert_on: Time.zone.local(2023, 12, 12),
+                      convert_to: Group::BottomLayer::Leader)
       end
 
-      it 'does not render triangle if delete_on is in the future' do
-        role.convert_on = tomorrow
-        expect(node).not_to have_css('i.fa.fa-exclamation-triangle')
-      end
-
-      it 'does renders triangle if outdated' do
-        role.convert_on = today
-        expect(node).to have_css('i.fa.fa-exclamation-triangle')
-        expect(node).to have_css('strong', text: role.to_s)
-        expect(title).to eq 'Die Rolle konnte nicht wie geplant per 13.11.2023 aktiviert werden. Falls das Speichern der Rolle diese nicht aktiviert, wende dich bitte an den Support.'
+      it 'never includes ab and is not bold' do
+        expect(decorated_name).to eq('Leader')
       end
     end
-  end
 
-  context 'terminated role' do
-    let(:context) { double('context') }
-    let(:role) { roles(:bottom_member) }
-
-    [:for_aside, :for_history].each do |method|
-      describe "##{method}" do
-        it 'should return the role name' do
-          assert decorator.terminated? == false
-
-          expect(decorator.send(method)).to eq '<strong>Member</strong>'
-        end
-
-        it 'should return the role name and the termination date' do
-          role.write_attribute(:terminated, true)
-          role.deleted_at = today
-
-          expect(decorator.send(method)).to eq "<strong>Member</strong><br><span>Austritt per #{I18n.l(role.terminated_on)}</span>"
-        end
-
-        it 'should return the role name, outdated info and the termination date' do
-          role.write_attribute(:terminated, true)
-          role.delete_on = today
-
-          outdated_and_terminated_text = "<i title=\"Die Rolle konnte nicht wie geplant am 13.11.2023 terminiert werden. Falls das Speichern der Rolle diese nicht terminiert, wende dich bitte an den Support.\" class=\"fa fa-exclamation-triangle\"></i>&nbsp;<strong>Member (Bis 13.11.2023)</strong><br><span>Austritt per 13.11.2023</span>" 
-
-          expect(decorator.send(method)).to eq outdated_and_terminated_text
-        end
-      end
-    end
   end
 end

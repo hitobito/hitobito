cf14a7df5cb13a06c0d362e555471bf3c16b7234
Andreas Maierhofer <maierhofer@puzzle.ch>
2023-12-22 16:05:31 +0100

    Revert spec helper changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ require 'graphiti_spec_helpers/rspec'
 require 'view_component/test_helpers'
 require 'view_component/system_test_helpers'
 
-require 'test_prof/recipes/logging'
+require "test_prof/recipes/logging"
 
 TestProf::StackProf.configure do |config|
   config.format = 'json'
@@ -59,7 +59,7 @@ end
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each do |f|
   # Do not require the core testgroup/layer files when running in wagon
-  next if f =~ %r{spec/support/group/(?!0_base.rb)} && (ENV['APP_ROOT'].present? || ENV['RAILS_USE_TEST_GROUPS'].blank?)
+  next if f =~ %r[spec/support/group/(?!0_base.rb)] && (ENV['APP_ROOT'].present? || ENV['RAILS_USE_TEST_GROUPS'].blank?)
 
   require f
 end
@@ -71,7 +71,7 @@ RSpec::Matchers.define_negated_matcher :not_change, :change
 
 RSpec.configure do |config|
 
-  config.fixture_path = "#{Rails.root.join('spec', 'fixtures', 'fixtures')}"
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -209,19 +209,13 @@ end
 require 'capybara/rails'
 require 'capybara-screenshot/rspec'
 
-# Disable driver deprecations until we upgrade to latest selenium
-Selenium::WebDriver.logger.ignore(:logger_info)
-Selenium::WebDriver.logger.ignore(:add_option)
-Selenium::WebDriver.logger.ignore(:option_symbols)
-
-Capybara.server = :puma, { Silent: true }
 Capybara.server_port = ENV['CAPYBARA_SERVER_PORT'].to_i if ENV['CAPYBARA_SERVER_PORT']
 Capybara.default_max_wait_time = ENV.fetch('CAPYBARA_MAX_WAIT_TIME', 6).to_f
 Capybara.automatic_label_click = true
 
 Capybara::Screenshot.prune_strategy = :keep_last_run
 Capybara::Screenshot::RSpec::REPORTERS['RSpec::Core::Formatters::ProgressFormatter'] =
-  CapybaraScreenshotPlainTextReporter
+CapybaraScreenshotPlainTextReporter
 Capybara::Screenshot.register_driver(:chrome) do |driver, path|
   driver.browser.save_screenshot(path)
 end
@@ -234,11 +228,8 @@ Capybara.register_driver :chrome do |app|
   options.args << '--disable-dev-shm-usage' # helps with docker resource limitations
   options.args << '--window-size=1800,1000'
   options.args << '--crash-dumps-dir=/tmp'
-  options.add_option('prefs', { 'intl.accept_languages': 'de-CH,de' })
-  if ENV['CAPYBARA_CHROME_BINARY'].present?
-    options.add_option('binary',
-                       ENV['CAPYBARA_CHROME_BINARY'])
-  end
+  options.add_option('prefs', {'intl.accept_languages': 'de-CH,de'})
+  options.add_option('binary', ENV['CAPYBARA_CHROME_BINARY']) if ENV['CAPYBARA_CHROME_BINARY'].present?
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
@@ -257,8 +248,8 @@ end)
 
 module ActiveRecordFixture
   def initialize(fixture, model_class)
-    if model_class == Person && !fixture.key?('confirmed_at')
-      fixture['confirmed_at'] = 1.day.ago
+    if model_class == Person
+      fixture['confirmed_at'] = 1.day.ago unless fixture.key?('confirmed_at')
     end
 
     super(fixture, model_class)
@@ -266,4 +257,4 @@ module ActiveRecordFixture
 end
 ActiveRecord::Fixture.prepend(ActiveRecordFixture)
 
-# GraphitiSpecHelpers::RSpec.schema!
+#GraphitiSpecHelpers::RSpec.schema!

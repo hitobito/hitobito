146eb9a34275d1877f3f38797a6bde11eb6b05bb
Daniel Illi <daniel@illi.zone>
2024-01-09 17:09:54 +0100

    Reorganize mailing list form with tabs

--- spec/features/mailing_lists_spec.rb
+++ spec/features/mailing_lists_spec.rb
@@ -18,45 +18,55 @@ describe MailingListsController, js: true do
   it 'removes two labels from existing mailing' do
     list.update(preferred_labels: %w(Mutter Vater))
     visit edit_group_mailing_list_path(list.group, list)
-
+    click_link("Mailing-Liste (E-Mail)")
     all('span.chip a')[0].click
-    find('.top button[type=submit]').click
+    click_button "Speichern"
+
     expect(page).not_to have_content 'Mutter'
+    expect(page).to have_content 'Vater'
 
     visit edit_group_mailing_list_path(list.group, list)
+    click_link("Mailing-Liste (E-Mail)")
     all('span.chip a')[0].click
-    find('.top button[type=submit]').click
+    click_button "Speichern"
+
     expect(page).not_to have_content 'Vater'
   end
 
   it 'adds single label to new mailing list' do
     visit new_group_mailing_list_path(list.group)
     fill_in 'Name', with: 'test'
-
+    click_link("Mailing-Liste (E-Mail)")
+    fill_in 'Mailinglisten Adresse', with: 'test'
     find('.chip-add').click
     fill_in id: 'label', with: 'Vater'
     page.find('body').click # blur
+    find_link('Allgemein', class: 'active') # wait for blur to complete
+    click_link("Mailing-Liste (E-Mail)")
     expect(page).to have_content 'Vater'
 
-    find('.top button[type=submit]').click
-
+    click_button "Speichern"
     expect(page).to have_content 'Vater'
   end
 
   it 'adds two preferred_labels to existing mailing list' do
     visit edit_group_mailing_list_path(list.group, list)
-
+    click_link("Mailing-Liste (E-Mail)")
     find('.chip-add').click
     fill_in id: 'label', with: 'Vater'
     page.find('body').click # blur
+    find_link('Allgemein', class: 'active') # wait for blur to complete
+    click_link("Mailing-Liste (E-Mail)")
     expect(page).to have_content 'Vater'
 
     find('.chip-add').click
     fill_in id: 'label', with: 'Mutter'
     page.find('body').click # blur
+    find_link('Allgemein', class: 'active') # wait for blur to complete
+    click_link("Mailing-Liste (E-Mail)")
     expect(page).to have_content 'Mutter'
 
-    find('.top button[type=submit]').click
+    click_button "Speichern"
 
     expect(page).to have_content 'Mutter, Vater'
   end

e013ece4fdb7ff57fbef54f8d9a3f58467f63fde
Niklas Jäggi <jaeggi@puzzle.ch>
2024-01-15 13:32:06 +0100

    adjust scripts, create database on docker compose up

---
+++ docker/postgresql-setup.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE hitobito_test
+  WITH TEMPLATE = template0
+       ENCODING = 'UTF8'
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8';
+       
+GRANT ALL PRIVILEGES ON DATABASE hitobito_test TO hitobito;
---
+++ docker/test-setup-postgresql.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE hitobito_test_core
+  WITH TEMPLATE = template0
+       ENCODING = 'UTF8'
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8';

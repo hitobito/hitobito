9651ed2d85cc7918b1fb88ca7b51c07ec2a76a26
Mountain Star <github@mtnstar.net>
2024-01-15 13:54:23 +0100

    Make it possible to provide person ids to duplicate locator job

--- app/jobs/people/duplicate_locator_job.rb
+++ app/jobs/people/duplicate_locator_job.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2020, CVP Schweiz. This file is part of
-#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  Copyright (c) 2012-2024, CVP Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_cvp.
+#  https://github.com/hitobito/hitobito.
 
 class People::DuplicateLocatorJob < RecurringJob
+  self.parameters = [:people_ids]
   run_every 1.day
 
+  def initialize(people_ids = nil)
+    super()
+    @people_scope = Person.where(id: people_ids) if people_ids
+  end
+
   def perform
-    People::DuplicateLocator.new.run
+    if @people_scope
+      locator = People::DuplicateLocator.new(@people_scope)
+    else
+      locator = People::DuplicateLocator.new
+    end
+    locator.run
   end
 
 end
---
+++ spec/jobs/people/duplicate_locator_job_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023, Schweizer Alpen-Club. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+#
+
+require 'spec_helper'
+
+describe People::DuplicateLocatorJob do
+  let(:person) { people(:bottom_member) }
+  let(:top_group) { groups(:top_group) }
+  let(:role_type) { Group::TopGroup::Member.sti_name }
+  let(:tomorrow) { Time.zone.tomorrow }
+
+
+  context 'with no job arguments' do
+    subject(:job) { described_class.new }
+
+    it 'Runs job concerning all person entries' do
+      expect(People::DuplicateLocator).to receive(:new)
+        .with(no_args)
+        .and_call_original
+
+      job.perform
+    end
+  end
+
+  context 'with specific person id' do
+    let(:people_ids) { Person.first.id }
+    subject(:job) { described_class.new(people_ids) }
+
+    it 'Runs job for a single person entry' do
+      people_scope = Person.where(id: Person.first.id)
+      expect(People::DuplicateLocator).to receive(:new)
+        .with(people_scope)
+        .and_call_original
+
+      job.perform
+    end
+  end
+end

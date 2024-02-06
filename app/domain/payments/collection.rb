4b866ee6fbe549554850a0bab912184daec0c0a4
Niklas Jäggi <jaeggi@puzzle.ch>
2024-02-06 17:10:43 +0100

    fix invoice item evaluation spec

--- app/domain/payments/collection.rb
+++ app/domain/payments/collection.rb
@@ -90,9 +90,9 @@ class Payments::Collection
 
   def of_non_fully_paid_invoices
     invoice_ids =
-      @payments.select(:'invoice.total', :invoice_id)
+      @payments.select('MAX(\'invoice.total\')', :invoice_id)
                .joins('INNER JOIN invoices AS invoice ON invoice.id = payments.invoice_id')
-               .having('SUM(payments.amount) < invoice.total')
+               .having('SUM(payments.amount) < MAX(invoice.total)')
                .group(:invoice_id)
                .map(&:invoice_id)
     @payments = @payments.where(invoice_id: invoice_ids)
@@ -102,9 +102,9 @@ class Payments::Collection
 
   def of_overpaid_invoices
     invoice_ids =
-      @payments.select(:'invoice.total', :invoice_id)
+      @payments.select('MAX(\'invoice.total\')', :invoice_id)
                .joins('INNER JOIN invoices AS invoice ON invoice.id = payments.invoice_id')
-               .having('SUM(payments.amount) > invoice.total')
+               .having('SUM(payments.amount) > MAX(invoice.total)')
                .group(:invoice_id)
                .map(&:invoice_id)
     @payments = @payments.where(invoice_id: invoice_ids)

3a346620d0d4c73e29b5c727768a6d1a5a2285ba
Matthias Viehweger <viehweger@puzzle.ch>
2024-02-12 09:09:19 +0100

    Allow warnings to be resolved

--- app/domain/export/tabular/groups/list.rb
+++ app/domain/export/tabular/groups/list.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2014-2021, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2014-2024, Pfadibewegung Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -8,13 +8,15 @@
 module Export::Tabular::Groups
   class List < Export::Tabular::Base
 
+    # rubocop:disable Style/MutableConstant meant to be extended in wagons
     EXCLUDED_ATTRS = %w(lft rgt contact_id require_person_add_requests logo
                         created_at deleted_at archived_at letter_logo
                         creator_id updater_id deleter_id self_registration_role_type
                         encrypted_text_message_username encrypted_text_message_password
                         letter_address_position text_message_provider text_message_originator
                         self_registration_notification_email custom_self_registration_title
-                        main_self_registration_group privacy_policy_title privacy_policy).freeze
+                        main_self_registration_group privacy_policy_title privacy_policy)
+    # rubocop:enable Style/MutableConstant
 
     self.model_class = Group
     self.row_class = Export::Tabular::Groups::Row

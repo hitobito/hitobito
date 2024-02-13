fc3706a81c36cf6c4b8f97456a69ef47fbb71d98
Matthias Viehweger <viehweger@puzzle.ch>
2024-02-13 16:30:30 +0100

    Update schema to current version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_19_120948) do
+ActiveRecord::Schema.define(version: 2024_01_26_100757) do
 
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false

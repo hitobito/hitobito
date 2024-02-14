8617163ee4722af75cc8865b9f3396d40d69c169
Mountain Star <github@mtnstar.net>
2024-02-14 07:38:28 +0100

    Add missing license header

--- db/migrate/20240212145649_add_multi_column_index_on_hitobito_log_entries.rb
+++ db/migrate/20240212145649_add_multi_column_index_on_hitobito_log_entries.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito
+
 class AddMultiColumnIndexOnHitobitoLogEntries < ActiveRecord::Migration[6.1]
   def change
     add_index :hitobito_log_entries,

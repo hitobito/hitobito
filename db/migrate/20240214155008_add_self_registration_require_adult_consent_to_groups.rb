5eb723bdd4ec090f779d3b336d7e5e456cd11542
Mountain Star <github@mtnstar.net>
2024-02-21 09:08:48 +0100

    Add missing license header

--- db/migrate/20240214155008_add_self_registration_require_adult_consent_to_groups.rb
+++ db/migrate/20240214155008_add_self_registration_require_adult_consent_to_groups.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito
+
 class AddSelfRegistrationRequireAdultConsentToGroups < ActiveRecord::Migration[6.1]
   def change
     change_table(:groups) do |t|

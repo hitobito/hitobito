ce092444c499c75c396c5cc965e6b6e931c6258a
Nils Rauch <rauch@puzzle.ch>
2024-02-22 13:23:01 +0100

    Do not freeze column constant as it should be extendable in wagons
    Co-authored-by: Matthias Viehweger <viehweger@puzzle.ch>

--- app/domain/export/tabular/payments/list.rb
+++ app/domain/export/tabular/payments/list.rb
@@ -11,11 +11,11 @@ module Export::Tabular::Payments
     self.model_class = Payment
     self.row_class = Export::Tabular::Payments::Row
 
+    # rubocop:disable Style/MutableConstant meant to be extended in wagons
     INCLUDED_ATTRS = %w(id amount received_at reference
-                        transaction_identifier status).freeze
-
-
+                        transaction_identifier status)
     CUSTOM_METHODS = %w(payee_person_name payee_person_address)
+    # rubocop:enable Style/MutableConstant
 
     def attributes
       INCLUDED_ATTRS + CUSTOM_METHODS

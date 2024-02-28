1a071279c3bfe682eee618de462711aa76428c21
Matthias Viehweger <viehweger@puzzle.ch>
2024-02-28 10:00:49 +0100

    Conform to linter and styleguide

--- spec/features/event/participation_spec.rb
+++ spec/features/event/participation_spec.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024-2024, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
 
 require 'spec_helper'
 
@@ -21,8 +27,10 @@ describe :event_participation do
       image = ActiveStorage::Blob.create_and_upload!(io: File.open(file, 'rb'),
                                                      filename: 'logo.png',
                                                      content_type: 'image/png').signed_id
-      top_layer.layer_group.update(privacy_policy: image, privacy_policy_title: 'Privacy Policy Top Layer')
-      group.update(privacy_policy: image, privacy_policy_title: 'Additional Policies Bottom Layer')
+      top_layer.layer_group.update(privacy_policy: image,
+                                   privacy_policy_title: 'Privacy Policy Top Layer')
+      group.update(privacy_policy: image,
+                   privacy_policy_title: 'Additional Policies Bottom Layer')
     end
 
     it 'creates an event participation if privacy policy is accepted' do
@@ -39,9 +47,12 @@ describe :event_participation do
 
       expect do
         click_button('Anmelden')
-        is_expected.to have_text("Teilnahme von #{person.full_name} in #{event.name} wurde erfolgreich erstellt. Bitte überprüfe die Kontaktdaten und passe diese gegebenenfalls an.")
+        is_expected.to have_text(
+          "Teilnahme von #{person.full_name} in #{event.name} wurde erfolgreich erstellt. " \
+          'Bitte überprüfe die Kontaktdaten und passe diese gegebenenfalls an.'
+        )
       end.to change { Event::Participation.count }.by(1)
-      
+
       participation = Event::Participation.find_by(event: event, person: person)
 
       expect(participation).to be_present

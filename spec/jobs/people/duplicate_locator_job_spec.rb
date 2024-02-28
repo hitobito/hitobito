e611f5e618d621c415067776124c5cd52996805a
Matthias Viehweger <viehweger@puzzle.ch>
2024-02-28 08:49:16 +0100

    Give it a little more leeway

--- spec/jobs/people/duplicate_locator_job_spec.rb
+++ spec/jobs/people/duplicate_locator_job_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2023, Schweizer Alpen-Club. This file is part of
+#  Copyright (c) 2023-2024, Schweizer Alpen-Club. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -16,6 +16,6 @@ describe People::DuplicateLocatorJob do
     expect(People::DuplicateLocator).to receive_message_chain(:new, :run)
     expect { job.perform }.to not_change { Person.count }
       .and change { job.delayed_jobs.count }.by(1)
-    expect(job.delayed_jobs.first.run_at).to be_within(5.minutes).of(1.day.from_now)
+    expect(job.delayed_jobs.first.run_at).to be_within(10.minutes).of(1.day.from_now)
   end
 end

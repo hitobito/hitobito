748c4b789fbb7e0cb76d3114fe0b9c085fc9eeee
Matthias Viehweger <viehweger@puzzle.ch>
2024-03-03 18:05:53 +0100

    Update error-message wording from external library

--- spec/models/event/attachment_spec.rb
+++ spec/models/event/attachment_spec.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2015, Pro Natura Schweiz. This file is part of
+#  Copyright (c) 2015-2024, Pro Natura Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -27,7 +27,7 @@ describe Event::Attachment do
       File.write(file, 'x' * 12.megabytes)
       a.file.attach(io: file, filename: 'foo.png')
       expect(a).not_to be_valid
-      expect(a.errors.full_messages.join).to match(/nicht grösser als 2 MB/)
+      expect(a.errors.full_messages.join).to match(/muss kleiner oder gleich 2 MB/)
     end
   end
 end

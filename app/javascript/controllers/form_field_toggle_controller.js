369a7dd0e31643a8902d33a6246030ac41c1f4eb
Nils Rauch <rauch@puzzle.ch>
2024-03-11 08:46:04 +0100

    Check for toggle state during connect

--- app/javascript/controllers/form_field_toggle_controller.js
+++ app/javascript/controllers/form_field_toggle_controller.js
@@ -1,11 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
-import debounce from "lodash.debounce";
 
 export default class extends Controller {
-  static targets = ["toggle"];
+  static targets = ["select", "toggle"];
+
+  connect() {
+    this.toggle()
+  }
 
   toggle(event) {
-    const selected = event.target.options[event.target.options.selectedIndex];
+    const selected = this.selectTarget.options[this.selectTarget.options.selectedIndex];
 
     if (selected.dataset.visibility === "true") {
       this.toggleTarget.classList.remove("hidden");

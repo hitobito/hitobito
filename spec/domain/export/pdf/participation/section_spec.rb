77fbe950fe9065ad1c5cac8470781b5ea17e97a3
Matthias Viehweger <viehweger@puzzle.ch>
2024-03-18 12:18:20 +0100

    Some dates are rendered wider than others in PDFs
    Therefore I reduced the gutter-width a little to avoid the wrapping

--- spec/domain/export/pdf/participation/section_spec.rb
+++ spec/domain/export/pdf/participation/section_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2023, Puzzle ITC. This file is part of
+#  Copyright (c) 2023-2024, Puzzle ITC. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -15,37 +15,37 @@ describe Export::Pdf::Participation::Section do
   describe '#render_columns' do
     def render_columns
       described_class.new(pdf, nil).send(:render_columns, left, right)
-      pdf.text "~~~END~~~"
+      pdf.text '~~~END~~~'
     end
 
     context 'high left low right' do
-      let(:left) { ->{ 3.times { pdf.text 'high' } } }
-      let(:right) { ->{ pdf.text 'low' } }
+      let(:left) { -> { 3.times { pdf.text 'high' } } }
+      let(:right) { -> { pdf.text 'low' } }
 
       it 'renders correctly' do
         render_columns
         expect(text_with_position.pretty_inspect).to eq [
-          [57, 777, "high"],
-          [57, 763, "high"],
-          [57, 749, "high"],
-          [303, 777, "low"],
-          [57, 725, "~~~END~~~"]
+          [57, 777, 'high'],
+          [57, 763, 'high'],
+          [57, 749, 'high'],
+          [301, 777, 'low'],
+          [57, 725, '~~~END~~~']
         ].pretty_inspect
       end
     end
 
     context 'low left high right' do
-      let(:left) { ->{ pdf.text 'low' } }
-      let(:right) { ->{ 3.times { pdf.text 'high' } } }
+      let(:left) { -> { pdf.text 'low' } }
+      let(:right) { -> { 3.times { pdf.text 'high' } } }
 
       it 'renders correctly' do
         render_columns
         expect(text_with_position.pretty_inspect).to eq [
-          [57, 777, "low"],
-          [303, 777, "high"],
-          [303, 763, "high"],
-          [303, 749, "high"],
-          [57, 725, "~~~END~~~"]
+          [57, 777, 'low'],
+          [301, 777, 'high'],
+          [301, 763, 'high'],
+          [301, 749, 'high'],
+          [57, 725, '~~~END~~~']
         ].pretty_inspect
       end
     end

14c594373999ea7b8fa19acada151e7e0b6e6210
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-03-20 14:40:54 +0100

    Fix some license headers

--- app/models/self_registration/main_person.rb
+++ app/models/self_registration/main_person.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2023-2024, Schweizer Alpen-Club. This file is part of
-#  hitobito_sac_cas and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_sac_cas.
-
+#  Copyright (c) 2023, Schweizer Alpen-Club. This file is part of hitobito and licensed under the
+#  Affero General Public License version 3 or later. See the COPYING file at the top-level directory
+#  or at https://github.com/hitobito/hitobito.
 
 class SelfRegistration::MainPerson < SelfRegistration::Person
 

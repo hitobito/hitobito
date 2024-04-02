dc4561fd6fcb1660ecdd87744bee5639f5f1a58a
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-04-02 11:51:28 +0200

    Add minimum participants to events
    refs hitobito/hitobito_sac_cas#355

--- app/models/event/course.rb
+++ app/models/event/course.rb
@@ -9,45 +9,51 @@
 #
 # Table name: events
 #
-#  id                          :integer          not null, primary key
-#  applicant_count             :integer          default(0)
-#  application_closing_at      :date
-#  application_conditions      :text(16777215)
-#  application_opening_at      :date
-#  applications_cancelable     :boolean          default(FALSE), not null
-#  cost                        :string(255)
-#  description                 :text(16777215)
-#  display_booking_info        :boolean          default(TRUE), not null
-#  external_applications       :boolean          default(FALSE)
-#  hidden_contact_attrs        :text(16777215)
-#  location                    :text(16777215)
-#  maximum_participants        :integer
-#  motto                       :string(255)
-#  name                        :string(255)      not null
-#  number                      :string(255)
-#  participant_count           :integer          default(0)
-#  participations_visible      :boolean          default(FALSE), not null
-#  priorization                :boolean          default(FALSE), not null
-#  required_contact_attrs      :text(16777215)
-#  requires_approval           :boolean          default(FALSE), not null
-#  signature                   :boolean
-#  signature_confirmation      :boolean
-#  signature_confirmation_text :string(255)
-#  state                       :string(60)
-#  teamer_count                :integer          default(0)
-#  type                        :string(255)
-#  waiting_list                :boolean          default(TRUE), not null
-#  created_at                  :datetime
-#  updated_at                  :datetime
-#  application_contact_id      :integer
-#  contact_id                  :integer
-#  creator_id                  :integer
-#  kind_id                     :integer
-#  updater_id                  :integer
+#  id                               :integer          not null, primary key
+#  applicant_count                  :integer          default(0)
+#  application_closing_at           :date
+#  application_conditions           :text(65535)
+#  application_opening_at           :date
+#  applications_cancelable          :boolean          default(FALSE), not null
+#  cost                             :string(255)
+#  description                      :text(65535)
+#  display_booking_info             :boolean          default(TRUE), not null
+#  external_applications            :boolean          default(FALSE)
+#  globally_visible                 :boolean
+#  hidden_contact_attrs             :text(65535)
+#  location                         :text(65535)
+#  maximum_participants             :integer
+#  minimum_participants             :integer
+#  motto                            :string(255)
+#  name                             :string(255)
+#  notify_contact_on_participations :boolean          default(FALSE), not null
+#  number                           :string(255)
+#  participant_count                :integer          default(0)
+#  participations_visible           :boolean          default(FALSE), not null
+#  priorization                     :boolean          default(FALSE), not null
+#  required_contact_attrs           :text(65535)
+#  requires_approval                :boolean          default(FALSE), not null
+#  shared_access_token              :string(255)
+#  signature                        :boolean
+#  signature_confirmation           :boolean
+#  signature_confirmation_text      :string(255)
+#  state                            :string(60)
+#  teamer_count                     :integer          default(0)
+#  training_days                    :decimal(5, 2)
+#  type                             :string(255)
+#  waiting_list                     :boolean          default(TRUE), not null
+#  created_at                       :datetime
+#  updated_at                       :datetime
+#  application_contact_id           :integer
+#  contact_id                       :integer
+#  creator_id                       :integer
+#  kind_id                          :integer
+#  updater_id                       :integer
 #
 # Indexes
 #
-#  index_events_on_kind_id  (kind_id)
+#  index_events_on_kind_id              (kind_id)
+#  index_events_on_shared_access_token  (shared_access_token)
 #
 
 # A course is a specialised Event that has by default applications,
@@ -58,7 +64,8 @@ class Event::Course < Event
   require_dependency 'event/course/role/participant'
 
   self.used_attributes += [:number, :kind_id, :state, :priorization, :group_ids,
-                           :requires_approval, :display_booking_info, :waiting_list]
+                           :requires_approval, :display_booking_info, :waiting_list,
+                           :minimum_participants]
 
   self.role_types = [Event::Role::Leader,
                      Event::Role::AssistantLeader,

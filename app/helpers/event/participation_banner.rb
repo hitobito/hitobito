4978a134930cdc4bae6a9966bf5e0eba1fa4eb31
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-04-12 16:13:27 +0200

    Pass in_button_group true in participation_banner

--- app/helpers/event/participation_banner.rb
+++ app/helpers/event/participation_banner.rb
@@ -63,7 +63,8 @@ class Event::ParticipationBanner
         confirm: t('event.participations.cancel_application.confirmation'),
         method: :delete
       },
-      class: 'ms-2'
+      class: 'ms-2',
+      in_button_group: true
     )
   end
 

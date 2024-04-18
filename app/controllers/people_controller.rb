52a47bec174d97a6ff5cd07051600622701d3cd1
Matthias Viehweger <viehweger@puzzle.ch>
2024-04-18 15:40:36 +0200

    Actually add :language to the permitted_attrs, not [:language]

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2022, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2024, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -22,7 +22,7 @@ class PeopleController < CrudController
                           [household_people_ids: []] +
                           [relations_to_tails_attributes: [:id, :tail_id, :kind, :_destroy]]
   FeatureGate.if(:person_language) do
-    self.permitted_attrs << [:language]
+    permitted_attrs << :language
   end
 
   # required to allow api calls

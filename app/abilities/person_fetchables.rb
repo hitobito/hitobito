2d99ebadc86a65979d74aad2a30321153678a146
Daniel Illi <daniel@illi.zone>
2024-04-22 09:28:50 +0200

    Re-introduce PersonFetchables to not disrupt inheriting classes in wagons
    refs #2537

---
+++ app/abilities/person_fetchables.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2024, Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+# Common Base Class for fetching people.
+class PersonFetchables < GroupBasedFetchables
+
+end

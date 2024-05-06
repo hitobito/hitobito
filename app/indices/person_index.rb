8a12a333ee89f4ec3daefb908f25742f4a58e75e
Matthias Viehweger <viehweger@puzzle.ch>
2024-05-06 15:05:30 +0200

    Index new fields for search as well

--- app/indices/person_index.rb
+++ app/indices/person_index.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2024, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -7,7 +7,9 @@ module PersonIndex; end
 
 ThinkingSphinx::Index.define_partial :person do
   indexes first_name, last_name, company_name, nickname, company, email, sortable: true
-  indexes address, zip_code, town, country, birthday, additional_information
+  indexes address_care_of, street, housenumber, postbox
+  indexes address # TODO: remove when cleaning structured_addresses migration
+  indexes zip_code, town, country, birthday, additional_information
 
   indexes phone_numbers.number, as: :phone_number
   indexes social_accounts.name, as: :social_account

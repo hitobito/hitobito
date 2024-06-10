c62aaa35b6ce14cd24b2c11b191d796775b4b697
Niklas Jäggi <jaeggi@puzzle.ch>
2024-06-10 10:49:25 +0200

    fix further regression and mailer specs

--- app/domain/export/tabular/people/participation_row.rb
+++ app/domain/export/tabular/people/participation_row.rb
@@ -21,7 +21,7 @@ module Export::Tabular::People
     end
 
     def created_at
-      normalize(participation.created_at.to_date)
+      normalize(participation.created_at&.to_date)
     end
 
     def question_attribute(attr)

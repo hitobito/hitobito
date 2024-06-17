676ecb4cfce4fcecd44431a6fe40a8f63293f9a6
megli2 <egli@puzzle.ch>
2024-06-17 15:46:19 +0200

    replace rescuing of MySql error with rescuing of Standard error

--- app/controllers/crud_controller.rb
+++ app/controllers/crud_controller.rb
@@ -124,7 +124,7 @@ class CrudController < ListController
   # perform the save operation
   def save_entry
     entry.save
-  rescue Mysql2::Error => e
+  rescue StandardError => e
     Airbrake.notify(e, parameters: params)
     logger.error e.message
     false

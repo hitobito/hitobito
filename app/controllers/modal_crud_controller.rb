c9c6aed34b88f0d3db11911d57119463c412c2ab
megli2 <egli@puzzle.ch>
2024-06-21 13:36:59 +0200

    resolve requested changes

---
+++ app/controllers/modal_crud_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2021, CVP Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+# A Crud controller for modal actions
+# except index
+class ModalCrudController < CrudController
+
+  respond_to :js
+
+end

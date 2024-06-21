c771f13342811cfa6214c6b494b364e889909d6d
megli2 <egli@puzzle.ch>
2024-06-21 16:38:10 +0200

    restructure sequence class and modify tests to fit new implementation

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -7,14 +7,16 @@
 
 class Sequence
   def self.current_value(sequence_name)
-    sql = "SELECT currval('#{ActiveRecord::Base.connection.quote_table_name(sequence_name)}')"
+    sql = "SELECT currval('#{sequence_name}')"
     ActiveRecord::Base.connection.select_value(sql)
   rescue ActiveRecord::StatementInvalid
-    select("MAX(household_key::integer) AS max_household_key").take.max_household_key
+    ActiveRecord::Base.connection.execute 'ROLLBACK'
+    current_sequence_id = Person.pluck("MAX(household_key::integer)").first
+    current_sequence_id.nil? ? 1 : current_sequence_id
   end
 
   def self.increment!(sequence_name)
-    sql = ActiveRecord::Base.send(:sanitize_sql_array, ["SELECT nextval(?)", sequence_name])
+    sql = "SELECT nextval('#{sequence_name}')"
     ActiveRecord::Base.connection.select_value(sql)
   end
 end

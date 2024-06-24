3f057ee5a9dd38835a59a6e99f60b1382e8d2444
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-06-24 15:39:49 +0200

    Add Wizards base controller

---
+++ app/controllers/wizards/base_controller.rb
@@ -0,0 +1,57 @@
+module Wizards
+  class BaseController < ApplicationController
+    helper_method :wizard
+
+    def new; end
+
+    def create
+      return render :new if params[:autosubmit].present?
+      return save_and_redirect if wizard.valid? && wizard.last_step?
+
+      wizard.move_on
+      render :new
+    end
+
+    private
+
+    def save_and_redirect
+      save_wizard
+      redirect_to redirect_target, notice: success_message
+    end
+
+    def save_wizard
+      ApplicationRecord.transaction do
+        wizard.save!
+      end
+    end
+
+    def wizard
+      @wizard ||= model_class.new(
+        current_ability:,
+        params: model_params.to_unsafe_h,
+        current_step: params[:step]
+      )
+    end
+
+    def model_params
+      params[model_identifier] || ActionController::Parameters.new
+    end
+
+    def model_identifier
+      @model_identifier ||= model_class.model_name.param_key
+    end
+
+    def model_class
+      raise 'Implement in subclass'
+    end
+
+    def success_message
+      raise 'Implement in subclass'
+    end
+
+    def redirect_target
+      raise 'Implement in subclass'
+    end
+
+  end
+end

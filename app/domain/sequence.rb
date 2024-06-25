a154d7319c75ffb879b0fe4fdcae35c151702175
megli2 <egli@puzzle.ch>
2024-06-25 10:55:19 +0200

    remove errorhandling for sequence and migrate string column to integer

--- app/domain/sequence.rb
+++ app/domain/sequence.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2023, Schweizer Alpen-Club. This file is part of
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
 #  hitobito_sac_cas and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sac_cas
@@ -9,10 +9,6 @@ class Sequence
   def self.current_value(sequence_name)
     sql = "SELECT currval('#{sequence_name}')"
     ActiveRecord::Base.connection.select_value(sql)
-  rescue ActiveRecord::StatementInvalid
-    ActiveRecord::Base.connection.execute 'ROLLBACK'
-    current_sequence_id = Person.pluck("MAX(household_key::integer)").first
-    current_sequence_id.nil? ? 1 : current_sequence_id
   end
 
   def self.increment!(sequence_name)

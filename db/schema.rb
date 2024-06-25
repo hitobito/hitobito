f82943c6624cc51056f3f7c2fdba5d41ce63987e
megli2 <egli@puzzle.ch>
2024-06-25 11:08:06 +0200

    reset changes in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_17_140927) do
+ActiveRecord::Schema.define(version: 2024_05_14_141836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

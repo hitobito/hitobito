1d12676de77a9adfb0178dead1261fbb74eb8764
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-07-30 12:28:30 +0200

    Support debounce when forwarding clicks

--- app/javascript/controllers/forwarder_controller.js
+++ app/javascript/controllers/forwarder_controller.js
@@ -3,12 +3,29 @@
 // or later. See the COPYING file at the top-level directory or at
 // https://github.com/hitobito/hitobito
 
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import debounce from "lodash.debounce";
 
 export default class extends Controller {
-  static targets = [ "click" ]
+  static targets = ["click"];
+  static values = {
+    delay: {
+      type: Number,
+      default: 0,
+    },
+  };
+
+  initialize() {
+    this.click = this.click.bind(this);
+  }
+
+  connect() {
+    if (this.delayValue > 0) {
+      this.click = debounce(this.click, this.delayValue);
+    }
+  }
 
   click() {
-    this.clickTargets.forEach(target => target.click())
+    this.clickTargets.forEach((target) => target.click());
   }
 }

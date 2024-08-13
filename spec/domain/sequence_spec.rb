5b7a891b7a6ff12325cecaef20b6d49898f8ba61
megli2 <egli@puzzle.ch>
2024-08-13 13:36:45 +0200

    make rubocop happy again

--- spec/domain/sequence_spec.rb
+++ spec/domain/sequence_spec.rb
@@ -8,23 +8,22 @@
 require "spec_helper"
 
 describe Sequence do
-
   setup do
     ActiveRecord::Base.connection.execute("CREATE SEQUENCE test_sequence START 1")
-    Sequence.increment!('test_sequence')
+    Sequence.increment!("test_sequence")
   end
 
-  context '::current_value' do
-    it 'returns the current_value of the sequence' do
-      expect(Sequence.current_value('test_sequence')).to eq 1
+  context "::current_value" do
+    it "returns the current_value of the sequence" do
+      expect(Sequence.current_value("test_sequence")).to eq 1
     end
   end
 
-  context '::increment!' do
-    it 'increments the current_value of the sequence' do
-      Sequence.increment!('test_sequence')
-      Sequence.increment!('test_sequence')
-      expect(Sequence.current_value('test_sequence')).to eq 3
+  context "::increment!" do
+    it "increments the current_value of the sequence" do
+      Sequence.increment!("test_sequence")
+      Sequence.increment!("test_sequence")
+      expect(Sequence.current_value("test_sequence")).to eq 3
     end
   end
 end

fba46112ee7ee18bdcfbce92a57d47ab18f491f0
megli2 <egli@puzzle.ch>
2024-09-04 16:50:42 +0200

    cleanup flakey fix

--- app/domain/export/tabular/people/households.rb
+++ app/domain/export/tabular/people/households.rb
@@ -26,9 +26,7 @@ module Export::Tabular::People
       @household_list ||= begin # rubocop:disable Naming/MemoizedInstanceVariableName @list is already used in the base-class, which shadows this extension
         people = super
         people = Person.where(id: people) unless people.respond_to?(:only_public_data)
-
-        people = people.order(:first_name)
-        ::People::HouseholdList.new(people.only_public_data.includes(:primary_group))
+        ::People::HouseholdList.new(people.order(:first_name).only_public_data.includes(:primary_group))
       end
     end
   end

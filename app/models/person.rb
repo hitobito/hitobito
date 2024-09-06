2a26812a2a60c50e707add00bcd4a53a46835f61
Chris <76159444+hunchr@users.noreply.github.com>
2024-09-06 11:40:46 +0000

    Update address of all household members on save

--- app/models/person.rb
+++ app/models/person.rb
@@ -297,6 +297,7 @@ class Person < ActiveRecord::Base # rubocop:disable Metrics/ClassLength
   before_validation :override_blank_email
   before_validation :remove_blank_relations
   after_update :schedule_duplicate_locator
+  after_save_commit :update_household_address
   before_destroy :destroy_roles
   before_destroy :destroy_person_duplicates
 
@@ -558,6 +559,12 @@ class Person < ActiveRecord::Base # rubocop:disable Metrics/ClassLength
     if !company? && first_name.blank? && last_name.blank? && nickname.blank?
       errors.add(:base, :name_missing)
     end
+  end
+
+  def update_household_address
+    return if (Person::ADDRESS_ATTRS & saved_changes.keys).empty?
+
+    household_people.update_all(attributes.slice(*Person::ADDRESS_ATTRS))
   end
 
   # Destroy all related roles before destroying this person.

642372e64b9c8976613dde91e60d67ac7f25c2c1
megli2 <egli@puzzle.ch>
2024-09-10 16:14:25 +0200

    Comment logger out to fix it back later

--- app/helpers/sheet/hitobito_log_entry.rb
+++ app/helpers/sheet/hitobito_log_entry.rb
@@ -4,8 +4,8 @@ module Sheet
   class HitobitoLogEntry < Sheet::Admin
     tab "hitobito_log_entries.tabs.all", "hitobito_log_entries_path", no_alt: true
 
-    Hitobito.logger.categories.each do |category|
-      tab category.capitalize, "hitobito_log_entries_path", params: {category: category}
-    end
+    # Hitobito.logger.categories.each do |category|
+    #   tab category.capitalize, "hitobito_log_entries_path", params: {category: category}
+    # end
   end
 end

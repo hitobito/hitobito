7dc560ed8061ead566e4cf4d82dba00785c4d91a
megli2 <egli@puzzle.ch>
2024-09-11 15:44:47 +0200

    Fix model specs by defining correct host in development and testsetup and use to_s method instead of to_fs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  routes.default_url_options[:host] = 'localhost:3000'
 end

31246218ae014fa027459dcd08527ad77074789b
Daniel Illi <daniel@illi.zone>
2024-09-12 14:38:21 +0200

    Implement preloading of unscoped/custom scoped person roles

---
+++ app/models/concerns/preload_roles_unscoped.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito
+
+module PreloadRolesUnscoped
+  def do_preload_roles(people)
+    roles_scope = @roles_scope || Role.with_inactive
+    ActiveRecord::Associations::Preloader.new.preload(people, :roles, roles_scope)
+  end
+
+  def find(...)
+    super.tap { |person| do_preload_roles([person]) }
+  end
+
+  def exec_queries
+    super.tap { |people| do_preload_roles(people) }
+  end
+
+  def roles_scope(roles_scope)
+    @roles_scope = roles_scope
+    self
+  end
+end

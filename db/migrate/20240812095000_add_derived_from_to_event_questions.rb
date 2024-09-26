78c49e1bbe92e293991ec61e690c4b2a7f9a0324
Diego Steiner <diego.steiner@u041.ch>
2024-09-26 23:11:34 +0200

    fix: extract migration into service (hitobito_youth#58)

--- db/migrate/20240812095000_add_derived_from_to_event_questions.rb
+++ db/migrate/20240812095000_add_derived_from_to_event_questions.rb
@@ -1,38 +1,8 @@
 class AddDerivedFromToEventQuestions < ActiveRecord::Migration[6.1]
   def change
-    Event::Question.reset_column_information
     add_reference :event_questions, :derived_from_question, null: true,
                   foreign_key: false, type: :integer
-    reversible do |direction|
-      direction.up { create_derived_questions }
-      direction.down { delete_derived_questions }
-    end
-  end
 
-  private
-
-  def create_derived_questions
-    Event::Question.where(event: nil).find_each do |global_question|
-      Event::Question.transaction do
-        existing_derived_questions = Event::Question.where(derived_from_question_id: global_question.id)
-        existing_event_ids = Event.where.not(id: existing_derived_questions.pluck(:event_id)).pluck(:id)
-        existing_event_ids.each do |event_id|
-          derived_question = global_question.dup
-          derived_question.update!(event_id: event_id, derived_from_question: global_question)
-          Event::Answer.joins(:participation)
-                       .where(participation: {event_id: event_id}, question_id: global_question.id)
-                       .update_all(question_id: derived_question.id)
-        end
-      end
-    end
-  end
-
-  def delete_derived_questions
-    global_question_ids = Event::Question.where(event: nil).pluck(:id)
-    global_question_ids.each do |global_question_id|
-      derived_question_ids = Event::Question.where.not(derived_from_question_id: global_question_id).pluck(:id)
-      Event::Answer.where(question_id: derived_question_ids).update_all(question_id: global_question_id)
-      Event::Question.where(id: derived_question_ids).destroy_all
-    end
+    Event::Question.reset_column_information
   end
 end

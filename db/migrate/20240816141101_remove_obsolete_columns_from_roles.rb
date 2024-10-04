14d60d6381efd1036743f821923d9a099c91f858
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-10-04 11:02:30 +0200

    Add missing license headers to migration

--- db/migrate/20240816141101_remove_obsolete_columns_from_roles.rb
+++ db/migrate/20240816141101_remove_obsolete_columns_from_roles.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 class RemoveObsoleteColumnsFromRoles < ActiveRecord::Migration[6.1]
   def change
     change_table :roles, bulk: true do |t|

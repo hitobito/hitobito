c6be044fbeb131dd24c379c3897f1b2d24f6862d
Daniel Illi <daniel@illi.zone>
2024-10-07 18:10:42 +0200

    Comment on PreloadRolesUnscoped

--- app/models/concerns/preload_roles_unscoped.rb
+++ app/models/concerns/preload_roles_unscoped.rb
@@ -5,18 +5,28 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito
 
+# This module is used to extend the roles relation in Person#preload_roles_unscoped /
+# #preload_roles to allow preloading the roles association unscoped or with a provided
+# scope despite the default scope that is present on Role.
 module PreloadRolesUnscoped
-  def do_preload_roles(people)
+
+  # Preloads the roles association for the given people, either unscoped or with the
+  # scope set by an earlier call to #roles_scope.
+  def do_preload_roles(entries)
     roles_scope = @roles_scope || Role.with_inactive
-    ActiveRecord::Associations::Preloader.new.preload(people, :roles, roles_scope)
+    ActiveRecord::Associations::Preloader.new.preload(entries, :roles, roles_scope)
   end
 
+  # This method is called by activerecord to find a single record by id.
+  # We override it to preload the roles association.
   def find(...)
-    super.tap { |person| do_preload_roles([person]) }
+    super.tap { |entry| do_preload_roles([entry]) }
   end
 
+  # This method is called by activerecord to execute the query on a relation.
+  # We override it to preload the roles association for the people that are returned.
   def exec_queries
-    super.tap { |people| do_preload_roles(people) }
+    super.tap { |entries| do_preload_roles(entries) }
   end
 
   def roles_scope(roles_scope)

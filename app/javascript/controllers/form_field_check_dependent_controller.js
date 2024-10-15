99e0c9378b3890dbab233efd9426ff15e89f3419
Niklas <68543592+njaeggi@users.noreply.github.com>
2024-10-15 15:42:29 +0200

    Add Stimulus controller to check dependent checkboxes (hitobito/hitobito_sac_cas#901)
    (#2865)
    
    * Remove console log
    
    * Add action to handle master checkbox
    
    * Implement review
    
    * Rename methods

---
+++ app/javascript/controllers/form_field_check_dependent_controller.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+// hitobito_sac_cas and licensed under the Affero General Public License version 3
+// or later. See the COPYING file at the top-level directory or at
+// https://github.com/hitobito/hitobito
+//
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["main", "dependent"]
+
+  // unselects all dependent checkboxes when unchecking master checkbox
+  toggleMain() {
+    if(!event.target.checked) {
+      this.dependentTargets.forEach((checkbox) => {
+        checkbox.checked = false;
+      });
+    }
+  }
+
+  // selects master checkbox if any dependent checkbox is checked
+  toggleDependent() {    
+    this.mainTarget.checked = true;
+  }
+}

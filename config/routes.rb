d6a51a3b824b3c921055d74ae687f03aa2550aa5
Chris <76159444+hunchr@users.noreply.github.com>
2024-10-18 09:56:35 +0000

    Add Event::KindCategory API endpoint (hitobito/hitobito_sac_cas#1055) (#2920)

--- config/routes.rb
+++ config/routes.rb
@@ -416,6 +416,7 @@ Hitobito::Application.routes.draw do
     resources :groups, only: [:index, :show]
     resources :events, only: [:index, :show]
     resources :event_kinds, module: :event, controller: :kinds, only: [:index, :show]
+    resources :event_kind_categories, module: :event, controller: :kind_categories, only: [:index, :show]
     resources :invoices, only: [:index, :show, :update]
     resources :roles, except: [:edit, :new]
   end

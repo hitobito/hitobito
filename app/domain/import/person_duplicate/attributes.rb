48cc68b6f24bb5b13b3cfae9161e876652030a19
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-10-24 09:15:01 +0200

    Add license, inline detector into locator

--- app/domain/import/person_duplicate/attributes.rb
+++ app/domain/import/person_duplicate/attributes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of hitobito and licensed under the
+#  Affero General Public License version 3 or later. See the COPYING file at the top-level directory
+#  or at https://github.com/hitobito/hitobito.
+
 module Import::PersonDuplicate::Attributes
   extend ActiveSupport::Concern
   DUPLICATE_ATTRIBUTES = [

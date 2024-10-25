c43d1bca5c9f767f7d59b6d20fc7c944faa4c16a
megli2 <egli@puzzle.ch>
2024-10-25 14:22:05 +0200

    Set load_default to 7 because all framework defaults seem to work

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Hitobito
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

6f73cdc2c8645a5c26e751f68071a52edc7e26aa
Andi Idogawa <Largo@users.noreply.github.com>
2024-10-25 09:03:35 +0200

    Reduce DuplicateLocator memory usage by extracting conditions from finder  (#2824)

--- app/domain/import/person_duplicate_finder.rb
+++ app/domain/import/person_duplicate_finder.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #  Copyright (c) 2012-2020, CVP Schweiz. This file is part of
 #  hitobito_cvp and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
@@ -8,93 +10,49 @@ module Import
     include Translatable
 
     # if multiple rows match the same existing person, always return the same object
-    attr_reader :duplicate_entries
-
-    DUPLICATE_ATTRIBUTES = [
-      :first_name,
-      :last_name,
-      :company_name,
-      :zip_code,
-      :birthday
-    ]
+    attr_reader :unique_entries
 
     def initialize
-      @duplicate_entries = {}
+      @unique_entries = {}
     end
 
+    def unique_count
+      unique_entries.size
+    end
+
+    # returns the first duplicate with errors if there are multiple
     def find(attrs)
-      people = duplicates(attrs)
-      if people.present?
-        person = people.first
-        if people.size == 1
-          duplicate_entries[person.id] ||= person
-        else
-          person.errors.add(:base, translate(:duplicates, count: people.size))
-          person
-        end
-      end
-    end
+      result = duplicate_ids_with_first_person(attrs)
+      people_ids = result[:people_ids]
+      return if people_ids.blank?
 
-    def duplicate_count
-      duplicate_entries.size
+      person = result[:first_person]
+      handle_duplicate(person, people_ids)
     end
 
     private
 
-    def duplicates(attrs)
-      conditions = duplicate_conditions(attrs)
-      if conditions.first.present?
-        ::Person.where(conditions).to_a
+    def find_first_person(people_ids)
+      ::Person.find(people_ids.first) if people_ids.present?
+    end
+
+    def handle_duplicate(person, people_ids)
+      if people_ids.size == 1
+        # set new unique entry or use and return existing entry
+        unique_entries[person.id] ||= person
       else
-        []
+        person.errors.add(:base, translate(:duplicates, count: people_ids.size))
+        person
       end
     end
 
-    def duplicate_conditions(attrs)
-      [""].tap do |conditions|
-        append_duplicate_conditions(attrs, conditions)
-        append_email_condition(attrs, conditions)
-      end
-    end
-
-    def append_duplicate_conditions(attrs, conditions)
-      exisiting_duplicate_attrs(attrs).each do |key, value|
-        conditions.first << " AND " if conditions.first.present?
-        conditions.first << if %w[first_name last_name company_name].include?(key.to_s)
-          "#{key} = ?"
-        else
-          "(#{key} = ? OR #{key} IS NULL)"
-        end
-        value = parse_date(value) if key.to_sym == :birthday
-        conditions << value
-      end
-    end
-
-    def append_email_condition(attrs, conditions)
-      if attrs[:email].present?
-        if conditions.first.present?
-          conditions[0] = "(#{conditions[0]}) OR "
-        end
-        conditions.first << "email = ?"
-        conditions << attrs[:email]
-      end
-    end
-
-    def exisiting_duplicate_attrs(attrs)
-      existing = attrs.select do |key, value|
-        value.present? && DUPLICATE_ATTRIBUTES.include?(key.to_sym)
-      end
-      existing.delete(:birthday) unless parse_date(existing[:birthday])
-      existing
-    end
-
-    def parse_date(date_string)
-      if date_string.present?
-        begin
-          ActiveRecord::Type::Date.new.cast(date_string)
-        rescue ArgumentError
-          nil
-        end
+    def duplicate_ids_with_first_person(attrs)
+      conditions = People::DuplicateConditions.new(attrs).build
+      if conditions.first.present?
+        people_ids = ::Person.where(conditions).pluck(:id)
+        {people_ids:, first_person: find_first_person(people_ids)}
+      else
+        {people_ids: [], first_person: nil}
       end
     end
   end

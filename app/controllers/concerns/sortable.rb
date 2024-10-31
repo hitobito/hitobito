92d665234e75eb069ae6aa2668f5daee96c25086
Niklas Jäggi <jaeggi@puzzle.ch>
2024-10-31 14:39:40 +0100

    Adjust sortable

--- app/controllers/concerns/sortable.rb
+++ app/controllers/concerns/sortable.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2012-2015, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2024, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -11,6 +11,7 @@ module Sortable
   SUBQUERY = /\bFROM\s+\(\s*SELECT\b/i
   GROUPED_QUERY = /\bGROUP\s+BY\b/i
   TABLE_WITH_COLUMN = /\b\w+\.(\w+)/
+  SIMPLE_SORT_EXPRESSION = /^\s*[a-zA-Z_][a-zA-Z0-9_]*\.[a-zA-Z_][a-zA-Z0-9_]*\s+(asc|desc)\s+(NULLS\s+(FIRST|LAST))?\s*$/
 
   # Adds a :sort_mappings class attribute.
   included do
@@ -38,12 +39,15 @@ module Sortable
     def list_entries
       return super unless sorting?
 
-      scope = super.reorder(Arel.sql(sort_expression))
-      scope = scope.joins(join_tables)
-      return scope unless scope.distinct_value
+      if sort_expression.match?(SIMPLE_SORT_EXPRESSION) && table_exists?(sort_expression_attrs.split(".").first)
+        sort_by_sort_expression(super)
+      else
+        scope = super.joins(join_tables).reorder(Arel.sql(sort_expression))
+        return scope unless scope.distinct_value
 
-      select_values = scope.select_values.presence || "#{model_class.table_name}.*"
-      scope.select(select_values, sort_expression_attrs)
+        select_values = scope.select_values.presence || "#{model_class.table_name}.*"
+        scope.select(select_values, sort_expression_attrs)
+      end
     end
 
     def sort_by_sort_expression(entries)
@@ -56,21 +60,25 @@ module Sortable
       elsif entries.to_sql.match?(GROUPED_QUERY) # already selecting from a grouped query (e.g. sbv/song_counts_controller.rb)
         entries.reorder(sort_expression)
       else
-        subquery = entries.select(sort_expression_attrs).joins(join_tables).unscope(:order).distinct_on(:id)
+        subquery = entries.unscope(:select, :order).select(sort_expression_attrs, model_class.column_names).joins(join_tables).unscope(:order).distinct_on(:id)
         model_class.select("*").from(subquery, :subquery)
           .reorder(Arel.sql(sort_expression.gsub(TABLE_WITH_COLUMN, '\1')))
       end
+    end
+
+    def model_table_name
+      model_class.table_name
     end
 
     def sorting?
       params[:sort].present? && sortable?(params[:sort])
     end
 
-    # Return sort columns from defined mappings or as null_safe_sort from parameter.
     def sort_columns
-      sort_mappings = sort_mappings_with_indifferent_access
-      sort_columns_expression = sort_mappings[params[:sort]].is_a?(Hash) ? sort_mappings.dig(params[:sort], :order) : sort_mappings[params[:sort]]
-      sort_columns_expression || "#{model_class.table_name}.#{params[:sort]}"
+      sort_columns_expression = sort_mappings_with_indifferent_access[params[:sort]].is_a?(Hash) ?
+                                sort_mappings_with_indifferent_access[params[:sort]][:order] :
+                                sort_mappings_with_indifferent_access[params[:sort]]
+      sort_columns_expression || params[:sort].to_s
     end
 
     def join_tables
@@ -80,15 +88,21 @@ module Sortable
 
     # Return the sort expression to be used in the list query.
     def sort_expression
-      Array(sort_columns).collect { |c| "#{c} #{sort_dir}" }.join(", ") + " NULLS LAST"
+      if sort_expression_attrs.empty?
+        Array(sort_columns).collect { |c|
+          "#{model_table_name}.#{c} #{sort_dir} NULLS LAST"
+        }.join(", ")
+      else
+        Array(sort_columns).collect { |c| "#{c} #{sort_dir} NULLS LAST" }.join(", ")
+      end
     end
 
     # Return the sort expression attributes without sort directory, to add to query select list
     # Reject sort expression attributes from same table, to prevent ambiguous selection
     # of attributes
     def sort_expression_attrs
-      Array(sort_columns).reject { |col| model_class.column_names.include?(col.split(".")[-1]) }
-        .map(&:to_s)
+      Array(sort_columns).reject { |col| model_class.column_names.include?(col) }
+        .collect { |c| c.to_s }
         .join(", ")
     end
 
@@ -101,6 +115,10 @@ module Sortable
     def sortable?(attr)
       model_class.column_names.include?(attr.to_s) ||
         sort_mappings_with_indifferent_access.include?(attr)
+    end
+
+    def table_exists?(table_name)
+      ActiveRecord::Base.connection.table_exists?(table_name)
     end
   end
 end

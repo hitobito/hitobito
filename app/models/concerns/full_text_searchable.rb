753cbe9896fd312494fa361a304dec0f1212721f
Niklas Jäggi <jaeggi@puzzle.ch>
2024-11-04 10:36:27 +0100

    Replace pg_search_scope with method

---
+++ app/models/concerns/full_text_searchable.rb
@@ -0,0 +1,37 @@
+#  Copyright (c) 2012-2024, Hitobito AG. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+module FullTextSearchable
+  def self.included(model)
+    model.define_singleton_method(:search) do |term|
+      # Use & to make sure every word in term has to match the result
+      sanitized_term = term.split.map { |t| ActiveRecord::Base.sanitize_sql_like(t) + ":*" }.join(" & ")
+
+      # Generate base search query and rank for main model
+      base_query = "#{model.table_name}.search_column @@ to_tsquery('simple', '#{sanitized_term}')"
+      base_rank = "COALESCE(ts_rank(#{model.table_name}.search_column, to_tsquery('simple', '#{sanitized_term}')), 0)"
+
+      associated_models = model::SEARCHABLE_ATTRS.select { |attr| attr.is_a?(Hash) }.map(&:keys).flatten
+  
+      # Build queries and ranks for each associated model
+      associated_queries = associated_models.map do |assoc_model|
+        "#{assoc_model}.search_column @@ to_tsquery('simple', '#{sanitized_term}')"
+      end
+      associated_ranks = associated_models.map do |assoc_model|
+        "COALESCE(ts_rank(#{assoc_model}.search_column, to_tsquery('simple', '#{sanitized_term}')), 0)"
+      end
+
+      # Combine main model ans associated model for query
+      search_conditions = ([base_query] + associated_queries).join(' OR ')
+      search_ranks = ([base_rank] + associated_ranks).join(', ')
+  
+      select(model.column_names, "GREATEST(#{search_ranks}) AS rank")
+        .left_joins(associated_models)
+        .where(search_conditions, term: "#{term}:*")
+        .order("rank DESC")
+        .distinct
+    end
+  end  
+end

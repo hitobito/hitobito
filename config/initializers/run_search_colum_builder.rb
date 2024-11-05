f84effe598b04a5454e31ff58a1e123a15f092e3
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-11-05 08:37:59 +0100

    Move to class and into domain folder

---
+++ config/initializers/run_search_colum_builder.rb
@@ -0,0 +1,12 @@
+# Copyright (c) 2017-2024, Hitobito AG. This file is part of
+# hitobito and licensed under the Affero General Public License version 3
+# or later. See the COPYING file at the top-level directory or at
+# https://github.com/hitobito/hitobito.
+
+# Adds a tsvector column to each model that includes the FullTextSearchable module,
+# enabling full-text search functionality. Also adds a GIN index for faster querying.
+
+Rails.application.config.after_initialize do
+  Rails.application.eager_load!
+  SearchColumnBuilder.new.run
+end

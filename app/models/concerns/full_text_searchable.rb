48706c1282fbf7bd7b47a6bfde4346fad341f9b6
Niklas Jäggi <jaeggi@puzzle.ch>
2024-11-07 14:05:50 +0100

    Add search_column variable to module

--- app/models/concerns/full_text_searchable.rb
+++ app/models/concerns/full_text_searchable.rb
@@ -4,38 +4,42 @@
 #  https://github.com/hitobito/hitobito.
 
 module FullTextSearchable
+  SEARCH_COLUMN = :search_column
+
   def self.included(model)
+    model.ignored_columns += [SEARCH_COLUMN]
+
     model.define_singleton_method(:search) do |term|
       # Use & to make sure every word in term has to match the result
       sanitized_term = term.split.map { |t| ActiveRecord::Base.sanitize_sql_like(t) + ":*" }.join(" & ")
 
       # Generate base search query and rank for main model
-      base_query = "#{model.table_name}.search_column @@ to_tsquery('simple', '#{sanitized_term}')"
-      base_rank = "COALESCE(ts_rank(#{model.table_name}.search_column, to_tsquery('simple', '#{sanitized_term}')), 0)"
+      base_query = "#{model.table_name}.#{SEARCH_COLUMN} @@ to_tsquery('simple', '#{sanitized_term}')"
+      base_rank = "COALESCE(ts_rank(#{model.table_name}.#{SEARCH_COLUMN}, to_tsquery('simple', '#{sanitized_term}')), 0)"
 
-      associated_models = model::SEARCHABLE_ATTRS.select { |attr| attr.is_a?(Hash) }.map(&:keys).flatten
+      associated_tables = model::SEARCHABLE_ATTRS.select { |attr| attr.is_a?(Hash) }.map(&:keys).flatten
 
       # Build queries and ranks for each associated model
-      associated_queries = associated_models.map do |assoc_model|
-        "#{assoc_model}.search_column @@ to_tsquery('simple', '#{sanitized_term}')"
+      associated_queries = associated_tables.map do |assoc_model|
+        "#{assoc_model}.#{SEARCH_COLUMN} @@ to_tsquery('simple', '#{sanitized_term}')"
       end
-      associated_ranks = associated_models.map do |assoc_model|
-        "COALESCE(ts_rank(#{assoc_model}.search_column, to_tsquery('simple', '#{sanitized_term}')), 0)"
+      associated_ranks = associated_tables.map do |assoc_model|
+        "COALESCE(ts_rank(#{assoc_model}.#{SEARCH_COLUMN}, to_tsquery('simple', '#{sanitized_term}')), 0)"
       end
 
       # Combine main model ans associated model for query
       search_conditions = ([base_query] + associated_queries).join(" OR ")
       search_ranks = ([base_rank] + associated_ranks).join(", ")
 
-      join_tables = associated_models.map do |associated_model|
-        if associated_model.to_s.end_with?("_translations")
-          if model.table_name.to_sym == associated_model.to_s.split("_").first.pluralize.to_sym
+      join_tables = associated_tables.map do |associated_table|
+        if associated_table.to_s.end_with?("_translations")
+          if model.table_name.to_sym == associated_table.to_s.split("_").first.pluralize.to_sym
             :translations
           else
-            [associated_model.to_s.split("_").first.pluralize.to_sym, associated_model.to_s.split("_").first.pluralize.to_sym => :translations]
+            [associated_table.to_s.split("_").first.pluralize.to_sym, associated_table.to_s.split("_").first.pluralize.to_sym => :translations]
           end
         else
-          associated_model
+          associated_table
         end
       end
 

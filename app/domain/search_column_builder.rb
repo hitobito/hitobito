0b6559be57b0dd2ebbcee4490274dc9e01691ff8
Niklas Jäggi <jaeggi@puzzle.ch>
2024-11-07 14:05:10 +0100

    Adjust builder to only use table names

--- app/domain/search_column_builder.rb
+++ app/domain/search_column_builder.rb
@@ -3,25 +3,19 @@
 # or later. See the COPYING file at the top-level directory or at
 # https://github.com/hitobito/hitobito.
 
-# Adds a tsvector column to each model that includes the FullTextSearchable module,
+# Adds a tsvector column to each table that includes the FullTextSearchable module,
 # enabling full-text search functionality. Also adds a GIN index for faster querying.
 
 class SearchColumnBuilder
   class_attribute :searchable_models, default: [
     Person, Group, Event, Invoice, Address
   ]
-
-  class_attribute :association_mappings, default: {
-    event_translations: Event::Translation
-  }
-
-  SEARCH_COLUMN = :search_column
+  SEARCH_COLUMN = FullTextSearchable::SEARCH_COLUMN
 
   def initialize(drop_columns: true)
     @drop_columns = drop_columns
   end
 
-  # Select models with FullTextSearchable module included (exlcuding STI subclasses)
   def run
     # do not run if there are still migrations needed (to prevent running this before wagon migrations)
     return if ActiveRecord::Base.connection.migration_context.needs_migration?
@@ -30,23 +24,15 @@ class SearchColumnBuilder
     searchable_models.each do |model|
       next unless connection.table_exists?(model.table_name)
 
-      with_resetting_model(model) do
-        create_columns_for_attrs(model, model::SEARCHABLE_ATTRS)
-      end
+      create_columns_for_attrs(model.table_name, model::SEARCHABLE_ATTRS)
     end
   end
 
   private
 
-  def with_resetting_model(model)
-    model.ignored_columns += [:search_column]
-    yield
-    model.reset_column_information
-  end
-
   # Create searchable columns for main table and associations
-  def create_columns_for_attrs(model, attrs)
-    create_searchable_column_and_index(model, model.table_name, attrs.select { |attr| attr.is_a?(Symbol) })
+  def create_columns_for_attrs(table_name, attrs)
+    create_searchable_column_and_index(table_name, attrs.select { |attr| attr.is_a?(Symbol) })
 
     attrs.select { |attr| attr.is_a?(Hash) }.each do |associated_columns|
       create_columns_for_association(associated_columns)
@@ -56,15 +42,12 @@ class SearchColumnBuilder
   # Create searchable column on associated tables
   def create_columns_for_association(associated_columns)
     associated_columns.each do |table, columns|
-      model_class = table.to_s.classify.safe_constantize || association_mappings.fetch(table.to_sym)
-      with_resetting_model(model_class) do
-        create_searchable_column_and_index(model_class, model_class.table_name, columns)
-      end
+      create_searchable_column_and_index(table, columns)
     end
   end
 
   # Adds or replaces a tsvector column and associated GIN index on specified columns
-  def create_searchable_column_and_index(model, table_name, attrs)
+  def create_searchable_column_and_index(table_name, attrs)
     # check if every attribute exists on the table (after_initilize is also called before the wagon migrations so the wagon attributes still don't exist)
     return unless attrs.all? { |attr| connection.column_exists?(table_name, attr.to_s) }
     # return if the search_column is already generated on this table, unless drop_columns is true
@@ -86,13 +69,17 @@ class SearchColumnBuilder
         #{ts_vector_statement(attrs)}
       ) STORED;
     SQL
-    connection.execute(statement)
+    migration.say_with_time "Creating Search Column #{table_name}" do
+      connection.execute(statement)
+    end
   end
 
   def create_search_index(table_name, quoted_table_name)
-    connection.execute <<~SQL
-      CREATE INDEX "#{table_name}_search_column_gin_idx" ON #{quoted_table_name} USING GIN (#{SEARCH_COLUMN});
-    SQL
+    migration.say_with_time "Creating Search Index #{table_name}" do
+      connection.execute <<~SQL
+        CREATE INDEX "#{table_name}_search_column_gin_idx" ON #{quoted_table_name} USING GIN (#{SEARCH_COLUMN});
+      SQL
+    end
   end
 
   def ts_vector_statement(attrs)
@@ -108,6 +95,7 @@ class SearchColumnBuilder
     )"
   end
 
+  # directly parsing a date to text is not immutable and not possible inside a generated column
   def convert_date_field_to_text(quoted_column_name)
     "CASE
       WHEN #{quoted_column_name} IS NOT NULL THEN

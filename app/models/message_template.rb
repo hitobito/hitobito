e1ffeb308670be1fe2802e40031449bc87fe06d9
Diego Steiner <diego.steiner@u041.ch>
2024-11-12 09:30:21 +0100

    fix: add header comments for new files (hitobito_sww#28)

--- app/models/message_template.rb
+++ app/models/message_template.rb
@@ -1,3 +1,10 @@
+#  frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Wanderwege. This file is part of
+#  hitobito_sww and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww.class MessageTemplate < ApplicationRecord
+
 class MessageTemplate < ApplicationRecord
   belongs_to :templated, polymorphic: true
 
--- db/migrate/20241031081851_create_message_templates.rb
+++ db/migrate/20241031081851_create_message_templates.rb
@@ -1,3 +1,10 @@
+#  frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Wanderwege. This file is part of
+#  hitobito_sww and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww.class MessageTemplate < ApplicationRecord
+
 class CreateMessageTemplates < ActiveRecord::Migration[6.1]
   def change
     create_table :message_templates do |t|

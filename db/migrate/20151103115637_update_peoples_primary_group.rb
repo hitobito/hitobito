f5a7b5dc4f70c07924bf36c9506c6f4bd0f082af
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-11-14 08:41:52 +0100

    Support migrating and seeding withing same process

--- db/migrate/20151103115637_update_peoples_primary_group.rb
+++ db/migrate/20151103115637_update_peoples_primary_group.rb
@@ -5,8 +5,9 @@
 
 class UpdatePeoplesPrimaryGroup < ActiveRecord::Migration[4.2]
   def up
+    Person.reset_column_information
     # people with no primary group and only one active role
-    people = Person.joins(:roles_unscoped).where(primary_group_id: nil).group('people.id').having('count(distinct roles.group_id) = 1')
+    people = Person.joins(:roles_unscoped).where(primary_group_id: nil).group("people.id").having("count(distinct roles.group_id) = 1")
     people.find_each do |p|
       group_id = p.roles.first.group_id
       p.update_column(:primary_group_id, group_id)

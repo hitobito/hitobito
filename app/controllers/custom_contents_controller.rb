02ad9e96c7d92ea290b4cb5e55babf84a4b03766
Pascal Zumkehr <zumkehr@puzzle.ch>
2024-11-21 15:34:18 +0100

    Revert "Fix 500 errors when clicking on table sorting headers (#1190)"
    This reverts commit 28f0136212e30c85dfdd2ab01029a233e9cbddc5.

--- app/controllers/custom_contents_controller.rb
+++ app/controllers/custom_contents_controller.rb
@@ -8,18 +8,15 @@
 class CustomContentsController < SimpleCrudController
   self.permitted_attrs = [:label, :body, :subject]
 
-  self.sort_mappings = {label: {
-                          joins: [:translations],
-                          order: ["custom_content_translations.label"]
-                        },
-                         subject: {
-                           joins: [:translations],
-                           order: ["custom_content_translations.subject"]
-                         },
-                         body: {
-                           joins: [:translations],
-                           order: ["custom_content_translations.label"]
-                         }}
+  self.sort_mappings = {label: "custom_content_translations.label",
+                         subject: "custom_content_translations.subject",
+                         body: "custom_content_translations.body"}
 
   decorates :custom_content
+
+  private
+
+  def list_entries
+    super.list
+  end
 end

2a2106b913df80be63781d79f11729eac7527bd9
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-11-22 17:32:06 +0100

    Do not cache_classes in test env to continue using spring

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Eager loading loads your whole application. When running a single test locally,
   # this probably isn't necessary. It's a good idea to do in a continuous integration
@@ -21,7 +21,7 @@ Rails.application.configure do
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.cache_store = :memory_store
 
@@ -40,7 +40,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: "test.host", locale: nil }
+  config.action_mailer.default_url_options = {host: "test.host", locale: nil}
   config.action_mailer.asset_host = "http://test.host"
 
   # Print deprecation notices to the stderr.
@@ -57,5 +57,5 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-  routes.default_url_options[:host] = 'test.host'
+  routes.default_url_options[:host] = "test.host"
 end

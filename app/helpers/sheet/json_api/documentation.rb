835552e115a1f0d8a1d906ec3db194ee0809bf8c
z <zumkehr@puzzle.ch>
2024-11-26 10:11:24 +0100

    Restructure admin navigation, fix active links (#3022)

---
+++ app/helpers/sheet/json_api/documentation.rb
@@ -0,0 +1,10 @@
+#  Copyright (c) 2019, Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+module Sheet
+  module JsonApi
+    class Documentation < Sheet::Admin; end
+  end
+end

f063eb1f2bc17d58cc28df730221c933c6a36b26
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-11-27 17:58:52 +0100

    Support id_token_hint for oidc/logout (hitobito/hitobito_sac_cas#1286)

--- app/controllers/doorkeeper/hitobito/oidc_sessions_controller.rb
+++ app/controllers/doorkeeper/hitobito/oidc_sessions_controller.rb
@@ -5,14 +5,42 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
-class Doorkeeper::Hitobito::OidcSessionsController < ::Doorkeeper::ApplicationMetalController
-  before_action -> { doorkeeper_authorize! :openid }
-
+class Doorkeeper::Hitobito::OidcSessionsController < ActionController::Base
   def destroy
-    doorkeeper_token.destroy!
-    reset_session
-    redirect_target = params[:post_logout_redirect_uri].presence || new_person_session_url(oauth: true)
+    if id_token
+      reset_session
+      access_tokens.destroy_all
+      redirect_to redirect_target, allow_other_host: true, notice: I18n.t("devise.sessions.signed_out")
+    else
+      render plain: "failed to process token", status: :unprocessable_entity
+    end
+  end
 
-    redirect_to URI.parse(redirect_target).to_s, allow_other_host: true, notice: I18n.t("devise.sessions.signed_out")
+  private
+
+  def id_token
+    @id_token ||= decode_id_token&.symbolize_keys
+  end
+
+  def access_tokens
+    Oauth::AccessToken.active.joins(:application)
+      .where(resource_owner_id: id_token[:sub], application: { uid: id_token[:aud] })
+  end
+
+  def decode_id_token
+    return unless params[:id_token_hint].present?
+
+    config = Doorkeeper::OpenidConnect.configuration
+    public_key = OpenSSL::PKey.read(config.signing_key).public_key
+    algorithm = config.signing_algorithm.upcase.to_s
+    JWT.decode(params[:id_token_hint], public_key, false, {algorithm: algorithm})[0]
+  rescue JWT::ExpiredSignature => e
+    JWT.decode(id_token, nil, false)[0].tap do |token|
+      Airbrake.notify(e, token[0])
+    end
+  end
+
+  def redirect_target
+    params[:post_logout_redirect_uri].presence || new_person_session_url(oauth: true)
   end
 end

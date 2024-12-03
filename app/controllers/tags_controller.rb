93f682723355af77b09fe8d7f7b4d216f5bdb3d7
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-12-03 13:01:03 +0100

    Add pagination to tags controller (hitobito/hitobito_sac_cas_#1309)

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,19 @@
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 class TagsController < SimpleCrudController
   self.permitted_attrs = [:name]
 
-  private
-
   def self.model_class
     ActsAsTaggableOn::Tag
+  end
+
+  private
+
+  def list_entries
+    super.page(params[:page])
   end
 
   def index_path

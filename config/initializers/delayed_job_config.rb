92c7c062bc53f5769b3920b88dbba99cd619cd5d
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-12-13 17:54:43 +0100

    Support limiting per job concurrency via settings (#1354)

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -10,6 +10,7 @@ Rails.application.reloader.to_prepare do
   # By default, BaseJobs have a shorter max_run_time configured.
   Delayed::Worker.max_run_time = 24.hours
   Delayed::Worker.plugins << BackgroundJobs::Logging
+  Delayed::Worker.plugins << BackgroundJobs::LimitConcurrentExecutions
 end
 
 # ActiveJob will reload codes if necessary. DelayedJob consumes CPU and memory for reloading on every 5 secs.

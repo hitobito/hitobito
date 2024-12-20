29651fadf09347524bc098fc34f57543a25c67c0
Diego Steiner <diego.steiner@u041.ch>
2024-12-20 13:12:14 +0100

    refactor: multiselect coffee to vanilla js

--- app/helpers/multiselect_helper.rb
+++ app/helpers/multiselect_helper.rb
@@ -1,10 +1,10 @@
 
 module MultiselectHelper
   # Render
-  def extended_all_checkbox(total_count)
+  def extended_all_checkbox(ids)
     content_tag(:label, class: "extended_all btn btn-link") do
-      check_box_tag(:extended_all, total_count, false, class: 'd-none') +
-        content_tag(:span, t('global.extended_select_all', count: total_count))
+      check_box_tag(:extended_all, ids.count, false, data: { ids: JSON.generate(ids) }, class: 'd-none') +
+        content_tag(:span, t('global.extended_select_all', count: ids.count))
     end
   end
 end
--- app/javascript/javascripts/modules/multiselect.js
+++ app/javascript/javascripts/modules/multiselect.js
@@ -4,6 +4,87 @@
 // https://github.com/hitobito/hitobito.
 
 
+function setupMultiselect() {
+  document.querySelectorAll('table[data-checkable=true]').forEach(table => {
+    setupMultiselectCheckboxes(table)
+    setupMultiselectActions(table)
+  })
+
+  // unfortunately, targets are not scoped to a table, so it just uses the first one
+  const table = document.querySelector('table[data-checkable')
+  setupMultiselectTargets(table)
+}
+
+function setupMultiselectTargets(table) {
+  // swap href form data-checkable links
+  document.querySelectorAll('a[data-checkable]:not(data-method)').forEach(link => link.addEventListener('click', () => {
+    link.href = buildLinkWithIds(link.href, table)
+  }))
+
+  // swap href form data-checkable links with method
+  document.querySelectorAll('form[data-checkable]').forEach(form => form.addEventListener('submit', () => {
+    const ids = getSelectedIds(table)
+    form.querySelector('input:input[data-checkable]').value = ids
+  }))
+
+  // tied to the use of jquery-ujs
+  $.rails.href = (element) => {
+    const href = element[0].href
+    if(!$(element).is('a[data-checkable]')) return href
+      
+    return buildLinkWithIds(href, table)
+  }
+}
+
+function setupMultiselectCheckboxes(table) {
+  const checkboxElements = table.querySelectorAll('tbody input[type=checkbox]')
+  const allCheckboxElement = table.querySelector('thead th:first-child input[type=checkbox]')
+
+  // toggle all checkboxes when the «all» checkbox is changed 
+  allCheckboxElement.addEventListener('click', () => { 
+    checkboxElements.forEach(checkboxElement => {
+      (checkboxElement.checked = allCheckboxElement.checked)
+    })
+    toggleActions(table)
+  })
+
+  // hook into change of any checkbox
+  checkboxElements.forEach(checkbox => checkbox.addEventListener('change', () => toggleActions(table))) 
+}
+
+function setupMultiselectActions(table) {
+  // inject multiselect actions element into table
+  const actionsElement = document.querySelector("template#multiselectActions")?.content?.cloneNode(true)
+  if(!actionsElement) return
+    
+  table.querySelector('thead tr').appendChild(actionsElement)
+  table.querySelector('thead input[name=extended_all]').addEventListener('change', () => toggleActions(table))
+}
+
+function checkedCounts(table) {
+  const checkboxElements = table.querySelectorAll('td:first-child input[type=checkbox]')
+
+  return Array.from(checkboxElements).reduce((counts, checkboxElement) => {
+    counts[checkboxElement.checked ? 'checked' : 'unchecked'] += 1;
+    return counts
+  }, { checked: 0, unchecked: 0 })
+}
+
+function getSelectedIds(table) {
+  const extendedAllElement = document.querySelector('thead input[name=extended_all]') 
+  if(extendedAllElement?.checked) return JSON.parse(extendedAllElement.dataset.ids)
+    
+  const checkboxElements = table.querySelectorAll('tbody input[type=checkbox]:checked')
+  return Array.from(checkboxElements).map(checkboxElement => checkboxElement.value)
+}
+
+function buildLinkWithIds(templateHref, table) {
+  const ids = getSelectedIds(table)
+  const separator = templateHref.indexOf('?') != -1 ? '&' : '?'
+  const match = window.location.href.match(/.+?\/(\d+)$/)
+  return templateHref + separator + (match ? `ids=${[match[1]]}&singular=true` : `ids=${ids}`)
+}
+
 function toggleActions(table) {
   const allElement = table.querySelector('thead input[name=all]') 
   const counts = checkedCounts(table)
@@ -22,23 +103,4 @@ function toggleActions(table) {
   table.querySelector('.multiselect .count').innerText = showCount
 }
 
-function setupMultiselectActions() {
-  document.querySelectorAll('table[data-checkable=true]').forEach(table => {
-    const actionsElement = document.querySelector("template#multiselectActions")?.content?.cloneNode(true)
-    if(!actionsElement) return
-      
-    table.querySelector('thead tr').appendChild(actionsElement)
-    table.querySelectorAll('input[type=checkbox]').forEach(checkbox => checkbox.addEventListener('change', () => toggleActions(table))) 
-  })
-}
-
-function checkedCounts(table) {
-  const checkboxElements = table.querySelectorAll('td:first-child input[type=checkbox]')
-
-  return Array.from(checkboxElements).reduce((counts, checkboxElement) => {
-    counts[checkboxElement.checked ? 'checked' : 'unchecked'] += 1;
-    return counts
-  }, { checked: 0, unchecked: 0 })
-}
-
-document.addEventListener('turbo:load', setupMultiselectActions)
+document.addEventListener('turbo:load', setupMultiselect)

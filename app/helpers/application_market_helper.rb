00f4423ef6f9495a31bb0577241709c0acc07a8b
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-12-31 10:50:06 +0100

    Support wagon application market application link customization

---
+++ app/helpers/application_market_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of hitobito and licensed under the
+#  Affero General Public License version 3 or later. See the COPYING file at the top-level directory
+#  or at https://github.com/hitobito/hitobito.
+
+module ApplicationMarketHelper
+  def application_market_application_link(p)
+    link_to(icon("arrow-left"),
+      participant_group_event_application_market_path(@group, @event, p),
+      remote: true,
+      method: :put)
+  end
+end

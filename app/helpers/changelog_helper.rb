e310b6c286d600e5c91f6ae7e82e5d4a55ae6f81
Niklas Jäggi <jaeggi@puzzle.ch>
2025-01-07 13:49:08 +0100

    Use heler to build changelog link

---
+++ app/helpers/changelog_helper.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2022, Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+module ChangelogHelper
+  SOURCE_CODE_URL = 'https://github.com/hitobito'
+  LICENSE_URL = 'http://www.gnu.org/licenses/agpl-3.0.html'
+  LICENSE_NAME = 'GNU Affero General Public License'
+  DEVELOPER_URL = 'http://hitobito.ch'
+  DEVELOPER_NAME = 'Hitobito'
+
+  def render_changelog_link
+    safe_join([collapse_toggle_link, version_label, detail_info_div])
+  end
+  
+  def collapse_toggle_link
+    content_tag(:a, content_tag(:i, '', class: 'fas fa-chevron-right'), data: { bs_toggle: 'collapse' }, href: '#detail-info')
+  end
+  
+  def version_label(display_as_link: true)
+    if Wagons.app_version.to_s > '0.0'
+      if display_as_link
+        link_to("Version #{Wagons.app_version}", changelog_path)
+      else
+        "Version #{Wagons.app_version}"
+      end
+    end
+  end
+  
+  def detail_info_div
+    content_tag(:div, class: 'collapse', id: 'detail-info') do
+      safe_join(detail_info_content, ' ')
+    end
+  end
+  
+  def detail_info_content
+    [
+      app_version_links,
+      link_to(t('.source_code'), SOURCE_CODE_URL, target: '_blank'),
+      t('.available_under_license'),
+      link_to(LICENSE_NAME, LICENSE_URL, target: '_blank'),
+      tag(:br),
+      t('.developed_by'),
+      link_to(DEVELOPER_NAME, DEVELOPER_URL, target: '_blank'),
+      " 2012 - #{Time.zone.now.year}".html_safe
+    ]
+  end
+end

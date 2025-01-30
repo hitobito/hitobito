fea59ef5b0080813adab1ebc8c2e3dfa5834a253
Niklas Jäggi <jaeggi@puzzle.ch>
2025-01-30 13:13:09 +0100

    Add option to manually select sending email to participant (hitobito/hitobito_sac_cas#1572)

--- app/helpers/application_market_helper.rb
+++ app/helpers/application_market_helper.rb
@@ -1,21 +1,26 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of hitobito and licensed under the
-#  Affero General Public License version 3 or later. See the COPYING file at the top-level directory
-#  or at https://github.com/hitobito/hitobito.
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
 
 module ApplicationMarketHelper
   def application_market_application_link(group, event, participation)
     link_to(icon("arrow-left"),
-      participant_group_event_application_market_path(group, event, participation),
-      remote: true,
-      method: :put)
+      "#",
+      onclick: "event.preventDefault();
+                $('#modal-placeholder').html('#{j render(partial: "modal_form",
+                  locals: {group: group, event: event, participation: participation, method: :put})}');
+                $('#application-confirmation').modal('show');")
   end
 
   def application_market_participant_link(group, event, participation)
     link_to(icon("arrow-right"),
-      participant_group_event_application_market_path(group, event, participation),
-      remote: true,
-      method: :delete)
+      "#",
+      onclick: "event.preventDefault();
+                $('#modal-placeholder').html('#{j render(partial: "modal_form",
+                  locals: {group: group, event: event, participation: participation, method: :delete})}');
+                $('#application-confirmation').modal('show');")
   end
 end

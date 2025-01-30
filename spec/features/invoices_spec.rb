7a1d629cff5f22ae6146e07bd16a01916a770665
Matthias Viehweger <kronn@kronn.de>
2025-01-30 11:41:25 +0100

    Conform to Linter and Styleguide

--- spec/features/invoices_spec.rb
+++ spec/features/invoices_spec.rb
@@ -15,25 +15,25 @@ describe :invoices, js: true do
 
   before { sign_in(user) }
 
-  describe 'multiselect' do
+  describe "multiselect" do
     let!(:invoices) do
       invoice_count.times.map do
-        Fabricate(:invoice, creator: user,  group: group, recipient: people(:bottom_member), state: :draft,
-                            invoice_items: [InvoiceItem.new(count: 1, unit_cost: 100, name: "Test")])
+        Fabricate(:invoice, creator: user, group: group, recipient: people(:bottom_member), state: :draft,
+          invoice_items: [InvoiceItem.new(count: 1, unit_cost: 100, name: "Test")])
       end
     end
 
     context "with < 50 items" do
       let(:invoice_count) { 3 }
 
-      it 'selects all only visible invoices' do
+      it "selects all only visible invoices" do
         visit group_invoices_path(group)
 
-        find('#all').click
+        find("#all").click
         expect(page).to have_content("3 Ausgewählt")
         expect(page).not_to have_content("auswählen")
 
-        find('#all').click
+        find("#all").click
         expect(page).not_to have_content("Ausgewählt")
 
         find_all("input[name='ids[]']").sample.click
@@ -45,28 +45,28 @@ describe :invoices, js: true do
     context "with > 50 items" do
       let(:invoice_count) { 51 }
 
-      it 'selects all invoices and appends the ids to the action link' do
+      it "selects all invoices and appends the ids to the action link" do
         visit group_invoices_path(group)
 
-        find('#all').click
+        find("#all").click
         expect(page).to have_content("50 Ausgewählt")
         expect(page).to have_content("Alle 51 auswählen")
-        find('label.extended_all').click
+        find("label.extended_all").click
         expect(page).to have_content("51 Ausgewählt")
-        click_link('Rechnung stellen / mahnen')
-        click_link('Status setzen (Gestellt/Gemahnt)')
+        click_link("Rechnung stellen / mahnen")
+        click_link("Status setzen (Gestellt/Gemahnt)")
 
         expect(group.invoices).to all(be_issued)
         expect(page).to have_content("51 Rechnungen wurden gestellt")
       end
 
-      it 'selects all invoices but regresses to visible invoices when deselected' do
+      it "selects all invoices but regresses to visible invoices when deselected" do
         visit group_invoices_path(group)
 
-        find('#all').click
+        find("#all").click
         expect(page).to have_content("50 Ausgewählt")
         expect(page).to have_content("Alle 51 auswählen")
-        find('label.extended_all').click
+        find("label.extended_all").click
         expect(page).to have_content("51 Ausgewählt")
 
         find_all("input[name='ids[]']").sample.click
@@ -75,5 +75,4 @@ describe :invoices, js: true do
       end
     end
   end
-
 end

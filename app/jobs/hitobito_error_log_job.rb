3ea719514a49fb46bd67ab9823098e8c8604f69e
Nils Rauch <rauch@puzzle.ch>
2025-02-24 14:06:42 +0100

    Check for recipients and use created_at from job for time period

--- app/jobs/hitobito_error_log_job.rb
+++ app/jobs/hitobito_error_log_job.rb
@@ -6,7 +6,11 @@
 #  https://github.com/hitobito/hitobito.
 
 class HitobitoErrorLogJob < RecurringJob
+  private
+
   def perform_internal
+    return unless recipients_defined?
+
     if error_log_entry_ids.present?
       HitobitoLogMailer.error(error_log_entry_ids, time_period).deliver_later
     end
@@ -19,5 +23,12 @@ class HitobitoErrorLogJob < RecurringJob
 
   def error_log_entry_ids = HitobitoLogEntry.where(level: "error", created_at: time_period).pluck(:id)
 
-  def time_period = 1.day.ago.change(hour: 5, min: 0, sec: 0)..Time.current.change(hour: 5, min: 0, sec: 0)
+  def time_period = last_ran_at.change(hour: 5, min: 0, sec: 0)..Time.current.change(hour: 5, min: 0, sec: 0)
+
+  def last_ran_at
+    # created_at is when the previous error log job ended and scheduled this one
+    delayed_jobs.first&.created_at || 1.day.ago
+  end
+
+  def recipients_defined? = Settings.hitobito_log.recipient_emails.present?
 end
--- spec/jobs/hitobito_error_log_job_spec.rb
+++ spec/jobs/hitobito_error_log_job_spec.rb
@@ -12,42 +12,77 @@ describe HitobitoErrorLogJob do
 
   subject { HitobitoErrorLogJob.new }
 
-  it "reschedules to tomorrow at 5am" do
-    subject.perform
+  context "without recipient emails defined" do
+    before do
+      allow(Settings.hitobito_log).to receive(:recipient_emails).and_return(nil)
+    end
 
-    expect(subject.delayed_jobs.last.run_at).to eq(Time.zone.tomorrow
-                                                       .at_beginning_of_day
-                                                       .change(hour: 5)
-                                                       .in_time_zone)
+    it "does not send any mails" do
+      expect do
+        subject.perform
+      end.not_to have_enqueued_mail(HitobitoLogMailer, :error)
+    end
   end
 
-  it "sends email when error log entries exist in past 24 hours" do
-    expect do
+  context "with recipient emails defined" do
+    before do
+      allow(Settings.hitobito_log).to receive(:recipient_emails).and_return(["it@hitobito.example.com"])
+    end
+
+    it "reschedules to tomorrow at 5am" do
       subject.perform
-    end.to have_enqueued_mail(HitobitoLogMailer, :error)
-  end
 
-  it "does not send email when no error log entries exits in past 24 hours" do
-    HitobitoLogEntry.where(created_at: 2.days.ago..Time.zone.now).destroy_all
-    expect do
-      subject.perform
-    end.not_to have_enqueued_mail(HitobitoLogMailer, :error)
-  end
+      expect(subject.delayed_jobs.last.run_at).to eq(Time.zone.tomorrow
+        .at_beginning_of_day
+        .change(hour: 5)
+        .in_time_zone)
+    end
 
-  it "does not include error logs not in certain time period" do
-    expect(subject.error_log_entry_ids).to include(hitobito_log_entries(:error_ebics).id)
-    expect(subject.error_log_entry_ids).not_to include(hitobito_log_entries(:error_webhook).id)
-  end
+    it "sends email when error log entries exist in past 24 hours" do
+      expect do
+        subject.perform
+      end.to have_enqueued_mail(HitobitoLogMailer, :error)
+    end
 
-  it "does not include logs without level error" do
-    expect(subject.error_log_entry_ids).to include(hitobito_log_entries(:error_ebics).id)
-    expect(subject.error_log_entry_ids).not_to include(hitobito_log_entries(:entry_with_payload).id)
-  end
+    it "does not send email when no error log entries exist in past 24 hours" do
+      HitobitoLogEntry.where(created_at: 2.days.ago..Time.zone.now).destroy_all
+      expect do
+        subject.perform
+      end.not_to have_enqueued_mail(HitobitoLogMailer, :error)
+    end
 
-  it "uses 5am to 5am as time period, no matter when job is running" do
-    travel_to Time.zone.local(2005, 12, 9, 14) do
-      expect(subject.time_period.begin).to eq Time.zone.local(2005, 12, 8, 5)
-      expect(subject.time_period.end).to eq Time.zone.local(2005, 12, 9, 5)
+    it "does not include error logs not in certain time period" do
+      expect(subject.send(:error_log_entry_ids)).to include(hitobito_log_entries(:error_ebics).id)
+      expect(subject.send(:error_log_entry_ids)).not_to include(hitobito_log_entries(:error_webhook).id)
+    end
+
+    it "does not include logs without level error" do
+      expect(subject.send(:error_log_entry_ids)).to include(hitobito_log_entries(:error_ebics).id)
+      expect(subject.send(:error_log_entry_ids)).not_to include(hitobito_log_entries(:entry_with_payload).id)
+    end
+
+    context "#time_period" do
+      it "uses 5am to 5am" do
+        travel_to Time.zone.local(2005, 12, 9, 14) do
+          expect(subject.send(:time_period).begin).to eq Time.zone.local(2005, 12, 8, 5)
+          expect(subject.send(:time_period).end).to eq Time.zone.local(2005, 12, 9, 5)
+        end
+      end
+
+      it "uses created_at as beginning if delayed job entry is found" do
+        Delayed::Job.create!(handler: subject.to_yaml, created_at: Time.zone.local(2025, 2, 21))
+        travel_to Time.zone.local(2025, 2, 24) do
+          expect(subject.send(:time_period).begin).to eq Time.zone.local(2025, 2, 21, 5)
+          expect(subject.send(:time_period).end).to eq Time.zone.local(2025, 2, 24, 5)
+        end
+      end
+
+      it "uses 1 day ago as beginning if no delayed job entry is found" do
+        travel_to Time.zone.local(2025, 2, 24) do
+          expect(subject.send(:time_period).begin).to eq Time.zone.local(2025, 2, 23, 5)
+          expect(subject.send(:time_period).end).to eq Time.zone.local(2025, 2, 24, 5)
+        end
+      end
     end
   end
 end

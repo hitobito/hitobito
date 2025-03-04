52153c98d923eba4463467ea21774c42fa9ec005
Niklas Jäggi <jaeggi@puzzle.ch>
2025-03-04 11:16:26 +0100

    Add stimulus controller to update form field values on event

---
+++ app/javascript/controllers/update_field_value_controller.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2025, Schweizer Alpen-Club. This file is part of
+// hitobito_sac_cas and licensed under the Affero General Public License version 3
+// or later. See the COPYING file at the top-level directory or at
+// https://github.com/hitobito/hitobito
+
+import { Controller } from "@hotwired/stimulus"
+
+// This controller can update form field values on changes to other form fields
+// Example for usage event/courses/invoices/new.html.haml in sac wagon
+
+export default class extends Controller {
+  static targets = ["field_to_update"]
+
+  dependantFieldChanged(event) {
+    const fieldName = event.target.name;
+    const fieldValue = event.target.value;
+    const url = this.element.dataset.url;
+
+    const queryParam = encodeURIComponent(fieldName) + "=" + encodeURIComponent(fieldValue);
+
+    fetch(`${url}?${queryParam}`, {
+      headers: { "Accept": "application/json" }
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (this.hasField_to_updateTarget && data.updatedValue) {
+        this.field_to_updateTarget.value = data.updatedValue;
+      }
+    })
+    .catch(error => console.error("Error:", error));
+  }
+}

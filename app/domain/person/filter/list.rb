cd2df3290c5a9ccc18559f42f5a569d98889968a
Matthias Viehweger <kronn@kronn.de>
2025-03-19 17:36:01 +0100

    Extract person_filter for reuse
    This allows the 'all'-keyword to be resolved in the controller.

--- app/domain/person/filter/list.rb
+++ app/domain/person/filter/list.rb
@@ -8,13 +8,14 @@
 class Person::Filter::List
   attr_reader :group, :user, :chain, :range, :name
 
-  def initialize(group, user, params = {})
+  def initialize(group, user, params = {}, accessibles_class = nil)
     @group = group
     @user = user
     @chain = Person::Filter::Chain.new(params[:filters])
     @range = params[:range]
     @name = params[:name]
     @ids = params[:ids].to_s.split(",")
+    @accessibles_class = accessibles_class
   end
 
   def entries
@@ -70,12 +71,11 @@ class Person::Filter::List
   end
 
   def accessibles_class
-    abilities = chain.required_abilities
-    if abilities.include?(:full)
-      PersonFullReadables
-    else
-      PersonReadables
-    end
+    @accessibles_class ||= full_ability_needed? ? PersonFullReadables : PersonReadables
+  end
+
+  def full_ability_needed?
+    chain.required_abilities.include?(:full)
   end
 
   def group_range?

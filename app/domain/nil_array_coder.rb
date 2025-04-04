d14394951d685fd537ed6d2054f0175926b1b5d5
Matthias Viehweger <kronn@kronn.de>
2025-04-04 22:10:23 +0200

    Add license

--- app/domain/nil_array_coder.rb
+++ app/domain/nil_array_coder.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+#  Copyright (c) 2025-2025, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 class NilArrayCoder
   def self.dump(object)
     object.to_json # Convert Ruby array to JSON string

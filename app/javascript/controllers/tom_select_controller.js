ab1fe966747b6141b6cc396114ad80d542896a2a
Daniel Illi <daniel@illi.zone>
2025-05-07 13:01:35 +0200

    Do not limit TomSelect options by default, make it configurable
    * Changes to `TomSelectController` stimulus controller:
      + Do not limit the options by default
      + Make the options limit configurable value data tag on the select element
    
    * Change all country_select calls to use the stimulus controller
    
    fixes #3364

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -5,11 +5,43 @@
 //
 import { Controller } from "@hotwired/stimulus";
 
+/*
+  To enable TomSelect on a select element, add this data attribute:
+    data-controller="tom-select"
+
+  Following value attributes are available to configure TomSelect:
+
+    data-tom-select-url: The URL to fetch options from. This should be a JSON API
+      endpoint that returns an array of objects with the following structure:
+        [
+          { id: 1, label: "Option 1" },
+          { id: 2, label: "Option 2" },
+          ...
+        ]
+
+    data-tom-select-label-value: The name of the label field in the options. If not set,
+      the default is "label".
+
+    data-tom-select-no-results-valu: The message to display when no results are found.
+      If not set, the default is "No results found.".
+
+    data-tom-select-max-options-value: The maximum number of options to show in the
+      dropdown. If not set, it will not limit the option numbers. To limit the listed options,
+      set the data-tom-select-max-options attribute to the desired number.
+      Regardless of a limit, the user can still select any option when filtering the list
+      by typing.
+
+  For example:
+
+    select_content_tag :my_select, options_for_select(@options),
+      data: { controller: "tom-select", tom_select_max_options_value: 42 }
+ */
 export default class extends Controller {
   static values = {
     url: String,
     label: { type: String, default: "label" },
     noResults: { type: String, default: "No results found." },
+    maxOptions: { type: Number, default: null } // no limit by default (null)
   };
 
   initialize() {
@@ -39,6 +71,7 @@ export default class extends Controller {
       plugins: multiple ? ["remove_button"] : undefined,
       create: false,
       load: this.urlValue ? this.load : undefined,
+      maxOptions: this.maxOptionsValue,
       render: {
         no_results: this.noResults,
       },
---
+++ spec/features/stimulus/tom_select_spec.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+require "spec_helper"
+
+describe "TomSelect Stimulus Controller", js: true do
+  before do
+    stub_const("TomSelectController", Class.new(ActionController::Base) { # rubocop:disable Rails/ApplicationController
+      include Webpacker::Helper
+      include ActionView::Helpers::AssetTagHelper
+
+      helper_method :tom_select_tag
+
+      def new
+        # The HTML for your form element
+        render inline: <<~HTML
+          <head>
+            #{stylesheet_pack_tag "application", media: "screen", "data-turbo-track": true}
+            #{javascript_pack_tag "application", "data-turbo-track": true}
+            <!-- add fake inline favicon to avoid 404 error -->
+            <link rel="icon" href="data:image/x-icon;," type="image/x-icon">
+          </head>
+          <body>
+            <h1>TomSelect Test</h1>
+            #{tom_select_tag}
+          </body>
+        HTML
+      end
+
+      def tom_select_tag
+        id = "tom-select-#{Fabrication::Sequencer.sequence(:tom_select)}"
+        <<~HTML
+          <select name="#{id}" id="#{id}" data-controller="tom-select"
+            #{tom_select_params.map { |k, v| "#{k}=\"#{v}\"" }.join(" ")}>
+            #{1000.times.map { |i| "<option value=\"#{i}\">Option #{i}</option>" }.join("\n")}
+          </select>
+        HTML
+      end
+
+      def tom_select_params = params.permit!.to_h.select { |k, _| k =~ /^data-tom-select-/ }
+    })
+
+    Rails.application.routes.send(:eval_block, lambda do
+      get "/tom_select", to: "tom_select#new"
+    end)
+  end
+
+  after do
+    Rails.application.reload_routes!
+  end
+
+  it "initializes tom_select" do
+    visit "/tom_select"
+
+    # Check that the original select is present (and likely hidden by TomSelect)
+    expect(page).to have_selector("select[data-controller='tom-select']", visible: false)
+
+    # Check that TomSelect's wrapper has been created
+    expect(page).to have_selector(".ts-wrapper", visible: true)
+
+    # Interact with TomSelect
+    find(".ts-control").click
+    expect(page).to have_selector(".ts-dropdown .option", text: "Option 0")
+  end
+
+  describe "Stimulus Values Configuration" do
+    describe "max_options" do
+      it "when missing it has no limit" do
+        visit "/tom_select"
+
+        expect(page).to have_no_css("select[data-tom-select-max-options-value]", visible: false)
+
+        find(".ts-control").click
+        expect(page).to have_selector(".ts-dropdown .option", text: "Option 0")
+        expect(page).to have_selector(".ts-dropdown .option", text: "Option 999")
+        expect(page).to have_selector(".ts-dropdown .option", count: 1000)
+      end
+
+      it "respects max_options value" do
+        visit "/tom_select?data-tom-select-max-options-value=42"
+
+        expect(page).to have_css("select[data-tom-select-max-options-value='42']", visible: false)
+
+        find(".ts-control").click
+        expect(page).to have_selector(".ts-dropdown .option", text: "Option 0")
+        expect(page).to have_selector(".ts-dropdown .option", text: "Option 41")
+        expect(page).to have_no_selector(".ts-dropdown .option", text: "Option 42")
+        expect(page).to have_selector(".ts-dropdown .option", count: 42)
+      end
+    end
+  end
+end

5849f16e318eb6a6acce25948a3928b5785abba5
Matthias Viehweger <kronn@kronn.de>
2025-05-08 18:04:53 +0200

    Record bounces for later use

--- app/models/bounce.rb
+++ app/models/bounce.rb
@@ -8,6 +8,21 @@
 class Bounce < ApplicationRecord
   BLOCK_THRESHOLD = 5
 
+  before_validation :evaluate_blocking
+
+  class << self
+    def record(email, mailing_list_id: nil)
+      bounce = find_or_create_by(email: email)
+      if mailing_list_id.present?
+        bounce.mailing_list_ids ||= []
+        bounce.mailing_list_ids  << mailing_list_id
+      end
+      bounce.increment(:bounce_count)
+
+      bounce.save
+    end
+  end
+
   def person
     Person.find_by(email: email)
   end
@@ -16,4 +31,12 @@ class Bounce < ApplicationRecord
   # blocked?
   # record_for(mailing_list_id:)
   # mailing_lists
+
+  private
+
+  def evaluate_blocking
+    if bounce_count >= BLOCK_THRESHOLD
+      self.blocked_at = DateTime.current
+    end
+  end
 end

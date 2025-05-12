50be7ffbbb5cbd3a5d138d433737fe457ab911fb
Matthias Viehweger <kronn@kronn.de>
2025-05-12 11:21:19 +0200

    Spec and improve column-names

--- app/models/bounce.rb
+++ app/models/bounce.rb
@@ -6,7 +6,15 @@
 #  https://github.com/hitobito/hitobito.
 
 class Bounce < ApplicationRecord
+  class UnexpectedBlock < StandardError
+    def initialize(mail, msg = nil)
+      super(msg || "The checked mail (#{mail}) was blocked, but not expected to be.")
+    end
+  end
+
   BLOCK_THRESHOLD = 5
+
+  scope :blocked, -> { where.not(blocked_at: nil) }
 
   before_validation :evaluate_blocking
 
@@ -17,25 +25,56 @@ class Bounce < ApplicationRecord
         bounce.mailing_list_ids ||= []
         bounce.mailing_list_ids << mailing_list_id
       end
-      bounce.increment(:bounce_count)
+      bounce.increment(:count)
 
       bounce.save
+      bounce
+    end
+
+    # TODO: maybe extract this in a extra helper that controls blocking of mail-sending,
+    #       independently of bounces, but that also controls the validity/futility.
+    def blocked?(email)
+      where(email: email).blocked.exists?
     end
   end
 
-  def person
-    Person.find_by(email: email)
+  def block!
+    return blocked_at if blocked?
+
+    now = DateTime.current
+    update_attribute(:blocked_at, now)
+
+    now
   end
 
-  # block!
-  # blocked?
-  # record_for(mailing_list_id:)
-  # mailing_lists
+  def blocked?
+    self.blocked_at.present?
+  end
+
+  # find the first person there is to find
+  def person
+    Person.find_by(email: email) ||
+      AdditionalEmail.find_by(email: email, contactable_type: "Person").contactable
+  end
+
+  def people
+    Person.where(id: people_ids).all
+  end
+
+  # get a list of all people-ids associated with the email
+  def people_ids
+    Person.where(email: email).pluck(:id) |
+      AdditionalEmail.where(email: email, contactable_type: "Person").pluck(:contactable_id)
+  end
+
+  def mailing_lists
+    MailingList.where(id: mailing_list_ids).all
+  end
 
   private
 
   def evaluate_blocking
-    if bounce_count >= BLOCK_THRESHOLD
+    if count >= BLOCK_THRESHOLD
       self.blocked_at = DateTime.current
     end
   end
--- db/seeds/support/bounce_seeder.rb
+++ db/seeds/support/bounce_seeder.rb
@@ -9,21 +9,20 @@ class BounceSeeder
   def seed_one
     created_at = Faker::Time.between(from: DateTime.now - 14.days, to: DateTime.now - 3.days)
 
-    bounce_count = Faker::Number.between(from: 1, to: 10)
+    count = Faker::Number.between(from: 1, to: 10)
     email = Faker::Internet.email(domain: 'example.net')
-    blocked_count = bounce_count > Bounce::BLOCK_THRESHOLD ? 1 : 0
-    blocked_at = blocked_count.positive? ? created_at.advance(day: 1).to_date : nil
+    blocked_at = count > Bounce::BLOCK_THRESHOLD ? created_at.advance(day: 1).to_date : nil
     mailing_list_ids = []
 
     updated_at = [blocked_at, created_at].compact.max
 
     Bounce.seed({
       email:,
-      bounce_count:,
-      blocked_count:,
+      count:,
       blocked_at:,
       mailing_list_ids:,
-      created_at:
+      created_at:,
+      updated_at:
     }).first
   end
 end

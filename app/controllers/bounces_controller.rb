caa44eee0f299674933a2482785dfc83ee8fb912
Matthias Viehweger <kronn@kronn.de>
2025-05-13 09:40:09 +0200

    Show bounces on the respective mailing-list

---
+++ app/controllers/bounces_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025-2025, Schweizer Wanderwege. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class BouncesController < ListController
+  self.nesting = [Group, MailingList]
+
+  helper_method :group, :mailing_list
+
+  prepend_before_action :group
+  prepend_before_action :mailing_list
+
+  private
+
+  def group
+    @group ||= Group.find(params[:group_id])
+  end
+
+  def mailing_list
+    @mailing_list ||= MailingList.find(params[:mailing_list_id])
+  end
+
+  def model_scope
+    model_class.of_mailing_list(@mailing_list.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -313,6 +313,7 @@ Hitobito::Application.routes.draw do
 
         resources :mailchimp_synchronizations, only: [:create]
         resources :recipient_counts, controller: 'mailing_lists/recipient_counts', only: [:index]
+        resources :bounces
       end
 
       resources :calendars do

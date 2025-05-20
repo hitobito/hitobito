3cdb26306e24dcca11c35828e84d5e8aa61a4237
Niklas Jäggi <jaeggi@puzzle.ch>
2025-05-20 15:31:40 +0200

    Validate merged roles before merging (#3396)

--- app/models/person_duplicate.rb
+++ app/models/person_duplicate.rb
@@ -29,6 +29,8 @@ class PersonDuplicate < ActiveRecord::Base
 
   validates :person_1_id, uniqueness: {scope: [:person_2_id]}
 
+  validate :valid_role_operations, if: -> { validation_context == {context: :merge} }
+
   before_save :assign_persons_sorted_by_id
   # Sorting by id to only allow a single PersonDuplicate entry per Person combination
   def assign_persons_sorted_by_id
@@ -37,5 +39,29 @@ class PersonDuplicate < ActiveRecord::Base
 
   def persons_valid?
     person_1.valid? && person_2.valid?
+  end
+
+  # This validation checks if all roles are valid when merging, to prevent exceptions when merging.
+  # We check both cases for target and source, no matter which one was selected as the prefered option.
+  def valid_role_operations
+    validate_roles(person_1, person_2_id)
+    validate_roles(person_2, person_1_id)
+  end
+
+  private
+
+  def validate_roles(person, new_person_id)
+    person.roles.with_inactive.each do |role|
+      role.person_id = new_person_id
+      unless role.valid?
+        errors.add(
+          :base,
+          I18n.t("activerecord.errors.models.person_duplicates.invalid_role",
+            role: role.to_s,
+            person: person,
+            message: role.errors.full_messages.join(", "))
+        )
+      end
+    end
   end
 end

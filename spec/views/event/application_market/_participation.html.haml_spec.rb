9d00804d10e1eb003a9d191a6112d1f4347fc2f8
megli2 <egli@puzzle.ch>
2025-05-21 14:50:05 +0200

    Write view spec to ensure correct behaviour

---
+++ spec/views/event/application_market/_participation.html.haml_spec.rb
@@ -0,0 +1,15 @@
+#  Copyright (c) 2025, Hitobito AG. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+require "spec_helper"
+
+describe "event/application_market/_participation.html.haml" do
+  let(:event) { EventDecorator.decorate(Fabricate(:course, groups: [groups(:top_layer)])) }
+  let(:participation) { Fabricate(:event_participation, event: event) }
+
+  it "participation without application isn't causing an internal server error" do
+    expect{ render locals: {p: participation.decorate }}.not_to raise_error
+  end
+end

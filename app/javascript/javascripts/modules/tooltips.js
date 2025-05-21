be75df4e67c82f5e3a4d4bc020fb64c8b93ab00d
megli2 <egli@puzzle.ch>
2025-05-21 11:42:58 +0200

    Add event listeners to js to ensure that tooltip works in popovers too

--- app/javascript/javascripts/modules/tooltips.js
+++ app/javascript/javascripts/modules/tooltips.js
@@ -1,12 +1,19 @@
-//  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
+//  Copyright (c) 2025, Hitobito. This file is part of
 //  hitobito and licensed under the Affero General Public License version 3
 //  or later. See the COPYING file at the top-level directory or at
 //  https://github.com/hitobito/hitobito.
 
-document.addEventListener('DOMContentLoaded', function() {
-  $('[data-bs-toggle="tooltip"]').tooltip({ placement: 'right' });
+// enable tooltip on page load
+$(document).on('turbo:load', function() {
+  $('[data-bs-toggle="tooltip"]').tooltip({ placement: 'right'});
 });
 
-$(document).on('turbo:load', function() {
+// enable tooltip on turbo render (e.g. form submits)
+window.addEventListener('turbo:render', function () {
+  $('[data-bs-toggle="tooltip"]').tooltip({ placement: 'right'});
+});
+
+// enable tooltip on popover open event
+document.addEventListener("shown.bs.popover", () => {
   $('[data-bs-toggle="tooltip"]').tooltip({ placement: 'right'});
 });

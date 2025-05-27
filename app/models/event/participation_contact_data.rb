560857cea5c480592281f1b21c176376ea0884c2
Niklas Jäggi <jaeggi@puzzle.ch>
2025-05-27 13:23:06 +0200

    Adjust show_address to only check real address attrs

--- app/models/event/participation_contact_data.rb
+++ app/models/event/participation_contact_data.rb
@@ -13,10 +13,8 @@ class Event::ParticipationContactData
 
   self.mandatory_contact_attrs = [:email, :first_name, :last_name]
 
-  self.address_attrs = [:street, :housenumber, :postbox, :zip_code, :town]
-
   self.contact_attrs = [:first_name, :last_name, :nickname, :company_name, :email,
-    :address_care_of, :street, :housenumber, :postbox, :zip_code, :town, :country,
+    :address_care_of, :street, :housenumber, :postbox, :zip_code, :town, :country, 
     :gender, :birthday, :phone_numbers, :language]
 
   self.contact_associations = [:additional_emails, :social_accounts]

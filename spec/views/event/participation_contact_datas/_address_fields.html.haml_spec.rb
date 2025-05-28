6f564c63c80d0ae7d3257597eaf53282815a5da0
megli2 <egli@puzzle.ch>
2025-05-28 13:48:07 +0200

    Add looping to test to cover all use cases

--- spec/views/event/participation_contact_datas/_address_fields.html.haml_spec.rb
+++ spec/views/event/participation_contact_datas/_address_fields.html.haml_spec.rb
@@ -13,18 +13,22 @@ describe "event/participation_contact_datas/_address_fields.html.haml" do
     allow(view).to receive(:f).and_return(StandardFormBuilder.new(:entry, participation_contact_data, view, {}))
   end
 
-  it "should not render address label if no address attributes are set to display" do
-    render locals: {entry: participation_contact_data, event: event, group: group}
-    expect(rendered).to have_no_text("Adresse")
+  [:address_care_of, :street, :housenumber, :postbox, :zip_code, :town, :country].each do |attribute|
+    it "does not render input with id entry_#{attribute} when attribute is hidden" do
+      event = Fabricate(:event, hidden_contact_attrs: [attribute]).decorate
+      data = participation_contact_data(event)
+      render locals: {entry: data, event: event, group: group}
+      expect(rendered).not_to have_selector("input#entry_#{attribute}")
+    end
   end
 
   it "should render address label when only housenumber is hidden" do
     event.update!(hidden_contact_attrs: ["housenumber"])
-    render locals: {entry: participation_contact_data, event: event, group: group}
+    render locals: {entry: participation_contact_data(event), event: event, group: group}
     expect(rendered).to have_text("Adresse")
   end
 
-  def participation_contact_data
+  def participation_contact_data(event)
     Event::ParticipationContactData.new(event, people(:top_leader), {})
   end
 end

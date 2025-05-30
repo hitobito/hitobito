21c74533749a2045325e79f8b83f200d3c4cf679
megli2 <egli@puzzle.ch>
2025-05-30 09:43:56 +0200

    Resolve requested changes

--- spec/views/event/participation_contact_datas/_address_fields.html.haml_spec.rb
+++ spec/views/event/participation_contact_datas/_address_fields.html.haml_spec.rb
@@ -10,22 +10,27 @@ describe "event/participation_contact_datas/_address_fields.html.haml" do
   let(:group) { Fabricate(:group, type: "Group::TopLayer").decorate }
 
   before do
-    allow(view).to receive(:f).and_return(StandardFormBuilder.new(:entry, participation_contact_data, view, {}))
+    allow(view).to receive(:f).and_return(StandardFormBuilder.new(:entry, participation_contact_data(event), view, {}))
   end
 
   [:address_care_of, :street, :housenumber, :postbox, :zip_code, :town, :country].each do |attribute|
     it "does not render input with id entry_#{attribute} when attribute is hidden" do
-      event = Fabricate(:event, hidden_contact_attrs: [attribute]).decorate
-      data = participation_contact_data(event)
-      render locals: {entry: data, event: event, group: group}
+      event.update!(hidden_contact_attrs: [attribute])
+      render locals: {entry: participation_contact_data(event), event:, group:}
       expect(rendered).not_to have_selector("input#entry_#{attribute}")
     end
   end
 
   it "should render address label when only housenumber is hidden" do
-    event.update!(hidden_contact_attrs: ["housenumber"])
-    render locals: {entry: participation_contact_data(event), event: event, group: group}
+    event.update!(hidden_contact_attrs: [:housenumber])
+    render locals: {entry: participation_contact_data(event), event: , group:}
     expect(rendered).to have_text("Adresse")
+  end
+
+  it "should not render address label if no address attributes are set to display" do
+    event.update!(hidden_contact_attrs: [:housenumber, :street])
+    render locals: {entry: participation_contact_data(event), event:, group:}
+    expect(rendered).to have_no_text("Adresse")
   end
 
   def participation_contact_data(event)

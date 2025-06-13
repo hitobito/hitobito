73f1b2197c845ecc6e1a4f0e7f8b35e1bf33122d
megli2 <egli@puzzle.ch>
2025-06-13 10:35:11 +0200

    Format files with rubocop

--- app/domain/people_filter_displays/role_with_group_and_layer.rb
+++ app/domain/people_filter_displays/role_with_group_and_layer.rb
@@ -4,7 +4,9 @@ module PeopleFilterDisplays
       role_types.flat_map do |layer, hash|
         hash.flat_map { |g, role_types|
           role_types.map { |r|
-            [[layer, g, r.label].uniq.join(" -> "), r.id, r.id] } }
+            [[layer, g, r.label].uniq.join(" -> "), r.id, r.id]
+          }
+        }
       end
     end
   end

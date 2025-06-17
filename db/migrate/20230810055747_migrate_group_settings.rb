8240b79b3720bdce4f8e579ea6ce9534253356a2
Matthias Viehweger <kronn@kronn.de>
2025-06-17 11:18:35 +0200

    Keep schema.rb correct
    The settings-table is not in use and should have been deleted.

--- db/migrate/20230810055747_migrate_group_settings.rb
+++ db/migrate/20230810055747_migrate_group_settings.rb
@@ -6,7 +6,6 @@
 #  https://github.com/hitobito/hitobito.
 
 class MigrateGroupSettings < ActiveRecord::Migration[6.1]
-
   def up
     say_with_time('create group attributes') do
       add_column :groups, :encrypted_text_message_username, :string
@@ -16,9 +15,22 @@ class MigrateGroupSettings < ActiveRecord::Migration[6.1]
       add_column :groups, :letter_address_position, :string, null: false, default: 'left'
       add_column :groups, :letter_logo, :string
     end
+
+    drop_table :settings
   end
 
   def down
+    create_table "sessions" do |t|
+      t.string "session_id", null: false
+      t.text "data"
+      t.bigint "person_id"
+      t.index "person_id"
+      t.index "session_id"
+      t.index "updated_at"
+
+      t.timestamps
+    end
+
     say_with_time('remove group attributes') do
       remove_column :groups, :encrypted_text_message_username
       remove_column :groups, :encrypted_text_message_password

828be9193b8c117f88bb66011c14cbc5a9187a8a
Matthias Viehweger <kronn@kronn.de>
2025-07-09 18:54:30 +0200

    Updating schema.rb with existing migrations
    These were persistent, but unrelated schema-changes which were not in
    the schema.rb. They were persistent in the way that recreating the
    database and the schema.rb led to the same changes.

--- db/schema.rb
+++ db/schema.rb
@@ -467,14 +467,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_30_120000) do
   end
 
   create_table "group_translations", force: :cascade do |t|
-    t.integer "group_id", null: false
+    t.bigint "group_id", null: false
     t.string "locale", null: false
+    t.string "privacy_policy_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "privacy_policy_title"
     t.string "custom_self_registration_title"
+    t.index ["group_id", "locale"], name: "index_group_translations_on_group_id_and_locale", unique: true
     t.index ["group_id"], name: "index_group_translations_on_group_id"
-    t.index ["locale"], name: "index_group_translations_on_locale"
   end
 
   create_table "group_type_orders", force: :cascade do |t|
@@ -1232,6 +1232,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_30_120000) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "calendar_tags", "tags", on_delete: :cascade
+  add_foreign_key "group_translations", "groups"
   add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
   add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
   add_foreign_key "oauth_openid_requests", "oauth_access_grants", column: "access_grant_id", on_delete: :cascade

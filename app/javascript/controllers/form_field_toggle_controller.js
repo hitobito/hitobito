9fc98543cbafda5fe5b6733ed15fcc11ae3865ac
Daniel Illi <daniel@illi.zone>
2025-07-17 09:36:02 +0200

    Remove console.log debug statement

--- app/javascript/controllers/form_field_toggle_controller.js
+++ app/javascript/controllers/form_field_toggle_controller.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2023, Schweizer Alpen-Club. This file is part of
+// Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
 // hitobito_sac_cas and licensed under the Affero General Public License version 3
 // or later. See the COPYING file at the top-level directory or at
 // https://github.com/hitobito/hitobito
@@ -13,7 +13,6 @@ export default class extends Controller {
   }
 
   toggle(event) {
-    console.log("toggle")
     if(event.target.tagName === "SELECT") {
       const selected = event.target.options[event.target.options.selectedIndex];
 

b20028d8ee5563f1f91be09c0e2dcbf4d703932e
Matthias Viehweger <kronn@kronn.de>
2025-07-22 13:58:34 +0200

    Conform to Linter and Styleguide and remove obsolete code (hitobito/hitobito_sww#262)

--- app/controllers/event/participations_controller.rb
+++ app/controllers/event/participations_controller.rb
@@ -98,10 +98,7 @@ class Event::ParticipationsController < CrudController # rubocop:disable Metrics
       end
       format.pdf { render_entries_pdf(filter_entries) }
       format.csv { render_tabular_in_background(:csv) }
-      format.vcf {
-        render_vcf(filter_entries.includes(person: :phone_numbers)
-                                                      .collect(&:person))
-      }
+      format.vcf { render_vcf(filter_entries.includes(person: :phone_numbers).collect(&:person)) }
       format.xlsx { render_tabular_in_background(:xlsx) }
       format.email { render_emails(filter_entries.collect(&:person), ",") }
       format.email_outlook { render_emails(filter_entries.collect(&:person), ";") }

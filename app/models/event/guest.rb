3f029c2e37ce9c83afa0e3dd8e40e2cc542ba9ab
carlobeltrame <carlo@beltra.me>
2025-07-30 11:05:36 +0200

    Allow squinting your eyes to see the person in a guest (hitobito/hitobito_sww#263)

--- app/models/event/guest.rb
+++ app/models/event/guest.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2025, Hitobito AG. This file is part of
+#  Copyright (c) 2025, Schweizer Wanderwege. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -6,8 +6,20 @@
 class Event::Guest < ActiveRecord::Base
   belongs_to :main_applicant, class_name: "Event::Participation"
 
+  delegate :to_s, :gender_label, to: :to_person
+
+  GUEST_SPECIFIC_ATTRIBUTES = [:id, :phone_number, :main_applicant_id]
+
   def additional_emails
     AdditionalEmail.none
+  end
+
+  def to_person
+    Person.new(person_attributes).tap do |person|
+      if phone_number?
+        person.phone_numbers.build(number: phone_number)
+      end
+    end
   end
 
   class << self
@@ -24,5 +36,11 @@ class Event::Guest < ActiveRecord::Base
         SQL
       )
     end
+  end
+
+  private
+
+  def person_attributes
+    attributes.reject { |attr| GUEST_SPECIFIC_ATTRIBUTES.include?(attr.to_sym) }
   end
 end

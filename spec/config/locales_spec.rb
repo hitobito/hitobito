6e70c9a2257231257c1b6bda07b09a7b8e84b09e
Daniel Illi <daniel@illi.zone>
2025-08-08 11:11:45 +0200

    Simplify locale fallback config

--- spec/config/locales_spec.rb
+++ spec/config/locales_spec.rb
@@ -8,6 +8,75 @@
 require "spec_helper"
 
 describe "locales" do
+  context "fallbacks" do
+    let(:translations) do
+      {
+        de: {hello_test: "Hallo"},
+        fr: {hello_test: "Bonjour"},
+        it: {hello_test: "Ciao"},
+        en: {hello_test: "Hello"},
+      }
+    end
+
+    def t(locale) = translations.dig(locale, :hello_test)
+
+    it "de falls back to fr, it, en, original" do
+      with_translations(translations.except(:de)) do
+        expect(I18n.t(:hello_test, locale: :de)).to eq t(:fr)
+      end
+
+      with_translations(translations.except(:de, :fr)) do
+        expect(I18n.t(:hello_test, locale: :de)).to eq t(:it)
+      end
+
+      with_translations(translations.except(:de, :fr, :it)) do
+        expect(I18n.t(:hello_test, locale: :de)).to eq t(:en)
+      end
+    end
+
+    it "fr falls back to it, en, de, original" do
+      with_translations(translations.except(:fr)) do
+        expect(I18n.t(:hello_test, locale: :fr)).to eq t(:it)
+      end
+
+      with_translations(translations.except(:fr, :it)) do
+        expect(I18n.t(:hello_test, locale: :fr)).to eq t(:en)
+      end
+
+      with_translations(translations.except(:fr, :it, :en)) do
+        expect(I18n.t(:hello_test, locale: :fr)).to eq t(:de)
+      end
+    end
+
+    it "it falls back to fr, en, de, original" do
+      with_translations(translations.except(:it)) do
+        expect(I18n.t(:hello_test, locale: :it)).to eq t(:fr)
+      end
+
+      with_translations(translations.except(:it, :fr)) do
+        expect(I18n.t(:hello_test, locale: :it)).to eq t(:en)
+      end
+
+      with_translations(translations.except(:it, :fr, :en)) do
+        expect(I18n.t(:hello_test, locale: :it)).to eq t(:de)
+      end
+    end
+
+    it "en falls back to de, fr, it, original" do
+      with_translations(translations.except(:en)) do
+        expect(I18n.t(:hello_test, locale: :en)).to eq t(:de)
+      end
+
+      with_translations(translations.except(:en, :de)) do
+        expect(I18n.t(:hello_test, locale: :en)).to eq t(:fr)
+      end
+
+      with_translations(translations.except(:en, :de, :fr)) do
+        expect(I18n.t(:hello_test, locale: :en)).to eq t(:it)
+      end
+    end
+  end
+
   context 'do not contain wrong spellings of "E-Mail":' do
     it "Email" do
       expect(`grep 'Email' config/locales/*.de.yml | wc -l`.chomp.to_i).to be_zero

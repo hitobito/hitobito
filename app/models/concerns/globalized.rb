f54b47912edbd922e324950e63eb6353a4e6d725
Jannik Pulfer <pulfer@puzzle.ch>
2025-08-11 11:28:14 +0200

    Add license of globalized gem to globalized file

--- app/models/concerns/globalized.rb
+++ app/models/concerns/globalized.rb
@@ -3,6 +3,28 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
+# Copyright (c) 2009, 2010, 2011, 2012, 2013 Tomek "Tomash" Stachewicz,
+# Robert Pankowecki, Chris Salzberg
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 module Globalized
   extend ActiveSupport::Concern
 
@@ -19,6 +41,7 @@ module Globalized
       globalize_accessors
     end
 
+    # Globalize Accessors code from https://github.com/globalize/globalize-accessors. See license in the file header.
     def globalize_accessors(options = {})
       options.reverse_merge!(locales: I18n.available_locales, attributes: translated_attribute_names)
       class_attribute :globalize_locales, :globalize_attribute_names, instance_writer: false

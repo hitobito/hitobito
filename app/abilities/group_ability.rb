91f48007267cf6a04aeb074685b643d4a1e54e03
Andreas Maierhofer <maierhofer@puzzle.ch>
2025-08-13 11:15:12 +0200

    Update app/abilities/group_ability.rb
    Co-authored-by: Matthias Viehweger <viehweger@puzzle.ch>

--- app/abilities/group_ability.rb
+++ app/abilities/group_ability.rb
@@ -104,7 +104,7 @@ class GroupAbility < AbilityDsl::Base
   end
 
   def in_layer_group
-    user_context.permission_layer_ids(:finance).include?(subject.layer_group_id)
+    finance_layer_ids.include?(subject.layer_group_id)
   end
 
   def with_parent_in_same_layer

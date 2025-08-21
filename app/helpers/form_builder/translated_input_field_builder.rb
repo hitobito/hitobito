fce48d8edffd7356272e623ab19dc428e2be5fde
Jannik Pulfer <pulfer@puzzle.ch>
2025-08-21 13:51:18 +0200

    Add copyright header to new translated input field builder module

--- app/helpers/form_builder/translated_input_field_builder.rb
+++ app/helpers/form_builder/translated_input_field_builder.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+#  Copyright (c) 2012-2025, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 module FormBuilder::TranslatedInputFieldBuilder
   def translated_input_field(attr, args = {})
     rich_text = args.delete(:rich_text) || false

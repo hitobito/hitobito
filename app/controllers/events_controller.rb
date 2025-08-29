8e53b6c034c944d6ff84c116d249b869a5032a03
Jannik Pulfer <pulfer@puzzle.ch>
2025-08-29 16:26:04 +0200

    Outsource logic to globalize permitted params to domain class and expand it to also globalize nested attributes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -119,7 +119,7 @@ class EventsController < CrudController # rubocop:todo Metrics/ClassLength
     p = model_params.dup
     p.delete(:type)
     p.delete(:contact)
-    p.permit(permitted_attrs + globalized_accessors_to_permit)
+    p.permit(PermittedGlobalizedAttrs.new.permitted_attrs(self.permitted_attrs, entry))
   end
 
   def group
@@ -268,18 +268,7 @@ class EventsController < CrudController # rubocop:todo Metrics/ClassLength
   def assign_attributes
     assign_contact_attrs
     assign_visible_contact_attrs
-    super.tap do |attrs|
-      assign_global_question_translations
-    end
-  end
-
-  # NOTE - as form only sends translations for current locale, we manually add other translations
-  def assign_global_question_translations
-    (entry.admin_questions + entry.application_questions).select(&:derived?).each do |q|
-      template = Event::Question.find(q.derived_from_question_id)
-      q.question_translations = template.question_translations
-      q.choices_translations = template.choices_translations
-    end
+    super
   end
 
   def assign_contact_attrs

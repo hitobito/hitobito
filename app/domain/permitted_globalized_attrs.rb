8f745bc18c9c8c4de8694566bfb8617a1df002f6
Jannik Pulfer <pulfer@puzzle.ch>
2025-09-03 09:38:41 +0200

    Rewrite permitted globalized attrs by mapping the attrs instead of using direct manipulation and make the code more readable by splitting it up

--- app/domain/permitted_globalized_attrs.rb
+++ app/domain/permitted_globalized_attrs.rb
@@ -6,40 +6,62 @@
 #  https://github.com/hitobito/hitobito.
 
 class PermittedGlobalizedAttrs
-  def permitted_attrs(original_permitted_attrs, entry)
+  def initialize(entry)
     @entry = entry
-    permitted_attrs = original_permitted_attrs.dup
-    permitted_attrs.each do |permitted_attr|
-      if permitted_attr.is_a?(Symbol) && should_permit?(@entry.class, permitted_attr)
-        permitted_attrs.push(globalized_names_for_attr(permitted_attr))
-      else
-        globalize_nested_attrs(permitted_attr)
-      end
+  end
+
+  def permitted_attrs(original_permitted_attrs)
+    original_permitted_attrs.flat_map do |permitted_attr|
+      next permit(permitted_attr, @entry.class)
     end
-    permitted_attrs
   end
 
   private
 
-  def globalize_nested_attrs(permitted_attr)
-    if permitted_attr.is_a?(Hash)
-      permitted_attr.each do |k, v|
-        if k.end_with?("_attributes")
-          if v.is_a?(Array)
-            relation = k.to_s.sub(/_[^_]*$/, "").to_sym
-            klass = @entry.class.reflect_on_all_associations.find do |reflection|
-              reflection.name == relation
-            end&.klass
-            next if klass.blank?
-            v.each do |attr|
-              v.push(globalized_names_for_attr(attr)) if should_permit?(klass, attr)
-            end
-          else
-            globalize_nested_attrs(v)
-          end
+  def permit(permitted_attr, klass)
+    case permitted_attr
+    when Symbol
+      permit_symbol(permitted_attr, klass)
+    when Hash
+      permit_hash(permitted_attr)
+    else
+      permitted_attr
+    end
+  end
+
+  def permit_symbol(permitted_attr, klass)
+    if should_permit?(klass, permitted_attr)
+      return [permitted_attr, *globalized_names_for_attr(permitted_attr)]
+    end
+    permitted_attr
+  end
+
+  def permit_hash(permitted_attr)
+    permitted_attr.map do |k, v|
+      if k.end_with?("_attributes")
+        case v
+        when Array
+          next permit_relation_array(k, v)
+        when Hash
+          next {k => permit_hash(v)}
+        else
+          next {k => v}
         end
       end
+      next {k => v}
+    end.reduce(:merge)
+  end
+
+  def permit_relation_array(relation_name, relation_array)
+    relation = relation_name.to_s.sub(/_[^_]*$/, "").to_sym
+    klass = @entry.class.reflect_on_all_associations.find do |reflection|
+      reflection.name == relation
+    end&.klass
+    return {relation_name => relation_array} if klass.blank?
+    updated_relation_array = relation_array.flat_map do |permitted_attr|
+      next permit(permitted_attr, klass)
     end
+    {relation_name => updated_relation_array}
   end
 
   def should_permit?(klass, attr)

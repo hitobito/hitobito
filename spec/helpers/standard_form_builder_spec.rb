e54de297fa816ff17696c1063b7605fd2ac80c0f
Jannik Pulfer <pulfer@puzzle.ch>
2025-09-05 09:55:18 +0200

    Eliminate race condition when copying validators to globalized fields by moving logic from initializer to application class and early return in logic when there is only one locale

--- spec/helpers/standard_form_builder_spec.rb
+++ spec/helpers/standard_form_builder_spec.rb
@@ -285,6 +285,7 @@ describe "StandardFormBuilder" do
   describe "translated fields" do
     before do
       allow(Settings.application).to receive(:languages).and_return({de: "Deutsch", en: "English", fr: "Français"})
+      stub_const("Globalized::INPUTS_GLOBALIZED", true)
       CrudTestModel.globalize_accessors
     end
 
@@ -348,6 +349,7 @@ describe "StandardFormBuilder" do
       expect(dom).to have_css("div[class='d-flex'] div[class='input-group me-2 mb-2'] input[name='entry[#{attr}]']")
 
       allow(Settings.application).to receive(:languages).and_return({de: "Deutsch"})
+      stub_const("Globalized::INPUTS_GLOBALIZED", false)
 
       dom = Capybara::Node::Simple.new(form.input_field(attr))
 

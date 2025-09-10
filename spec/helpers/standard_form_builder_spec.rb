9b539fbaef709cfaa30018d5240e908dc1c9c743
Jannik Pulfer <pulfer@puzzle.ch>
2025-09-10 11:56:42 +0200

    Make setup of globalized specs easier with helper methods and fix specs

--- spec/helpers/standard_form_builder_spec.rb
+++ spec/helpers/standard_form_builder_spec.rb
@@ -290,9 +290,7 @@ describe "StandardFormBuilder" do
 
   describe "translated fields" do
     before do
-      allow(Settings.application).to receive(:languages).and_return({de: "Deutsch", en: "English", fr: "Français"})
-      stub_const("Globalized::INPUTS_GLOBALIZED", true)
-      CrudTestModel.globalize_accessors
+      with_globalized_models(CrudTestModel)
     end
 
     let(:dom) {
@@ -354,8 +352,7 @@ describe "StandardFormBuilder" do
       expect(dom).to have_css("input[name='entry[#{attr}]']")
       expect(dom).to have_css("div[class='d-flex'] div[class='input-group me-2 mb-2'] input[name='entry[#{attr}]']")
 
-      allow(Settings.application).to receive(:languages).and_return({de: "Deutsch"})
-      stub_const("Globalized::INPUTS_GLOBALIZED", false)
+      stub_languages({de: "Deutsch"})
 
       dom = Capybara::Node::Simple.new(form.input_field(attr))
 

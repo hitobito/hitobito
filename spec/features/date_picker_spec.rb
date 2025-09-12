f309e3cca113d7ed4f254c59c3a7c29e141420e6
Jannik Pulfer <pulfer@puzzle.ch>
2025-09-12 10:06:19 +0200

    Fix spec directly changing application settings

--- spec/features/date_picker_spec.rb
+++ spec/features/date_picker_spec.rb
@@ -12,7 +12,7 @@ describe "Datepicker", js: true do
   let(:person) { people(:root) }
 
   before do
-    Settings.application.languages = {de: "Deutsch", fr: "Français"}
+    stub_languages({de: "Deutsch", fr: "Français"})
     sign_in(person)
     visit list_courses_path(locale: lang) # any path with datepicker is possible
   end

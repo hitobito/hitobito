6f0f6054915e556ee9a6cd2cd48128927353daab
Jannik Pulfer <pulfer@puzzle.ch>
2025-09-19 13:49:43 +0200

    Fix main specs after bringing back default url options

--- spec/domain/export/ics/events_spec.rb
+++ spec/domain/export/ics/events_spec.rb
@@ -53,7 +53,7 @@ describe Export::Ics::Events do
       is_expected.to include(event.description)
       is_expected.to include(contact.person_name)
       is_expected.to include(contact.email)
-      url = Rails.application.routes.url_helpers.group_event_url(event.groups.first, event, host: ENV["RAILS_HOST_NAME"], locale: nil)
+      url = Rails.application.routes.url_helpers.group_event_url(event.groups.first, event, host: ENV["RAILS_HOST_NAME"], locale: :de)
       is_expected.to include(url)
     end
   end

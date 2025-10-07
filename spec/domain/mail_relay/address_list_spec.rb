bbc816271ba4259c59d9afe3761fe1693a8d5f9c
Philipp Koster <koster@puzzle.ch>
2025-10-07 13:51:38 +0200

    migrate mail relay

--- spec/domain/mail_relay/address_list_spec.rb
+++ spec/domain/mail_relay/address_list_spec.rb
@@ -90,4 +90,85 @@ describe MailRelay::AddressList do
   def entries(people = Person.all, labels = [])
     described_class.new(people, labels).entries
   end
+
+  context "with people_managers feature" do
+    let(:top_leader) { people(:top_leader) }
+    let(:bottom_member) { people(:bottom_member) }
+    let(:manager) { Fabricate(:person, first_name: "People Manager", email: "people_manager@example.com", primary_group: groups(:bottom_group_two_one)) }
+    let(:managed) { Fabricate(:person, first_name: "People Managed", email: "people_managed@example.com", primary_group: groups(:bottom_group_two_one)) }
+
+    before do
+      managed.update! managers: [manager]
+
+      allow(FeatureGate).to receive(:enabled?).and_call_original
+      allow(FeatureGate).to receive(:enabled?).with("people.people_managers").and_return true
+    end
+
+    context "entries" do
+      it "contains main and additional managers mailing emails" do
+        e1 = Fabricate(:additional_email, contactable: manager, mailings: true)
+        e2 = Fabricate(:additional_email, contactable: top_leader, mailings: true)
+
+        Fabricate(:additional_email, contactable: manager, mailings: false)
+        Fabricate(:additional_email, contactable: managed, mailings: false)
+        expect(entries([managed, top_leader])).to match_array([
+          managed.email,
+          "top_leader@example.com",
+          manager.email,
+          e1.email,
+          e2.email
+        ])
+      end
+
+      it "does not contain blank manager emails" do
+        manager.update!(email: " ")
+
+        expect(entries(managed)).to match_array([
+          managed.email
+        ])
+      end
+
+      it "it uses only manager additional_email if label matches" do
+        e1 = Fabricate(:additional_email, contactable: managed, label: "foo")
+        e2 = Fabricate(:additional_email, contactable: manager, label: "foo")
+        Fabricate(:additional_email, contactable: manager, label: "bar")
+        expect(entries(managed, %w[foo])).to match_array([
+          e1.email,
+          e2.email
+        ])
+      end
+
+      it "it uses manager additional_email and main address if matches" do
+        e1 = Fabricate(:additional_email, contactable: managed, label: "foo")
+        e2 = Fabricate(:additional_email, contactable: manager, label: "foo")
+        expect(entries(managed, %W[foo #{MailingList::DEFAULT_LABEL}])).to match_array([
+          e1.email,
+          e2.email,
+          managed.email,
+          manager.email
+        ])
+      end
+
+      it "for new person returns only the person" do
+        new_manager = Fabricate.build(:person)
+        new_person = Fabricate.build(:person, managers: [new_manager])
+        expect(entries(new_person)).to match_array([
+          new_person.email,
+          new_manager.email
+        ])
+      end
+
+      it "contains only manager emails when managed has no email" do
+        managed.update!(email: nil)
+
+        expect(entries(managed)).to match_array([
+          manager.email
+        ])
+      end
+    end
+
+    def entries(people = Person.all, labels = [])
+      described_class.new(people, labels).entries
+    end
+  end
 end

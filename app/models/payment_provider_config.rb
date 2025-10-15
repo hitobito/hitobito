a4cea5abeea31a48981ae9fd7b44353698fc214e
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-10-15 15:40:17 +0200

    Remove redundant enum values and deprecation

--- app/models/payment_provider_config.rb
+++ app/models/payment_provider_config.rb
@@ -30,7 +30,7 @@ class PaymentProviderConfig < ActiveRecord::Base
   include Encryptable
   include I18nEnums
 
-  enum status: {draft: 0, pending: 1, registered: 2}
+  enum :status, [:draft, :pending, :registered]
   i18n_enum :payment_provider, Settings.payment_providers.map(&:name)
 
   belongs_to :invoice_config

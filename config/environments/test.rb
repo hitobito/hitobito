3bb05516c05bbfc9d007937631b9df41c34c9d17
Daniel Illi <daniel@illi.zone>
2025-10-17 16:35:32 +0200

    Follow config file changes from rails 8.0 upgrade

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,12 +39,17 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
+  # Disable caching for Action Mailer templates even if Action Controller
+  # caching is enabled.
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+
+  # Unlike controllers, the mailer instance doesn't have any context about the
+  # incoming request so you'll need to provide the :host parameter yourself.
   config.action_mailer.default_url_options = {host: "test.host", locale: nil}
   config.action_mailer.asset_host = "http://test.host"
 
@@ -65,6 +70,10 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Raise error when a before_action's only/except options reference missing actions.
+  config.action_controller.raise_on_missing_callback_actions = true
+
   routes.default_url_options[:host] = "test.host"
 
   unless ENV["RAILS_ENABLE_TEST_LOG"]

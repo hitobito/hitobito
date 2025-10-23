1ebc5b20ff6a09e17625ef21f9c4bf49d969eb3f
Jannik Pulfer <pulfer@puzzle.ch>
2025-10-23 14:08:08 +0200

    Add missing license header

--- db/migrate/20250815105604_remove_null_constraints_from_custom_content_translations.rb
+++ db/migrate/20250815105604_remove_null_constraints_from_custom_content_translations.rb
@@ -1,3 +1,7 @@
+#  Copyright (c) 2012-2025, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
 class RemoveNullConstraintsFromCustomContentTranslations < ActiveRecord::Migration[7.1]
   def change
     change_column_null :custom_content_translations, :label, true

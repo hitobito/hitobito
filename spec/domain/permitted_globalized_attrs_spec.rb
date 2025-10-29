1ff6510dd953d8c9065464f0aca205161a5a9b70
Jannik Pulfer <pulfer@puzzle.ch>
2025-10-29 12:49:17 +0100

    Rewrite specs for permitted globalized attrs with new test classes

--- spec/domain/permitted_globalized_attrs_spec.rb
+++ spec/domain/permitted_globalized_attrs_spec.rb
@@ -8,37 +8,99 @@
 require "spec_helper"
 
 describe PermittedGlobalizedAttrs do
-  it "adds globalized version of permitted attrs but not for current locale" do
-    permitted_attrs = EventsController.permitted_attrs.deep_dup + Event.used_attributes.deep_dup
-    permitted_globalized_attrs = described_class.new(Event, permitted_attrs).permitted_attrs
-    permitted_globalized_nested_attrs =
-      permitted_globalized_attrs.find { |attr| attr.is_a? Hash }[:application_questions_attributes]
-
-    expected_globalized_attrs, current_locale_attrs =
-      Event.globalize_attribute_names.partition { |a| !a.end_with?("_#{I18n.locale}") }
-    expected_globalized_nested_attrs, current_locale_nested_attrs =
-      Event::Question.globalize_attribute_names.partition { |a| !a.end_with?("_#{I18n.locale}") }
-    expect(permitted_globalized_attrs).to include(*expected_globalized_attrs)
-    expect(permitted_globalized_attrs).not_to include(*current_locale_attrs)
-    expect(permitted_globalized_nested_attrs).to include(*expected_globalized_nested_attrs)
-    expect(permitted_globalized_nested_attrs).not_to include(*current_locale_nested_attrs)
+  let(:permitted_attrs) do
+    [:globalized_attr, :other_globalized_attr, {
+      related_test_model_attributes: [:globalized_relation_attr, :other_globalized_relation_attr]
+    }]
   end
 
-  it "doesnt add globalized version of permitted attrs when base attr is not permitted" do
-    permitted_attrs = EventsController.permitted_attrs.deep_dup + Event.used_attributes.deep_dup
+  let(:permitted_globalized_attrs) { described_class.new(TestModel, permitted_attrs).permitted_attrs }
 
-    removed_attr = permitted_attrs.delete(:name)
-    removed_nested_attr =
-      permitted_attrs.find { |attr| attr.is_a? Hash }[:application_questions_attributes].delete(:question)
+  it "adds globalized version of permitted attrs except for current locale" do
+    expected_globalized_attrs = attrs_with_globalized_versions(:globalized_attr, :other_globalized_attr)
 
-    expect(removed_attr).not_to be_nil
-    expect(removed_nested_attr).not_to be_nil
+    expect(permitted_globalized_attrs).to include(*expected_globalized_attrs)
+    expect(permitted_globalized_attrs).not_to include(:globalized_attr_de, :other_globalized_attr_de)
+  end
 
-    permitted_globalized_attrs = described_class.new(Event, permitted_attrs).permitted_attrs
-    permitted_globalized_nested_attrs =
-      permitted_globalized_attrs.find { |attr| attr.is_a? Hash }[:application_questions_attributes]
+  it "doesnt add globalized version of permitted attrs when base attr not permitted" do
+    permitted_attrs.delete(:other_globalized_attr)
 
-    expect(permitted_globalized_attrs).not_to include(:name_de, :name_en, :name_fr)
-    expect(permitted_globalized_nested_attrs).not_to include(:question_de, :question_en, :question_fr)
+    expected_globalized_attrs = attrs_with_globalized_versions(:globalized_attr)
+    unexpected_globalized_attrs = attrs_with_globalized_versions(:other_globalized_attr, only_additional: false)
+
+    expect(permitted_globalized_attrs).to include(*expected_globalized_attrs)
+    expect(permitted_globalized_attrs).not_to include(*unexpected_globalized_attrs)
+  end
+
+  it "adds globalized version of permitted attrs for relations except for current locale" do
+    permitted_globalized_relation_attrs =
+      permitted_globalized_attrs.find { |attr| attr.is_a? Hash }[:related_test_model_attributes]
+
+    expected_globalized_relation_attrs =
+      attrs_with_globalized_versions(:globalized_relation_attr, :other_globalized_relation_attr)
+
+    expect(permitted_globalized_relation_attrs).to include(*expected_globalized_relation_attrs)
+    expect(permitted_globalized_relation_attrs).not_to include(
+      :globalized_relation_attr_de, :other_globalized_relation_attr_de
+    )
+  end
+
+  it "doesnt add globalized version of permitted attrs for relations when base attr not permitted" do
+    permitted_attrs.find { |attr| attr.is_a? Hash }[:related_test_model_attributes]
+                   .delete(:other_globalized_relation_attr)
+
+    permitted_globalized_relation_attrs =
+      permitted_globalized_attrs.find { |attr| attr.is_a? Hash }[:related_test_model_attributes]
+
+    expected_globalized_relation_attrs =
+      attrs_with_globalized_versions(:globalized_relation_attr)
+    unexpected_globalized_relation_attrs =
+      attrs_with_globalized_versions(:other_globalized_relation_attr, only_additional: false)
+
+    expect(permitted_globalized_relation_attrs).to include(*expected_globalized_relation_attrs)
+    expect(permitted_globalized_relation_attrs).not_to include(*unexpected_globalized_relation_attrs)
+  end
+
+  def attrs_with_globalized_versions(*attrs, only_additional: true)
+    (attrs + attrs.map { |attr| Globalized.globalized_names_for_attr(attr, only_additional) }).flatten
+  end
+end
+
+class TestModel
+  def self.include?(module_)
+    return true if module_ == Globalized
+
+    super
+  end
+
+  def self.translated_attribute_names
+    %i[globalized_attr other_globalized_attr]
+  end
+
+  def self.reflect_on_all_associations
+    [StubbedReflection.new]
+  end
+end
+
+class RelatedTestModel
+  def self.include?(module_)
+    return true if module_ == Globalized
+
+    super
+  end
+
+  def self.translated_attribute_names
+    %i[globalized_relation_attr other_globalized_relation_attr]
+  end
+end
+
+class StubbedReflection
+  def name
+    :related_test_model
+  end
+
+  def klass
+    RelatedTestModel
   end
 end

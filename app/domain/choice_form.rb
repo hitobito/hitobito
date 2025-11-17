ecef7e705a7d435aa0d9a85320aa90a5c7b2f445
Jannik Pulfer <pulfer@puzzle.ch>
2025-11-17 10:04:57 +0100

    Move choices to class method and make them work with unnormalized data, remove ancestor check

--- app/domain/choice_form.rb
+++ app/domain/choice_form.rb
@@ -1,26 +1,11 @@
 module ChoiceForm
-  class Form
-    def choices(question)
-      languages = [I18n.locale] + Globalized.additional_languages
-      globalized_grouped_choice_items = languages.map do |lang|
-        I18n.with_locale(lang) do
-          question.choice_items
-        end
-      end.transpose
-
-      globalized_grouped_choice_items.map { |choice_translations| Choice.new(choice_translations) }
-    end
-  end
-
   class Choice
     include ActiveModel::Model
 
-    def initialize(choice_translations)
-      define_singleton_method(:choice) { choice_translations.first }
-
-      languages = [I18n.locale] + Globalized.additional_languages
-      languages.each do |lang|
-        define_singleton_method(:"choice_#{lang}") { choice_translations.shift }
+    def initialize(choice_translations = {})
+      define_singleton_method(:choice) { choice_translations[I18n.locale] }
+      Globalized.languages.each do |lang|
+        define_singleton_method(:"choice_#{lang}") { choice_translations[lang] }
       end
     end
 
@@ -37,10 +22,6 @@ module ChoiceForm
 
     def self.translated_attribute_names
       [:choice]
-    end
-
-    def self.ancestors
-      [Globalized] + super
     end
   end
 

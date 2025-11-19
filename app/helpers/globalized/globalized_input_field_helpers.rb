e5899f13f928885561834e5340ead8b723c1fd7a
Jannik Pulfer <pulfer@puzzle.ch>
2025-11-19 15:16:33 +0100

    Only show locale indicator when all inputs are visible and make its background transparent

--- app/helpers/globalized/globalized_input_field_helpers.rb
+++ app/helpers/globalized/globalized_input_field_helpers.rb
@@ -97,7 +97,7 @@ module Globalized::GlobalizedInputFieldHelpers
   def locale_indicator(locale)
     content_tag(
       :span, locale.to_s.upcase,
-      class: "input-group-text d-flex justify-content-center rounded-start locale-indicator",
+      class: "input-group-text d-flex justify-content-center locale-indicator bg-transparent",
       "data-globalized-fields-target": "localeIndicator",
       "data-bs-toggle": "tooltip", "data-bs-title": I18n.t("global.form.locale_indicator")
     )

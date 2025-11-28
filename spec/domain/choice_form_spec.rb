bc5fae3bab20220505c09a60a1d2efe21550b4dd
Jannik Pulfer <pulfer@puzzle.ch>
2025-11-28 14:55:27 +0100

    Fix choice form spec interfering with other specs

--- spec/domain/choice_form_spec.rb
+++ spec/domain/choice_form_spec.rb
@@ -13,21 +13,21 @@ module ChoiceForm
       let(:choice) { Choice.new(@choice_translations) }
 
       it "should use current locale when fallbacks are false" do
-        I18n.fallbacks = false
+        stub_fallbacks(false)
 
         @choice_translations = {de: "", en: "", fr: "Fallback", it: ""}
         expect(choice.choice).to eql("")
       end
 
       it "should use current locale when fallbacks are nil" do
-        I18n.fallbacks = nil
+        stub_fallbacks(nil)
 
         @choice_translations = {de: "", en: "", fr: "Fallback", it: ""}
         expect(choice.choice).to eql("")
       end
 
       it "should use default locale when fallbacks are true" do
-        I18n.fallbacks = true
+        stub_fallbacks(true)
         I18n.default_locale = :it
 
         @choice_translations = {de: "", en: "", fr: "Fallback", it: "Default locale"}
@@ -35,20 +35,20 @@ module ChoiceForm
       end
 
       it "should use correct fallback when fallbacks are an array" do
-        I18n.fallbacks = [:de, :fr, :it, :en]
+        stub_fallbacks([:de, :fr, :it, :en])
 
         @choice_translations = {de: "", en: "Fallback 3", fr: "Fallback 1", it: "Fallback 2"}
         expect(choice.choice).to eql("Fallback 1")
-        I18n.fallbacks = [:de, :it, :fr, :en]
+        stub_fallbacks([:de, :it, :fr, :en])
         expect(choice.choice).to eql("Fallback 2")
-        I18n.fallbacks = [:de, :en, :it, :fr]
+        stub_fallbacks([:de, :en, :it, :fr])
         expect(choice.choice).to eql("Fallback 3")
-        I18n.fallbacks = [:de]
+        stub_fallbacks([:de])
         expect(choice.choice).to eql("")
       end
 
       it "should use correct fallback when fallbacks are a hash" do
-        I18n.fallbacks = {de: :it, fr: [:de, :en]}
+        stub_fallbacks({de: :it, fr: [:de, :en]})
 
         @choice_translations = {de: "", en: "Fallback 2", fr: "", it: "Fallback 1"}
 
@@ -59,7 +59,7 @@ module ChoiceForm
       end
 
       it "should return original value if fallbacks are also not present" do
-        I18n.fallbacks = {de: :it, en: :fr}
+        stub_fallbacks({de: :it, en: :fr})
 
         @choice_translations = {de: "", en: nil, fr: "", it: nil}
 
@@ -67,6 +67,10 @@ module ChoiceForm
 
         I18n.locale = :en
         expect(choice.choice).to be_nil
+      end
+
+      def stub_fallbacks(fallbacks)
+        allow(I18n).to receive(:fallbacks).and_return(fallbacks)
       end
     end
   end

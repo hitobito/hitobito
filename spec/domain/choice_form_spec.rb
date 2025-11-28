3e8b547dc0566151dc81481397d3877ac0eb0bee
Jannik Pulfer <pulfer@puzzle.ch>
2025-11-28 16:03:12 +0100

    Test if answers are recognised as checked in all locales and reset default locale after spec to not interfere with other specs

--- spec/domain/choice_form_spec.rb
+++ spec/domain/choice_form_spec.rb
@@ -32,6 +32,8 @@ module ChoiceForm
 
         @choice_translations = {de: "", en: "", fr: "Fallback", it: "Default locale"}
         expect(choice.choice).to eql("Default locale")
+
+        I18n.default_locale = :de
       end
 
       it "should use correct fallback when fallbacks are an array" do
@@ -71,6 +73,32 @@ module ChoiceForm
 
       def stub_fallbacks(fallbacks)
         allow(I18n).to receive(:fallbacks).and_return(fallbacks)
+      end
+    end
+
+    describe "#checked?" do
+      let(:choice1) { Choice.new({de: "Choice 1 DE", en: "Choice 1 EN", fr: "Choice 1 FR", it: "Choice 1 IT"}) }
+      let(:choice2) { Choice.new({de: "Choice 2 DE", en: "Choice 2 EN", fr: "Choice 2 FR", it: "Choice 2 IT"}) }
+      let(:choice3) { Choice.new({de: "Choice 3 DE", en: "Choice 3 EN", fr: "Choice 3 FR", it: "Choice 3 IT"}) }
+
+      it "should return true for checked option if single choice is selectable" do
+        answers = "Choice 1 EN"
+        Globalized.languages.each do |lang|
+          I18n.locale = lang
+          expect(choice1.checked?(answers)).to be_truthy
+          expect(choice2.checked?(answers)).to be_falsy
+          expect(choice3.checked?(answers)).to be_falsy
+        end
+      end
+
+      it "should return true for all checked options if multiple choices are selectable" do
+        answers = "Choice 1 EN,Choice 3 EN"
+        Globalized.languages.each do |lang|
+          I18n.locale = lang
+          expect(choice1.checked?(answers)).to be_truthy
+          expect(choice2.checked?(answers)).to be_falsy
+          expect(choice3.checked?(answers)).to be_truthy
+        end
       end
     end
   end

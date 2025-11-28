0607cff3f8c1111ae6bb8d673fd19d49168b58d8
Jannik Pulfer <pulfer@puzzle.ch>
2025-11-28 14:24:56 +0100

    Fix where previously selected choices were not selected when editing participation in another language and clean up default question model

--- app/domain/choice_form.rb
+++ app/domain/choice_form.rb
@@ -3,13 +3,16 @@ module ChoiceForm
     include ActiveModel::Model
 
     def initialize(choice_translations = {})
+      @choice_translations = choice_translations
       define_singleton_method(:choice) do
-        choice_with_fallbacks(choice_translations)
+        choice_with_fallbacks
       end
       Globalized.languages.each do |lang|
-        define_singleton_method(:"choice_#{lang}") { choice_translations[lang] }
+        define_singleton_method(:"choice_#{lang}") { @choice_translations[lang] }
       end
     end
+
+    attr_accessor :choice_translations
 
     def _destroy
     end
@@ -29,31 +32,35 @@ module ChoiceForm
       [:choice]
     end
 
+    def checked?(answer)
+      answer.to_s.split(",").map(&:strip).any? { |a| choice_translations.value?(a) }
+    end
+
     private
 
-    def choice_with_fallbacks(choice_translations, fallbacks = I18n.fallbacks)
+    def choice_with_fallbacks(fallbacks = I18n.fallbacks)
       case fallbacks
       when true
-        choice_with_fallback(choice_translations, I18n.default_locale)
+        choice_with_fallback(I18n.default_locale)
       when Symbol || String
-        choice_with_fallback(choice_translations, fallbacks.to_sym)
+        choice_with_fallback(fallbacks.to_sym)
       when Array
-        fallback_from_array(choice_translations, fallbacks)
+        fallback_from_array(fallbacks)
       when Hash
-        choice_with_fallbacks(choice_translations, fallbacks[I18n.locale])
+        choice_with_fallbacks(fallbacks[I18n.locale])
       else
-        choice_translations[I18n.locale]
+        @choice_translations[I18n.locale]
       end
     end
 
-    def fallback_from_array(choice_translations, fallbacks)
-      fallbacks.map { |fallback| choice_translations[fallback] }.find(&:present?) ||
-        choice_translations[I18n.locale]
+    def fallback_from_array(fallbacks)
+      fallbacks.map { |fallback| @choice_translations[fallback] }.find(&:present?) ||
+        @choice_translations[I18n.locale]
     end
 
-    def choice_with_fallback(choice_translations, fallback)
-      current_choice = choice_translations[I18n.locale]
-      fallback_choice = choice_translations[fallback]
+    def choice_with_fallback(fallback)
+      current_choice = @choice_translations[I18n.locale]
+      fallback_choice = @choice_translations[fallback]
 
       if current_choice.present? || (current_choice.blank? && fallback_choice.blank?)
         return current_choice
--- spec/models/event/question/default_spec.rb
+++ spec/models/event/question/default_spec.rb
@@ -39,21 +39,21 @@ describe Event::Question::Default do
     end
 
     it "is valid without choices" do
-      expect(subject.choice_items).to be_empty
+      expect(subject.choices).to be_empty
 
       is_expected.to be_valid
     end
 
     it "is valid with several choices" do
       subject.choices = "ja,nein,vielleicht"
-      expect(subject.choice_items).to have(3).items
+      expect(subject.choices).to have(3).items
 
       is_expected.to be_valid
     end
 
     it "is valid with one choice" do
       subject.choices = "ja"
-      expect(subject.choice_items).to have(1).item
+      expect(subject.choices).to have(1).item
 
       is_expected.to be_valid
     end
@@ -63,7 +63,7 @@ describe Event::Question::Default do
     subject { described_class.new(question: "Test?", choices: "ja") }
 
     it "knows that it only has one answer" do
-      is_expected.to be_one_answer_available
+      expect(subject.choices).to be_one
     end
 
     it "may be required" do

0b7bce8717426316b7a4e43873c11be8681c674d
Jannik Pulfer <pulfer@puzzle.ch>
2025-11-28 09:38:17 +0100

    Implement fallbacks for choice, test the behavior and make choice items return choices with fallbacks

---
+++ spec/domain/choice_form_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2025, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+require "spec_helper"
+
+module ChoiceForm
+  describe Choice do
+    describe "choice fallbacks" do
+      let(:choice) { Choice.new(@choice_translations) }
+
+      it "should use current locale when fallbacks are false" do
+        I18n.fallbacks = false
+
+        @choice_translations = {de: "", en: "", fr: "Fallback", it: ""}
+        expect(choice.choice).to eql("")
+      end
+
+      it "should use current locale when fallbacks are nil" do
+        I18n.fallbacks = nil
+
+        @choice_translations = {de: "", en: "", fr: "Fallback", it: ""}
+        expect(choice.choice).to eql("")
+      end
+
+      it "should use default locale when fallbacks are true" do
+        I18n.fallbacks = true
+        I18n.default_locale = :it
+
+        @choice_translations = {de: "", en: "", fr: "Fallback", it: "Default locale"}
+        expect(choice.choice).to eql("Default locale")
+      end
+
+      it "should use correct fallback when fallbacks are an array" do
+        I18n.fallbacks = [:de, :fr, :it, :en]
+
+        @choice_translations = {de: "", en: "Fallback 3", fr: "Fallback 1", it: "Fallback 2"}
+        expect(choice.choice).to eql("Fallback 1")
+        I18n.fallbacks = [:de, :it, :fr, :en]
+        expect(choice.choice).to eql("Fallback 2")
+        I18n.fallbacks = [:de, :en, :it, :fr]
+        expect(choice.choice).to eql("Fallback 3")
+        I18n.fallbacks = [:de]
+        expect(choice.choice).to eql("")
+      end
+
+      it "should use correct fallback when fallbacks are a hash" do
+        I18n.fallbacks = {de: :it, fr: [:de, :en]}
+
+        @choice_translations = {de: "", en: "Fallback 2", fr: "", it: "Fallback 1"}
+
+        expect(choice.choice).to eql("Fallback 1")
+
+        I18n.locale = :fr
+        expect(choice.choice).to eql("Fallback 2")
+      end
+
+      it "should return original value if fallbacks are also not present" do
+        I18n.fallbacks = {de: :it, en: :fr}
+
+        @choice_translations = {de: "", en: nil, fr: "", it: nil}
+
+        expect(choice.choice).to eql("")
+
+        I18n.locale = :en
+        expect(choice.choice).to be_nil
+      end
+    end
+  end
+end

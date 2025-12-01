5b5939578fe9b89c266e49a2e4402f0d450d94d7
Nils Rauch <rauch@puzzle.ch>
2025-12-01 11:05:07 +0100

    Rename mysql pkey index to postgres convention before table rename (#3801)
    * Rename mysql pkey index to postgres convention before table rename
    Refs: #3746
    
    * Do not query by namespace
    
    * Avoid rename if the index already named correctly
    
    ---------
    
    Co-authored-by: carlobeltrame <carlo@beltra.me>

--- db/migrate/20251121120000_rename_invoice_lists_to_invoice_runs.rb
+++ db/migrate/20251121120000_rename_invoice_lists_to_invoice_runs.rb
@@ -1,7 +1,33 @@
 class RenameInvoiceListsToInvoiceRuns < ActiveRecord::Migration[8.0]
   def change
+    # Since the table was initially created with mysql (on old instances), the pkey index does not
+    # follow the naming convention the postgres adapter expects.
+    # Thus we rename it beforehand if the name is wrong
+    old_pkey = find_pkey_index_name
+    unless old_pkey.to_sym == :invoice_lists_pkey
+      rename_index(:invoice_lists, old_pkey, :invoice_lists_pkey)
+    end
+
     rename_table(:invoice_lists, :invoice_runs)
     rename_column(:invoices, :invoice_list_id, :invoice_run_id)
     rename_column(:messages, :invoice_list_id, :invoice_run_id)
+  end
+
+  def find_pkey_index_name
+    result = execute <<~SQL
+      SELECT
+        i.relname AS primary_key_index_name
+      FROM
+        pg_constraint c
+      JOIN
+        pg_class t ON c.conrelid = t.oid
+      JOIN
+        pg_class i ON c.conindid = i.oid
+      WHERE
+        t.relname = 'invoice_lists'
+        AND c.contype = 'p'
+        AND t.relkind = 'r' -- 'r' for regular table;
+    SQL
+    result.first["primary_key_index_name"]
   end
 end

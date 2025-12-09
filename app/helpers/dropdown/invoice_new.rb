a3b18b3c5ffed3fa7d594ede72b3570592461d0b
Matthias Viehweger <kronn@kronn.de>
2025-12-09 17:03:06 +0100

    Address Bullet-Warnings

--- app/helpers/dropdown/invoice_new.rb
+++ app/helpers/dropdown/invoice_new.rb
@@ -109,7 +109,9 @@ module Dropdown
 
     def finance_groups
       # rubocop:todo Layout/LineLength
-      @finance_groups ||= Group.where(id: current_ability.user_finance_layer_ids).includes(:invoice_config)
+      @finance_groups ||= Group.where(id: current_ability.user_finance_layer_ids)
+        .includes(:invoice_config)
+        .includes(invoice_config: [:rich_text_header, :logo_attachment])
       # rubocop:enable Layout/LineLength
     end
 

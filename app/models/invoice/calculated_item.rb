1088fe6759e525670824f9ce71984efcf4bcbfb8
Carlo Beltrame <beltrame@puzzle.ch>
2026-01-07 17:25:37 +0100

    Move classes to correct place (#3750)

--- app/models/invoice/calculated_item.rb
+++ app/models/invoice/calculated_item.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2025, Swiss Badminton. This file is part of
+#  Copyright (c) 2012-2026, Swiss Badminton. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
-class InvoiceItem::FixedFee < InvoiceItem
+class Invoice::CalculatedItem < InvoiceItem
   attr_readonly :name, :dynamic_cost_parameters
 
   def name
---
+++ app/models/period_invoice_template/role_count_item.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2026, Swiss Badminton. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class PeriodInvoiceTemplate::RoleCountItem < PeriodInvoiceTemplate::Item
+  def initialize(fee:, key:, unit_cost:, roles:, layer_group_ids: nil)
+    super(fee:, key:, unit_cost:, layer_group_ids:)
+    @roles = roles
+  end
+
+  def scope = Role.joins(:group).where(type: @roles)
+end

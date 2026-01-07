9e252e4b10f9eaf17a3725dd8c3f39cf8a0e018a
Carlo Beltrame <beltrame@puzzle.ch>
2026-01-07 17:25:37 +0100

    Move classes to correct place (#3750)

---
+++ app/models/invoice/role_count_item.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2026, Swiss Badminton. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class Invoice::RoleCountItem < Invoice::CalculatedItem
+  attr_accessor :roles
+
+  has_many :billed_models,
+    inverse_of: :invoice_item,
+    foreign_key: :invoice_item_id,
+    dependent: :delete_all
+
+  after_create :create_billed_models
+
+  private
+
+  def create_billed_models
+    rows = roles.map do |role|
+      {
+        invoice_item_id: id,
+        billing_period_id: BillingPeriod.active.id,
+        model_id: role.id,
+        model_type: Role.sti_name
+      }
+    end
+    BilledModel.insert_all(rows)
+  end
+end
---
+++ app/models/period_invoice_template/item.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2026, Swiss Badminton. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class PeriodInvoiceTemplate::Item < ActiveRecord::Base
+  attr_reader :fee, :key, :unit_cost, :layer_group_ids
+
+  def initialize(fee:, key:, unit_cost:, layer_group_ids: nil)
+    @fee = fee
+    @key = key
+    @unit_cost = unit_cost
+    @layer_group_ids = layer_group_ids
+  end
+
+  def present? = count.positive?
+
+  def count = @count ||= models.count
+
+  def total_cost = count * unit_cost
+
+  def to_invoice_item
+    InvoiceItem::FixedFee
+      .new(name: key, unit_cost:, count:, dynamic_cost_parameters: {fixed_fees: fee})
+      .tap(&:recalculate)
+  end
+
+  def models
+    scope.then { |scope| layer_group_ids ? with_matching_layer_group(scope) : scope }
+  end
+
+  protected
+
+  def with_matching_layer_group(scope)
+    scope.where(groups: {layer_group_id: layer_group_ids})
+  end
+end

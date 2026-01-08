39754cc5947a3dd51bda1972bdfc12a0855699a7
Niklas Jäggi <jaeggi@puzzle.ch>
2026-01-08 22:54:54 +0100

    Add group invoice tab (#3865)

--- spec/models/concerns/contactable_spec.rb
+++ spec/models/concerns/contactable_spec.rb
@@ -8,12 +8,12 @@
 require "spec_helper"
 
 describe Contactable do
-  shared_examples "fixing common autocomplete issues" do |column|
-    let(:street) { "Main Street" }
-    let(:housenumber) { "23a" }
-    let(:zip_code) { 1234 }
-    let(:town) { "Jamestown" }
+  let(:street) { "Main Street" }
+  let(:housenumber) { "23a" }
+  let(:zip_code) { 1234 }
+  let(:town) { "Jamestown" }
 
+  shared_examples "fixing common autocomplete issues" do |column|
     subject(:model) { described_class.new(street:, housenumber:, zip_code:, town:) }
 
     relevant_attrs = [:street, :housenumber, :zip_code, :town]
@@ -52,5 +52,19 @@ describe Contactable do
   describe Group do
     it_behaves_like "fixing common autocomplete issues", :postbox
     it_behaves_like "fixing common autocomplete issues", :address_care_of
+  end
+
+  context "#invoice_address" do
+    let(:group) { groups(:top_layer) }
+
+    it "returns additional_email with invoice flag" do
+      group.additional_emails.create!(email: "foo@bar.com", contactable: group, label: "Privat", invoices: true)
+      expect(group.invoice_email).to eq "foo@bar.com"
+    end
+
+    it "returns nil when no additional_email with invoice_flag" do
+      group.additional_emails.create!(email: "foo@bar.com", contactable: group, label: "Privat", invoices: false)
+      expect(group.invoice_email).to be_nil
+    end
   end
 end

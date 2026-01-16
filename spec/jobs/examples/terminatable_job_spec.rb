18785c2d7a64ac84b5600674ebc400e65d7c9a58
Jannik Pulfer <pulfer@puzzle.ch>
2026-01-16 17:12:38 +0100

    Add spec to ensure jobs can be terminated and add missing license information

---
+++ spec/jobs/examples/terminatable_job_spec.rb
@@ -0,0 +1,26 @@
+#  Copyright (c) 2012-2025, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+require "spec_helper"
+
+describe Examples::TerminatableJob do
+  it "should terminate running job" do
+    terminatable_job = described_class.new
+    expect do
+      terminatable_job = terminatable_job.enqueue!
+    end.to change(Delayed::Job, :count).by(1)
+
+    expect do
+      Thread.new do
+        sleep 3
+        terminatable_job.status_control = "terminate"
+        terminatable_job.save!
+      end
+      worker = Delayed::Worker.new
+      worker.max_run_time = 10.seconds
+      worker.work_off
+    end.to change(Delayed::Job, :count).by(-1)
+  end
+end

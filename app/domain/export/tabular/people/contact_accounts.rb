927da19a15250f416fa6f8a19358832d29be0aab
Niklas <68543592+njaeggi@users.noreply.github.com>
2026-01-27 09:18:00 +0100

    Implement feedback

--- app/domain/export/tabular/people/contact_accounts.rb
+++ app/domain/export/tabular/people/contact_accounts.rb
@@ -7,17 +7,17 @@ module Export::Tabular::People
   module ContactAccounts
     class << self
       def key(model, label)
-        :"#{model.model_name.to_s.underscore}_#{(label || default_label(model)).downcase}"
+        :"#{model.model_name.to_s.underscore}_#{label_or_default(label, model).downcase}"
       end
 
       def human(model, label)
-        "#{model.model_name.human} #{label || default_label(model)}"
+        "#{model.model_name.human} #{label_or_default(label, model)}"
       end
 
       private
 
-      def default_label(model)
-        Settings.send(model.table_name.singularize).predefined_labels.first
+      def label_or_default(label, model)
+        label || Settings.send(model.table_name.singularize).predefined_labels.first
       end
     end
   end

8f28d013db73adfa234317f7d4940d501f4bc694
Carlo Beltrame <beltrame@puzzle.ch>
2026-01-28 17:09:38 +0100

    Try to get as far as possible with nested_form (#3751)

--- app/controllers/period_invoice_templates_controller.rb
+++ app/controllers/period_invoice_templates_controller.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2026, Hitobito AG. This file is part of
+#  Copyright (c) 2026, BdP and DPSG. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -6,5 +6,10 @@
 class PeriodInvoiceTemplatesController < CrudController
   self.nesting = Group
 
-  self.permitted_attrs = [:name, :start_on, :end_on]
+  self.permitted_attrs = [:name, :start_on, :end_on,
+    {
+      items: [
+        :id, :name, :type, :cost_center, :account, :dynamic_cost_params, :_destroy
+      ]
+    }]
 end

0daa8be8d70ea53db247ce6eb2362448bd3d2607
Daniel Illi <daniel@illi.zone>
2026-02-16 23:04:51 +0100

    Make contactable label input type configurable (#1800)

--- app/helpers/contactable_helper.rb
+++ app/helpers/contactable_helper.rb
@@ -4,6 +4,31 @@
 #  https://github.com/hitobito/hitobito.
 
 module ContactableHelper
+  def contact_method_label_field(form)
+    feature_gate_key = "#{form.object.class.name.underscore}.free_text_label"
+
+    return contact_method_label_text_field(form) if FeatureGate.enabled?(feature_gate_key)
+
+    contact_method_label_select(form)
+  end
+
+  def contact_method_label_text_field(form)
+    form.input_field(:translated_label,
+      placeholder: t(".placeholder_type"),
+      data: {provide: :typeahead, source: form.object.class.available_labels})
+  end
+
+  def contact_method_label_select(form)
+    contact_method = form.object
+    current_label = contact_method.label
+    options = (contact_method.class.predefined_labels | [current_label].compact).map do |value|
+      translated = contact_method.class.translate_label(value)
+      OpenStruct.new(value: value, translated: translated)
+    end
+    form.collection_select(:translated_label, options, :value, :translated, {},
+      class: "form-select form-select-sm")
+  end
+
   def contactable_public_field_icon
     content_tag(:span, data: {bs_toggle: :tooltip},
       title: t("contactable.public_check_box.tooltip")) do

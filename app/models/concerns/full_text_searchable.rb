2f43557ea6f3f1d8786733121b1f6b4d46b02873
Carlo Beltrame <beltrame@puzzle.ch>
2026-03-10 17:00:37 +0100

    Fix searching invoice items now that they are translateable (#3879)
    The calculation of the intermediate join table was wrong before. Now
    that we have to join invoice_item_translations, the old logic calculated
    invoices as intermediate join table, when it should be invoice_items.
    
    Also, this module was riddled with rubocop todos and consisted of one
    single define_singleton_method call. This is now split into multiple
    methods and uses ActiveSupport::Concern for better readability.
    
    While rewriting and optimizing this logic, I noticed that much of the
    effort is spent in parsing the search query into a database-safe string,
    which rails and postgres can do already.
    
    Where we were previously using the postgres function to_tsquery, we can
    instead use websearch_to_tsquery in order to get support for more
    advanced search syntax similar to web searching. The only thing missing
    was the prefix search which we previously had; this can be added to
    websearch_to_tsquery with some clever string manipulation. Sources:
    https://www.postgresql.org/docs/current/textsearch-controls.html
    https://stackoverflow.com/a/78595463/3711110
    
    We can then continue to use postgresql's built-in word stemming
    capabilities to improve the search feature even more.
    
    Future improvements may improve ranking results more highly if they come
    from a translation that matches the current application locale. However,
    that would necessitate separate search_columns per locale, and would
    definitely go too far for this fix and cleanup commit.

--- app/models/concerns/full_text_searchable.rb
+++ app/models/concerns/full_text_searchable.rb
@@ -1,72 +1,106 @@
-#  Copyright (c) 2012-2024, Hitobito AG. This file is part of
+#  Copyright (c) 2012-2026, Hitobito AG. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
 module FullTextSearchable
+  extend ActiveSupport::Concern
+
   SEARCH_COLUMN = :search_column
+  SUPPORTED_LANGUAGES = {
+    de: "german",
+    fr: "french",
+    it: "italian",
+    en: "english",
+    _: "simple"
+  }.with_indifferent_access.tap { |s| s.default = "simple" }
 
-  TS_QUERY_CHARS = ["(", ")", ":", "&", "|", "!", "'", "?", "%", "<", " ", " "]
+  included do
+    self.ignored_columns += [SEARCH_COLUMN]
+  end
 
-  # rubocop:todo Metrics/MethodLength
-  # rubocop:todo Metrics/AbcSize
-  def self.included(model) # rubocop:todo Metrics/CyclomaticComplexity # rubocop:todo Metrics/AbcSize # rubocop:todo Metrics/MethodLength
-    model.ignored_columns += [SEARCH_COLUMN]
+  module ClassMethods
+    def search(term)
+      associated_tables = self::SEARCHABLE_ATTRS.select { |attr| attr.is_a?(Hash) }
+        .map(&:keys).flatten
+      tables = [table_name] + associated_tables
 
-    model.define_singleton_method(:search) do |term| # rubocop:todo Metrics/BlockLength
-      # Use & to make sure every word in term has to match the result
-      sanitized_term = term.split.map do |t|
-        sanitized_t = ActiveRecord::Base.sanitize_sql_like(t).delete(*TS_QUERY_CHARS.join)
-
-        sanitized_t.present? ? "#{sanitized_t}:*" : nil
-      end.compact.join(" & ")
-
-      # Generate base search query and rank for main model
-      # rubocop:todo Layout/LineLength
-      base_query = "#{model.table_name}.#{SEARCH_COLUMN} @@ to_tsquery('simple', '#{sanitized_term}')"
-      # rubocop:enable Layout/LineLength
-      # rubocop:todo Layout/LineLength
-      base_rank = "COALESCE(ts_rank(#{model.table_name}.#{SEARCH_COLUMN}, to_tsquery('simple', '#{sanitized_term}')), 0)"
-      # rubocop:enable Layout/LineLength
-
-      associated_tables = model::SEARCHABLE_ATTRS.select { |attr|
-        attr.is_a?(Hash)
-      }.map(&:keys).flatten
-
-      # Build queries and ranks for each associated model
-      associated_queries = associated_tables.map do |assoc_model|
-        "#{assoc_model}.#{SEARCH_COLUMN} @@ to_tsquery('simple', '#{sanitized_term}')"
-      end
-      associated_ranks = associated_tables.map do |assoc_model|
-        # rubocop:todo Layout/LineLength
-        "COALESCE(ts_rank(#{assoc_model}.#{SEARCH_COLUMN}, to_tsquery('simple', '#{sanitized_term}')), 0)"
-        # rubocop:enable Layout/LineLength
-      end
-
-      # Combine main model ans associated model for query
-      search_conditions = ([base_query] + associated_queries).join(" OR ")
-      search_ranks = ([base_rank] + associated_ranks).join(", ")
-
-      join_tables = associated_tables.map do |associated_table|
-        if associated_table.to_s.end_with?("_translations")
-          if model.table_name.to_sym == associated_table.to_s.split("_").first.pluralize.to_sym
-            :translations
-          else
-            [associated_table.to_s.split("_").first.pluralize.to_sym,
-              associated_table.to_s.split("_").first.pluralize.to_sym => :translations]
-          end
-        else
-          associated_table
-        end
-      end
-
-      select(model.column_names, "GREATEST(#{search_ranks}) AS rank")
-        .left_joins(join_tables.flatten)
-        .where(search_conditions, term: "#{term}:*")
+      select(column_names, "GREATEST(#{tsquery_search_ranks(tables, term)}) AS rank")
+        .left_joins(tsquery_join_tables(associated_tables))
+        .where(tsquery_search_conditions(tables, term))
         .order("rank DESC")
         .distinct
     end
+
+    private
+
+    # Returns SQL to generate a tsquery expression for searching. This supports:
+    # - Associative search, i.e. every word from the query must be present in the results.
+    #   websearch_to_tsquery inserts the operator & in between all normal search terms.
+    # - Prefix search, i.e. every word in the query can also match if it is only a prefix
+    #   of a word in the result.
+    #   We acheive this by appending :* to every quoted query word.
+    # - Quoting multiple words in the search query to only match results which contain
+    #   this word sequence, e.g. the search term `for search` will match the text
+    #   `search for`, but the search term `"for search"` with quotes will not.
+    # - Querying e.g. by `termA OR termB` will match both the text `terma` and the text
+    #   `termb`.
+    # - Excluding search terms by adding a dash in front of them. E.g. the query
+    #   `foo -bar` will match the text `foo baz` but not the text `foo bar baz`.
+    # - Language-dependent word stemming and stopwords. E.g. when using the application
+    #   in French, the query `la matinée der` is automatically changed as if it were
+    #   `matin der`, but if using the application in German, it's changed to mean
+    #   `la matiné`, and in English it's `la matiné der`.
+    # - Automatically removing special characters from the search terms.
+    def tsquery(term)
+      ActiveRecord::Base.sanitize_sql_array([
+        <<~SQL.squish,
+          replace(
+            websearch_to_tsquery(:lang, :term)::text || ' ',
+            ''' ',
+            ''':* '
+          )::tsquery
+        SQL
+        term: term,
+        lang: tsquery_lang
+      ])
+    end
+
+    # Returns the ts_config name which most closely matches the current application language.
+    # The ts_config contains word stemming rules and word ignore rules.
+    def tsquery_lang
+      # PostgreSQL supports a large range of languages out of the box. This here is just a
+      # selection of the ones most relevant to our application. To list all supported configs,
+      # run `\dF` or `select * from pg_catalog.pg_ts_config;` in the psql client.
+
+      SUPPORTED_LANGUAGES[I18n.locale || :simple]
+    end
+
+    def tsquery_search_ranks(tables, term)
+      tables.map do |table|
+        "COALESCE(ts_rank(#{table}.#{SEARCH_COLUMN}, #{tsquery(term)}), 0)"
+      end.join(", ")
+    end
+
+    def tsquery_search_conditions(tables, term)
+      tables.map do |table|
+        "#{table}.#{SEARCH_COLUMN} @@ #{tsquery(term)}"
+      end.join(" OR ")
+    end
+
+    def tsquery_join_tables(tables)
+      tables.map do |table|
+        if table.to_s.end_with?("_translations")
+          intermediate = table.to_s.gsub(/_translations$/, "").pluralize.to_sym
+          if table_name.to_sym == intermediate
+            :translations
+          else
+            [intermediate, intermediate => :translations]
+          end
+        else
+          table
+        end
+      end.flatten
+    end
   end
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/MethodLength
 end

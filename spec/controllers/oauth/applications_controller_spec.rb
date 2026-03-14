1bd3688b3218aa481f95753ee9d03b8936939a00
Patrick Uhlmann <github@uhlme.ch>
2026-03-14 20:30:26 +0100

    Add description field to OAuth applications
    - Adds a `description` text field to the `Oauth::Application` model
    - Field appears in the create/edit form (below "Name")
    - Field is displayed in the application detail view (show page)
    - Translations included for all four supported locales (de, en, fr, it)

--- spec/controllers/oauth/applications_controller_spec.rb
+++ spec/controllers/oauth/applications_controller_spec.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2012-2019, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2026, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -15,5 +15,42 @@ describe Oauth::ApplicationsController do
       params: {oauth_application: {name: "MyApp", redirect_uri: "urn:ietf:wg:oauth:2.0:oob", scopes: %w[name email]}}
     application = Oauth::Application.find_by(name: "MyApp")
     expect(application.scopes).to eq %w[name email]
+  end
+
+  describe "description field" do
+    let(:description_text) { "Wird verwendet für den internen Mitgliederbereich." }
+
+    it "POST#create saves description" do
+      post :create,
+        params: {oauth_application: {
+          name: "MyApp",
+          redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+          description: description_text
+        }}
+      application = Oauth::Application.find_by(name: "MyApp")
+      expect(application.description).to eq description_text
+    end
+
+    context "with existing application" do
+      let!(:application) do
+        Oauth::Application.create!(
+          name: "ExistingApp",
+          redirect_uri: "urn:ietf:wg:oauth:2.0:oob"
+        )
+      end
+
+      it "PATCH#update saves description" do
+        patch :update,
+          params: {id: application.id, oauth_application: {description: description_text}}
+        expect(application.reload.description).to eq description_text
+      end
+
+      it "GET#show assigns entry with description" do
+        application.update!(description: description_text)
+        get :show, params: {id: application.id}
+        expect(response).to be_successful
+        expect(assigns(:application).description).to eq description_text
+      end
+    end
   end
 end

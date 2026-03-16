c6faa5b923ab710ab2e5ae08da2e5e9cdfb30b6e
Daniel Illi <daniel@illi.zone>
2026-03-16 16:45:34 +0100

    Implement Passes template registry (#3989)

---
+++ spec/support/pass_template_registry.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    # Register default pass template for all specs
+    Passes::TemplateRegistry.register("default",
+      pdf_class: Class.new,
+      pass_view_partial: "default",
+      wallet_data_provider: Passes::WalletDataProvider)
+  end
+end

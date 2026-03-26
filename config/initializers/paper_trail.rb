eed465891f3fb7052bf377bdd0425f06e49d4495
Niklas Jäggi <jaeggi@puzzle.ch>
2026-03-26 13:39:41 +0100

    Add updated_at to default paper trail skip options

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -10,6 +10,11 @@ require_dependency 'paper_trail/version'
 
 # We don't want paper_trail to create versions on touch events
 # default is: [create update destroy touch]
+# When we don't define a local skip option for a model, we want updated_at
+# to be ignored per default. When a model adds own skip options,
+# updated_at is not in the defaults anymore and has to be added
+# manually if it should be skipped
 PaperTrail.config.has_paper_trail_defaults = {
-  on: %i[create update destroy]
+  on: %i[create update destroy],
+  skip: %i[updated_at]
 }

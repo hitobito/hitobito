47e2ce18b5264f4b572f78c55fb885c66b9a264f
Daniel Illi <daniel@illi.zone>
2026-04-02 21:16:20 +0200

    Set To header to 'Undisclosed recipients:' when empty (#3953)
    Mails sent to mailing lists via BCC had empty To headers, causing
    syntactically invalid mails that were rejected by systems like GMX.
    Now sets To header to 'Undisclosed recipients:' when blank to ensure
    RFC-compliant mails.

--- app/domain/messages/bulk_mail/mail_factory.rb
+++ app/domain/messages/bulk_mail/mail_factory.rb
@@ -32,10 +32,32 @@ module Messages
       private
 
       def set_headers
+        # Preserve original sender in Reply-To so recipients can reply directly to them
         @mail["Reply-To"] = @mail.from
+        # Add unique message identifier for tracking and deduplication
         @mail["X-Hitobito-Message-UID"] = @bulk_mail_message.uid
+        # Rewrite From header to show message came via mailing list
         @mail.from = sender_via_mailinglist
+        # Set SMTP envelope sender to mailing list address for bounce handling
         @mail.smtp_envelope_from = mailing_list_address
+        # Set To header if empty (e.g., BCC mails) to ensure RFC-compliant mail
+        # that won't be rejected by mail systems like GMX.
+        set_undisclosed_recipients if @mail.to.blank?
+      end
+
+      # Set To header to 'Undisclosed recipients:;'
+      # Uses RFC 5322 group syntax (https://datatracker.ietf.org/doc/html/rfc5322#section-3.4)
+      def set_undisclosed_recipients
+        # We want to circumvent the normal parsing of address header values of the mail gem.
+        # So we use a plain StructuredField instead of the regular ToField and we add the
+        # header manually instead of using the setter.
+        # The gem's api allows us to replace a header field by name. So we first make sure
+        # the header exists, then we replace it with the correct value.
+        # Using #insert_field directly without the to header preexisting does not work as
+        # StructuredField does not implement the method #<() which is used by #insert_field.
+        # @mail.to = ""
+        undisclosed = Mail::StructuredField.new("To", "Undisclosed recipients:;")
+        @mail.header.fields.replace_field(undisclosed)
       end
 
       def sender_via_mailinglist

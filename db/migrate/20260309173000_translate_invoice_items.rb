8cc5dbdca44ed401e9426dd8cbe8b2a9994ed758
Matthias Viehweger <kronn@kronn.de>
2026-04-08 15:49:54 +0200

    Transfer data manually because SPEEEEEED
    The built-in mechanism to transfer the data is very error-resilient, but
    pipes every little string through ruby. In this case, we can optimize
    this for larger datasets. And since we can, we do.
    
    Otherwise the transfer of 2.5 million strings takes more than 24hrs. We
    do not know how much longer and do not want to find out. Therefore: this.

--- db/migrate/20260309173000_translate_invoice_items.rb
+++ db/migrate/20260309173000_translate_invoice_items.rb
@@ -4,31 +4,44 @@ class TranslateInvoiceItems < ActiveRecord::Migration[8.0]
   # get to clean them up.
   class InvoiceItem::FixedFee < InvoiceItem
   end
+
   class InvoiceItem::Roles < InvoiceItem::FixedFee
   end
 
   def up
     remove_column :invoice_items, :search_column, if_exists: true
-    say_with_time('creating translation table for period invoice template items') do
+    say_with_time("creating translation table for period invoice template items") do
       PeriodInvoiceTemplate::Item.create_translation_table!(
         {
-          name: :string,
+          name: :string
         },
-        { migrate_data: true, remove_source_columns: true }
+        {migrate_data: true, remove_source_columns: true}
       )
     end
-    say_with_time('creating translation table for invoice items') do
+
+    say_with_time("creating translation table for invoice items") do
       InvoiceItem.create_translation_table!(
         {
-          name: :string,
+          name: :string
         },
-        { migrate_data: true, remove_source_columns: true }
+        {migrate_data: false, remove_source_columns: false}
       )
+    end
+
+    say_with_time("transferring data into invoice_item_translations") do
+      execute <<~SQL
+        INSERT INTO invoice_item_translations (invoice_item_id, locale, name, created_at, updated_at)
+        SELECT id, '#{I18n.default_locale}', name, NOW(), NOW() FROM invoice_items
+      SQL
+    end
+
+    say_with_time("removing source column") do
+      remove_column :invoice_items, :name
     end
   end
 
   def down
-    say_with_time('dropping translation table for invoice items') do
+    say_with_time("dropping translation table for invoice items") do
       add_column :invoice_items, :name, :string, null: true
       InvoiceItem.find_each { |item| item.update_column(:name, item.name) }
       change_column_null :invoice_items, :name, false
@@ -36,13 +49,12 @@ class TranslateInvoiceItems < ActiveRecord::Migration[8.0]
       InvoiceItem.drop_translation_table!
     end
 
-    say_with_time('dropping translation table for period invoice template items') do
+    say_with_time("dropping translation table for period invoice template items") do
       add_column :period_invoice_template_items, :name, :string, null: true
       PeriodInvoiceTemplate::Item.find_each { |item| item.update_column(:name, item.name) }
       change_column_null :period_invoice_template_items, :name, false
 
       PeriodInvoiceTemplate::Item.drop_translation_table!
     end
-
   end
 end

e795283315227af15a6a0dbaf23af1ce53f69d14
Daniel Illi-Zuberbühler <daniel-illi@users.noreply.github.com>
2026-04-10 10:35:52 +0200

    Implement Passes models (#3956)

---
+++ app/models/wallets/apple_wallet/device_registration.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito
+
+class Wallets::AppleWallet::DeviceRegistration < ActiveRecord::Base
+  # push_token: APNs token for sending push notifications to this device when the pass updates.
+  # device_library_identifier: Unique identifier for the Apple device (provided by Apple PassKit).
+  attr_readonly :device_library_identifier, :push_token
+
+  ### ASSOCIATIONS
+
+  belongs_to :pass_installation, class_name: "Wallets::PassInstallation"
+
+  ### VALIDATIONS
+
+  validates_by_schema
+  validates :device_library_identifier, presence: true, uniqueness: {scope: :pass_installation_id}
+  validates :push_token, presence: true
+end

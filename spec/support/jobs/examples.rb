bd4fa2428cdd44956eae921986730b2fdf8988b3
Jannik Pulfer <pulfer@puzzle.ch>
2026-04-14 17:05:25 +0200

    Move example jobs from application code to spec support

---
+++ spec/support/jobs/examples.rb
@@ -0,0 +1,43 @@
+module Examples
+  class UserManagedJobWithProgress < BaseJob
+    prepend UserManageableJob
+
+    self.reports_progress = true
+
+    def perform
+      5.times do |i|
+        Rails.logger.debug "Working..."
+        report_progress(i, 5)
+      end
+    end
+  end
+
+  class UnsuccessfulUserManagedJob < BaseJob
+    prepend UserManageableJob
+
+    def perform
+      Rails.logger.debug "Working..."
+      raise "Something went wrong during job execution"
+    end
+  end
+
+  class SuccessfulUserManagedJob < BaseJob
+    prepend UserManageableJob
+
+    self.job_name = "Custom job name"
+
+    def perform
+      Rails.logger.debug "Working..."
+    end
+  end
+
+  class LongRunningUserManagedJob < BaseJob
+    prepend UserManageableJob
+
+    def perform
+      loop do
+        Rails.logger.debug "Working..."
+      end
+    end
+  end
+end

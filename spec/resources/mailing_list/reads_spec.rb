cc4c0ca6be5f5b4dfad5e5c7b0ef1ef4c21cd8cf
carlobeltrame <carlo@beltra.me>
2026-04-17 17:03:49 +0200

    Fix mailing lists api with read-only service token
    Follow-up from #4104

--- spec/resources/mailing_list/reads_spec.rb
+++ spec/resources/mailing_list/reads_spec.rb
@@ -10,6 +10,89 @@ RSpec.describe MailingListResource, type: :resource do
       expect(data.id).to eq(mailing_list.id)
       expect(data.jsonapi_type).to eq("mailing_lists")
     end
+
+    context "with service token" do
+      ServiceToken::PERMISSIONS.each do |permission|
+        context "with #{permission} permission" do
+          let(:group) { groups(:top_layer) }
+          let(:token) { Fabricate(:service_token, layer: group, permission:, mailing_lists: true) }
+          let(:current_scopes) { %w[mailing_lists] }
+          let(:ability) { TokenAbility.new(token) }
+          let(:data) { jsonapi_data[0] }
+
+          before do
+            params[:filter] = {id: {eq: mailing_list.id}}
+            render
+          end
+
+          context "mailing list in same group" do
+            let(:mailing_list) { groups(:top_layer).mailing_lists.create!(name: "list") }
+
+            it "works" do
+              expect(data.id).to eq(mailing_list.id)
+              expect(data.jsonapi_type).to eq("mailing_lists")
+            end
+          end
+
+          context "mailing list in same layer" do
+            let(:mailing_list) { groups(:top_group).mailing_lists.create!(name: "list") }
+
+            it "works" do
+              expect(data.id).to eq(mailing_list.id)
+              expect(data.jsonapi_type).to eq("mailing_lists")
+            end
+          end
+
+          context "mailing list in sub layer" do
+            let(:mailing_list) { groups(:bottom_layer_one).mailing_lists.create!(name: "list") }
+
+            it "does not work" do
+              expect(data).to be_nil
+            end
+          end
+        end
+      end
+
+      context "without mailing_lists scope" do
+        let(:group) { groups(:top_layer) }
+        let(:token) {
+          Fabricate(:service_token, layer: group, permission: :layer_and_below_full,
+            mailing_lists: false, people: true)
+        }
+        let(:current_scopes) { %w[people] }
+        let(:ability) { TokenAbility.new(token) }
+        let(:data) { jsonapi_data[0] }
+
+        before do
+          params[:filter] = {id: {eq: mailing_list.id}}
+          render
+        end
+
+        context "mailing list in same group" do
+          let(:mailing_list) { groups(:top_layer).mailing_lists.create!(name: "list") }
+
+          it "does not work" do
+            expect(data).to be_nil
+          end
+        end
+
+        context "mailing list in same layer" do
+          let(:mailing_list) { groups(:top_group).mailing_lists.create!(name: "list") }
+
+          it "does not work" do
+            expect(data).to be_nil
+          end
+        end
+
+        context "mailing list in sub layer" do
+          let(:mailing_list) { groups(:bottom_layer_one).mailing_lists.create!(name: "list") }
+
+          it "does not work" do
+            expect(data).to be_nil
+          end
+        end
+      end
+    end
   end
 
   describe "filtering" do

bec4ed5fad24883bdd5f805929f70318b1245817
Daniel Illi <daniel@illi.zone>
2026-04-22 10:41:30 +0200

    Add translations for custom contents

--- db/seeds/custom_contents.rb
+++ db/seeds/custom_contents.rb
@@ -531,15 +531,27 @@ CustomContent::Translation.seed_once(:custom_content_id, :locale,
 
   {custom_content_id: participation_notification_pending_id,
   locale: 'fr',
-  label: 'Événement: E-Mail Participant/-e a pré-enregistré' },
+  label: 'Événement: E-Mail Participant/-e a pré-enregistré',
+  subject: 'Événement: Participant/-e a pré-enregistré',
+  body: 'Bonjour<br><br>' \
+        '{participant-name} s\'est pré-enregistré/-e à l\'événement "{event-name}":<br><br>' \
+        '{application-url}<br>' },
 
   {custom_content_id: participation_notification_pending_id,
   locale: 'en',
-  label: 'Event: E-Mail Participant has pre-registerd' },
+  label: 'Event: E-Mail Participant has pre-registered',
+  subject: 'Event: Participant has pre-registered',
+  body: 'Hello<br><br>' \
+        '{participant-name} has pre-registered for the event "{event-name}":<br><br>' \
+        '{application-url}<br>' },
 
   {custom_content_id: participation_notification_pending_id,
   locale: 'it',
-  label: 'Evento: E-Mail Participante ha pre-registrato'},
+  label: 'Evento: E-Mail Participante ha pre-registrato',
+  subject: 'Evento: Participante ha pre-registrato',
+  body: 'Ciao<br><br>' \
+        '{participant-name} si è pre-registrato all\'evento "{event-name}":<br><br>' \
+        '{application-url}<br>'},
 
   {custom_content_id: participation_notification_waiting_list_id,
   locale: 'de',
@@ -551,15 +563,27 @@ CustomContent::Translation.seed_once(:custom_content_id, :locale,
 
   {custom_content_id: participation_notification_waiting_list_id,
   locale: 'fr',
-  label: 'Événement: E-Mail Participant/-e s\'est ilnscrit/-e sur la liste d\'attante' },
+  label: 'Événement: E-Mail Participant/-e s\'est inscrit/-e sur la liste d\'attente',
+  subject: 'Événement: Participant/-e s\'est inscrit/-e sur la liste d\'attente',
+  body: 'Bonjour<br><br>' \
+        '{participant-name} s\'est inscrit/-e sur la liste d\'attente de l\'événement "{event-name}":<br><br>' \
+        '{application-url}<br>' },
 
   {custom_content_id: participation_notification_waiting_list_id,
   locale: 'en',
-  label: 'Event: E-Mail Participant has pre-registerd' },
+  label: 'Event: E-Mail Participant has joined the waiting list',
+  subject: 'Event: Participant has joined the waiting list',
+  body: 'Hello<br><br>' \
+        '{participant-name} has joined the waiting list for the event "{event-name}":<br><br>' \
+        '{application-url}<br>' },
 
   {custom_content_id: participation_notification_waiting_list_id,
   locale: 'it',
-  label: 'Evento: E-Mail Partecipante si è iscritto alla lista d\'attesa'},
+  label: 'Evento: E-Mail Partecipante si è iscritto alla lista d\'attesa',
+  subject: 'Evento: Partecipante si è iscritto alla lista d\'attesa',
+  body: 'Ciao<br><br>' \
+        '{participant-name} si è iscritto alla lista d\'attesa dell\'evento "{event-name}":<br><br>' \
+        '{application-url}<br>'},
 
   {custom_content_id: cancel_application_id,
    locale: 'de',
@@ -896,4 +920,3 @@ if FeatureGate.enabled? "custom_dashboard_page"
       subject: "Benvenuto su #{Settings.application.name}" }
   )
 end
-

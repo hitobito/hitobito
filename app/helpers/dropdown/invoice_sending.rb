dbfc54ee24666ea38c625ee7d09a938d07343c8d
Matthias Viehweger <kronn@kronn.de>
2026-04-23 18:34:36 +0200

    Preserve currently selected filters for dropdown-actions
    Otherwise too many item might be filtered out if the selected filters
    do not match the default ones. A good example is the date-filter, which
    defaults to the current year, even a manual selection of ids would not
    matter if those ids belong to past years. With the selected filter
    passed along, the visually selected ids are again in the available set.
    
    tl;dr is more consistent, therefore better.

--- app/helpers/dropdown/invoice_sending.rb
+++ app/helpers/dropdown/invoice_sending.rb
@@ -13,6 +13,7 @@ module Dropdown
       super(template, translate(:button), :envelope)
       @params = params
       @invoice_run_id = template.invoice_run&.id
+      @filter_params = filter_params
       init_items
     end
 
@@ -29,8 +30,16 @@ module Dropdown
 
     def add_item(key, options = {})
       group = template.parent.is_a?(Group) ? template.parent : template.parent.group
-      path = template.group_invoice_run_path(group, options)
+      path = template.group_invoice_run_path(group, options.merge(@filter_params))
       super(translate(key), path, data: {method: :put, checkable: true})
+    end
+
+    def filter_params
+      if template.respond_to?(:filter_params)
+        template.filter_params
+      else
+        {}
+      end
     end
   end
 end

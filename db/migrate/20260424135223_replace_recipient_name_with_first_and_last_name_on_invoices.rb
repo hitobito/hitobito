c1b6c954e49011f5c40f0f2a68b7472da78b9071
Andreas Maierhofer <maierhofer@puzzle.ch>
2026-04-28 10:45:08 +0200

    Add missing license header

--- db/migrate/20260424135223_replace_recipient_name_with_first_and_last_name_on_invoices.rb
+++ db/migrate/20260424135223_replace_recipient_name_with_first_and_last_name_on_invoices.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2022-2025,  Eidgenössischer Jodlerverband. This file is part of
+#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 class ReplaceRecipientNameWithFirstAndLastNameOnInvoices < ActiveRecord::Migration[8.0]
   def up # rubocop:disable Metrics/MethodLength
     change_table :invoices do |t|

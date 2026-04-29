f71a5f29b30a3761178ee1bfd173f4977f7f43bc
Andreas Maierhofer <maierhofer@puzzle.ch>
2026-04-29 14:18:24 +0200

    No early return required, handled by find_each of relation

--- app/domain/passes/pass_updater.rb
+++ app/domain/passes/pass_updater.rb
@@ -31,8 +31,6 @@ module Passes
       return unless person.passes.exists?
 
       affected = Passes::Subscribers.affected_passes(person, role:)
-      return unless affected.any?
-
       affected.find_each do |pass|
         recompute_state!(pass)
         mark_installations_for_sync!(pass)

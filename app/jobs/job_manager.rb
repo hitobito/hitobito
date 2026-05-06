92571356d66fc30b661f166f8d80bbb1ec4216b2
Andreas Maierhofer <maierhofer@puzzle.ch>
2026-05-06 11:18:04 +0200

    Rename job

--- app/jobs/job_manager.rb
+++ app/jobs/job_manager.rb
@@ -73,7 +73,7 @@ class JobManager
       Oauth::AccessTokenCleanupJob,
       ReoccuringMailchimpSynchronizationJob,
       SessionsCleanerJob,
-      WalletSyncJob,
+      WalletSynchronizationJob,
       WorkerHeartbeatCheckJob,
       HitobitoErrorLogJob
     ]

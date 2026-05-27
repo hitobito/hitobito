ceddaa41b63d49d33e7d6b44e94a7eed7bc4852a
Jannik Pulfer <pulfer@puzzle.ch>
2026-05-27 09:38:24 +0200

    Add missing license header

--- db/migrate/20260504111421_add_needs_web_socket_connection_to_people.rb
+++ db/migrate/20260504111421_add_needs_web_socket_connection_to_people.rb
@@ -1,3 +1,8 @@
+#  Copyright (c) 2012-2026, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 class AddNeedsWebSocketConnectionToPeople < ActiveRecord::Migration[8.0]
   def change
     add_column :people, :needs_web_socket_connection, :boolean

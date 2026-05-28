de9120e19b4a51517c4be8d29b0117eaabf78b3a
Jannik Pulfer <pulfer@puzzle.ch>
2026-05-28 17:32:50 +0200

    Simplify migration from async download files to job observations by dropping old table
    We decided that the existing async download file records should be save
    to remove. In the worst case an export job has to be rerun because the
    async download file that belonged to it was removed.

--- db/migrate/20260225080027_migrate_async_download_file_to_job_observation.rb
+++ db/migrate/20260225080027_migrate_async_download_file_to_job_observation.rb
@@ -4,60 +4,40 @@
 #  https://github.com/hitobito/hitobito.
 
 class MigrateAsyncDownloadFileToJobObservation < ActiveRecord::Migration[8.0]
-  def change
-    rename_table :async_download_files, :job_observations
+  def up
+    drop_table :async_download_files
 
-    change_table :job_observations do |t|
-      t.rename :name, :job_class
-      t.rename :timestamp, :started_at
-
+    create_table :job_observations do |t|
+      t.string :job_class, null: false
       t.string :filename
-      t.datetime :finished_at
+      t.string :filetype, null: false
       t.string :status, null: false
       t.integer :attempts, null: false
       t.integer :max_attempts, null: false
+      t.integer :progress, null: false
       t.boolean :reports_progress, null: false
+      t.datetime :started_at, null: false
+      t.datetime :finished_at, null: false
       t.datetime :last_progress_update_broadcasted_at
 
+      t.references :person
       t.references :delayed_job
+
+      t.timestamps
     end
+  end
 
-    reversible do |dir|
-      dir.up do
-        execute("UPDATE job_observations SET filetype = 'txt' WHERE filetype IS NULL")
-        execute("UPDATE job_observations SET progress = 0 WHERE progress IS NULL")
+  def down
+    drop_table :job_observations
 
-        change_column(:job_observations, :person_id, :bigint)
-        change_column(
-          :job_observations, :started_at, :datetime,
-          using: "to_timestamp(started_at::numeric)"
-        )
-      end
+    create_table :async_download_files do |t|
+      t.string  :name, null: false
+      t.string  :filetype
+      t.integer :progress
+      t.integer :person_id, null: false
+      t.string  :timestamp, null: false
 
-      dir.down do
-        max_person_id = execute(
-          "SELECT COALESCE(MAX(person_id), 0) as max_person_id FROM job_observations"
-        ).to_a.first["max_person_id"]
-
-        # Min and max signed integers with 4 bytes
-        if (max_person_id > (-2**31)) && (max_person_id < (2**31 - 1))
-          change_column(:job_observations, :person_id, :bigint)
-        else
-          raise "Could not rollback migration:" \
-            " Changing type of column person_id on table job_observations from bigint" \
-            " back to integer not possible because max person_id is outside of integer limit."
-        end
-
-        change_column(
-          :job_observations, :started_at, :string,
-          using: "EXTRACT(EPOCH FROM started_at)::bigint::text"
-        )
-      end
+      t.timestamps
     end
-
-    add_index(:job_observations, :person_id)
-
-    change_column_null(:job_observations, :filetype, false)
-    change_column_null(:job_observations, :progress, false)
   end
 end

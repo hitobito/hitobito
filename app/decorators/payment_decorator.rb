920af708811cbffb1976ae235e05f3490f00b29d
Matthias Viehweger <kronn@kronn.de>
2026-06-02 21:53:58 +0200

    Add payments#index.html with sorting

---
+++ app/decorators/payment_decorator.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026-2026, Schweizer Wanderwege. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class PaymentDecorator < ApplicationDecorator
+  decorates :payment
+
+  def amount = format_currency(model.amount)
+
+  def received_at = model.received_at
+
+  def status = model.status_label
+
+  def invoice_amount = model.invoice.decorate.total
+
+  def invoice_due_at = model.invoice.due_at
+
+  def invoice_status = model.invoice.state_label
+
+  # FIXIT: check if this can be delegated to :helpers somehow
+  def format_currency(amount)
+    ActiveSupport::NumberHelper.number_to_currency(amount, {unit: currency, format: "%n %u"})
+  end
+
+  def currency = model.invoice.decorate.currency
+end

3176f3bfd95b089aedd2178ee6f352805ae502fc
Jannik Pulfer <pulfer@puzzle.ch>
2026-06-05 10:41:24 +0200

    Test that events controller successfully enqueues job and sets correct flash message

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -132,6 +132,14 @@ describe EventsController do
         expect(assigns(:events).first.name).to eq "Eventus"
       end
 
+      it "sets enqueues job on export" do
+        expect do
+          get :index, params: {group_id: group.id}, format: :csv
+          expect(flash[:notice])
+            .to match(/Export wird im Hintergrund gestartet und kann nach Fertigstellung auf der Jobübersicht/)
+        end.to change(Delayed::Job, :count).by(1)
+      end
+
       it "renders json with dates" do
         Fabricate(:event_date, event: @e1)
         get :index, params: {group_id: @g1}, format: :json

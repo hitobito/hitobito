d95d8b13b9709fabd18513839fd6a5fe9c58cf5d
mike <michael.gerber@renuo.ch>
2026-06-09 10:29:05 +0200

    linter fixes

--- spec/controllers/personal_documents_controller_spec.rb
+++ spec/controllers/personal_documents_controller_spec.rb
@@ -14,18 +14,19 @@ describe PersonalDocumentsController do
 
   describe "GET #index" do
     let!(:personal_document) { Fabricate(:personal_document, person: bottom_member) }
-    it 'renders successfully' do
+
+    it "renders successfully" do
       get :index, params: {group_id: group.id, person_id: personal_document.person_id}
       expect(response).to be_successful
       expect(assigns(:personal_documents).to_a).to eq [personal_document]
     end
 
-    context 'when other documents exist' do
+    context "when other documents exist" do
       before do
         Fabricate.times(5, :personal_document)
       end
 
-      it 'renders only the ones of the person' do
+      it "renders only the ones of the person" do
         get :index, params: {group_id: group.id, person_id: personal_document.person_id}
         expect(response).to be_successful
         expect(assigns(:personal_documents)).to eq [personal_document]
@@ -41,18 +42,20 @@ describe PersonalDocumentsController do
         person_id: bottom_member.id,
         personal_document: {
           person_id: bottom_member.id,
-          file: Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'files', 'images', 'logo.png')),
+          file: Rack::Test::UploadedFile.new(Rails.root.join("spec", "fixtures", "files", "images", "logo.png")),
           personal_document_label_id: personal_document_label.id
         }
       }
     end
+
     it "creates new personal_document" do
       expect do
         post :create, params: create_params
       end.to change(PersonalDocument, :count).by(1)
 
       expect(response).to redirect_to(
-                            group_person_personal_documents_path(group, bottom_member))
+        group_person_personal_documents_path(group, bottom_member)
+      )
     end
   end
 end

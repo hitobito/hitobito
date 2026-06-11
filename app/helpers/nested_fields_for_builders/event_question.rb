d16a9e0e43411447909419bb4f08b7f0055b5d6d
Nils Rauch <rauch@puzzle.ch>
2026-06-11 11:39:19 +0200

    Fix rubocop

--- app/helpers/nested_fields_for_builders/event_question.rb
+++ app/helpers/nested_fields_for_builders/event_question.rb
@@ -11,7 +11,7 @@ module NestedFieldsForBuilders
       stimulus_controller = "events--question-template-nested-form"
       add_button = Dropdown::Event::QuestionAdd.new(template, @form.object.groups.first,
         @form.object, admin:).to_s
-      question_template_record = object.class.reflect_on_association(assoc)&.klass&.new(admin: admin)
+      question_template_record = object.class.reflect_on_association(assoc)&.klass&.new(admin:)
       templates = new_record_template(partial_name, stimulus_controller, &block) +
         new_record_template("event/questions/template_fields", stimulus_controller,
           model_object: question_template_record,

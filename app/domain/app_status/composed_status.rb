49aec78e38a695f45660afe0053d96e4fd56c53c
Jannik Pulfer <pulfer@puzzle.ch>
2026-06-12 16:26:13 +0200

    Implement memory usage app status check and make it possible to compose multiple statuses to one

---
+++ app/domain/app_status/composed_status.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2026, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class AppStatus::ComposedStatus < AppStatus
+  def initialize(*statuses)
+    super()
+
+    @statuses = statuses
+  end
+
+  def details
+    @statuses.map(&:details).reduce(:merge)
+  end
+
+  def code
+    @statuses.map(&:code).all?(:ok) ? :ok : :service_unavailable
+  end
+end

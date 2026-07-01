c860a14c03ab5fd02c430203bb229940419f02ce
Jannik Pulfer <pulfer@puzzle.ch>
2026-07-01 10:09:36 +0200

    Process invoices and letters in batches in pdf exports (#4337)

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -76,6 +76,7 @@ class Invoice < ActiveRecord::Base # rubocop:todo Metrics/ClassLength
   include PaymentSlips
   include FullTextSearchable
   include Shippable
+  include Findable
 
   ROUND_TO = BigDecimal("0.05")
 

b92f3c6bf97e6f16fd816a81e024d98f1c3d0ead
Daniel Illi <daniel@illi.zone>
2026-07-01 15:29:03 +0200

    Name context explicitely

--- spec/controllers/person/personal_documents_controller_spec.rb
+++ spec/controllers/person/personal_documents_controller_spec.rb
@@ -12,50 +12,52 @@ describe Person::PersonalDocumentsController do
 
   before { sign_in(top_leader) }
 
-  describe "GET #index" do
-    let!(:personal_document) { Fabricate(:personal_document, person: bottom_member) }
+  context "with admin permission" do
+    describe "GET #index" do
+      let!(:personal_document) { Fabricate(:personal_document, person: bottom_member) }
 
-    it "renders successfully" do
-      get :index, params: {group_id: group.id, person_id: personal_document.person_id}
-      expect(response).to be_successful
-      expect(assigns(:personal_documents).to_a).to eq [personal_document]
-    end
-
-    context "when other documents exist" do
-      before do
-        Fabricate.times(5, :personal_document)
-      end
-
-      it "renders only the ones of the person" do
+      it "renders successfully" do
         get :index, params: {group_id: group.id, person_id: personal_document.person_id}
         expect(response).to be_successful
-        expect(assigns(:personal_documents)).to eq [personal_document]
+        expect(assigns(:personal_documents).to_a).to eq [personal_document]
+      end
+
+      context "when other documents exist" do
+        before do
+          Fabricate.times(5, :personal_document)
+        end
+
+        it "renders only the ones of the person" do
+          get :index, params: {group_id: group.id, person_id: personal_document.person_id}
+          expect(response).to be_successful
+          expect(assigns(:personal_documents)).to eq [personal_document]
+        end
       end
     end
-  end
 
-  describe "POST #create" do
-    let(:personal_document_label) { Fabricate(:personal_document_label) }
-    let(:create_params) do
-      {
-        group_id: group.id,
-        person_id: bottom_member.id,
-        personal_document: {
+    describe "POST #create" do
+      let(:personal_document_label) { Fabricate(:personal_document_label) }
+      let(:create_params) do
+        {
+          group_id: group.id,
           person_id: bottom_member.id,
-          file: Rack::Test::UploadedFile.new(Rails.root.join("spec", "fixtures", "files", "images", "logo.png")),
-          label_id: personal_document_label.id
+          personal_document: {
+            person_id: bottom_member.id,
+            file: Rack::Test::UploadedFile.new(Rails.root.join("spec", "fixtures", "files", "images", "logo.png")),
+            label_id: personal_document_label.id
+          }
         }
-      }
-    end
+      end
 
-    it "creates new personal_document" do
-      expect do
-        post :create, params: create_params
-      end.to change(PersonalDocument, :count).by(1)
+      it "creates new personal_document" do
+        expect do
+          post :create, params: create_params
+        end.to change(PersonalDocument, :count).by(1)
 
-      expect(response).to redirect_to(
-        group_person_personal_documents_path(group, bottom_member, returning: true)
-      )
+        expect(response).to redirect_to(
+          group_person_personal_documents_path(group, bottom_member, returning: true)
+        )
+      end
     end
   end
 

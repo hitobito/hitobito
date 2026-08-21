b2ac1df7d4f6032c2b8c507d59749a9cbea88b8b
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-08-21 13:29:32 +0200

    Remove true mail healthz check (#4430)

--- spec/controllers/healthz_controller_spec.rb
+++ spec/controllers/healthz_controller_spec.rb
@@ -17,89 +17,29 @@ describe HealthzController do
     stub_file_read("/sys/fs/cgroup/memory.stat", "inactive_file 100000")
   end
 
-  describe "GET show with token" do
-    let(:json) { JSON.parse(response.body) }
-    let(:token) { AppStatus.auth_token }
-
-    context "when true mail can verify valid e-mail address" do
-      it "has HTTP status 200" do
-        expect(Truemail).to receive(:valid?).with("hitobito@puzzle.ch").and_return(true)
-
-        get :show, params: {token: token}
-
-        expect(response.status).to eq(200)
-
-        expect(json).to eq("app_status" =>
-                           {"code" => "ok",
-                            "details" => {
-                              "memory_usage_determinable" => true,
-                              "memory_usage_exceeds_limit" => false,
-                              "memory_usage_limit_percentage" => 95,
-                              "truemail_working" => true,
-                              "validated_email" => "hitobito@puzzle.ch"
-                            }})
-      end
-    end
-
-    context "when true mail cannot verify valid e-mail address" do
-      it "has HTTP status 503" do
-        expect(Truemail).to receive(:valid?).with("hitobito@puzzle.ch").and_return(false)
-
-        get :show, params: {token: token}
-
-        expect(response.status).to eq(503)
-
-        expect(json).to eq("app_status" =>
-                           {"code" => "service_unavailable",
-                            "details" => {
-                              "memory_usage_determinable" => true,
-                              "memory_usage_exceeds_limit" => false,
-                              "memory_usage_limit_percentage" => 95,
-                              "truemail_working" => false,
-                              "validated_email" => "hitobito@puzzle.ch"
-                            }})
-      end
-    end
-  end
-
   describe "GET show without token" do
     let(:json) { JSON.parse(response.body) }
 
-    context "when true mail can verify valid e-mail address" do
+    context "when memory usage determinable" do
       it "has HTTP status 200" do
-        expect(Truemail).to receive(:valid?).with("hitobito@puzzle.ch").and_return(true)
+        [
+          "/sys/fs/cgroup/memory.max", "/sys/fs/cgroup/memory.current", "/sys/fs/cgroup/memory.stat"
+        ].each do |memory_file|
+          allow(File).to receive(:exist?).and_return(true)
+          allow(File).to receive(:exist?).with(memory_file).and_return(true)
 
-        get :show
+          get :show
 
-        expect(response.status).to eq(200)
+          expect(response.status).to eq(200)
 
-        expect(json).to eq("app_status" =>
-                           {"code" => "ok",
-                            "details" => {
-                              "memory_usage_determinable" => true,
-                              "memory_usage_exceeds_limit" => false,
-                              "memory_usage_limit_percentage" => 95,
-                              "truemail_working" => true
-                            }})
-      end
-    end
-
-    context "when true mail cannot verify valid e-mail address" do
-      it "has HTTP status 503" do
-        expect(Truemail).to receive(:valid?).with("hitobito@puzzle.ch").and_return(false)
-
-        get :show
-
-        expect(response.status).to eq(503)
-
-        expect(json).to eq("app_status" =>
-                           {"code" => "service_unavailable",
-                            "details" => {
-                              "memory_usage_determinable" => true,
-                              "memory_usage_exceeds_limit" => false,
-                              "memory_usage_limit_percentage" => 95,
-                              "truemail_working" => false
-                            }})
+          expect(json).to eq("app_status" =>
+                               {"code" => "ok",
+                                "details" => {
+                                  "memory_usage_determinable" => true,
+                                  "memory_usage_exceeds_limit" => false,
+                                  "memory_usage_limit_percentage" => 95
+                                }})
+        end
       end
     end
 
@@ -119,8 +59,7 @@ describe HealthzController do
                              {"code" => "service_unavailable",
                               "details" => {
                                 "memory_usage_determinable" => false,
-                                "memory_usage_limit_percentage" => 95,
-                                "truemail_working" => true
+                                "memory_usage_limit_percentage" => 95
                               }})
         end
       end

11ecc5b212f6c9a7ae3a465cd175c91259a38363
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-08-28 16:26:27 +0200

    Fix migration and html tags getting removed (#1791)

--- db/migrate/20260827114819_event_description_as_rich_text.rb
+++ db/migrate/20260827114819_event_description_as_rich_text.rb
@@ -1,21 +1,24 @@
 class EventDescriptionAsRichText < ActiveRecord::Migration[8.0]
   def up
-    Event::Translation.all.each do |translation|
-      translation.description = CustomContent.create!(
-        locale: translation.locale,
-        label: translation.description,
-        custom_content: translation,
+    Event::Translation.find_each do |translation|
+      description = translation.description
+
+      ActionText::RichText.create!(
+        name: "description",
+        body: description,
+        record: translation
       )
-      translation.save!
     end
   end
 
   # def down
-  #   ActionText::RichText.where(record_type: Event::Translations) do |actionText|
-  #     translation = actionText.record
-  #     translation.description = actionText.label.to_plain_text
-  #     translation.save!
-  #     actionText.destroy!
+  #   ActionText::RichText
+  #     .where(record_type: "Event::Translation", name: "description").find_each do |action_text|
+  #     translation = action_text.record
+  #     translation.update!(
+  #       description: action_text.body.to_plain_text
+  #     ) unless action_text.body.nil?
+  #     action_text.destroy!
   #   end
   # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_08_160000) do
+ActiveRecord::Schema[8.0].define(version: 2026_08_27_114819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
